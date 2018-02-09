648230b4100a5c81d2f656461a5eb2bf2fca8768
José Carlos Chávez <jcchavezs@gmail.com>
2018-02-09 09:49:12 -0500

    Adds tags and types.

---
+++ src/DDTrace/Types.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DDTrace\Types;
+
+const HTTP_CLIENT = 'http';
+
+const WEB_SERVLET = 'web';
+
+const SQL = 'sql';
+
+const MONGO = 'mongodb';
+
+const CASSANDRA = 'cassandra';
+
+const MESSAGE_CONSUMER = 'queue';
+
+const MESSAGE_PRODUCER = 'queue';
