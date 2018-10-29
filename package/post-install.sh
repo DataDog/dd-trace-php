a622501e3f7e07ab7df9d777b16a5503b92d0be9
Pawel Chojnacki <pawel.chcki@gmail.com>
2018-10-29 17:59:06 +0100

    Improve post-install and add version.h

--- package/post-install.sh
+++ package/post-install.sh
@@ -4,10 +4,22 @@ EXTENSION_BASE_DIR=/opt/datadog-php
 EXTENSION_DIR=${EXTENSION_BASE_DIR}/extensions
 EXTENSION_CFG_DIR=${EXTENSION_BASE_DIR}/etc
 EXTENSION_LOGS_DIR=${EXTENSION_BASE_DIR}/log
+INI_FILE_NAME='ddtrace.ini'
+
+PATH="${PATH}:/usr/local/bin"
+
+mkdir -p $EXTENSION_DIR
+mkdir -p $EXTENSION_CFG_DIR
+mkdir -p $EXTENSION_LOGS_DIR
+
+echo -e '\nLogging php -i to a file\n'
+php -i > "$EXTENSION_LOGS_DIR/php-info.log"
+
+exec > >(tee -i "${EXTENSION_LOGS_DIR}/post-install.log")
+exec 2>&1
 
 PHP_VERSION=$(php -i | grep 'PHP API' | awk '{print $NF}')
 PHP_CFG_DIR=$(php --ini | grep 'Scan for additional .ini files in:' | sed -e 's/Scan for additional .ini files in://g' | head -n 1 | awk '{print $1}')
-
 PHP_THREAD_SAFETY=$(php -i | grep 'Thread Safety' | awk '{print $NF}' | grep -i enabled)
 
 VERSION_SUFFIX=""
@@ -15,19 +27,26 @@ if [[ -n $PHP_THREAD_SAFETY ]]; then
     VERSION_SUFFIX="-zts"
 fi
 
-mkdir -p $EXTENSION_DIR
-mkdir -p $EXTENSION_CFG_DIR
-sudo mkdir -p $EXTENSION_LOGS_DIR
-
 EXTENSION_NAME="ddtrace-${PHP_VERSION}${VERSION_SUFFIX}.so"
-INI_FILE_NAME='ddtrace.ini'
 INI_FILE_PATH="${EXTENSION_CFG_DIR}/$INI_FILE_NAME"
 
-# TODO: Log php --ini && php -i (we need to be able to debug if the extension was not installed previously)
-
-sudo tee $INI_FILE_PATH <<EOF
+echo -e "Creating ddtrace.ini\n###"
+tee $INI_FILE_PATH <<EOF
 [datadog]
 extension=${EXTENSION_DIR}/${EXTENSION_NAME}
 EOF
 
-sudo ln -s "$INI_FILE_PATH" "$PHP_CFG_DIR/$INI_FILE_NAME"
+PHP_DDTRACE_INI="$PHP_CFG_DIR/$INI_FILE_NAME"
+
+echo -e "###\nLinking ddtrace.ini to ${PHP_DDTRACE_INI}\n"
+rm "${PHP_DDTRACE_INI}" && true
+ln -s "$INI_FILE_PATH" "${PHP_DDTRACE_INI}"
+
+ENABLED_VERSION="$(php -r "echo phpversion('ddtrace');")"
+
+if [[ -n ${ENABLED_VERSION} ]]; then
+    echo -e "Extension ${ENABLED_VERSION} enabled successfully\n"
+else
+    echo -e "Failed enabling ddtrace extension\n"
+    exit 1
+fi
