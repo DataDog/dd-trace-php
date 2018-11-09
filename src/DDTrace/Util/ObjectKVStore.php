18ae2b71154c1aa5caa04b52784dcdcf4b4ad451
Luca Abbati <luca.abbati@gmail.com>
2018-11-09 16:13:08 +0100

    [mysqli] Add a comment with performance comparison between approaches to store objects

--- src/DDTrace/Util/ObjectKVStore.php
+++ src/DDTrace/Util/ObjectKVStore.php
@@ -5,6 +5,105 @@ namespace DDTrace\Util;
 
 /**
  * A key value store that stores metada into object instances.
+ *
+ * Performance considerations. We basically have a couple of simple approached to get values stored in memory per
+ * objects:
+ *   - save data into an in-line property, e.g. `__dd_store_<some_key>`, pf an obj instance
+ *   - use an array and a way of identifying an object to scope a key to a specific object.
+ *
+ * While more in-depth tests can be performed, we can provide a very quick implementation of a simple script that
+ * shows how using the inline approach can be a first attempt that is much more performing than using a registry. E.g.
+ *
+ *     $repetitions = 1000000;
+ *     $propertyName = '__dd_store_key';
+ *
+ *     $objects_in_place = [];
+ *     $objects_array = [];
+ *
+ *     // Preparing objects
+ *     for ($i = 0; $i < $repetitions; $i++) {
+ *         $objects_in_place[] = new stdClass();
+ *         $objects_array[] = new stdClass();
+ *     }
+ *
+ *     function measure($what, $callable) {
+ *         echo $what . "\n";
+ *         $start = microtime(true);
+ *         $callable();
+ *         $end = microtime(true);
+ *         echo "  -> took secs: " . ($end - $start);
+ *         echo "\n\n";
+ *     }
+ *
+ *     $registry_flat = [];
+ *     measure('Put object in flat array', function () use ($repetitions, $objects_array, $registry_flat) {
+ *         for ($i = 0; $i < $repetitions; $i++) {
+ *             $key = spl_object_hash($objects_array[$i]) . '_key';
+ *             $registry_flat[$key] = 'Sam';
+ *         }
+ *     });
+ *
+ *     measure('Get object in flat array', function () use ($repetitions, $objects_array, $registry_flat) {
+ *         for ($i = 0; $i < $repetitions; $i++) {
+ *             $key = spl_object_hash($objects_array[$i]) . '_key';
+ *             $a = $registry_flat[$key];
+ *         }
+ *     });
+ *
+ *     $registry_composed = [];
+ *     measure('Put object in structured array', function () use ($repetitions, $objects_array, $registry_composed) {
+ *         for ($i = 0; $i < $repetitions; $i++) {
+ *             $hash = spl_object_hash($objects_array[$i]);
+ *             if (!array_key_exists($hash, $registry_composed)) {
+ *                 $registry_composed[$hash] = [];
+ *             }
+ *             $registry_composed['key'] = 'Sam';
+ *         }
+ *     });
+ *
+ *     measure('Get object in structured array', function () use ($repetitions, $objects_array, $registry_composed) {
+ *         for ($i = 0; $i < $repetitions; $i++) {
+ *             $hash = spl_object_hash($objects_array[$i]);
+ *             if (!empty($registry_composed[$hash]['key'])) {
+ *                 $a = $registry_composed[$hash]['key'];
+ *             }
+ *         }
+ *     });
+ *
+ *     measure('Put object in place', function () use ($repetitions, $objects_in_place, $propertyName) {
+ *         for ($i = 0; $i < $repetitions; $i++) {
+ *             $objects_in_place[$i]->$propertyName = 'Sam';
+ *         }
+ *     });
+ *
+ *     measure('Get object in place', function () use ($repetitions, $objects_in_place, $propertyName) {
+ *         for ($i = 0; $i < $repetitions; $i++) {
+ *             $a = $objects_in_place[$i]->$propertyName;
+ *         }
+ *     });
+ *
+ *
+ * The above script outputs the following result:
+ *
+ *      $ php -d memory_limit=-1 playground.php
+ *
+ *      Put object in flat array
+ *        -> took secs: 3.916533946991
+ *
+ *      Get object in flat array
+ *        -> took secs: 3.8086051940918
+ *
+ *      Put object in structured array
+ *        -> took secs: 10.850040912628
+ *
+ *      Get object in structured array
+ *        -> took secs: 3.7683751583099
+ *
+ *      Put object in place
+ *        -> took secs: 0.62740993499756
+ *
+ *      Get object in place
+ *        -> took secs: 0.12619400024414
  */
 class ObjectKVStore
 {
