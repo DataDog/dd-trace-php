1619cf7b01846ede0482809202b80bdd2d48e367
Luca Abbati <luca.abbati@datadoghq.com>
2018-11-27 14:41:25 +0100

    Curl integration (#147)
    * [curl] Add curl integration
    
    * [curl] Add httobin docker image to CI for integration tests execution
    
    * [core] Fix docblock @param definition
    
    * [curl] Improve tests as suggested during CR

---
+++ tests/Integration/Integrations/Curl/CurlIntegrationTest.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace DDTrace\Tests\Integration\Integrations\Curl;
+
+use DDTrace\Integrations\Curl\CurlIntegration;
+use DDTrace\Tests\Integration\Common\IntegrationTestCase;
+use DDTrace\Tests\Integration\Common\SpanAssertion;
+
+
+final class CurlIntegrationTest extends IntegrationTestCase
+{
+    const URL = 'http://httpbin_integration';
+    const URL_NOT_EXISTS = '__i_am_not_real__.invalid';
+
+    public static function setUpBeforeClass()
+    {
+        CurlIntegration::load();
+    }
+
+    public function testLoad200UrlOnInit()
+    {
+        $traces = $this->isolateTracer(function () {
+            $ch = curl_init(self::URL . '/status/200');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($ch);
+            $this->assertSame('', $response);
+            curl_close($ch);
+        });
+
+        $this->assertSpans($traces, [
+            SpanAssertion::build('curl_exec', 'curl', 'http', 'http://httpbin_integration/status/200')
+                ->withExactTags([
+                    'http.url' => self::URL . '/status/200',
+                    'http.status_code' => '200',
+                ]),
+        ]);
+    }
+
+    public function testLoad200UrlAsOpt()
+    {
+        $traces = $this->isolateTracer(function () {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, self::URL . '/status/200');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($ch);
+            $this->assertSame('', $response);
+            curl_close($ch);
+        });
+
+        $this->assertSpans($traces, [
+            SpanAssertion::build('curl_exec', 'curl', 'http', 'http://httpbin_integration/status/200')
+                ->withExactTags([
+                    'http.url' => self::URL . '/status/200',
+                    'http.status_code' => '200',
+                ]),
+        ]);
+    }
+
+    public function testLoad404UrlOnInit()
+    {
+        $traces = $this->isolateTracer(function () {
+            $ch = curl_init(self::URL . '/status/404');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($ch);
+            $this->assertSame('', $response);
+            curl_close($ch);
+        });
+
+        $this->assertSpans($traces, [
+            SpanAssertion::build('curl_exec', 'curl', 'http', 'http://httpbin_integration/status/404')
+                ->withExactTags([
+                    'http.url' => self::URL . '/status/404',
+                    'http.status_code' => '404',
+                ]),
+        ]);
+    }
+
+    public function testLoadUnroutableIP()
+    {
+        $traces = $this->isolateTracer(function () {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, "http://10.255.255.1/");
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, 100);
+            curl_exec($ch);
+            curl_close($ch);
+        });
+
+        $this->assertSpans($traces, [
+            SpanAssertion::build('curl_exec', 'curl', 'http', 'http://10.255.255.1/')
+                ->withExactTags([
+                    'http.url' => 'http://10.255.255.1/',
+                    'http.status_code' => '0',
+                    'error.type' => 'curl error',
+                ])
+                ->withExistingTagsNames(['error.msg'])
+                ->setError(),
+        ]);
+    }
+
+    public function testLoadOperationTimeout()
+    {
+        $traces = $this->isolateTracer(function () {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, "http://10.255.255.1/");
+            curl_setopt($ch, CURLOPT_TIMEOUT_MS, 100);
+            curl_exec($ch);
+            curl_close($ch);
+        });
+
+        $this->assertSpans($traces, [
+            SpanAssertion::build('curl_exec', 'curl', 'http', 'http://10.255.255.1/')
+                ->withExactTags([
+                    'http.url' => 'http://10.255.255.1/',
+                    'http.status_code' => '0',
+                    'error.type' => 'curl error',
+                ])
+                ->withExistingTagsNames(['error.msg'])
+                ->setError(),
+        ]);
+    }
+
+    public function testNonExistingHost()
+    {
+        $traces = $this->isolateTracer(function () {
+            $ch = curl_init(self::URL_NOT_EXISTS);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($ch);
+            $this->assertFalse($response);
+            curl_close($ch);
+        });
+
+        $this->assertSpans($traces, [
+            SpanAssertion::build('curl_exec', 'curl', 'http', 'http://__i_am_not_real__.invalid/')
+                ->withExactTags([
+                    'http.url' => 'http://__i_am_not_real__.invalid/',
+                    'http.status_code' => '0',
+                    'error.msg' => 'Could not resolve host: __i_am_not_real__.invalid',
+                    'error.type' => 'curl error',
+                ])
+                ->setError(),
+        ]);
+    }
+}
