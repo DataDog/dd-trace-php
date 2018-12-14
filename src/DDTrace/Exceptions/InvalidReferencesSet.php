0629894753e8e4c0f656f56965bef1d61112534c
sammy.powers <sammyk@datadoghq.com>
2018-12-14 15:52:51 -0500

    Add OpenTracing exceptions

---
+++ src/DDTrace/Exceptions/InvalidReferencesSet.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Ported from opentracing/opentracing
+ * @see https://github.com/opentracing/opentracing-php/blob/master/src/OpenTracing/Exceptions/InvalidReferencesSet.php
+ */
+
+namespace DDTrace\Exceptions;
+
+use DomainException;
+
+/**
+ * Thrown when a reference has more than one parent in the SpanOptions
+ */
+final class InvalidReferencesSet extends DomainException
+{
+    /**
+     * @param string $message
+     * @return InvalidReferencesSet
+     */
+    public static function create($message)
+    {
+        return new self($message);
+    }
+
+    /**
+     * @return InvalidReferencesSet
+     */
+    public static function forMoreThanOneParent()
+    {
+        return new self('Span can not have more than one parent, either one as child_of or either one as follows_from');
+    }
+}
