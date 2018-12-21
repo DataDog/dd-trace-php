a61255c6f1cc0d471590ae8366b90d1183c1b0f9
Sammy Kaye Powers <sammyk@sammykmedia.com>
2018-12-21 09:14:38 -0500

    Replace hex string ID's with unsigned-63-bit ints (#204)
    * Replace hex string IDs for traces/spans with unsigned-63-bit-int-id strings
    
    * Update propagator tests to show the trace and span IDs serializing on the wire as ints instead of hex strings
    
    * Update CHANGELOG
    
    * Fix link in CHANGELOG

--- src/DDTrace/Encoders/Json.php
+++ src/DDTrace/Encoders/Json.php
@@ -66,19 +66,10 @@ final class Json implements Encoder
         ], [
             '"start":' . $span->getStartTime() . '000',
             '"duration":' . $span->getDuration() . '000',
-            '"trace_id":' . $this->hex2dec($span->getTraceId()),
-            '"span_id":' . $this->hex2dec($span->getSpanId()),
-            '"parent_id":' . $this->hex2dec($span->getParentId()),
+            '"trace_id":' . $span->getTraceId(),
+            '"span_id":' . $span->getSpanId(),
+            '"parent_id":' . $span->getParentId(),
         ], $json);
-    }
-
-    /**
-     * @param string $hex
-     * @return string
-     */
-    private function hex2dec($hex)
-    {
-        return base_convert($hex, 16, 10);
     }
 
     /**
--- src/DDTrace/SpanContext.php
+++ src/DDTrace/SpanContext.php
@@ -157,7 +157,15 @@ final class SpanContext implements OpenTracingSpanContext
 
     private static function nextId()
     {
-        return bin2hex(random_bytes(8));
+        /*
+         * Trace and span ID's need to be unsigned-63-bit-int strings in
+         * order to work well with other APM integrations. Since the tracer
+         * is not in a cryptographic context, we don't need to use PHP's
+         * CSPRNG random_bytes(); instead the more performant mt_rand()
+         * will do. And since all integers in PHP are signed, an int
+         * between 1 & PHP_INT_MAX will be 63-bit.
+         */
+        return (string) mt_rand(1, PHP_INT_MAX);
     }
 
     /**
