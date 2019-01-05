3aaa2e4eeda7b4b75ead7922c8e0905a1cae941c
Luca Abbati <luca.abbati@gmail.com>
2019-01-05 12:59:13 +0100

    [core] Transform IntegrationsLoader from a static-based to an instance-based approach

--- src/DDTrace/Integrations/IntegrationsLoader.php
+++ src/DDTrace/Integrations/IntegrationsLoader.php
@@ -19,35 +19,63 @@ use DDTrace\Integrations\Predis\PredisIntegration;
 class IntegrationsLoader
 {
     /**
-     * @var array Registry to keep track of integrations loading status.
+     * @var IntegrationsLoader
      */
-    private static $loadings = [];
+    private static $instance;
 
     /**
-     * @return array A list of supported library integrations. Web frameworks ARE NOT INCLUDED.
+     * @var array
      */
-    public static function allLibraries()
+    private $integrations = [];
+
+    /**
+     * @var array
+     */
+    public static $officiallySupportedIntegrations = [
+        CurlIntegration::NAME => '\DDTrace\Integrations\Curl\CurlIntegration',
+        ElasticSearchIntegration::NAME => '\DDTrace\Integrations\ElasticSearch\V1\ElasticSearchIntegration',
+        EloquentIntegration::NAME => '\DDTrace\Integrations\Eloquent\EloquentIntegration',
+        GuzzleIntegration::NAME => '\DDTrace\Integrations\Guzzle\V5\GuzzleIntegration',
+        MemcachedIntegration::NAME => '\DDTrace\Integrations\Memcached\MemcachedIntegration',
+        MongoIntegration::NAME => '\DDTrace\Integrations\Mongo\MongoIntegration',
+        MysqliIntegration::NAME => '\DDTrace\Integrations\Mysqli\MysqliIntegration',
+        PDOIntegration::NAME => '\DDTrace\Integrations\PDO\PDOIntegration',
+        PredisIntegration::NAME => '\DDTrace\Integrations\Predis\PredisIntegration',
+    ];
+
+    /**
+     * @var array Registry to keep track of integrations loading status.
+     */
+    private $loadings = [];
+
+    /**
+     * @param array|null $integrations
+     */
+    public function __construct(array $integrations)
     {
-        return [
-            CurlIntegration::NAME => '\DDTrace\Integrations\Curl\CurlIntegration',
-            ElasticSearchIntegration::NAME => '\DDTrace\Integrations\ElasticSearch\V1\ElasticSearchIntegration',
-            EloquentIntegration::NAME => '\DDTrace\Integrations\Eloquent\EloquentIntegration',
-            GuzzleIntegration::NAME => '\DDTrace\Integrations\Guzzle\V5\GuzzleIntegration',
-            MemcachedIntegration::NAME => '\DDTrace\Integrations\Memcached\MemcachedIntegration',
-            MongoIntegration::NAME => '\DDTrace\Integrations\Mongo\MongoIntegration',
-            MysqliIntegration::NAME => '\DDTrace\Integrations\Mysqli\MysqliIntegration',
-            PDOIntegration::NAME => '\DDTrace\Integrations\PDO\PDOIntegration',
-            PredisIntegration::NAME => '\DDTrace\Integrations\Predis\PredisIntegration',
-        ];
+        $this->integrations = $integrations;
     }
 
     /**
-     * Loads all the enabled library integrations.
+     * Returns the singleton integration loader.
+     *
+     * @return IntegrationsLoader
      */
-    public static function load()
+    public static function get()
+    {
+        if (null === self::$instance) {
+            self::$instance = new IntegrationsLoader(self::$officiallySupportedIntegrations);
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Loads all the integrations registered with this loader instance.
+     */
+    public function loadAll()
     {
         $globalConfig = Configuration::get();
-
         if (!$globalConfig->isEnabled()) {
             return;
         }
@@ -60,7 +88,7 @@ class IntegrationsLoader
             return;
         }
 
-        foreach (self::allLibraries() as $name => $class) {
+        foreach ($this->integrations as $name => $class) {
             if (!$globalConfig->isIntegrationEnabled($name)) {
                 continue;
             }
@@ -68,13 +96,41 @@ class IntegrationsLoader
             // If the integration has already been loaded, we don't need to reload it. On the other hand, with
             // auto-instrumentation this method may be called many times as the hook is the autoloader callback.
             // So we want to make sure that we do not load the same integration twice if not required.
-            if (isset(self::$loadings[$name])
-                    && in_array(self::$loadings[$name], [Integration::LOADED, Integration::NOT_AVAILABLE])
-            ) {
+            if (in_array($this->getLoadingStatus($name), [Integration::LOADED, Integration::NOT_AVAILABLE])) {
                 continue;
             }
 
-            self::$loadings[$name] = call_user_func([$class, 'load']);
+            $this->loadings[$name] = call_user_func([$class, 'load']);
         }
+    }
+
+    /**
+     * Returns the registered integrations.
+     *
+     * @return array
+     */
+    public function getIntegrations()
+    {
+        return $this->integrations;
+    }
+
+    /**
+     * Returns the provide integration current loading status.
+     *
+     * @param string $integrationName
+     * @return int
+     */
+    public function getLoadingStatus($integrationName)
+    {
+        return isset($this->loadings[$integrationName]) ? $this->loadings[$integrationName] : Integration::NOT_LOADED;
+    }
+
+    /**
+     * Loads all the enabled library integrations using the global singleton integrations loader which in charge
+     * only of the officially supported integrations.
+     */
+    public static function load()
+    {
+        self::get()->loadAll();
     }
 }
--- tests/Unit/Integrations/IntegrationsLoaderTest.php
+++ tests/Unit/Integrations/IntegrationsLoaderTest.php
@@ -2,20 +2,151 @@
 
 namespace DDTrace\Tests\Unit\Integrations;
 
+use DDTrace\Configuration;
+use DDTrace\Integrations\Integration;
 use DDTrace\Integrations\IntegrationsLoader;
-use PHPUnit\Framework;
+use DDTrace\Tests\Unit\BaseTestCase;
 
-final class IntegrationsLoaderTest extends Framework\TestCase
+final class IntegrationsLoaderTest extends BaseTestCase
 {
+    private static $dummyIntegrations = [
+        'integration_1' => 'DDTrace\Tests\Unit\Integrations\DummyIntegration1',
+        'integration_2' => 'DDTrace\Tests\Unit\Integrations\DummyIntegration2',
+    ];
+
+    public function testGlobalLoaderDefaultsToOfficiallySupportedIntegrations()
+    {
+        $this->assertEquals(
+            IntegrationsLoader::$officiallySupportedIntegrations,
+            IntegrationsLoader::get()->getIntegrations()
+        );
+    }
+
+    public function testIntegrationsCanBeProvidedToLoader()
+    {
+        $integration = [
+            'name' => 'class',
+        ];
+        $this->assertEquals($integration, (new IntegrationsLoader($integration))->getIntegrations());
+    }
+
+    public function testGlobalConfigCanDisableLoading()
+    {
+        Configuration::replace(\Mockery::mock('\DDTrace\Configuration', [
+            'isEnabled' => false,
+        ]));
+
+        DummyIntegration1::$value = Integration::LOADED;
+        $loader = new IntegrationsLoader(self::$dummyIntegrations);
+        $loader->loadAll();
+
+        $this->assertSame(Integration::NOT_LOADED, $loader->getLoadingStatus('integration_1'));
+    }
+
+    public function testSingleIntegrationLoadingCanBeDisabled()
+    {
+        Configuration::replace(\Mockery::mock('\DDTrace\Configuration', [
+            'isEnabled' => true,
+            'isIntegrationEnabled' => false,
+        ]));
+
+        DummyIntegration1::$value = Integration::LOADED;
+        $loader = new IntegrationsLoader(self::$dummyIntegrations);
+        $loader->loadAll();
+
+        $this->assertSame(Integration::NOT_LOADED, $loader->getLoadingStatus('integration_1'));
+    }
+
+    public function testIntegrationsAreLoaded()
+    {
+        Configuration::replace(\Mockery::mock('\DDTrace\Configuration', [
+            'isEnabled' => true,
+            'isIntegrationEnabled' => true,
+        ]));
+        $loader = new IntegrationsLoader(self::$dummyIntegrations);
+
+        DummyIntegration1::$value = Integration::LOADED;
+        DummyIntegration2::$value = Integration::NOT_AVAILABLE;
+        $loader->loadAll();
+
+        $this->assertSame(Integration::LOADED, $loader->getLoadingStatus('integration_1'));
+        $this->assertSame(Integration::NOT_AVAILABLE, $loader->getLoadingStatus('integration_2'));
+    }
+
+    public function testIntegrationAlreadyLoadedIsNotReloaded()
+    {
+        Configuration::replace(\Mockery::mock('\DDTrace\Configuration', [
+            'isEnabled' => true,
+            'isIntegrationEnabled' => true,
+        ]));
+        $loader = new IntegrationsLoader(self::$dummyIntegrations);
+
+        // Initially the integration is not loaded
+        $this->assertSame(Integration::NOT_LOADED, $loader->getLoadingStatus('integration_1'));
+
+        // We load it
+        DummyIntegration1::$value = Integration::LOADED;
+        $loader->loadAll();
+        $this->assertSame(Integration::LOADED, $loader->getLoadingStatus('integration_1'));
+
+        // If now we change the returned value, it won't be reflected in the loadings statuses as it is not reloaded
+        DummyIntegration1::$value = Integration::NOT_AVAILABLE;
+        $loader->loadAll();
+        $this->assertSame(Integration::LOADED, $loader->getLoadingStatus('integration_1'));
+    }
+
+    public function testIntegrationNotAvailableIsNotReloaded()
+    {
+        Configuration::replace(\Mockery::mock('\DDTrace\Configuration', [
+            'isEnabled' => true,
+            'isIntegrationEnabled' => true,
+        ]));
+        $loader = new IntegrationsLoader(self::$dummyIntegrations);
+
+        // Initially the integration is not loaded
+        $this->assertSame(Integration::NOT_LOADED, $loader->getLoadingStatus('integration_1'));
+
+        // We load it
+        DummyIntegration1::$value = Integration::NOT_AVAILABLE;
+        $loader->loadAll();
+        $this->assertSame(Integration::NOT_AVAILABLE, $loader->getLoadingStatus('integration_1'));
+
+        // If now we change the returned value, it won't be reflected in the loadings statuses as it is not reloaded
+        DummyIntegration1::$value = Integration::LOADED;
+        $loader->loadAll();
+        $this->assertSame(Integration::NOT_AVAILABLE, $loader->getLoadingStatus('integration_1'));
+    }
+
+    public function testIntegrationNotLoadedIsReloaded()
+    {
+        Configuration::replace(\Mockery::mock('\DDTrace\Configuration', [
+            'isEnabled' => true,
+            'isIntegrationEnabled' => true,
+        ]));
+        $loader = new IntegrationsLoader(self::$dummyIntegrations);
+
+        // Initially the integration is not loaded
+        $this->assertSame(Integration::NOT_LOADED, $loader->getLoadingStatus('integration_1'));
+
+        // We load it, but the integration returned Integration::NOT_LOADED
+        DummyIntegration1::$value = Integration::NOT_LOADED;
+        $loader->loadAll();
+        $this->assertSame(Integration::NOT_LOADED, $loader->getLoadingStatus('integration_1'));
+
+        // If now we change the returned value, it won't be reflected in the loadings statuses as it is not reloaded
+        DummyIntegration1::$value = Integration::LOADED;
+        $loader->loadAll();
+        $this->assertSame(Integration::LOADED, $loader->getLoadingStatus('integration_1'));
+    }
 
     public function testWeDidNotForgetToRegisterALibraryForAutoLoading()
     {
         $expected = $this->normalize(glob(__DIR__ . '/../../../src/DDTrace/Integrations/*', GLOB_ONLYDIR));
         $expectedButFrameworks = array_diff($expected, $this->normalize(self::frameworks()));
-        $autoLoaded = $this->normalize(array_keys(IntegrationsLoader::allLibraries()));
+        $loaded = $this->normalize(array_keys(IntegrationsLoader::get()->getIntegrations()));
 
         // If this test fails you need to add an entry to IntegrationsLoader::LIBRARIES array.
-        $this->assertEquals(array_values($expectedButFrameworks), array_values($autoLoaded));
+        $this->assertEquals(array_values($expectedButFrameworks), array_values($loaded));
     }
 
     /**
@@ -47,5 +178,25 @@ final class IntegrationsLoaderTest extends Framework\TestCase
             'laravel',
             'symfony',
         ];
+    }
+}
+
+class DummyIntegration1
+{
+    public static $value = null;
+
+    public static function load()
+    {
+        return self::$value;
+    }
+}
+
+class DummyIntegration2
+{
+    public static $value = null;
+
+    public static function load()
+    {
+        return self::$value;
     }
 }
