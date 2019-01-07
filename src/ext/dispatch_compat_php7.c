f090ed1f872957b5980b231321d4cc2b7efa58be
Sammy Kaye Powers <sammyk@sammykmedia.com>
2019-01-07 08:34:29 -0500

    Fix compile-time error on PHP 7.3 (#215)

--- src/ext/dispatch_compat_php7.c
+++ src/ext/dispatch_compat_php7.c
@@ -43,7 +43,11 @@ HashTable *ddtrace_new_class_lookup(zend_class_entry *clazz) {
 }
 
 zend_bool ddtrace_dispatch_store(HashTable *lookup, ddtrace_dispatch_t *dispatch_orig) {
+#if PHP_VERSION_ID >= 70300
+    ddtrace_dispatch_t *dispatch = pemalloc(sizeof(ddtrace_dispatch_t), lookup->u.flags & IS_ARRAY_PERSISTENT);
+#else
     ddtrace_dispatch_t *dispatch = pemalloc(sizeof(ddtrace_dispatch_t), lookup->u.flags & HASH_FLAG_PERSISTENT);
+#endif
 
     memcpy(dispatch, dispatch_orig, sizeof(ddtrace_dispatch_t));
     return zend_hash_update_ptr(lookup, dispatch->function, dispatch) != NULL;
