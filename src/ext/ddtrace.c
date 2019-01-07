6655feb9158ca097e26704ea679d8d4821f16822
Pawel Chojnacki <pawel.chcki@gmail.com>
2019-01-07 19:13:41 +0100

    expand unused macro to more parameters

--- src/ext/ddtrace.c
+++ src/ext/ddtrace.c
@@ -28,8 +28,15 @@
         UNUSED_1(y);      \
         UNUSED_1(z);      \
     } while (0)
-#define _GET_UNUSED_MACRO_OF_ARITY(_1, _2, _3, ARITY, ...) UNUSED_##ARITY
-#define UNUSED(...) _GET_UNUSED_MACRO_OF_ARITY(__VA_ARGS__, 3, 2, 1)(__VA_ARGS__)
+#define UNUSED_4(x, y, z, q) \
+    do {                     \
+        UNUSED_1(x);         \
+        UNUSED_1(y);         \
+        UNUSED_1(z);         \
+        UNUSED_1(q);         \
+    } while (0)
+#define _GET_UNUSED_MACRO_OF_ARITY(_1, _2, _3, _4, ARITY, ...) UNUSED_##ARITY
+#define UNUSED(...) _GET_UNUSED_MACRO_OF_ARITY(__VA_ARGS__, 4, 3, 2, 1)(__VA_ARGS__)
 
 #if PHP_VERSION_ID < 70000
 #define PHP5_UNUSED(...) UNUSED(__VA_ARGS__)
