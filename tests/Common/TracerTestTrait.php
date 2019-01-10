6ad533087116f06ab76467b81e8f2fb9e6693c05
Luca Abbati <luca.abbati@gmail.com>
2019-01-10 15:50:00 +0100

    [tests] Re-parse data dumped by the fake agent into DDTrace\Span objects

--- tests/Common/TracerTestTrait.php
+++ tests/Common/TracerTestTrait.php
@@ -2,14 +2,19 @@
 
 namespace DDTrace\Tests\Common;
 
+use DDTrace\Encoders\Json;
 use DDTrace\Span;
+use DDTrace\SpanContext;
 use DDTrace\Tests\DebugTransport;
 use DDTrace\Tracer;
+use DDTrace\Transport\Http;
 use OpenTracing\GlobalTracer;
 
 
 trait TracerTestTrait
 {
+    private $dumpFilePath = __DIR__ . '/../../.request_dumper_data/dump.json';
+
     /**
      * @param $fn
      * @param null $tracer
@@ -23,6 +28,98 @@ trait TracerTestTrait
 
         $fn($tracer);
         return $this->flushAndGetTraces($transport);
+    }
+
+    /**
+     * This method can be used to request data to a real request dumper and to rebuild the traces
+     * based on the dumped data.
+     *
+     * @param $fn
+     * @param null $tracer
+     * @return Span[][]
+     */
+    public function simulateAgent($fn, $tracer = null)
+    {
+        // TODO: delete file before request
+        // file_delete($this->dumpFilePath);
+        $transport = new Http(new Json(), null, ['endpoint' => 'request_dumper']);
+        $tracer = $tracer ?: new Tracer($transport);
+        GlobalTracer::set($tracer);
+
+        $fn($tracer);
+        /** @var Tracer $tracer */
+        $tracer = GlobalTracer::get();
+        /** @var DebugTransport $transport */
+        $tracer->flush();
+
+        return $this->parseTracesFromDumpedData();
+    }
+
+    /**
+     * Parses the data dumped by the fake agent and returns the parsed traces.
+     *
+     * @return array
+     */
+    private function parseTracesFromDumpedData()
+    {
+        $loaded = json_decode(file_get_contents($this->dumpFilePath), true);
+        $rawTraces = json_decode($loaded['body'], true);
+        $traces = [];
+        foreach ($rawTraces as $spansInTrace) {
+            $spans = [];
+            foreach ($spansInTrace as $rawSpan) {
+                $spanContext = new SpanContext(
+                    $rawSpan['trace_id'],
+                    $rawSpan['span_id']
+                );
+                $span = new Span(
+                    $rawSpan['name'],
+                    $spanContext,
+                    $rawSpan['service'],
+                    $rawSpan['resource'],
+                    $rawSpan['start']
+                );
+
+                // We want to use reflection to set properties so that we do not fire
+                // potentials changes in setters.
+                $this->setRawPropertyFromArray($span, $rawSpan, 'operationName', 'name');
+                $this->setRawPropertyFromArray($span, $rawSpan, 'service');
+                $this->setRawPropertyFromArray($span, $rawSpan, 'resource');
+                $this->setRawPropertyFromArray($span, $rawSpan, 'startTime', 'start');
+                $this->setRawPropertyFromArray($span, $rawSpan, 'hasError', 'error', 'boolval');
+                $this->setRawPropertyFromArray($span, $rawSpan, 'type');
+                $this->setRawPropertyFromArray($span, $rawSpan, 'duration');
+                $this->setRawPropertyFromArray($span, $rawSpan, 'tags', 'meta');
+
+                $spans[] = $span;
+            }
+            $traces[] = $spans;
+        }
+        return $traces;
+    }
+
+    /**
+     * Set a property into an object from an array optionally applying a converter.
+     *
+     * @param $obj
+     * @param array $data
+     * @param string $property
+     * @param string|null $field
+     * @param mixed|null $converter
+     */
+    private function setRawPropertyFromArray($obj, array $data, $property, $field = null, $converter = null)
+    {
+        $field = $field ?: $property;
+        $reflection = new \ReflectionObject($obj);
+        $property = $reflection->getProperty($property);
+        $convertedValue = $converter ? call_user_func($converter, $data[$field]) : $data[$field];
+        if ($property->isPrivate() || $property->isProtected()) {
+            $property->setAccessible(true);
+            $property->setValue($obj, $convertedValue);
+            $property->setAccessible(false);
+        } else {
+            $property->setValue($obj, $convertedValue);
+        }
     }
 
     /**
