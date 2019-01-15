f3822d806efd6df29e0378900852d41079cc7dde
Luca Abbati <luca.abbati@gmail.com>
2019-01-15 09:51:55 -0500

    [tests] Improve request replayer returning traces

--- tests/request_replayer/index.php
+++ tests/request_replayer/index.php
@@ -1,6 +1,7 @@
 <?php
 
-$requestLog = '/tmp/php_request_replayer_' . getmypid() . '.json';
+$defaultRequestLog = '/tmp/php_request_replayer_' . getmypid() . '.json';
+$requestLog = getenv('DD_REQUEST_DUMPER_FILE') ? : $defaultRequestLog;
 
 if (php_sapi_name() == 'cli-server') {
     if ($_SERVER['REQUEST_URI'] == '/replay') {
@@ -8,8 +9,13 @@ if (php_sapi_name() == 'cli-server') {
             echo file_get_contents($requestLog);
         }
         file_put_contents($requestLog, '');
+    } elseif ($_SERVER['REQUEST_URI'] == '/clear-dumped-data') {
+        if (file_exists($requestLog)) {
+            unlink($requestLog);
+        }
     } else {
         file_put_contents($requestLog, json_encode([
+            'uri' => $_SERVER['REQUEST_URI'],
             'headers' => getallheaders(),
             'body' => file_get_contents('php://input'),
         ]));
