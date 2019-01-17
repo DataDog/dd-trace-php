3a28e7fd1114bee101d30b155e27879f57a71013
sammy.powers <sammyk@datadoghq.com>
2019-01-17 09:56:49 -0500

    Fix linting

--- tests/Frameworks/Util/CommonScenariosDataProviderTrait.php
+++ tests/Frameworks/Util/CommonScenariosDataProviderTrait.php
@@ -27,15 +27,19 @@ trait CommonScenariosDataProviderTrait
         // We expect that all the expectations that we defined have a corresponding request to serve
         $unexpectedExpectations = array_diff($allExpectationNames, $allRequestNames);
         if ($unexpectedExpectations) {
-            throw new PHPUnit_Framework_ExpectationFailedException('Found the following expectations not having any request defined: '
-                . implode(', ', $unexpectedExpectations));
+            throw new PHPUnit_Framework_ExpectationFailedException(
+                'Found the following expectations not having any request defined: '
+                . implode(', ', $unexpectedExpectations)
+            );
         }
 
         // We expect that all the scenarios that we defined have a corresponding expectation to serve
         $unexpectedRequest = array_diff($allRequestNames, $allExpectationNames);
         if ($unexpectedRequest) {
-            throw new PHPUnit_Framework_ExpectationFailedException('Found the following scenarios not having any expectation defined: '
-                . implode(', ', $unexpectedRequest));
+            throw new PHPUnit_Framework_ExpectationFailedException(
+                'Found the following scenarios not having any expectation defined: '
+                . implode(', ', $unexpectedRequest)
+            );
         }
 
         $dataProvider = [];
