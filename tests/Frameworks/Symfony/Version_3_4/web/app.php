49b793310453b7fdcc69901baf357035590fe9a1
Luca Abbati <luca.abbati@gmail.com>
2019-01-17 09:56:38 -0500

    [tests] Move back Symfony to temporarily use explicit instrumentation

--- tests/Frameworks/Symfony/Version_3_4/web/app.php
+++ tests/Frameworks/Symfony/Version_3_4/web/app.php
@@ -3,9 +3,13 @@
 use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../vendor/autoload.php';
+
 if (PHP_VERSION_ID < 70000) {
     include_once __DIR__.'/../var/bootstrap.php.cache';
 }
+
+\DDTrace\Bootstrap::once();
+\DDTrace\Integrations\IntegrationsLoader::load();
 
 $kernel = new AppKernel('prod', false);
 if (PHP_VERSION_ID < 70000) {
