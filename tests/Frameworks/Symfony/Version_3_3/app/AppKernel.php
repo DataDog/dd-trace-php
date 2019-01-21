abc973efbac860e807b96d86958518c57c15799f
Luca Abbati <luca.abbati@gmail.com>
2019-01-21 09:06:27 +0100

    [symfony] Initial tests setup

--- tests/Frameworks/Symfony/Version_3_3/app/AppKernel.php
+++ tests/Frameworks/Symfony/Version_3_3/app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new \DDTrace\Integrations\Symfony\V3\SymfonyBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
