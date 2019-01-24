f422e2c39c84894893cafc7e39d89063b44f8fb5
Luca Abbati <luca.abbati@gmail.com>
2019-01-24 07:16:58 +0100

    [core] Fix symfony 5.6 instrumentation for 5.6

--- src/DDTrace/Integrations/Symfony/V3/SymfonyIntegration.php
+++ src/DDTrace/Integrations/Symfony/V3/SymfonyIntegration.php
@@ -8,21 +8,28 @@ class SymfonyIntegration
 {
     public static function load()
     {
-        dd_trace('Symfony\Component\HttpKernel\Kernel', 'initializeBundles', function () {
-            $this->initializeBundles();
-            $name = DDSymfonyIntegration::BUNDLE_NAME;
+        // If this line is not here then the Symfony\Component\HttpKernel\Kernel::boot
+        // is not traced in php 5.6
+        dd_trace('AppKernel', 'handle', function() {
+            return call_user_func_array([$this, 'handle'], func_get_args());
+        });
 
-            // If the user has already registered the bundle, we do not register it again.
-            if (isset($this->bundles[$name])) {
-                return;
+        dd_trace('Symfony\Component\HttpKernel\Kernel', 'boot', function() {
+            $result = call_user_func_array([$this, 'boot'], func_get_args());
+
+            $name = DDSymfonyIntegration::BUNDLE_NAME;
+            if (!isset($this->bundles[$name])) {
+                $bundle = new SymfonyBundle();
+                // Simulating behavior of bundle initialization for bundles without any parent bundle based on:
+                // https://github.com/symfony/symfony/blob/05efd1243fb3910fbaaedabf9b4758604b397c0f/src/Symfony/Component/HttpKernel/Kernel.php#L481
+                $this->bundles[$name] = $bundle;
+                $this->bundleMap[$name] = [$bundle];
+
+                $bundle->setContainer($this->container);
+                $bundle->boot();
             }
 
-            $bundle = new SymfonyBundle();
-
-            // Simulating behavior of bundle initialization for bundles without any parent bundle based on:
-            // https://github.com/symfony/symfony/blob/05efd1243fb3910fbaaedabf9b4758604b397c0f/src/Symfony/Component/HttpKernel/Kernel.php#L481
-            $this->bundles[$name] = $bundle;
-            $this->bundleMap[$name] = [$bundle];
+            return $result;
         });
     }
 }
