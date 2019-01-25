795c2823633a54444015d59e7383053111dadd0e
Luca Abbati <luca.abbati@gmail.com>
2019-01-25 14:44:41 +0100

    [tmp] Playing with huge floating and fmod (a nice adventure)

--- src/DDTrace/Sampling/ConfigurableSampler.php
+++ src/DDTrace/Sampling/ConfigurableSampler.php
@@ -10,14 +10,60 @@ use DDTrace\Contracts\Span;
  */
 class ConfigurableSampler implements Sampler
 {
+    const KNUT_FACTOR = 1111111111111111111;
     /**
      * @param Span $span
      * @return int
      */
     public function getPrioritySampling(Span $span)
     {
-        $rate = Configuration::get()->getSamplingRate();
-        $shouldKeep = (int) $span->getSpanId() <= $rate * PHP_INT_MAX;
-        return $shouldKeep ? PrioritySampling::AUTO_KEEP : PrioritySampling::AUTO_REJECT;
+        echo "\n";
+        $rate = 0.5;
+//        $this->dump('KNUT   ', self::KNUT_FACTOR);
+//        $this->dump('PHP MAX', decbin(PHP_INT_MAX));
+//        $this->dump('span id', (float)$span->getSpanId());
+//        $this->dump('PHP_INT_MAX as float', (float)PHP_INT_MAX);
+//        $this->dump('Span id as float * KNUT', (float)$span->getSpanId() * self::KNUT_FACTOR);
+//        $this->dump('(Span id as float * KNUT) / MAX_INT', ((float)$span->getSpanId() * self::KNUT_FACTOR) / (float)PHP_INT_MAX);
+//
+//        $module = fmod((float)$span->getSpanId() * self::KNUT_FACTOR, (float)PHP_INT_MAX);
+//        $this->dump('Span id as float * KNUT % MAX_INT', $module);
+//        $this->dump(' ^^^ as binary', decbin($module));
+//
+//        $this->dump('example with floats', fmod(10.0, 3.0));
+//
+//        $this->dump('example with floats', fmod(10.0, 3.0));
+//        $this->dump('$rate * PHP_INT_MAX', $rate * PHP_INT_MAX);
+
+//        $this->dump('a float', (float)1.1);
+//        $this->dump('max float', PHP_FLOAT_MAX);
+//        $this->dump('PHP_INT_MAX as float', (float)PHP_INT_MAX);
+//        $this->dump('span id', (float)$span->getSpanId());
+//        $this->dump('(float)$span->getSpanId() * self::KNUT_FACTOR', (float)$span->getSpanId() * self::KNUT_FACTOR);
+//        $this->dump('fmod((float)PHP_INT_MAX, (float)$span->getSpanId())', fmod((float)PHP_INT_MAX, (float)$span->getSpanId()));
+//        $this->dump('fmod((float)$span->getSpanId(), (float)PHP_INT_MAX)', fmod((float)$span->getSpanId(), (float)PHP_INT_MAX));
+        $dividend = (float)$span->getSpanId() * self::KNUT_FACTOR;
+        $divisor = (float)PHP_INT_MAX;
+        $division = $dividend / $divisor;
+        $this->dump('PHP_INT_MAX as float', (float)PHP_INT_MAX);
+        $this->dump('$dividend', $dividend);
+        $this->dump('$divisor', $divisor);
+        $this->dump('$division', $division);
+        echo "Is division less than MAX INT? " . ($division <= PHP_INT_MAX) . "\n";
+        $this->dump('$dividend / $divisor', $dividend / $divisor);
+        $this->dump('$dividend / $divisor as int', intval($division));
+        $this->dump('($dividend / $divisor as int) * $divisor', intval($division) * $divisor);
+        $this->dump('fmod($dividend, $divisor)', fmod($dividend, $divisor));
+        $this->dump('trick', floatval($dividend - intval($dividend / $divisor) * $divisor));
+
+//        $rate = Configuration::get()->getSamplingRate();
+//        // $shouldKeep = (int) $span->getSpanId() <= $rate * PHP_INT_MAX;
+//        $shouldKeep = ((((float) $span->getSpanId()) * self::KNUT_FACTOR) % PHP_INT_MAX) <= ($rate * PHP_INT_MAX);
+//        return $shouldKeep ? PrioritySampling::AUTO_KEEP : PrioritySampling::AUTO_REJECT;
+    }
+
+    private function dump($what, $value)
+    {
+        echo $what . " of type '" . gettype($value) . "': " . $value . "\n";
     }
 }
--- tests/Unit/Sampling/ConfigurableSamplerTest.php
+++ tests/Unit/Sampling/ConfigurableSamplerTest.php
@@ -3,6 +3,7 @@
 namespace DDTrace\Tests\Unit\Sampling;
 
 use DDTrace\Configuration;
+use DDTrace\ID;
 use DDTrace\Sampling\ConfigurableSampler;
 use DDTrace\Span;
 use DDTrace\SpanContext;
@@ -10,43 +11,53 @@ use DDTrace\Tests\Unit\BaseTestCase;
 
 final class ConfigurableSamplerTest extends BaseTestCase
 {
+    const REPETITIONS = 1;
+    const DEVIATION = 1;
+
     public function testSpansAreKept()
     {
         Configuration::replace(\Mockery::mock('DDtrace\Configuration', [
             'getSamplingRate' => 0.5,
         ]));
         $sampler = new ConfigurableSampler();
-        $context = new SpanContext('', (string)(int)(PHP_INT_MAX * 0.49));
-        $this->assertSame(1, $sampler->getPrioritySampling(new Span('', $context, '', '')));
-    }
 
-    public function testSpansAreKeptRejected()
-    {
-        Configuration::replace(\Mockery::mock('DDtrace\Configuration', [
-            'getSamplingRate' => 0.5,
-        ]));
-        $sampler = new ConfigurableSampler();
-        $context = new SpanContext('', (string)(int)(PHP_INT_MAX * 0.51));
-        $this->assertSame(0, $sampler->getPrioritySampling(new Span('', $context, '', '')));
-    }
+        $output = 0;
 
-    public function testCanBeAlwaysOn()
-    {
-        Configuration::replace(\Mockery::mock('DDtrace\Configuration', [
-            'getSamplingRate' => 1.0,
-        ]));
-        $sampler = new ConfigurableSampler();
-        $context = new SpanContext('', '1');
-        $this->assertSame(1, $sampler->getPrioritySampling(new Span('', $context, '', '')));
-    }
+        for ($i = 0; $i < self::REPETITIONS; $i++) {
+            $context = new SpanContext('', ID::generate());
+            $output += $sampler->getPrioritySampling(new Span('', $context, '', ''));
+        }
 
-    public function testCanBeAlwaysOff()
-    {
-        Configuration::replace(\Mockery::mock('DDtrace\Configuration', [
-            'getSamplingRate' => 0.0,
-        ]));
-        $sampler = new ConfigurableSampler();
-        $context = new SpanContext('', (string) PHP_INT_MAX);
-        $this->assertSame(0, $sampler->getPrioritySampling(new Span('', $context, '', '')));
+        error_log("Output: " . print_r($output, 1));
     }
+//
+//    public function testSpansAreKeptRejected()
+//    {
+//        Configuration::replace(\Mockery::mock('DDtrace\Configuration', [
+//            'getSamplingRate' => 0.5,
+//        ]));
+//        $sampler = new ConfigurableSampler();
+//        $context = new SpanContext('', (string)(int)(PHP_INT_MAX * 0.51));
+//        $this->assertSame(0, $sampler->getPrioritySampling(new Span('', $context, '', '')));
+//    }
+//
+//    public function testCanBeAlwaysOn()
+//    {
+//        Configuration::replace(\Mockery::mock('DDtrace\Configuration', [
+//            'getSamplingRate' => 1.0,
+//        ]));
+//        $sampler = new ConfigurableSampler();
+//        $context = new SpanContext('', '1');
+//        $this->assertSame(1, $sampler->getPrioritySampling(new Span('', $context, '', '')));
+//    }
+//
+//    public function testCanBeAlwaysOff()
+//    {
+//        Configuration::replace(\Mockery::mock('DDtrace\Configuration', [
+//            'getSamplingRate' => 0.0,
+//        ]));
+//        $sampler = new ConfigurableSampler();
+//        $context = new SpanContext('', (string) PHP_INT_MAX);
+//        $this->assertSame(0, $sampler->getPrioritySampling(new Span('', $context, '', '')));
+//    }
 }
