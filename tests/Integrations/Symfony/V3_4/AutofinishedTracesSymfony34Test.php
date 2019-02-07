9aa69ffdc84b08e28c98807230eb2c3ba39943c7
Luca Abbati <luca.abbati@datadoghq.com>
2019-02-07 15:14:47 +0100

    Route not set in symfony 3.4 when user calls exit() (#289)
    * [symfony] Change the way route is set to support cases when user calls exit()
    
    * [core] Update changelog

---
+++ tests/Integrations/Symfony/V3_4/AutofinishedTracesSymfony34Test.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace DDTrace\Tests\Integrations\Symfony\V3_4;
+
+use DDTrace\Tests\Common\SpanAssertion;
+use DDTrace\Tests\Common\WebFrameworkTestCase;
+use DDTrace\Tests\Frameworks\Util\Request\GetSpec;
+
+final class AutofinishedTracesSymfony34Test extends WebFrameworkTestCase
+{
+    protected static function getAppIndexScript()
+    {
+        return __DIR__ . '/../../../Frameworks/Symfony/Version_3_4/web/app.php';
+    }
+
+    protected static function getEnvs()
+    {
+        return array_merge(parent::getEnvs(), [
+            'DD_AUTOFINISH_SPANS' => 'true',
+        ]);
+    }
+
+    public function testEndpointThatExitsWithNoProcess()
+    {
+        $traces = $this->tracesFromWebRequest(function () {
+            $this->call(GetSpec::create('Endpoint that invoke exit()', '/terminated_by_exit'));
+        });
+
+        $this->assertSpans($traces, [
+            SpanAssertion::build(
+                'symfony.request',
+                'symfony',
+                'web',
+                'terminated_by_exit'
+            )
+                ->withExactTags([
+                    'symfony.route.action' => 'AppBundle\Controller\HomeController@actionBeingTerminatedByExit',
+                    'symfony.route.name' => 'terminated_by_exit',
+                    'http.method' => 'GET',
+                    'http.url' => 'http://localhost:9999/terminated_by_exit',
+                    'http.status_code' => '200',
+                ]),
+            SpanAssertion::exists('symfony.kernel.handle'),
+            SpanAssertion::exists('symfony.kernel.request'),
+            SpanAssertion::exists('symfony.kernel.controller'),
+            SpanAssertion::exists('symfony.kernel.controller_arguments'),
+        ]);
+    }
+}
