3e62f56c6d4e33d4a6a695c8f3fb3ea56a1c05cf
Pawel Chojnacki <pawel.chcki@gmail.com>
2019-02-18 19:44:24 +0100

    support TSMRLS ZTS build for backtrace and logging extensions (#309)

--- src/ext/backtrace.c
+++ src/ext/backtrace.c
@@ -13,19 +13,22 @@
 ZEND_EXTERN_MODULE_GLOBALS(ddtrace);
 
 void ddtrace_backtrace_handler(int sig) {
-    ddtrace_log_err("Datadog PHP Trace extension (DEBUG MODE)");
-    ddtrace_log_errf("Received Signal %d", sig);
+    TSRMLS_FETCH();
+
+    ddtrace_log_err("Datadog PHP Trace extension (DEBUG MODE)" TSRMLS_CC);
+    ddtrace_log_errf("Received Signal %d" TSRMLS_CC, sig);
     void *array[MAX_STACK_SIZE];
     size_t size = backtrace(array, MAX_STACK_SIZE);
 
-    ddtrace_log_err("Note: Backtrace below might be incomplete and have wrong entries due to optimized runtime");
-    ddtrace_log_err("Backtrace:");
+    ddtrace_log_err(
+        "Note: Backtrace below might be incomplete and have wrong entries due to optimized runtime" TSRMLS_CC);
+    ddtrace_log_err("Backtrace:" TSRMLS_CC);
 
     char **backtraces = backtrace_symbols(array, size);
     if (backtraces) {
         size_t i;
         for (i = 0; i < size; i++) {
-            ddtrace_log_err(backtraces[i]);
+            ddtrace_log_err(backtraces[i] TSRMLS_CC);
         }
         free(backtraces);
     }
@@ -33,7 +36,7 @@ void ddtrace_backtrace_handler(int sig) {
     exit(sig);
 }
 
-void ddtrace_install_backtrace_handler() {
+void ddtrace_install_backtrace_handler(TSRMLS_D) {
     if (!ddtrace_get_bool_config("DD_LOG_BACKTRACE", DDTRACE_G(log_backtrace))) {
         return;
     }
--- src/ext/logging.h
+++ src/ext/logging.h
@@ -1,7 +1,9 @@
 #ifndef DD_LOGGING_H
 #define DD_LOGGING_H
+#include <php.h>
 
 #define ddtrace_log_err(message) php_log_err(message)
-void ddtrace_log_errf(const char *format, ...);
+
+void ddtrace_log_errf(const char *format TSRMLS_DC, ...);
 
 #endif  // DD_LOGGING_H
