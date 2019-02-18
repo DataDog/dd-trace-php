1c8f322cf1a0f2dc2160742dbc813d1b8f222793
Pawel Chojnacki <pawel.chcki@gmail.com>
2019-02-18 16:05:09 +0100

    use getenv directly + make it override SAPI env

--- src/ext/env_config.c
+++ src/ext/env_config.c
@@ -2,24 +2,38 @@
 #include <SAPI.h>
 #define EQUALS(stra, strb) (memcmp(stra, strb, sizeof(strb)-1) == 0)
 
+char *get_local_env_or_sapi_env(char *name){
+    char *env = NULL, *tmp = getenv(name);
+    if (tmp){
+        env = estrdup(tmp);
+    } else {
+        env = sapi_getenv(name, strlen(name));
+    }
+
+    return env;
+}
+
 zend_bool ddtrace_get_bool_config(char *name, zend_bool def){
-    char *env = sapi_getenv(name, strlen(name));
+    char *env = get_local_env_or_sapi_env(name);
     if (!env){
         return def;
     }
 
     size_t len = strlen(env);
     if (len > sizeof("false")){
+        efree(env);
         return def;
     }
 
     zend_str_tolower(env, len);
 
+    zend_bool rv = def;
     if (EQUALS(env, "1") || EQUALS(env, "true")) {
-        return 1;
+        rv = 1;
     } else if (EQUALS(env, "0") || EQUALS(env, "false")) {
-        return 0;
-    } else {
-        return def;
+        rv = 0;
     }
+
+    efree(env);
+    return rv;
 }
