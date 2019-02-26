7e8c9e582a2a72a0c2dc568454636c0e8d77cd32
Pawel Chojnacki <pawel.chcki@gmail.com>
2019-02-26 16:36:43 +0100

    duplicate string in < 7.x

--- src/ext/dispatch_setup.c
+++ src/ext/dispatch_setup.c
@@ -115,6 +115,9 @@ zend_bool ddtrace_trace(zval *class_name, zval *function_name, zval *callable TS
     dispatch.callable = *callable;
 
     zval_copy_ctor(&dispatch.function_name);
+#if PHP_VERSION_ID < 70000
+    ZVAL_STRING(&dispatch.function_name, function_name, 1)
+#endif
     zval_copy_ctor(&dispatch.callable);
 
     ddtrace_downcase_zval(&dispatch.function_name);
