ecec03741a3136e6344bbc84c67ce185b3bcd915
Luca Abbati <luca.abbati@gmail.com>
2019-03-05 17:54:19 +0100

    [symfony] Symfony 2.8 smaple app

---
+++ tests/Frameworks/Symfony/Version_2_8/web/app.php
@@ -0,0 +1,17 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+require __DIR__.'/../app/autoload.php';
+include_once __DIR__.'/../app/bootstrap.php.cache';
+
+$kernel = new AppKernel('prod', false);
+$kernel->loadClassCache();
+//$kernel = new AppCache($kernel);
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+//Request::enableHttpMethodParameterOverride();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
