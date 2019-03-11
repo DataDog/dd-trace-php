abfdbbec471c7953562afd910e2a82eb444f47b9
Luca Abbati <luca.abbati@gmail.com>
2019-03-11 10:54:31 +0100

    [symfony] Add Symfony 2.3 web tests for resource name setting

--- tests/Frameworks/Symfony/Version_2_3/src/AppBundle/Controller/DefaultController.php
+++ tests/Frameworks/Symfony/Version_2_3/src/AppBundle/Controller/DefaultController.php
@@ -11,11 +11,11 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function testingRouteNameAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ));
     }
 }
