e65faf4caad2038535bc040a51a4c495c42a7e87
Luca Abbati <luca.abbati@gmail.com>
2019-03-12 15:22:14 +0100

    [core] Treat generic web requets integration like others in integrations loader

--- src/DDTrace/Integrations/Web/WebIntegration.php
+++ src/DDTrace/Integrations/Web/WebIntegration.php
@@ -2,7 +2,21 @@
 
 namespace DDTrace\Integrations\Web;
 
+use DDTrace\Integrations\Integration;
+
 class WebIntegration
 {
     const NAME = 'web';
+
+    /**
+     * Loads the generic web request integration.
+     *
+     * @return int
+     */
+    public static function load()
+    {
+        // For now we do nothing, as this is done in the bootstrap logic at the moment. We may consider doing this
+        // here instead, but leaving this for a future refactoring.
+        return Integration::LOADED;
+    }
 }
