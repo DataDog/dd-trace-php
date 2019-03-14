e5696216700b126a67429f6657c19ee22303dcdf
Luca Abbati <luca.abbati@gmail.com>
2019-03-14 11:04:32 +0100

    [test] Add web frameworks integration owners definition, but tests do not run yet

--- tests/Common/SpanIntegrationChecker.php
+++ tests/Common/SpanIntegrationChecker.php
@@ -5,34 +5,49 @@ namespace DDTrace\Tests\Common;
 use DDTrace\Contracts\Span;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Checks that spans are assigned the proper integration.
+ */
 class SpanIntegrationChecker
 {
+    /**
+     * Returns the known matching patterns operation ==> integration class.
+     */
     public function defineIntegrationsByPattern()
     {
         // <regex pattern> => <integration class>
         return [
-            // Prepend your operation names with custom for custom spans
+            // Prepend your operation names with custom for custom spans to have the span check disabled
             '/custom.*/' => null,
             // Officially supported integrations
             '/curl_.*/' => 'DDTrace\Integrations\Curl\CurlIntegration',
             '/Elasticsearch(.).*/' => 'DDTrace\Integrations\ElasticSearch\V1\ElasticSearchIntegration',
             '/GuzzleHttp.*/' => 'DDTrace\Integrations\Guzzle\GuzzleIntegration',
+            '/laravel.*/' => 'DDTrace\Integrations\Laravel\LaravelIntegration',
             '/Memcached.*/' => 'DDTrace\Integrations\Memcached\MemcachedIntegration',
             '/Mongo(Client)|(DB)|(Collection).*/' => 'DDTrace\Integrations\Mongo\MongoIntegration',
             '/mysqli.*/' => 'DDTrace\Integrations\Mysqli\MysqliIntegration',
             '/PDO(.)|(Statement).*/' => 'DDTrace\Integrations\PDO\PDOIntegration',
             '/Predis.*/' => 'DDTrace\Integrations\Predis\PredisIntegration',
+            '/symfony.*/' => 'DDTrace\Integrations\Symfony\SymfonyIntegration',
+            '/web.*/' => 'DDTrace\Integrations\Web\WebIntegration',
+            '/zf.*/' => 'DDTrace\Integrations\ZendFramework\ZendFrameworkIntegration',
         ];
     }
 
+    /**
+     * Checks that $span belongs to the proper integration.
+     *
+     * @param TestCase $test
+     * @param Span $span
+     */
     public function checkIntegration(TestCase $test, Span $span)
     {
         $here = get_class($this) . '::defineOwners()';
         $operationName = $span->getOperationName();
         $definitionFound = false;
 
-        foreach ($this->defineIntegrationsByPattern() as $operationNameRegex => $integrationClass)
-        {
+        foreach ($this->defineIntegrationsByPattern() as $operationNameRegex => $integrationClass) {
             if (preg_match($operationNameRegex, $operationName)) {
                 $definitionFound = true;
 
@@ -52,10 +67,8 @@ class SpanIntegrationChecker
         }
 
         if (false === $definitionFound) {
-            $message = "Span's integration check is enabled but no matching pattern found for operation " .
-                       "'$operationName' in '$here'. If you are testing an integration where spans are " .
-                       "recreated from a request re-player, e.g. web tests, you can disable integration check " .
-                       "using the param '\$checkIntegration = false'. If you are testing custom " .
+            $message = "No matching pattern found for operation " .
+                       "'$operationName' in '$here'. If you are testing custom " .
                        "spans you can use operation name 'custom.*' or register the name in '$here'";
             $test->fail($message);
         }
