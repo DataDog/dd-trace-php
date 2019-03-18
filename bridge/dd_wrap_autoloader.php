69cb2462e38b359e75dc80da9b8df1ad29041770
Pawel Chojnacki <pawel.chcki@gmail.com>
2019-03-18 13:57:30 +0100

    dd_trace_disable_in_request() in autowrapper to avoid unnecessary processing

--- bridge/dd_wrap_autoloader.php
+++ bridge/dd_wrap_autoloader.php
@@ -5,10 +5,12 @@ namespace DDTrace\Bridge;
 require_once __DIR__ . '/functions.php';
 
 if (php_sapi_name() == 'cli' && getenv('APP_ENV') != 'dd_testing') {
+    dd_trace_disable_in_request();
     return;
 }
 
 if (!dd_tracing_enabled()) {
+    dd_trace_disable_in_request();
     return;
 }
 
