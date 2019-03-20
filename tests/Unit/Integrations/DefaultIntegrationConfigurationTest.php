bb225bfb041e0d2a3876292060073be98f1f0e5a
Luca Abbati <luca.abbati@gmail.com>
2019-03-20 17:23:38 +0100

    [tests] Add UTs for default integration configuration

---
+++ tests/Unit/Integrations/DefaultIntegrationConfigurationTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace DDTrace\Tests\Unit\Integrations;
+
+use DDTrace\Integrations\DefaultIntegrationConfiguration;
+use DDTrace\Tests\Unit\BaseTestCase;
+
+final class DefaultIntegrationConfigurationTest extends BaseTestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+        putenv('DD_ANALYTICS_ENABLED');
+        putenv('DD_DUMMY_ANALYTICS_ENABLED');
+        putenv('DD_DUMMY_ANALYTICS_SAMPLE_RATE');
+    }
+
+    public function testTraceAnalyticsOByDefault()
+    {
+        $conf = new DefaultIntegrationConfiguration('dummy');
+        $this->assertFalse($conf->isTraceAnalyticsEnabled());
+    }
+
+    public function testTraceAnalyticsIfIntegrationEnabled()
+    {
+        putenv('DD_DUMMY_ANALYTICS_ENABLED=true');
+        $conf = new DefaultIntegrationConfiguration('dummy');
+        $this->assertTrue($conf->isTraceAnalyticsEnabled());
+    }
+
+    public function testTraceAnalyticsGlobalEnabledAndNotRequiresExplicit()
+    {
+        putenv('DD_ANALYTICS_ENABLED=true');
+        $conf = new DefaultIntegrationConfiguration('dummy', false);
+        $this->assertTrue($conf->isTraceAnalyticsEnabled());
+    }
+
+    public function testTraceAnalyticsGlobalEnabledAndRequiresExplicit()
+    {
+        putenv('DD_ANALYTICS_ENABLED=true');
+        $conf = new DefaultIntegrationConfiguration('dummy');
+        $this->assertFalse($conf->isTraceAnalyticsEnabled());
+    }
+
+    public function testTraceAnalyticsIntegrationEnabledAndRequiresExplicit()
+    {
+        putenv('DD_DUMMY_ANALYTICS_ENABLED=true');
+        $conf = new DefaultIntegrationConfiguration('dummy');
+        $this->assertTrue($conf->isTraceAnalyticsEnabled());
+    }
+
+    public function testTraceAnalyticsGlobalDisabledIntegrationEnabledRequiresExplicit()
+    {
+        putenv('DD_ANALYTICS_ENABLED=false');
+        putenv('DD_DUMMY_ANALYTICS_ENABLED=true');
+        $conf = new DefaultIntegrationConfiguration('dummy');
+        $this->assertTrue($conf->isTraceAnalyticsEnabled());
+    }
+
+    public function testTraceAnalyticsSampleRateDefaultTo1()
+    {
+        $conf = new DefaultIntegrationConfiguration('dummy');
+        $this->assertEquals(1.0, $conf->getTraceAnalyticsSampleRate());
+    }
+
+    public function testTraceAnalyticsSampleRateCanBeSet()
+    {
+        putenv('DD_DUMMY_ANALYTICS_SAMPLE_RATE=0.3');
+        $conf = new DefaultIntegrationConfiguration('dummy');
+        $this->assertEquals(0.3, $conf->getTraceAnalyticsSampleRate());
+    }
+}
