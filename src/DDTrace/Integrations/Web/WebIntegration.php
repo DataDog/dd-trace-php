72779fbcf257ac35ba371c363e8db1859eba1693
Luca Abbati <luca.abbati@gmail.com>
2019-03-21 11:53:07 +0100

    [web] Use web integration as singleton

--- src/DDTrace/Integrations/Web/WebIntegration.php
+++ src/DDTrace/Integrations/Web/WebIntegration.php
@@ -9,6 +9,22 @@ class WebIntegration extends Integration
     const NAME = 'web';
 
     /**
+     * @var self
+     */
+    private static $instance;
+
+    /**
+     * @return self
+     */
+    public static function getInstance()
+    {
+        if (null === self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    /**
      * @return string The integration name.
      */
     public function getName()
