e1800a751573c2e1a8e1826ebb362115b63cfc16
sammy.powers <sammyk@datadoghq.com>
2019-03-25 14:34:32 -0700

    Tie MPack into ddtrace with example from MessagePack website

--- src/ext/serializer.c
+++ src/ext/serializer.c
@@ -1,7 +1,25 @@
 #include <php.h>
 #include <stdio.h>
+#include "mpack/mpack.h"
 
 int ddtrace_serialize_trace(zval *trace, zval *retval) {
-    ZVAL_STRING(retval, "IWasSerialized");
+    // encode to memory buffer
+    char* data;
+    size_t size;
+    mpack_writer_t writer;
+    mpack_writer_init_growable(&writer, &data, &size);
+    // write the example on the msgpack homepage
+    mpack_start_map(&writer, 2);
+    mpack_write_cstr(&writer, "compact");
+    mpack_write_bool(&writer, true);
+    mpack_write_cstr(&writer, "schema");
+    mpack_write_uint(&writer, 0);
+    mpack_finish_map(&writer);
+    // finish writing
+    if (mpack_writer_destroy(&writer) != mpack_ok) {
+        return 0;
+    }
+    ZVAL_STRING(retval, data);
+    //free(data);
     return 1;
 }
