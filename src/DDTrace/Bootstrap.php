3d0e5e51b895176109f1788dbaa7ccee75417f32
sammy.powers <sammyk@datadoghq.com>
2019-03-28 11:27:52 -0700

    Make MessagePack the default for auto-instrumentation

--- src/DDTrace/Bootstrap.php
+++ src/DDTrace/Bootstrap.php
@@ -2,7 +2,7 @@
 
 namespace DDTrace;
 
-use DDTrace\Encoders\Json;
+use DDTrace\Encoders\MessagePack;
 use DDTrace\Http\Request;
 use DDTrace\Integrations\IntegrationsLoader;
 use DDTrace\Integrations\Web\WebIntegration;
@@ -57,7 +57,7 @@ final class Bootstrap
     public static function resetTracer()
     {
         GlobalTracer::set(
-            new Tracer(new Http(new Json()))
+            new Tracer(new Http(new MessagePack()))
         );
     }
 
