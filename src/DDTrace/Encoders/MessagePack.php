dac250825833326dccc7fef5eda1edd63bd1a96a
sammy.powers <sammyk@datadoghq.com>
2019-03-27 17:05:19 -0700

    Add MessagePack encoder

---
+++ src/DDTrace/Encoders/MessagePack.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace DDTrace\Encoders;
+
+use DDTrace\Contracts\Tracer;
+use DDTrace\Encoder;
+use DDTrace\Log\LoggingTrait;
+
+final class MessagePack implements Encoder
+{
+    use LoggingTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function encodeTraces(Tracer $tracer)
+    {
+        $messagePack = dd_trace_serialize_trace($tracer);
+        if (false === $messagePack) {
+            self::logDebug('Failed to MessagePack-encode trace');
+            return '';
+        }
+        return $messagePack;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getContentType()
+    {
+        return 'application/msgpack';
+    }
+}
