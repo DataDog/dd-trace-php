56fdcdbdbe01ad9dd0fded2a64ad338366849e49
sammy.powers <sammyk@datadoghq.com>
2019-04-01 16:32:15 -0400

    Build with C99 for local builds

--- local-build.sh
+++ local-build.sh
@@ -12,7 +12,7 @@ function build_version() {
     PHP_API=$2
     SUFFIX=${3:-''}
     rm -rf tmp
-    docker run --rm -w /var/app -v $(pwd):/var/app circleci/php:${PHP_VERSION}${SUFFIX} make all CFLAGS="-O2 -Wall -Wextra -Wextra"
+    docker run --rm -w /var/app -v $(pwd):/var/app circleci/php:${PHP_VERSION}${SUFFIX} make all CFLAGS="-O2 -std=c99 -Wall -Wextra -Wextra"
     docker run --rm -w /var/app -v $(pwd):/var/app circleci/php:${PHP_VERSION}${SUFFIX} cp tmp/build_extension/.libs/ddtrace.so extensions/ddtrace-${PHP_API}${SUFFIX}.so
 }
 
@@ -20,7 +20,7 @@ function build_version_54() {
     PHP_VERSION=5.4
     PHP_API=20100412
     rm -rf tmp
-    docker run --rm -w /var/app -v $(pwd):/var/app datadog/docker-library:ddtrace_centos_6_php_5_4 make all CFLAGS="-O2 -Wall -Wextra -Wextra" ECHO_ARG="-e"
+    docker run --rm -w /var/app -v $(pwd):/var/app datadog/docker-library:ddtrace_centos_6_php_5_4 make all CFLAGS="-O2 -std=c99 -Wall -Wextra -Wextra" ECHO_ARG="-e"
     docker run --rm -w /var/app -v $(pwd):/var/app datadog/docker-library:ddtrace_centos_6_php_5_4 cp tmp/build_extension/.libs/ddtrace.so extensions/ddtrace-${PHP_API}.so
 }
 
