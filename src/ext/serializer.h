2d2b8f31f8b7cbd3783f42e21ef7d1b7940e636b
sammy.powers <sammyk@datadoghq.com>
2019-04-02 10:47:13 -0400

    Handle invalid serialize data with an error

--- src/ext/serializer.h
+++ src/ext/serializer.h
@@ -1,6 +1,6 @@
 #ifndef DD_SERIALIZER_H
 #define DD_SERIALIZER_H
 
-int ddtrace_serialize_simple_array(zval *trace, zval *retval);
+int ddtrace_serialize_simple_array(zval *trace, zval *retval TSRMLS_DC);
 
 #endif  // DD_SERIALIZER_H
