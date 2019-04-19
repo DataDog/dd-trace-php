9f0f25088da878212e2e253c2cc45d8c3b4f0885
sammy.powers <sammyk@datadoghq.com>
2019-04-19 08:36:45 -0700

    Add safer way to jump out of include files in PHP 5.6

--- src/ext/dispatch_compat_php5.c
+++ src/ext/dispatch_compat_php5.c
@@ -176,14 +176,17 @@ void ddtrace_forward_call(zend_execute_data *execute_data, zval *return_value TS
     zend_fcall_info fci;
     zend_fcall_info_cache fcc;
 
-    if (!DDTRACE_G(original_execute_data)) {
+    if (!DDTRACE_G(original_execute_data) || !EX(prev_execute_data)) {
         zend_throw_exception_ex(spl_ce_LogicException, 0 TSRMLS_CC,
                                 "Cannot use dd_trace_forward_call() outside of a tracing closure");
         return;
     }
 
-    // @TODO Refactor this to allow unlimited include contexts (and for PHP 7)
-    zend_execute_data *prev_ex = !EX(prev_execute_data) ? EX(prev_execute_data)->prev_execute_data : EX(prev_execute_data);
+    // Jump out of any include files
+    zend_execute_data *prev_ex = EX(prev_execute_data);
+    while (prev_ex->opline && prev_ex->opline->opcode == ZEND_INCLUDE_OR_EVAL) {
+        prev_ex = prev_ex->prev_execute_data;
+    }
     const char *callback_name = !prev_ex ? NULL : prev_ex->function_state.function->common.function_name;
 
     if (!callback_name
