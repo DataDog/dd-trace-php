44bf7024c5d0417f0c9904d01dc1778e2abf3b6b
sammy.powers <sammyk@datadoghq.com>
2019-04-19 07:54:13 -0700

    Forward original function args in PHP 5.6

--- src/ext/dispatch_compat_php5.c
+++ src/ext/dispatch_compat_php5.c
@@ -144,6 +144,33 @@ zend_bool ddtrace_dispatch_store(HashTable *lookup, ddtrace_dispatch_t *dispatch
                             sizeof(ddtrace_dispatch_t *), NULL) == SUCCESS;
 }
 
+// A modified version of func_get_args()
+// https://github.com/php/php-src/blob/PHP-5.6/Zend/zend_builtin_functions.c#L445
+static int get_args(zval *args, zend_execute_data *ex) {
+    if (!ex || !ex->function_state.arguments) {
+        return 0;
+    }
+    void **p = ex->function_state.arguments;
+    int param_count = (int)(zend_uintptr_t) *p;
+
+    array_init_size(args, param_count);
+    for (int i = 0; i < param_count; i++) {
+        zval *element, *arg;
+
+        arg = *((zval **) (p-(param_count-i)));
+        if (!Z_ISREF_P(arg)) {
+            element = arg;
+            Z_ADDREF_P(element);
+        } else {
+            ALLOC_ZVAL(element);
+            INIT_PZVAL_COPY(element, arg);
+            zval_copy_ctor(element);
+        }
+        zend_hash_next_index_insert(args->value.ht, &element, sizeof(zval *), NULL);
+    }
+    return 1;
+}
+
 void ddtrace_forward_call(zend_execute_data *execute_data, zval *return_value TSRMLS_DC) {
     zval *retval_ptr = NULL;
     zend_fcall_info fci;
@@ -184,10 +211,20 @@ void ddtrace_forward_call(zend_execute_data *execute_data, zval *return_value TS
     fci.no_separation = 1;
     fci.symbol_table = NULL;
 
+    zval args;
+    if (0 == get_args(&args, prev_ex)) {
+        zval_dtor(&args);
+        zend_throw_exception_ex(spl_ce_RuntimeException, 0 TSRMLS_CC,
+                                "Cannot forward original function arguments");
+        return;
+    }
+    zend_fcall_info_args(&fci, &args TSRMLS_CC);
+
     if (zend_call_function(&fci, &fcc TSRMLS_CC) == SUCCESS && fci.retval_ptr_ptr && *fci.retval_ptr_ptr) {
         COPY_PZVAL_TO_ZVAL(*return_value, *fci.retval_ptr_ptr);
     }
 
     zend_fcall_info_args_clear(&fci, 1);
+    zval_dtor(&args);
 }
 #endif  // PHP 5
