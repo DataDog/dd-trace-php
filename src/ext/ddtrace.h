79f3afa9905ad387e466dfa072ad3aceef2a027b
sammy.powers <sammyk@datadoghq.com>
2019-04-26 07:07:05 -0700

    Refactor original execution context data structures into a single struct

--- src/ext/ddtrace.h
+++ src/ext/ddtrace.h
@@ -3,6 +3,19 @@
 #include "version.h"
 extern zend_module_entry ddtrace_module_entry;
 
+typedef struct _ddtrace_original_context {
+    zend_function *fbc;
+    zend_function *calling_fbc;
+    zend_class_entry *calling_ce;
+    zend_execute_data *execute_data;
+#if PHP_VERSION_ID < 70000
+    zval *function_name;
+    zval *this;
+#else
+    zend_object *this;
+#endif
+} ddtrace_original_context;
+
 ZEND_BEGIN_MODULE_GLOBALS(ddtrace)
 zend_bool disable;
 zend_bool disable_in_current_request;
@@ -13,18 +26,7 @@ zend_bool strict_mode;
 HashTable class_lookup;
 HashTable function_lookup;
 zend_bool log_backtrace;
-zend_function *current_fbc;
-#if PHP_VERSION_ID < 70000
-zval *original_this;
-#else
-zend_object *original_this;
-#endif
-zend_class_entry *calling_ce;
-zend_function *calling_fbc;
-zend_execute_data *original_execute_data;
-#if PHP_VERSION_ID < 70000
-zval *original_function_name;
-#endif
+ddtrace_original_context original_context;
 
 user_opcode_handler_t ddtrace_old_fcall_handler;
 user_opcode_handler_t ddtrace_old_icall_handler;
