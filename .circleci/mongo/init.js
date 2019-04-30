a103828126e0746098ec8df0607a5e94b487bb77
sammy.powers <sammyk@datadoghq.com>
2019-04-30 12:40:59 -0700

    Add Mongo init script

---
+++ .circleci/mongo/init.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user: "test",
+        pwd: "test",
+        roles: [
+            {
+                role: "readWrite",
+                db: "test"
+            }
+        ]
+    }
+);
