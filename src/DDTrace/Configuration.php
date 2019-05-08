601513f8971eeaf04a62854ae4774322ae79b610
Luca Abbati <luca.abbati@gmail.com>
2019-05-08 17:10:01 +0200

    Emit a debug message to notify of deprecated service name config params

--- src/DDTrace/Configuration.php
+++ src/DDTrace/Configuration.php
@@ -3,12 +3,15 @@
 namespace DDTrace;
 
 use DDTrace\Configuration\AbstractConfiguration;
+use DDTrace\Log\LoggingTrait;
 
 /**
  * DDTrace global configuration object.
  */
 class Configuration extends AbstractConfiguration
 {
+    use LoggingTrait;
+
     /**
      * Whether or not tracing is enabled.
      *
@@ -134,11 +137,13 @@ class Configuration extends AbstractConfiguration
         // This is deprecated and will be removed in a future release
         $appName = $this->stringValue('trace.app.name');
         if ($appName) {
+            self::logDebug('Env variable \'DD_TRACE_APP_NAME\' is deprecated and will be removed soon. Use \'DD_SERVICE_NAME\' instead');
             return $appName;
         }
 
         $appName = getenv('ddtrace_app_name');
         if (false !== $appName) {
+            self::logDebug('Env variable \'ddtrace_app_name\' is deprecated and will be removed soon. Use \'DD_SERVICE_NAME\' instead');
             return trim($appName);
         }
         return $default;
