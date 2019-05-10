76374fb013c3afaf4fe2ed0c56f2c6ae98221dbc
sammy.powers <sammyk@datadoghq.com>
2019-05-10 08:06:07 -0700

    Fix timezone warning in PHP 5

--- bridge/dd_wrap_autoloader.php
+++ bridge/dd_wrap_autoloader.php
@@ -2,6 +2,10 @@
 
 namespace DDTrace\Bridge;
 
+if (PHP_VERSION_ID < 70000) {
+    date_default_timezone_set(date_default_timezone_get());
+}
+
 require_once __DIR__ . '/functions.php';
 
 if (!dd_tracing_enabled()) {
