b41a0bc1a8b0c7e89b6d0f19e64a0b5589692aad
Pawel Chojnacki <pawel.chcki@gmail.com>
2019-05-16 17:56:58 +0200

    Add hostname to root tag when enabled (on flush)

--- src/DDTrace/Tag.php
+++ src/DDTrace/Tag.php
@@ -29,6 +29,7 @@ class Tag
     const TARGET_PORT = 'out.port';
     const BYTES_OUT = 'net.out.bytes';
     const ANALYTICS_KEY = '_dd1.sr.eausr';
+    const HOSTNAME = '_dd.hostname';
 
     // Elasticsearch
     const ELASTICSEARCH_BODY = 'elasticsearch.body';
