62bb8bc1a947a78bc4fc03007497d4d56455ddcd
sammy.powers <sammyk@datadoghq.com>
2019-05-17 09:53:34 -0700

    Add required endpoints for testing

--- tests/Frameworks/Slim/Version_3_12/src/routes.php
+++ tests/Frameworks/Slim/Version_3_12/src/routes.php
@@ -7,6 +7,16 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
+    $app->get('/simple', function (Request $request, Response $response, $args) {
+        return "Simple :)\n";
+    })->setName('simple-route');
+
+    $app->get('/simple_view', '\\App\\SimpleViewController:index');
+
+    $app->get('/error', function (Request $request, Response $response, $args) {
+        throw new \Exception('Foo error');
+    });
+
     $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/' route");
