362a48864fcf1245b90255506a44505cf2d5d112
sammy.powers <sammyk@datadoghq.com>
2019-05-22 20:22:35 -0400

    Add dd_trace_generate_id() to generate a 63-bit int as a string

---
+++ src/ext/random.c
@@ -0,0 +1,17 @@
+#include <php.h>
+#include <ext/standard/php_rand.h>
+
+#include "env_config.h"
+#include "random.h"
+#include "third-party/mt19937-64.h"
+
+void dd_trace_seed_prng() {
+    unsigned long long seed = (unsigned long long)ddtrace_get_int_config(DD_TRACE_PRNG_SEED, GENERATE_SEED());
+    init_genrand64((unsigned long long) seed);
+}
+
+zend_string* dd_trace_generate_id() {
+    // We shift one bit to get 63-bit
+    long long id = (long long)(genrand64_int64() >> 1);
+    return zend_strpprintf(0, "%llu", id);
+}
---
+++ src/ext/random.h
@@ -0,0 +1,10 @@
+#ifndef DD_RANDOM_H
+#define DD_RANDOM_H
+#include <Zend/zend_types.h>
+
+#define DD_TRACE_PRNG_SEED "DD_TRACE_PRNG_SEED"
+
+void dd_trace_seed_prng();
+zend_string *dd_trace_generate_id();
+
+#endif  // DD_RANDOM_H
