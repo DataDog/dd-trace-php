63deb91f458da256c510608eff719dfb73fd339c
sammy.powers <sammyk@datadoghq.com>
2019-07-26 09:26:24 -0700

    Make dd-doctor.php PHP 5.3 compat

--- src/dd-doctor.php
+++ src/dd-doctor.php
@@ -1,11 +1,11 @@
 <?php
+
 function result($check)
 {
     if ($check) {
         return "  \t[OK]" . PHP_EOL;
-    } else {
-        return "  \t[FAIL]" . PHP_EOL;
     }
+    return "  \t[FAIL]" . PHP_EOL;
 }
 
 function check_agent_connectivity()
@@ -23,7 +23,7 @@ function check_agent_connectivity()
     echo "- configured agent host\t\t\t\t" . $host . PHP_EOL;
     echo "- agent can receive traces\t\t";
 
-    $verbose = fopen('php://temp', 'w+');
+    $verbose = fopen('php://temp', 'w+b');
     $ch = curl_init("http://" . $host . ":8126/v0.3/traces");
     curl_setopt($ch, CURLOPT_TIMEOUT, 5);
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
@@ -33,7 +33,7 @@ function check_agent_connectivity()
     curl_setopt($ch, CURLOPT_VERBOSE, true);
     curl_setopt($ch, CURLOPT_STDERR, $verbose);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-    $data = curl_exec($ch);
+    curl_exec($ch);
     $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     $success = $httpcode >= 200 && $httpcode < 300;
     echo result($success);
@@ -51,9 +51,10 @@ header('Content-Type: text/plain');
 echo 'DataDog trace extension verification' . PHP_EOL . PHP_EOL;
 echo 'Checks:' . PHP_EOL;
 echo "- ddtrace extension installed\t\t" . result(extension_loaded('ddtrace') || extension_loaded('dd_trace'));
-echo "- ddtrace extension version \t\t\t" . phpversion('ddtrace') . PHP_EOL;
+echo "- ddtrace extension version \t\t\t" . (phpversion('ddtrace') ?: '[N/A]') . PHP_EOL;
 echo "- dd_trace function available\t\t" . result(function_exists('dd_trace'));
-echo "- request_init_hook set\t\t\t" . result(!empty(ini_get('ddtrace.request_init_hook')));
+$initHook = ini_get('ddtrace.request_init_hook');
+echo "- request_init_hook set\t\t\t" . result(!empty($initHook));
 echo "- request_init_hook reachable\t\t" . result(file_exists(ini_get('ddtrace.request_init_hook')));
 echo "- DDTrace\Tracer class exists\t\t" . result(class_exists('\DDTrace\Tracer'));
 
@@ -65,6 +66,5 @@ if (file_exists(ini_get('ddtrace.request_init_hook'))) {
     echo '- requiring dd_init' . PHP_EOL;
     require dirname(ini_get('ddtrace.request_init_hook')) . '/dd_init.php';
 }
-echo PHP_EOL;
 
-?>
+echo PHP_EOL;
