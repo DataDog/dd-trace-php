a4a2327cd1fc955cd3e9f7c09b09050e1d9af702
Pawel Chojnacki <pawel.chcki@gmail.com>
2019-08-12 13:37:15 +0200

    Run symfony Baseline test in parallel to ddtrace enabled one

--- dockerfiles/frameworks/symfony/entrypoint.sh
+++ dockerfiles/frameworks/symfony/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -xe
-curl -o /tmp/ddtrace.deb http://nginx_file_server/ddtrace.deb || true
-
-if [ -e /tmp/ddtrace.deb ]; then
+if [[ -z "$NO_DDTRACE" ]]; then
+    curl -o /tmp/ddtrace.deb http://nginx_file_server/ddtrace.deb
     dpkg -i /tmp/ddtrace.deb
-fi
 
-export DD_TRACE_CLI_ENABLED=true
+    export DD_TRACE_CLI_ENABLED=true
+fi
 exec "$@"
