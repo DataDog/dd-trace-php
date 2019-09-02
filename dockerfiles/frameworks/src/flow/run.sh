c93e383bba054daa79b45f56e40a4154e7eb0719
Pawel Chojnacki <pawel.chcki@gmail.com>
2019-09-02 11:24:22 +0200

    Add flow tests to CI

--- dockerfiles/frameworks/src/flow/run.sh
+++ dockerfiles/frameworks/src/flow/run.sh
@@ -3,5 +3,5 @@ switch_php 7.1
 
 mysql -h mysql -e 'CREATE DATABASE IF NOT EXISTS flow_functional_testing;'
 
-bin/phpunit", "--colors", "-c", "Build/BuildEssentials/PhpUnit/UnitTests.xml
+bin/phpunit --colors -c Build/BuildEssentials/PhpUnit/UnitTests.xml
 bin/phpunit --colors --stop-on-failure -c Build/BuildEssentials/PhpUnit/FunctionalTests.xml --testsuite "Framework tests"
