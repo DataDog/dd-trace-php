f33ff316d5d8247e31aebab1fa54024e4785998a
Pawel Chojnacki <pawel.chcki@gmail.com>
2019-09-02 11:16:57 +0200

    Allow switching php versions for tests

--- dockerfiles/frameworks/src/flow/run.sh
+++ dockerfiles/frameworks/src/flow/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
+switch_php 7.1
+
 mysql -h mysql -e 'CREATE DATABASE IF NOT EXISTS flow_functional_testing;'
 
-# FLOW_CONTEXT=Testing/Behat ./flow doctrine:create
+bin/phpunit", "--colors", "-c", "Build/BuildEssentials/PhpUnit/UnitTests.xml
 bin/phpunit --colors --stop-on-failure -c Build/BuildEssentials/PhpUnit/FunctionalTests.xml --testsuite "Framework tests"
