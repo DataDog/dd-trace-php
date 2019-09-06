154143e0e42e90eabbbd99d444fa7ec097cc7f54
sammy.powers <sammyk@datadoghq.com>
2019-09-06 09:43:22 -0700

    Merge PDO sandbox test into original test

--- tests/Integrations/PDO/PDOSandboxedTest.php
+++ tests/Integrations/PDO/PDOSandboxedTest.php
@@ -4,395 +4,66 @@ namespace DDTrace\Tests\Integrations\PDO;
 
 use DDTrace\Integrations\IntegrationsLoader;
 use DDTrace\Tests\Common\IntegrationTestCase;
-use DDTrace\Tests\Common\SpanAssertion;
 
-final class PDOSandboxedTest extends IntegrationTestCase
+final class PDOSandboxedTest extends PDOTest
 {
-    const MYSQL_DATABASE = 'test';
-    const MYSQL_USER = 'test';
-    const MYSQL_PASSWORD = 'test';
-    const MYSQL_HOST = 'mysql_integration';
+    const IS_SANDBOX = true;
+
+    // phpcs:disable
+    const ERROR_CONSTRUCT = 'SQLSTATE[HY000] [1045] Access denied for user \'wrong_user\'@\'%s\' (using password: YES)';
+    const ERROR_EXEC = 'SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near \'WRONG QUERY)\' at line 1';
+    const ERROR_QUERY = 'SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near \'WRONG QUERY\' at line 1';
+    const ERROR_STATEMENT = 'SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near \'WRONG QUERY\' at line 1';
+    // phpcs:enable
 
     public static function setUpBeforeClass()
     {
-        parent::setUpBeforeClass();
+        IntegrationTestCase::setUpBeforeClass();
         putenv('DD_TRACE_SANDBOX_ENABLED=true');
         putenv('DD_PDO_ANALYTICS_ENABLED=true');
         IntegrationsLoader::reload();
     }
 
+    public static function tearDownAfterClass()
+    {
+        parent::tearDownAfterClass();
+        putenv('DD_PDO_ANALYTICS_ENABLED');
+        putenv('DD_TRACE_SANDBOX_ENABLED');
+    }
+
     protected function setUp()
     {
-        parent::setUp();
         if (PHP_VERSION_ID < 50600) {
             $this->markTestSkipped('Sandbox API not available on < PHP 5.6');
             return;
         }
-        $this->setUpDatabase();
+        parent::setUp();
     }
 
-    protected function tearDown()
-    {
-        $this->clearDatabase();
-        parent::tearDown();
-    }
-
-    public function testPDOConstructOk()
-    {
-        $traces = $this->isolateTracer(function () {
-                $this->pdoInstance();
-        });
-        $this->assertSpans($traces, [
-            SpanAssertion::build('PDO.__construct', 'PDO', 'sql', 'PDO.__construct')
-                ->withExactTags($this->baseTags()),
-        ]);
-    }
-
-    public function testPDOConstructError()
-    {
-        $traces = $this->isolateTracer(function () {
-            try {
-                new \PDO($this->mysqlDns(), 'wrong_user', 'wrong_password');
-            } catch (\PDOException $ex) {
-            }
-        });
-        $this->assertSpans($traces, [
-            SpanAssertion::build('PDO.__construct', 'PDO', 'sql', 'PDO.__construct')
-                ->withExactTags(array_merge($this->baseTags(), [
-                    'db.user' => 'wrong_user',
-                ]))
-                // phpcs:disable
-                ->setException('PDOException', 'SQLSTATE[HY000] [1045] Access denied for user \'wrong_user\'@\'%s\' (using password: YES)'),
-                // phpcs:enable
-        ]);
-    }
-
-    public function testPDOExecOk()
-    {
-        $query = "INSERT INTO tests (id, name) VALUES (100, 'Sam')";
-        $traces = $this->isolateTracer(function () use ($query) {
-            $pdo = $this->pdoInstance();
-            $pdo->beginTransaction();
-            $pdo->exec($query);
-            $pdo->commit();
-            $pdo = null;
-        });
-        $this->assertSpans($traces, [
-            SpanAssertion::exists('PDO.commit'),
-            SpanAssertion::build('PDO.exec', 'PDO', 'sql', $query)
-                ->setSandboxedTraceAnalyticsCandidate()
-                ->withExactTags(array_merge($this->baseTags(), [
-                    'db.rowcount' => '1',
-                ])),
-            SpanAssertion::exists('PDO.__construct'),
-        ]);
-    }
-
-    public function testPDOExecError()
-    {
-        $query = "WRONG QUERY)";
-        $traces = $this->isolateTracer(function () use ($query) {
-            try {
-                $pdo = $this->pdoInstance();
-                $pdo->beginTransaction();
-                $pdo->exec($query);
-                $pdo->commit();
-                $pdo = null;
-            } catch (\PDOException $ex) {
-            }
-        });
-        $this->assertSpans($traces, [
-            SpanAssertion::exists('PDO.commit'),
-            SpanAssertion::build('PDO.exec', 'PDO', 'sql', $query)
-                ->setTraceAnalyticsCandidate()
-                ->setError('PDO error', 'SQL error: 42000. Driver error: 1064')
-                ->withExactTags($this->baseTags()),
-            SpanAssertion::exists('PDO.__construct'),
-        ]);
-    }
-
-    public function testPDOExecException()
-    {
-        $query = "WRONG QUERY)";
-        $traces = $this->isolateTracer(function () use ($query) {
-            try {
-                $pdo = $this->pdoInstance();
-                $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-                $pdo->beginTransaction();
-                $pdo->exec($query);
-                $pdo->commit();
-                $pdo = null;
-                $this->fail('Should throw an exception');
-            } catch (\PDOException $ex) {
-            }
-        });
-        $this->assertSpans($traces, [
-            SpanAssertion::build('PDO.exec', 'PDO', 'sql', $query)
-                ->setTraceAnalyticsCandidate()
-                // phpcs:disable
-                ->setException('PDOException', 'SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near \'WRONG QUERY)\' at line 1')
-                // phpcs:enable
-                ->withExactTags($this->baseTags()),
-            SpanAssertion::exists('PDO.__construct'),
-        ]);
-    }
-
-    public function testPDOQuery()
-    {
-        $query = "SELECT * FROM tests WHERE id=1";
-        $traces = $this->isolateTracer(function () use ($query) {
-            $pdo = $this->pdoInstance();
-            $pdo->query($query);
-            $pdo = null;
-        });
-        $this->assertSpans($traces, [
-            SpanAssertion::build('PDO.query', 'PDO', 'sql', $query)
-                ->setTraceAnalyticsCandidate()
-                ->withExactTags(array_merge($this->baseTags(), [
-                    'db.rowcount' => '1',
-                ])),
-            SpanAssertion::exists('PDO.__construct'),
-        ]);
-    }
-
-    public function testPDOQueryError()
-    {
-        $query = "WRONG QUERY";
-        $traces = $this->isolateTracer(function () use ($query) {
-            try {
-                $pdo = $this->pdoInstance();
-                $pdo->query($query);
-                $pdo = null;
-            } catch (\PDOException $ex) {
-            }
-        });
-        $this->assertSpans($traces, [
-            SpanAssertion::build('PDO.query', 'PDO', 'sql', $query)
-                ->setTraceAnalyticsCandidate()
-                ->setError('PDO error', 'SQL error: 42000. Driver error: 1064')
-                ->withExactTags($this->baseTags()),
-            SpanAssertion::exists('PDO.__construct'),
-        ]);
-    }
-
-    public function testPDOQueryException()
-    {
-        $query = "WRONG QUERY";
-        $traces = $this->isolateTracer(function () use ($query) {
-            try {
-                $pdo = $this->pdoInstance();
-                $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-                $pdo->query($query);
-                $pdo = null;
-            } catch (\PDOException $ex) {
-            }
-        });
-        $this->assertSpans($traces, [
-            SpanAssertion::build('PDO.query', 'PDO', 'sql', $query)
-                ->setTraceAnalyticsCandidate()
-                // phpcs:disable
-                ->setException('PDOException', 'SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near \'WRONG QUERY\' at line 1')
-                // phpcs:enable
-                ->withExactTags($this->baseTags()),
-            SpanAssertion::exists('PDO.__construct'),
-        ]);
-    }
-
-    public function testPDOCommit()
-    {
-        $query = "INSERT INTO tests (id, name) VALUES (100, 'Sam')";
-        $traces = $this->isolateTracer(function () use ($query) {
-            $pdo = $this->pdoInstance();
-            $pdo->beginTransaction();
-            $pdo->exec($query);
-            $pdo->commit();
-            $pdo = null;
-        });
-        $this->assertSpans($traces, [
-            SpanAssertion::build('PDO.commit', 'PDO', 'sql', 'PDO.commit')
-                ->withExactTags(array_merge($this->baseTags(), [])),
-            SpanAssertion::exists('PDO.exec'),
-            SpanAssertion::exists('PDO.__construct'),
-        ]);
-    }
-
-    public function testPDOStatementOk()
-    {
-        $query = "SELECT * FROM tests WHERE id = ?";
-        $traces = $this->isolateTracer(function () use ($query) {
-            $pdo = $this->pdoInstance();
-            $stmt = $pdo->prepare($query);
-            $stmt->execute([1]);
-            $results = $stmt->fetchAll();
-            $this->assertEquals('Tom', $results[0]['name']);
-            $stmt->closeCursor();
-            $stmt = null;
-            $pdo = null;
-        });
-        $this->assertSpans($traces, [
-            SpanAssertion::build(
-                'PDOStatement.execute',
-                'PDO',
-                'sql',
-                "SELECT * FROM tests WHERE id = ?"
-            )
-                ->setTraceAnalyticsCandidate()
-                ->withExactTags(array_merge($this->baseTags(), [
-                    'db.rowcount' => '1',
-                ])),
-            SpanAssertion::build(
-                'PDO.prepare',
-                'PDO',
-                'sql',
-                "SELECT * FROM tests WHERE id = ?"
-            )->withExactTags(array_merge($this->baseTags(), [])),
-            SpanAssertion::exists('PDO.__construct'),
-        ]);
-    }
-
-    public function testPDOStatementError()
-    {
-        $query = "WRONG QUERY";
-        $traces = $this->isolateTracer(function () use ($query) {
-            try {
-                $pdo = $this->pdoInstance();
-                $stmt = $pdo->prepare($query);
-                $stmt->execute([1]);
-                $stmt->fetchAll();
-                $stmt->closeCursor();
-                $stmt = null;
-                $pdo = null;
-            } catch (\PDOException $ex) {
-            }
-        });
-        $this->assertSpans($traces, [
-            SpanAssertion::build('PDOStatement.execute', 'PDO', 'sql', "WRONG QUERY")
-                ->setTraceAnalyticsCandidate()
-                ->setError('PDOStatement error', 'SQL error: 42000. Driver error: 1064')
-                    ->withExactTags($this->baseTags()),
-            SpanAssertion::build('PDO.prepare', 'PDO', 'sql', "WRONG QUERY")
-                ->withExactTags($this->baseTags()),
-            SpanAssertion::exists('PDO.__construct'),
-        ]);
-    }
-
-    public function testPDOStatementException()
-    {
-        $query = "WRONG QUERY";
-        $traces = $this->isolateTracer(function () use ($query) {
-            try {
-                $pdo = $this->pdoInstance();
-                $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-                $stmt = $pdo->prepare($query);
-                $stmt->execute([1]);
-                $stmt->fetchAll();
-                $stmt->closeCursor();
-                $stmt = null;
-                $pdo = null;
-            } catch (\PDOException $ex) {
-            }
-        });
-        $this->assertSpans($traces, [
-            SpanAssertion::build('PDOStatement.execute', 'PDO', 'sql', $query)
-                ->setTraceAnalyticsCandidate()
-                // phpcs:disable
-                ->setException('PDOException', 'SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near \'WRONG QUERY\' at line 1')
-                // phpcs:enable
-                ->withExactTags($this->baseTags()),
-            SpanAssertion::build('PDO.prepare', 'PDO', 'sql', $query)
-                ->withExactTags($this->baseTags()),
-            SpanAssertion::exists('PDO.__construct'),
-        ]);
-    }
-
-    // Add limited tracer to sandboxed closures
-    /*
+    // Remove this fake test after limited tracer added to sandbox API
     public function testLimitedTracerPDO()
     {
-        $query = "SELECT * FROM tests WHERE id = ?";
-        $traces = $this->isolateLimitedTracer(function () use ($query) {
-            $pdo = $this->pdoInstance();
-            $stmt = $pdo->prepare($query);
-            $stmt->execute([1]);
-            $results = $stmt->fetchAll();
-            $this->assertEquals('Tom', $results[0]['name']);
-            $stmt->closeCursor();
-            $stmt = null;
-            $pdo = null;
-        });
-
-        $this->assertEmpty($traces);
+        $this->assertTrue(true);
     }
-    */
 
     // @see https://github.com/DataDog/dd-trace-php/issues/510
     public function testPDOStatementsAreReleased()
     {
         $query = "SELECT * FROM tests WHERE id = ?";
-        $traces = $this->isolateTracer(function () use ($query) {
-            $pdo = new \PDO(
-                $this->mysqlDns(),
-                self::MYSQL_USER,
-                self::MYSQL_PASSWORD,
-                [\PDO::ATTR_EMULATE_PREPARES => false]
-            );
-            $stmt = $pdo->prepare($query);
-            $stmt->execute([1]);
-            unset($stmt);
+        $pdo = new \PDO(
+            $this->mysqlDns(),
+            self::MYSQL_USER,
+            self::MYSQL_PASSWORD,
+            [\PDO::ATTR_EMULATE_PREPARES => false]
+        );
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([1]);
+        unset($stmt);
 
-            $pdo->prepare($query)->execute([2]);
+        $pdo->prepare($query)->execute([2]);
 
-            $closedCount = $pdo->query("SHOW SESSION STATUS LIKE 'Com_stmt_close'")->fetchColumn(1);
+        $closedCount = $pdo->query("SHOW SESSION STATUS LIKE 'Com_stmt_close'")->fetchColumn(1);
 
-            $this->assertEquals(2, $closedCount);
-        });
-    }
-
-    private function pdoInstance()
-    {
-        return new \PDO($this->mysqlDns(), self::MYSQL_USER, self::MYSQL_PASSWORD);
-    }
-
-    private function setUpDatabase()
-    {
-        $this->isolateTracer(function () {
-            $pdo = $this->pdoInstance();
-            $pdo->beginTransaction();
-            $pdo->exec("
-                CREATE TABLE tests (
-                    id integer not null primary key,
-                    name varchar(100)
-                )
-            ");
-            $pdo->exec("INSERT INTO tests (id, name) VALUES (1, 'Tom')");
-            $pdo->commit();
-            $dbh = null;
-        });
-    }
-
-    private function clearDatabase()
-    {
-        $this->isolateTracer(function () {
-            $pdo = $this->pdoInstance();
-            $pdo->beginTransaction();
-            $pdo->exec("DROP TABLE tests");
-            $pdo->commit();
-            $dbh = null;
-        });
-    }
-
-    public function mysqlDns()
-    {
-        return "mysql:host=" . self::MYSQL_HOST . ";dbname=" . self::MYSQL_DATABASE;
-    }
-
-    private function baseTags()
-    {
-        return [
-            'db.engine' => 'mysql',
-            'out.host' => self::MYSQL_HOST,
-            'db.name' => self::MYSQL_DATABASE,
-            'db.user' => self::MYSQL_USER,
-        ];
+        $this->assertEquals(2, $closedCount);
     }
 }
