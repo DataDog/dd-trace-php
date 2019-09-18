9e373cdb7c0bada2858b9b6d328f9889d9ba0441
Luca Abbati <luca.abbati@datadoghq.com>
2019-09-18 14:51:20 +0200

    Include Eloquent tests in the 5.8 test suite

--- tests/Frameworks/Laravel/Version_5_8/routes/web.php
+++ tests/Frameworks/Laravel/Version_5_8/routes/web.php
@@ -13,8 +13,6 @@
 Route::get('/simple', ['as' => 'simple_route', 'uses' => 'CommonSpecsController@simple']);
 Route::get('/simple_view', 'CommonSpecsController@simple_view');
 Route::get('/error', 'CommonSpecsController@error');
-Route::get('/pipeline_once', 'TestsController@pipelineCalledOnce');
-Route::get('/pipeline_twice', 'TestsController@pipelineCalledTwice');
 Route::get('/eloquent/get', 'EloquentTestController@get');
 Route::get('/eloquent/insert', 'EloquentTestController@insert');
 Route::get('/eloquent/update', 'EloquentTestController@update');
