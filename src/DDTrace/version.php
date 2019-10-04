39c23a71936996e38bae9f8109c593bccba81810
sammy.powers <sammyk@datadoghq.com>
2019-10-04 13:17:56 -0700

    Bump version to 0.32.0

--- src/DDTrace/version.php
+++ src/DDTrace/version.php
@@ -2,4 +2,4 @@
 
 // Must begin with a number for Debian packaging requirements
 // Must use single-quotes for packaging script to work
-return '1.0.0-nightly'; // Update Tracer::VERSION too
+return '0.32.0'; // Update Tracer::VERSION too
