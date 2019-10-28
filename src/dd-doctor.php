4340ea01ad3c9ad033f7aa4b3a5a966c40559e29
Luca Abbati <luca.abbati@datadoghq.com>
2019-10-28 14:27:30 +0100

    Add warn message to dd-doctor.php when open_basedir prevents access to '/proc/self/cgroup'

--- src/dd-doctor.php
+++ src/dd-doctor.php
@@ -1,6 +1,11 @@
 <?php
 
+const OK = '__OK__';
+const FAIL = '__FAIL_';
+const WARN = '__WARN__';
+
 const TEXT_WIDTH = 43;
+
 
 if ('cli' === PHP_SAPI) {
     echo 'WARNING: Script is running from the CLI SAPI.' . PHP_EOL;
@@ -24,24 +29,39 @@ if ('cli' === PHP_SAPI) {
     echo '<pre>' . PHP_EOL;
 }
 
-function result($check)
+function result($value)
 {
     if ('cli' !== PHP_SAPI) {
-        return $check
-            ? '<span style="color:#090">OK</span>'
-            : '<span style="color:#900">FAIL</span>';
+        if ($value === OK) {
+            return '<span style="color:#009900">OK</span>';
+        } else if ($value === FAIL) {
+            return '<span style="color:#990000">FAIL</span>';
+        } else if ($value === WARN) {
+            return '<span style="color:#FFCC00">WARN</span>';
+        } else {
+            return $value;
+        }
     }
-    return $check
-        ? "\e[0;32mOK\e[0m"
-        : "\e[0;31mFAIL\e[0m";
+
+    if ($value === OK) {
+        return "\e[0;32mOK\e[0m";
+    } else if ($value === FAIL) {
+        return "\e[0;31mFAIL\e[0m";
+    } else if ($value === WARN) {
+        return "\e[0;33mWARN\e[0m";
+    } else {
+        return $value;
+    }
 }
 
 function render($message, $value)
 {
-    if (is_bool($value)) {
-        $value = result($value);
-    }
-    printf('- %-42s [%s]%s', $message, $value, PHP_EOL);
+    printf('- %-42s [%s]%s', $message, result($value), PHP_EOL);
+}
+
+function renderSuccessOrFailure($message, $value)
+{
+    render($message, $value ? OK : FAIL);
 }
 
 /**
@@ -51,7 +71,7 @@ function render($message, $value)
  */
 function sub_paragraph($message)
 {
-    $wrapped = wordwrap(sprintf('  > %s%s', remove_newline($message), PHP_EOL), TEXT_WIDTH - 4, PHP_EOL . '    ');
+    $wrapped = wordwrap(sprintf('  - %s%s', remove_newline($message), PHP_EOL), TEXT_WIDTH - 4, PHP_EOL . '    ');
     printf('%s', $wrapped);
 }
 
@@ -93,7 +113,7 @@ function check_agent_connectivity()
     curl_exec($ch);
     $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     $success = $httpcode >= 200 && $httpcode < 300;
-    render('Agent can receive traces', $success);
+    renderSuccessOrFailure('Agent can receive traces', $success);
     curl_close($ch);
 
     if (!$success) {
@@ -113,7 +133,7 @@ function quiet_file_exists($file)
 echo 'DataDog trace extension verification' . PHP_EOL . PHP_EOL;
 
 render('PHP version and SAPI', PHP_VERSION . ' - ' . PHP_SAPI);
-render('ddtrace extension installed', extension_loaded('ddtrace') || extension_loaded('dd_trace'));
+renderSuccessOrFailure('ddtrace extension installed', extension_loaded('ddtrace') || extension_loaded('dd_trace'));
 $versionInstalled = phpversion('ddtrace') ?: false;
 render('ddtrace version (installed)', $versionInstalled);
 $versionConst = defined('DD_TRACE_VERSION') ? DD_TRACE_VERSION : false;
@@ -125,21 +145,21 @@ if (!empty($initHook)) {
     $versionUserland = quiet_file_exists($userlandVersionFile) ? include $userlandVersionFile : false;
 }
 render('ddtrace version (userland)', $versionUserland);
-render('ddtrace versions in sync', $versionInstalled === $versionConst && $versionConst === $versionUserland);
-render('dd_trace() function available', function_exists('dd_trace'));
-render('dd_trace_env_config() function available', function_exists('dd_trace_env_config'));
+renderSuccessOrFailure('ddtrace versions in sync', $versionInstalled === $versionConst && $versionConst === $versionUserland);
+renderSuccessOrFailure('dd_trace() function available', function_exists('dd_trace'));
+renderSuccessOrFailure('dd_trace_env_config() function available', function_exists('dd_trace_env_config'));
 
-render('ddtrace.request_init_hook set', !empty($initHook));
+renderSuccessOrFailure('ddtrace.request_init_hook set', !empty($initHook));
 $initHookReachable = quiet_file_exists($initHook);
-render('ddtrace.request_init_hook reachable', $initHookReachable);
-$openBaseDirs = $openBaseDir = ini_get('open_basedir') ? explode(':', $openBaseDir) : [];
+renderSuccessOrFailure('ddtrace.request_init_hook reachable', $initHookReachable);
+$openBaseDirs = ini_get('open_basedir') ? explode(':', ini_get('open_basedir')) : [];
 if ($initHookReachable) {
     $initHookHasRun = function_exists('DDTrace\\Bridge\\dd_wrap_autoloader');
-    render('ddtrace.request_init_hook has run', $initHookHasRun);
+    renderSuccessOrFailure('ddtrace.request_init_hook has run', $initHookHasRun);
 } elseif($initHook && $openBaseDirs) {
     $initHookDir = dirname($initHook);
     if (!in_array($initHookDir, $openBaseDirs)) {
-        render("open_basedir includes initialization dir", false);
+        render("open_basedir includes initialization dir", FAIL);
         $hint = <<<EOT
 Ini parameter 'open_basedir' has been set but it does not include the directory where
 the extension is installed '$initHookDir'. After consulting with your system admin, you might
@@ -151,6 +171,19 @@ EOT;
     render('open_basedir INI directive', ini_get('open_basedir') ?: 'empty');
 }
 
+// Open base dire prevents/allows access to '/proc/self/cgroup'
+$isProcSelfForbiddenByOpenBaseDir = !empty($openBaseDirs) && !in_array('/proc/self', $openBaseDirs);
+render("'open_basedir' allows access to '/proc/self'", $isProcSelfForbiddenByOpenBaseDir ? WARN : OK);
+if ($isProcSelfForbiddenByOpenBaseDir) {
+    $hint = <<<EOT
+Directive 'open_basedir' prevents access to '/proc/self/cgroup' that is used to extract container info.
+If your app does not run in a containerized environment ignore this message. If your app runs in a containerized
+environment, e.g. Docker or k8s, you might want to add /proc/self to 'open_basedir' in order to have container
+info added to your tracer metadata.
+EOT;
+    sub_paragraph($hint);
+}
+
 class AutoloadTest
 {
     public static function load($class)
@@ -160,7 +193,7 @@ class AutoloadTest
 }
 
 $integrationsLoaderExists = class_exists('\\DDTrace\\Integrations\\IntegrationsLoader');
-render('IntegrationsLoader exists', $integrationsLoaderExists);
+renderSuccessOrFailure('IntegrationsLoader exists', $integrationsLoaderExists);
 if ($integrationsLoaderExists) {
     $notLoaded = \DDTrace\Integrations\IntegrationsLoader::get()->getLoadingStatus('web');
     render('Integrations not loaded yet', 0 === $notLoaded);
@@ -169,10 +202,10 @@ if ($integrationsLoaderExists) {
     spl_autoload_register('AutoloadTest::load');
 
     $loaded = \DDTrace\Integrations\IntegrationsLoader::get()->getLoadingStatus('web');
-    render('Integrations loaded', 0 !== $loaded);
+    renderSuccessOrFailure('Integrations loaded', 0 !== $loaded);
 }
 
-render('DDTrace\\Tracer class exists', class_exists('\\DDTrace\\Tracer'));
+renderSuccessOrFailure('DDTrace\\Tracer class exists', class_exists('\\DDTrace\\Tracer'));
 
 check_agent_connectivity();
 
