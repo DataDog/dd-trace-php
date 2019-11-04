6e92c14951d6926f752dd07ef55724ebb7c30c04
Levi Morrison <levi.morrison@datadoghq.com>
2019-11-04 10:54:06 -0700

    Add incomplete fix to segfault with v5.6+xdebug
    This makes it more reliable when run by hand, but when
    run by test it still fails most of the time.
    
    In other words, there is probably another piece to
    the puzzle.

--- bridge/dd_init.php
+++ bridge/dd_init.php
@@ -5,7 +5,14 @@ namespace DDTrace\Bridge;
 use DDTrace\Bootstrap;
 use DDTrace\Integrations\IntegrationsLoader;
 
-require_once __DIR__ . '/functions.php';
+/* Do not use require_once here. On PHP 5.6 with xdebug enabled, the
+ * require_once will segfault if DD_TRACE_NO_AUTOLOADER=1 is set.
+ * This has to do with the cyclic dependency between these two files, and for
+ * whatever reason the require_once fails under certain circumstances.
+ */
+if (!function_exists('dd_tracing_enabled')) {
+    require __DIR__ . '/functions.php';
+}
 
 // trigger configuration reload to memoize values of all configuration options as set by environment variables
 function_exists('dd_trace_internal_fn') && dd_trace_internal_fn('ddtrace_reload_config');
