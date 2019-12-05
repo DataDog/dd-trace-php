3f70866374f69c1e36a17a9f35758dfa3bcc56e8
Luca Abbati <luca.abbati@datadoghq.com>
2019-12-05 22:00:58 +0100

    start experimenting with non-class based tracing

---
+++ lib-php/files/config.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/files/config.php';
+
+function dd_config_get_service_name()
+{
+}
---
+++ lib-php/index.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once __DIR__ . '/contrib/index.php';
+require_once __DIR__ . '/files/config.php';
