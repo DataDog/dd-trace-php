ab7b56919a7c7a20b8ba538553ab6b1d015041d8
Luca Abbati <luca.abbati@datadoghq.com>
2019-12-05 21:32:52 +0100

    Remove unused file for memory overhead analysis

--- tests/overhead/dump-memory.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-grep rw-p /proc/$1/maps \
-| sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' \
-| while read start stop; do \
-    gdb --batch --pid $1 -ex \
-        "dump memory $1-$start-$stop.dump 0x$start 0x$stop"; \
-done
