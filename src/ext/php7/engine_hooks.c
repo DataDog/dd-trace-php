a1c1419c0ba355998d8100113ff4cf023231bdb8
sammy.powers <sammyk@datadoghq.com>
2019-12-19 16:04:14 -0800

    Add limited tracing check (PHP 7)

--- src/ext/php7/engine_hooks.c
+++ src/ext/php7/engine_hooks.c
@@ -64,6 +64,9 @@ static BOOL_T ddtrace_should_trace_call(zend_execute_data *execute_data, zend_fu
     if (!*dispatch || (*dispatch)->busy) {
         return FALSE;
     }
+    if (ddtrace_tracer_is_limited() && ((*dispatch)->options & DDTRACE_DSPCH_INSTRUMENT_WHEN_LIMITED) == 0) {
+        return FALSE;
+    }
 
     return TRUE;
 }
