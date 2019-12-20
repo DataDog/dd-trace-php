4aeb26c600a68d24be61b96d0d602a161cb991a6
sammy.powers <sammyk@datadoghq.com>
2019-12-20 09:44:43 -0800

    Add ddtrace_log_debugf() for better debug logging
    Also move the TSRMLS_* macros out of the logging API

--- src/ext/logging.c
+++ src/ext/logging.c
@@ -2,13 +2,19 @@
 
 #include <php.h>
 
-void _ddtrace_log_errf(TSRMLS_FC const char *format, ...) {
+void ddtrace_log_err(char *message) {
+    TSRMLS_FETCH();
+    php_log_err(message TSRMLS_CC);
+}
+
+void _ddtrace_log_errf(const char *format, ...) {
     va_list args;
     char *buffer;
 
     va_start(args, format);
     vspprintf(&buffer, 0, format, args);
-    ddtrace_log_err(buffer);
+    TSRMLS_FETCH();
+    php_log_err(buffer TSRMLS_CC);
 
     efree(buffer);
     va_end(args);
