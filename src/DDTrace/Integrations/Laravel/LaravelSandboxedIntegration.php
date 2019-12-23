3589cd8ce57f8b33809fd73109a3871f48d8a73f
Luca Abbati <luca.abbati@datadoghq.com>
2019-12-23 09:57:12 +0100

    Initial draft of methods for sandboxed laravel application

--- src/DDTrace/Integrations/Laravel/LaravelSandboxedIntegration.php
+++ src/DDTrace/Integrations/Laravel/LaravelSandboxedIntegration.php
@@ -2,9 +2,19 @@
 
 namespace DDTrace\Integrations\Laravel;
 
+use DDTrace\Configuration;
+use DDTrace\GlobalTracer;
+use DDTrace\SpanData;
 use DDTrace\Integrations\Laravel\V5\LaravelIntegrationLoader;
 use DDTrace\Integrations\SandboxedIntegration;
 use DDTrace\Util\Versions;
+use DDTrace\Integrations\Integration;
+use DDTrace\Integrations\Laravel\LaravelIntegration;
+use DDTrace\Scope;
+use DDTrace\Tag;
+use DDTrace\Type;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Route;
 
 /**
  * The base Laravel integration which delegates loading to the appropriate integration version.
@@ -34,7 +44,40 @@ class LaravelSandboxedIntegration extends SandboxedIntegration
      */
     public function init()
     {
-        $kernelClass = null;
+        if (!Configuration::get()->isIntegrationEnabled(LaravelSandboxedIntegration::NAME)) {
+            return SandboxedIntegration::NOT_LOADED;
+        }
+
+        $rootScope = GlobalTracer::get()->getRootScope();
+        $rootSpan = null;
+
+        if (null === $rootScope || null === ($rootSpan = $rootScope->getSpan())) {
+            return SandboxedIntegration::NOT_LOADED;
+        }
+
+        $integration = $this;
+
+        dd_trace_method(
+            'Illuminate\Routing\Events\RouteMatched',
+            '__construct',
+            function (SpanData $span, $args) use ($integration, $rootSpan) {
+                list($route, $request) = $args;
+                // Overwriting the default web integration
+                $rootSpan->setIntegration($integration);
+                $rootSpan->setTraceAnalyticsCandidate();
+                $rootSpan->setTag(
+                    Tag::RESOURCE_NAME,
+                    $route->getActionName() . ' ' . (Route::currentRouteName() ?: 'unnamed_route')
+                );
+                $rootSpan->setTag('laravel.route.name', Route::currentRouteName());
+                $rootSpan->setTag('laravel.route.action', $route->getActionName());
+                $rootSpan->setTag('http.url', $request->url());
+                $rootSpan->setTag('http.method', $request->method());
+
+                return false;
+            }
+        );
+
         return SandboxedIntegration::LOADED;
     }
 }
