cb7fde1546a9c743656f828a0648bf9b6451cb8c
sammy.powers <sammyk@datadoghq.com>
2020-01-08 10:54:24 -0800

    Move framework Docker containers to dd-trace-ci

--- dockerfiles/frameworks/src/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-if [[ -z "$NO_DDTRACE" ]]; then
-    curl -o /tmp/ddtrace.deb http://nginx_file_server/ddtrace.deb
-    dpkg -i /tmp/ddtrace.deb
-
-    export DD_TRACE_CLI_ENABLED=true
-fi
-exec "$@"
