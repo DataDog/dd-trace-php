1714a61bf3bd4c41a77fdaf911c1c48fa152529d
Alexey Deriyenko <oderiyenko@wayfair.com>
2020-02-14 13:15:04 +0100

    * CI2.2 tests

--- tests/Frameworks/TestScenarios.php
+++ tests/Frameworks/TestScenarios.php
@@ -12,8 +12,8 @@ class TestScenarios
             GetSpec::create('A simple GET request returning a string', '/simple'),
             GetSpec::create('A simple GET request with a view', '/simple_view'),
             GetSpec::create('A GET request with an exception', '/error')->expectStatusCode(500),
-            GetSpec::create('A GET request ended in http_status_code(200)', '/http_response_code_success'),
-            GetSpec::create('A GET request ended in http_status_code(500)', '/http_response_code_failure'),
+            GetSpec::create('A GET request ended in http_status_code(200)', '/http_response_code/success'),
+            GetSpec::create('A GET request ended in http_status_code(500)', '/http_response_code/failure'),
         ];
     }
 }
