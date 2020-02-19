c4d9fb14509bccbd11de096554ea05e4e9324c40
Levi Morrison <levi.morrison@datadoghq.com>
2020-02-19 10:03:18 -0700

    Test background sender ignores circuit breaker

--- tests/Frameworks/Custom/Version_Autoloaded/src/routes.php
+++ tests/Frameworks/Custom/Version_Autoloaded/src/routes.php
@@ -4,4 +4,6 @@ return function(FastRoute\RouteCollector $r) {
     $r->addRoute('GET', '/simple', '\App\SimpleController');
     $r->addRoute('GET', '/simple_view', '\App\SimpleViewController');
     $r->addRoute('GET', '/error', '\App\ErrorController');
+
+    $r->addRoute('GET', '/circuit_breaker', '\App\CircuitBreakerController');
 };
