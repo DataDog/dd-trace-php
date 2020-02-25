a0abed8ddd6ce1e8ab4c6b86b5bbbd4ec5ad6819
Luca Abbati <luca.abbati@datadoghq.com>
2020-02-25 17:10:52 +0100

    Cause the apk verification script to exit 1 if extension not installed

--- dockerfiles/verify_packages/verify_apk.sh
+++ dockerfiles/verify_packages/verify_apk.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 apk add /build_src/build/packages/*.apk --allow-untrusted --no-cache
-php -r 'echo phpversion("ddtrace") . PHP_EOL;'
+
+# We attempt in this order the following binary names:
+#    1. php
+#    2. php7 (alpine intall php 7.x from main repo to this binary)
+#    3. php5 (alpine intall php 5.x from main repo to this binary)
+DD_TRACE_PHP_BIN=$(command -v php)
+if [[ -z "$DD_TRACE_PHP_BIN" ]]; then
+    DD_TRACE_PHP_BIN=$(command -v php7)
+fi
+if [[ -z "$DD_TRACE_PHP_BIN" ]]; then
+    DD_TRACE_PHP_BIN=$(command -v php5)
+fi
+
+# -q causes grep to exit 1 if string not found
+${DD_TRACE_PHP_BIN} -m | grep -q ddtrace
--- package/post-install.sh
+++ package/post-install.sh
@@ -13,8 +13,8 @@ PATH="${PATH}:/usr/local/bin"
 
 # We attempt in this order the following binary names:
 #    1. php
-#    1. php7
-#    1. php5
+#    2. php7 (alpine intall php 7.x from main repo to this binary)
+#    3. php5 (alpine intall php 5.x from main repo to this binary)
 if [[ -z "$DD_TRACE_PHP_BIN" ]]; then
     DD_TRACE_PHP_BIN=$(command -v php)
 fi
