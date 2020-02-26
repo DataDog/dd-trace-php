041d86ba54a56ea5051cad6ab78df7661e746f3a
Alexey Deriyenko <oderiyenko@wayfair.com>
2020-02-26 17:06:45 +0100

    * phpcbf; * tests modified as per reviews

--- tests/Integration/ResponseStatusCodeTest_files/index.php
+++ tests/Integration/ResponseStatusCodeTest_files/index.php
@@ -1,3 +1,9 @@
 <?php
 
-http_response_code(200);
+switch ($_SERVER['REQUEST_URI']) {
+    case "/success":
+        http_response_code(200);
+        break;
+    case "/error":
+        http_response_code(500);
+}
