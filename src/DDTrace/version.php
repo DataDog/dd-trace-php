767cd94d005f343784e3a4025fe00bea0af381ae
Levi Morrison <levi.morrison@datadoghq.com>
2020-03-03 15:27:21 -0700

    Release 0.41.1
    - Fix pecl installs #781
    - Fix background sender logging for ZTS #783

--- src/DDTrace/version.php
+++ src/DDTrace/version.php
@@ -2,4 +2,4 @@
 
 // Must begin with a number for Debian packaging requirements
 // Must use single-quotes for packaging script to work
-return '0.41.0'; // Update Tracer::VERSION too
+return '0.41.1'; // Update Tracer::VERSION too
