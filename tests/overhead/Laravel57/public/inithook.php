6b850ca81dcbea91b8e55377ea31b99c8974f744
Luca Abbati <luca.abbati@datadoghq.com>
2020-04-14 17:04:01 +0200

    add multitime benchmarking tool to the overhead measurement test

--- tests/overhead/Laravel57/public/inithook.php
+++ tests/overhead/Laravel57/public/inithook.php
@@ -2,8 +2,4 @@
 
 require_once '/dd-trace-php/bridge/dd_wrap_autoloader.php';
 
-// Let's make sure that bootstrap happened
-$tracer = DDTrace\GlobalTracer::get();
-
-echo "Class: " . get_class($tracer) . "\n";
 echo "Hi from init hook script!\n";
