82f5cbd5b5cb5f427f3b6d05a7b156289ff06963
Pawel Chojnacki <pawelchcki@gmail.com>
2020-04-17 09:58:20 +0000

    Xdebug configuration working

--- tests/overhead/dockerfiles/start_app.sh
+++ tests/overhead/dockerfiles/start_app.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-composer --working-dir=/var/www install
-
 if [[ -n "${DD_TRACE_LIBRARY_VERSION}" ]]; then
     # Install DDTrace deb
     wget -O /datadog-php-tracer.deb https://github.com/DataDog/dd-trace-php/releases/download/${DD_TRACE_LIBRARY_VERSION}/datadog-php-tracer_${DD_TRACE_LIBRARY_VERSION}_amd64.deb
     dpkg -i /datadog-php-tracer.deb
 fi
+php-fpm -i | grep xdebug
+
+sudo chmod go+w /var/www/callgrind-files
 
 php-fpm
