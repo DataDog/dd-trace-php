048d3ee68ca8e95b8f222e25ebe7d4f58b64a595
Luca Abbati <luca.abbati@datadoghq.com>
2020-04-20 18:59:34 +0200

    add pdo and curl requests

--- dockerfiles/testing-environment/Laravel57/app/Http/Controllers/ExampleController.php
+++ dockerfiles/testing-environment/Laravel57/app/Http/Controllers/ExampleController.php
@@ -3,12 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class ExampleController extends BaseController
 {
     public function example()
     {
         error_log('This is the example action');
-        return "hi!\n";
+        $this->pdoQueries();
+        $this->curlRequest();
+        return "Hi from Laravel 5.7 app!\n";
+    }
+
+    private function pdoQueries()
+    {
+        $pdo = DB::connection()->getPdo();
+        $stm = $pdo->query("select * from status_by_user");
+        $stm->fetchAll();
+    }
+
+    private function curlRequest()
+    {
+        $url = (\getenv('DD_AGENT_HOST') ?: 'localhost' ) . ':8126/not-existing';
+        $ch = \curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        \curl_exec($ch);
     }
 }
--- dockerfiles/testing-environment/Laravel57/config/database.php
+++ dockerfiles/testing-environment/Laravel57/config/database.php
@@ -41,12 +41,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'mysql_integration'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'test'),
-            'username' => env('DB_USERNAME', 'test'),
-            'password' => env('DB_PASSWORD', 'test'),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'database' => env('DB_DATABASE', 'performance_schema'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            // 'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
