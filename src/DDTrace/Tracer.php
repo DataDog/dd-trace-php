6f1f52e72f8ae241fd46b193103f4e58b5af1232
Sammy Powers <sammyk@datadoghq.com>
2020-04-22 10:33:56 -0700

    Bump version to 0.43.0

--- src/DDTrace/Tracer.php
+++ src/DDTrace/Tracer.php
@@ -29,7 +29,7 @@ final class Tracer implements TracerInterface
     /**
      * @deprecated Use Tracer::version() instead
      */
-    const VERSION = '1.0.0-nightly'; // Update ./version.php too
+    const VERSION = '0.43.0'; // Update ./version.php too
 
     /**
      * @var Span[][]
