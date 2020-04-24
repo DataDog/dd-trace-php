fa5917328d353e07c71481e81cc129a39527768e
Luca Abbati <luca.abbati@datadoghq.com>
2020-04-24 10:09:51 +0200

    assign names to tests in autinstrmentation test suite

--- tests/AutoInstrumentation/AutoInstrumentationTest.php
+++ tests/AutoInstrumentation/AutoInstrumentationTest.php
@@ -29,32 +29,38 @@ class AutoInstrumentationTest extends BaseTestCase
         return [
             // In a typical scenario, when the user does not declare a dependency on datadog/dd-trace in the composer
             // file we should pick the tracer from the installed bundle
-            ['composer_without_ddtrace_dependency', $currentTracerVersion, true],
+            'composer_without_ddtrace_dependency'
+                => ['composer_without_ddtrace_dependency', $currentTracerVersion, true],
 
             // We want to make sure that the version declared in composer.json is picked up instead of the installed
             // version.
-            ['composer_with_ddtrace_dependency', '0.31.0', true],
+            'composer_with_ddtrace_dependency'
+                => ['composer_with_ddtrace_dependency', '0.31.0', true],
 
             // We want to make sure that users can safely declare a dependency in the latest version of our
             // tracer in composer even if not required.
-            ['composer_with_local_code_dependency', $currentTracerVersion, true],
+            'composer_with_local_code_dependency'
+                => ['composer_with_local_code_dependency', $currentTracerVersion, true],
 
             // Symfony 3.3 has a loader Symfony\Component\Config\Resource\ClassExistenceResource which registers a
             // private method as the actual class loader. Because of https://github.com/DataDog/dd-trace-php/issues/224
             // we do not support this scenario, yet. As a result, we have to make sure that the workaround we applied
             // works.
-            ['symfony_33_private_loader', $currentTracerVersion, true],
+            'symfony_33_private_loader'
+                => ['symfony_33_private_loader', $currentTracerVersion, true],
 
             // In some cases, e.g. Zend Framework 1.12's Zend_Loader, loaders are not lenient, e.g. returning null/false
             // if they do not recognize their namespace. Instead they try to load the file, no matter what. This causes
             // an issue when we run `class_exist` during the first phases of auto-instrumentation because the loader
             // would try to `include_once` a file (e.g. 'DDTrace/Tracer.php') that does not exists.
-            ['autoloader_includes_even_non_existing', $currentTracerVersion, false],
+            'autoloader_includes_even_non_existing'
+                => ['autoloader_includes_even_non_existing', $currentTracerVersion, false],
 
             // In some cases, autoloaders exit() or die() if they don't find a class. This test makes sure that in our
             // auto-instrumentation code we never try to `class_exists()`, `method_exists()` or `defined()` outside of
             // DDTrace namespace.
-            ['autoloader_die_on_not_found', $currentTracerVersion, false],
+            'autoloader_die_on_not_found'
+                => ['autoloader_die_on_not_found', $currentTracerVersion, false],
         ];
     }
 
