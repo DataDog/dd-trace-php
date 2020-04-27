56731cfe6b51c269254fac4a44497f5a30c6248f
Levi Morrison <levi.morrison@datadoghq.com>
2020-04-27 10:38:37 -0600

    Add ddtrace_getenv and use it ddtrace_config_* PHP functions
    The functionality these ddtrace_config_* functions replaced used
    PHP's getenv(), which will by default use sapi_getenv first, and
    getenv second.
    
    I avoided the use of existing env related functions because they
    will duplicate the string to get it out of the Zend Memory Manager,
    but these functions are perf sensitive, so I didn't want to copy
    any more than is necessary.
    
    This also means ddtrace_getenv() cannot be used from the background
    writer thread.

--- src/ext/env_config.h
+++ src/ext/env_config.h
@@ -8,6 +8,12 @@
 #define TRUE (1)
 #define FALSE (0)
 
+/* ddtrace_getenv duplicates; efree it when done.
+ * Do not call ddtrace_getenv from the background thread.
+ * Returns the sapi_getenv or getenv.
+ */
+char *ddtrace_getenv(char *name, size_t name_len TSRMLS_DC);
+
 BOOL_T ddtrace_get_bool_config(char *name, BOOL_T def TSRMLS_DC);
 char *ddtrace_get_c_string_config(char *name TSRMLS_DC);
 int64_t ddtrace_get_int_config(char *name, int64_t def TSRMLS_DC);
