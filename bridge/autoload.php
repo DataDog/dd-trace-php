aab3b48ff4ba631d63ff261dcbc2a6888e7b4811
Luca Abbati <luca.abbati@datadoghq.com>
2020-04-28 10:30:20 +0200

    in autoload.php use require in place of require_once in development mode

--- bridge/autoload.php
+++ bridge/autoload.php
@@ -4,7 +4,7 @@ if (getenv('DD_AUTOLOAD_NO_COMPILE') === 'true') {
     // Development
     $files = include __DIR__ . '/_files.php';
     foreach ($files as $file) {
-        require_once $file;
+        require $file;
     }
 } else {
     // Production
