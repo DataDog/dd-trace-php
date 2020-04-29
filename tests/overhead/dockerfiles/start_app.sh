3472b8965e888f91d762870d29c8aa7e14781d20
Luca Abbati <luca.abbati@datadoghq.com>
2020-04-29 12:18:49 +0200

    in overhead app, fix installation of latest release

--- tests/overhead/dockerfiles/start_app.sh
+++ tests/overhead/dockerfiles/start_app.sh
@@ -2,7 +2,7 @@
 
 composer --working-dir=/var/www install
 
-if [[ -z "${DD_TRACE_LIBRARY_VERSION}" ]]; then
+if [[ ! -z "${DD_TRACE_LIBRARY_VERSION}" ]]; then
     # Install DDTrace deb
     wget -O /datadog-php-tracer.deb https://github.com/DataDog/dd-trace-php/releases/download/${DD_TRACE_LIBRARY_VERSION}/datadog-php-tracer_${DD_TRACE_LIBRARY_VERSION}_amd64.deb
     dpkg -i /datadog-php-tracer.deb
