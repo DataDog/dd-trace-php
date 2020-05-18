a49be5d9f15e59553386720e63406def01021620
Sammy Powers <sammyk@datadoghq.com>
2020-05-18 10:08:13 -0700

    Bump version to 0.45.1

--- src/DDTrace/version.php
+++ src/DDTrace/version.php
@@ -2,4 +2,4 @@
 
 // Must begin with a number for Debian packaging requirements
 // Must use single-quotes for packaging script to work
-return '0.45.0'; // Update Tracer::VERSION too
+return '0.45.1'; // Update Tracer::VERSION too
