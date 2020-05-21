ed92a616d81ba589c47c4061796d9e03329e235d
hm_yokoyamatatsuo <yokoyama.tatsuo@hamee.co.jp>
2020-05-21 10:51:19 +0900

    add return

--- src/dd-doctor.php
+++ src/dd-doctor.php
@@ -120,7 +120,7 @@ function env($key)
 function check_opcache()
 {
     if (!function_exists('opcache_get_configuration')) {
-        render('Opcache enabled:', 'NO');
+        return render('Opcache enabled:', 'NO');
     }
     render('Opcache enabled:', 'YES');
     $configs = opcache_get_configuration();
