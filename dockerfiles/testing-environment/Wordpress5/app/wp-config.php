d63d4a3664810505cb2a9091f27bbde4080837cb
Luca Abbati <luca.abbati@datadoghq.com>
2020-06-05 11:12:38 +0200

    relenv: make wp5 dbhost configurable

--- dockerfiles/testing-environment/Wordpress5/app/wp-config.php
+++ dockerfiles/testing-environment/Wordpress5/app/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'mysql' );
+define( 'DB_HOST', getenv('DB_HOST') ?: 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
