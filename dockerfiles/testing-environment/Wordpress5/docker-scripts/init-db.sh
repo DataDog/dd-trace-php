ee776948c5efdb8bd66277e144bbdf1f1700aa9f
Luca Abbati <luca.abbati@datadoghq.com>
2020-06-05 10:25:00 +0200

    rel-env: wait up to 30 secs before giving up on mysql available

--- dockerfiles/testing-environment/Wordpress5/docker-scripts/init-db.sh
+++ dockerfiles/testing-environment/Wordpress5/docker-scripts/init-db.sh
@@ -2,11 +2,22 @@
 
 set -e
 
-EXISTS=$(mysql -h mysql -u root -e "SHOW DATABASES LIKE 'wordpress'")
+# Wait until MySQL is really available
+MAXCOUNTER=30
+COUNTER=1
+while ! mysql -u root -h mysql -e "show databases;" > /dev/null 2>&1; do
+    sleep 1
+    counter=`expr $counter + 1`
+    if [ $COUNTER -gt $MAXCOUNTER ]; then
+        >&2 echo "We have been waiting for MySQL too long already; failing."
+        exit 1
+    fi;
+done
 
+# Init DB if not exists
+EXISTS=$(mysql -h mysql -u root -e "SHOW DATABASES LIKE 'wordpress'")
 if [[ -z "$EXISTS" ]]; then
-    echo "Initializing the DB"
-    sleep 10
+    echo "Initializing DB"
     mysql -h mysql -u root -e "CREATE DATABASE wordpress"
     mysql -h mysql -u root wordpress < /scripts/db-data.sql
 fi
