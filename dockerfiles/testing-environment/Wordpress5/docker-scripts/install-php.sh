51c63994e0fbb4f749aba290453a0ac3ee95a033
Luca Abbati <luca.abbati@datadoghq.com>
2020-06-11 12:22:31 +0200

    relenv: build Wordpress 5 images for both 7.3 and 5.6

--- dockerfiles/testing-environment/Wordpress5/docker-scripts/install-php.sh
+++ dockerfiles/testing-environment/Wordpress5/docker-scripts/install-php.sh
@@ -6,19 +6,22 @@ apt update
 
 apt install -y software-properties-common
 add-apt-repository -y ppa:ondrej/php
+
+# Installing PHP
 apt install -y \
-    php7.3-cli \
-    php7.3-curl \
-    php7.3-fpm \
-    php7.3-mbstring \
-    php7.3-mysql \
-    php7.3-opcache \
-    php7.3-xml \
-    php7.3-zip
-
-rm -rf /var/lib/apt/lists/*
-
+    php${PHP_VERSION}-cli \
+    php${PHP_VERSION}-curl \
+    php${PHP_VERSION}-fpm \
+    php${PHP_VERSION}-mbstring \
+    php${PHP_VERSION}-mysql \
+    php${PHP_VERSION}-opcache \
+    php${PHP_VERSION}-xml \
+    php${PHP_VERSION}-zip
 # remove unused php-fpm files that will be provided from outside
 rm -rf \
-    /etc/php/7.3/fpm/php-fpm.conf \
-    /etc/php/7.3/fpm/pool.d
+    /etc/php/${PHP_VERSION}/fpm/php-fpm.conf \
+    /etc/php/${PHP_VERSION}/fpm/pool.d
+
+update-alternatives --install /usr/bin/php-fpm php-fpm /usr/sbin/php-fpm${PHP_VERSION} 1
+
+rm -rf /var/lib/apt/lists/*
