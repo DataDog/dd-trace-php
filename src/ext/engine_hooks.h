4e494288073e2410ff6c2a2da6d1a2c42f765579
Levi Morrison <levi.morrison@datadoghq.com>
2020-06-30 16:22:49 -0600

    Extract ddtrace_sandbox_{begin,end} helpers
    Most of the time the sandboxing code is the same, so let's
    de-duplicate it.

--- src/ext/engine_hooks.h
+++ src/ext/engine_hooks.h
@@ -6,6 +6,7 @@
 #include <php.h>
 #include <stdint.h>
 
+#include "compatibility.h"
 #include "ddtrace.h"
 #include "ddtrace_string.h"
 
@@ -33,6 +34,15 @@ struct ddtrace_error_handling {
     zend_error_handling error_handling;
 };
 typedef struct ddtrace_error_handling ddtrace_error_handling;
+
+struct ddtrace_sandbox_backup {
+    ddtrace_error_handling eh;
+    ddtrace_exception_t *exception, *prev_exception;
+#if PHP_VERSION_ID < 70000
+    zend_op *opline_before_exception;
+#endif
+};
+typedef struct ddtrace_sandbox_backup ddtrace_sandbox_backup;
 
 inline void ddtrace_backup_error_handling(ddtrace_error_handling *eh, zend_error_handling_t mode TSRMLS_DC) {
     eh->type = PG(last_error_type);
@@ -88,6 +98,50 @@ inline void ddtrace_maybe_clear_exception(void) {
     }
 }
 #endif
+
+#if PHP_VERSION_ID < 70000
+inline ddtrace_sandbox_backup ddtrace_sandbox_begin(zend_op *opline_before_exception TSRMLS_DC) {
+#else
+inline ddtrace_sandbox_backup ddtrace_sandbox_begin(void) {
+#endif
+    ddtrace_sandbox_backup backup = {.exception = NULL, .prev_exception = NULL};
+    if (EG(exception)) {
+        backup.exception = EG(exception);
+        backup.prev_exception = EG(prev_exception);
+        EG(exception) = NULL;
+        EG(prev_exception) = NULL;
+
+#if PHP_VERSION_ID < 70000
+        backup.opline_before_exception = opline_before_exception;
+#endif
+    }
+
+#if PHP_VERSION_ID < 70000
+    zend_error_handling_t mode = EH_SUPPRESS;
+#else
+    zend_error_handling_t mode = EH_THROW;
+#endif
+
+    ddtrace_backup_error_handling(&backup.eh, mode TSRMLS_CC);
+    return backup;
+}
+
+inline void ddtrace_sandbox_end(ddtrace_sandbox_backup *backup TSRMLS_DC) {
+    ddtrace_restore_error_handling(&backup->eh TSRMLS_CC);
+    ddtrace_maybe_clear_exception(TSRMLS_C);
+
+    if (backup->exception) {
+        EG(exception) = backup->exception;
+        EG(prev_exception) = backup->prev_exception;
+
+#if PHP_VERSION_ID < 70000
+        EG(opline_before_exception) = backup->opline_before_exception;
+        EG(current_execute_data)->opline = EG(exception_op);
+#else
+        zend_throw_exception_internal(NULL);
+#endif
+    }
+}
 
 #if PHP_VERSION_ID >= 70000
 PHP_FUNCTION(ddtrace_internal_function_handler);
