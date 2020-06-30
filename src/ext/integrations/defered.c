ce4dc10084513d8dc53cb95057b9916cc46ae4e8
Pawel Chojnacki <pawel.chcki@gmail.com>
2020-06-30 13:36:03 +0200

    Fix compilation on 5.x and add missing files to package.xml

--- src/ext/integrations/defered.c
+++ src/ext/integrations/defered.c
@@ -4,15 +4,12 @@
 #include "dispatch.h"
 
 void dd_load_defered_integration_list(ddtrace_defered_integration *list, size_t size) {
-    // Defered loading feature is not yet implemented for PHP 5.x
-#if PHP_VERSION_ID >= 70000
     for (size_t i = 0; i < size; ++i) {
         ddtrace_defered_integration integration = list[i];
         zval class_name;
         zval function_name;
-        zval callable;
         zval loader;
-        ZVAL_NULL(&callable);
+#if PHP_VERSION_ID >= 70000
         if (integration.class_name.ptr) {
             ZVAL_STRINGL(&class_name, integration.class_name.ptr, integration.class_name.len);
         } else {
@@ -26,6 +23,16 @@ void dd_load_defered_integration_list(ddtrace_defered_integration *list, size_t 
         zval_dtor(&function_name);
         zval_dtor(&class_name);
         zval_dtor(&loader);
-    }
+#else
+        if (integration.class_name.ptr) {
+            ZVAL_STRINGL(&class_name, integration.class_name.ptr, integration.class_name.len, 0);
+        } else {
+            ZVAL_NULL(&class_name);
+        }
+        ZVAL_STRINGL(&function_name, integration.fname.ptr, integration.fname.len, 0);
+        ZVAL_STRINGL(&loader, integration.loader.ptr, integration.loader.len, 0);
+
+        ddtrace_defered_load_via_function(class_name, function_name, loader, 0);
 #endif
+    }
 }
