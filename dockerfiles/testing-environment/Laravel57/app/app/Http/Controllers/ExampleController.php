0c280478aef2dfc74da01ecffd524b9f0af5b02f
Luca Abbati <luca.abbati@datadoghq.com>
2020-07-01 11:44:33 +0200

    move laravel 5.7 testing app to script based dockerfile

--- dockerfiles/testing-environment/Laravel57/app/app/Http/Controllers/ExampleController.php
+++ dockerfiles/testing-environment/Laravel57/app/app/Http/Controllers/ExampleController.php
@@ -9,24 +9,6 @@ class ExampleController extends BaseController
 {
     public function example()
     {
-        // $this->pdoQueries();
-        // $this->curlRequest();
         return "Hi from Laravel 5.7 app!\n";
-    }
-
-    private function pdoQueries()
-    {
-        $pdo = DB::connection()->getPdo();
-        $stm = $pdo->query("show tables");
-    }
-
-    private function curlRequest()
-    {
-        $url = (\getenv('DD_HTTPBIN_HOST') ?: 'localhost' ) . ':4000/status/200';
-        $ch = \curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT_MS, 1);
-        \curl_exec($ch);
     }
 }
