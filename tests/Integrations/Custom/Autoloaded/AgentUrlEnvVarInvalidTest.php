bbbe56bc90c3bf97761e2100e543a7852d41b168
Sammy Powers <sammyk@datadoghq.com>
2020-07-15 07:40:54 -0700

    Add tests for DD_TRACE_AGENT_URL

---
+++ tests/Integrations/Custom/Autoloaded/AgentUrlEnvVarInvalidTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DDTrace\Tests\Integrations\Custom\Autoloaded;
+
+use DDTrace\Tests\Common\WebFrameworkTestCase;
+use DDTrace\Tests\Frameworks\Util\Request\GetSpec;
+
+final class AgentUrlEnvVarInvalidTest extends WebFrameworkTestCase
+{
+    protected static function getAppIndexScript()
+    {
+        return __DIR__ . '/../../../Frameworks/Custom/Version_Autoloaded/public/index.php';
+    }
+
+    protected static function getEnvs()
+    {
+        return array_merge(parent::getEnvs(), [
+            'DD_TRACE_AGENT_URL' => 'http://invalid_hostname:1337',
+        ]);
+    }
+
+    public function testInvalidAgentUrlEnvVarTakesPrecedence()
+    {
+        $traces = $this->tracesFromWebRequest(function () {
+            $spec = GetSpec::create('Invalid DD_TRACE_AGENT_URL takes precedence', '/simple');
+            $this->call($spec);
+        });
+
+        self::assertEmpty($traces);
+    }
+}
