5ab45a267459d6218f11e94b99b5f5506675fb23
Sammy Kaye Powers <sammyk@datadoghq.com>
2020-07-20 10:58:17 -0700

    Merge pull request #962 from DataDog/sammyk/env-vars/agent-url
    Consistent env vars: DD_TRACE_AGENT_URL

--- src/ext/coms.c
+++ src/ext/coms.c
@@ -608,7 +608,7 @@ static ddtrace_coms_stack_t *_dd_coms_attempt_acquire_stack(void) {
 }
 
 #define TRACE_PATH_STR "/v0.4/traces"
-#define HOST_FORMAT_STR "http://%s:%u" TRACE_PATH_STR
+#define HOST_FORMAT_STR "http://%s:%u"
 
 atomic_uintptr_t memoized_agent_curl_headers;
 
@@ -633,37 +633,41 @@ void ddtrace_curl_set_connect_timeout(CURL *curl) {
     curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT_MS, timeout);
 }
 
-void ddtrace_curl_set_hostname(CURL *curl) {
+char *ddtrace_agent_url(void) {
     char *url = get_dd_trace_agent_url();
+    if (url && url[0]) {
+        return url;
+    }
+    free(url);
+    url = NULL;
+
+    char *hostname = get_dd_agent_host();
+    if (hostname) {
+        size_t agent_url_len =
+            strlen(hostname) + sizeof(HOST_FORMAT_STR) + 10;  // port digit allocation + some headroom
+        url = malloc(agent_url_len);
+        int64_t port = get_dd_trace_agent_port();
+        if (port <= 0 || port > 65535) {
+            port = 8126;
+        }
+        snprintf(url, agent_url_len, HOST_FORMAT_STR, hostname, (uint32_t)port);
+    } else {
+        url = ddtrace_strdup("http://localhost:8126");
+    }
+    free(hostname);
+    return url;
+}
+
+void ddtrace_curl_set_hostname(CURL *curl) {
+    char *url = ddtrace_agent_url();
     if (url && url[0]) {
         size_t agent_url_len = strlen(url) + sizeof(TRACE_PATH_STR);
         char *agent_url = malloc(agent_url_len);
         sprintf(agent_url, "%s%s", url, TRACE_PATH_STR);
         curl_easy_setopt(curl, CURLOPT_URL, agent_url);
-        free(url);
         free(agent_url);
-        return;
     }
     free(url);
-
-    char *hostname = get_dd_agent_host();
-    int64_t port = get_dd_trace_agent_port();
-    if (port <= 0 || port > 65535) {
-        port = 8126;
-    }
-
-    if (hostname) {
-        size_t agent_url_len =
-            strlen(hostname) + sizeof(HOST_FORMAT_STR) + 10;  // port digit allocation + some headroom
-        char *agent_url = malloc(agent_url_len);
-        snprintf(agent_url, agent_url_len, HOST_FORMAT_STR, hostname, (uint32_t)port);
-
-        curl_easy_setopt(curl, CURLOPT_URL, agent_url);
-        free(hostname);
-        free(agent_url);
-    } else {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8126/v0.4/traces");
-    }
 }
 
 static struct timespec _dd_deadline_in_ms(uint32_t ms) {
