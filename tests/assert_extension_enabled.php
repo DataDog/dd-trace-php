e76bc462a49557d5011811f22e2667d756441372
Luca Abbati <luca.abbati@datadoghq.com>
2020-08-05 08:44:10 +0200

    phpredis: remove unused file

--- tests/assert_extension_enabled.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// We use a real script as -r '<command>' does not execute the init procedure.
-
-if (!function_exists('\DDTrace\Bridge\dd_tracing_enabled') || false === \DDTrace\Bridge\dd_tracing_enabled()) {
-    echo "ddtrace extension is not loaded or it is disabled.\n";
-    exit(1);
-}
