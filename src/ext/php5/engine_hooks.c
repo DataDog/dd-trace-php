0e2aa8e8194fcf43b6c45e4391aded630e114d49
Levi Morrison <levi.morrison@datadoghq.com>
2020-08-19 12:30:14 -0600

    Update URL for PHP 5 stack overflow docs

--- src/ext/php5/engine_hooks.c
+++ src/ext/php5/engine_hooks.c
@@ -614,7 +614,8 @@ static void ddtrace_execute_ex(zend_execute_data *execute_data TSRMLS_DC) {
         DDTRACE_G(should_warn_call_depth) = false;
         php_log_err(
             "ddtrace has detected a call stack depth of 512. If the call stack depth continues to grow the application "
-            "may encounter a segmentation fault; see TODO: URL for details." TSRMLS_CC);
+            "may encounter a segmentation fault; see "
+            "https://docs.datadoghq.com/tracing/troubleshooting/php_5_deep_call_stacks/ for details." TSRMLS_CC);
     }
     execute_hook(execute_data TSRMLS_CC);
     --DDTRACE_G(call_depth);
