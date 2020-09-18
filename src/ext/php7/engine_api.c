ba542dd794e37eb83453a17888ad88ce71831a2d
Levi Morrison <levi.morrison@datadoghq.com>
2020-09-18 15:44:13 -0600

    Defensively initialize local zvals

--- src/ext/php7/engine_api.c
+++ src/ext/php7/engine_api.c
@@ -2,6 +2,7 @@
 
 extern inline zval ddtrace_zval_long(zend_long num);
 extern inline zval ddtrace_zval_null(void);
+extern inline zval ddtrace_zval_undef(void);
 
 zval ddtrace_zval_stringl(const char *str, size_t len) {
     zval zv;
