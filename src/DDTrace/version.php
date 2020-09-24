bf8a6119196d80a5bde4c00b5f6f6a465f057a81
Luca Abbati <luca.abbati@datadoghq.com>
2020-09-24 13:58:31 +0200

    move from ddtrace_src_version to DDTrace\VERSION constant

--- src/DDTrace/version.php
+++ src/DDTrace/version.php
@@ -1,13 +1,7 @@
 <?php
 
-/**
- * Returns the tracer sources versions.
- *
- * @return string
- */
-function ddtrace_src_version()
-{
-    // Must begin with a number for Debian packaging requirements
-    // Must use single-quotes for packaging script to work
-    return '1.0.0-nightly'; // Update Tracer::VERSION too
-}
+namespace DDTrace;
+
+// Must begin with a number for Debian packaging requirements
+// Must use single-quotes for packaging script to work
+const VERSION = '1.0.0-nightly'; // Update Tracer::VERSION too
