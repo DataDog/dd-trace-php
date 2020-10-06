49f863f619231d34bfca62e46d4ad5b787f4c7d5
Luca Abbati <luca.abbati@datadoghq.com>
2020-10-06 15:44:37 +0200

    make phpunit adapter compatible with 7 as well

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,10 +3,10 @@
 error_reporting(E_ALL);
 
 // it has to be more complex than this actually: https://phpunit.de/supported-versions.html
-if (PHP_MAJOR_VERSION === 8) {
-    require __DIR__ . '/Common/MultiPHPUnitVersionAdapter_8.php';
+if (in_array(PHP_MAJOR_VERSION, [7, 8])) {
+    require __DIR__ . '/Common/MultiPHPUnitVersionAdapter_typed.php';
 } else {
-    require __DIR__ . '/Common/MultiPHPUnitVersionAdapter_5_7.php';
+    require __DIR__ . '/Common/MultiPHPUnitVersionAdapter_untyped.php';
 }
 
 require __DIR__ . '/bootstrap_utils.php';
