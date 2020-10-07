ad5f6189e754d687ba8965835bfb82d8b935d3f7
Luca Abbati <luca.abbati@datadoghq.com>
2020-10-07 17:18:53 +0200

    make tests/bootstrap.php suitable to all phpunit versions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,13 +2,13 @@
 
 error_reporting(E_ALL);
 
-// it has to be more complex than this actually: https://phpunit.de/supported-versions.html
-if (in_array(PHP_MAJOR_VERSION, [7, 8])) {
+$phpunitVersionParts = class_exists('\PHPUnit\Runner\Version')
+    ? explode('.', \PHPUnit\Runner\Version::id())
+    : explode('.', PHPUnit_Runner_Version::id());
+$phpunitMajor = intval($phpunitVersionParts[0]);
+
+if ($phpunitMajor >= 8) {
     require __DIR__ . '/Common/MultiPHPUnitVersionAdapter_typed.php';
 } else {
     require __DIR__ . '/Common/MultiPHPUnitVersionAdapter_untyped.php';
 }
-
-require __DIR__ . '/bootstrap_utils.php';
-
-\DDTrace\Tests\prepend_test_autoloaders();
