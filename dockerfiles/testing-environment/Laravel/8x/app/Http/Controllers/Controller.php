c1096a5d887cbbf7c82cd2b2b1334a2497d3f020
Luca Abbati <luca.abbati@datadoghq.com>
2020-11-23 19:38:12 +0100

    testing-env: differentiate between laravel 57 and 8x apps

---
+++ dockerfiles/testing-environment/Laravel/8x/app/Http/Controllers/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function example()
+    {
+        return "Hi from Laravel 8.x app on PHP " . \phpversion() . "!\n";
+    }
+}
