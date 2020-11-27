b290ab528f447be93de2469e6e69b7cef1606002
Luca Abbati <luca.abbati@datadoghq.com>
2020-11-27 17:43:42 +0100

    rel-env: random tests on 7.4

---
+++ dockerfiles/testing-environment/RandomTesting/public/index.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Random app\n";
---
+++ dockerfiles/testing-environment/dockerfiles/ubuntu/local-run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Starting up local testing supervisord."
+echo "Tracer to be installed: ${TRACER_DOWNLOAD_URL}"
+
+if [ ! -z ${TRACER_DOWNLOAD_URL} ]; then
+    echo "Installing tracer at: ${TRACER_DOWNLOAD_URL}"
+    wget -O /dd-trace-php.deb ${TRACER_DOWNLOAD_URL}
+    dpkg -i /dd-trace-php.deb
+fi
+
+supervisord
