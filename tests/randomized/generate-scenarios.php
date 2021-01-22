df8659b8b921f2ab839258e0b8a4ba779f20a43f
Luca Abbati <luca.abbati@datadoghq.com>
2021-01-22 16:51:30 +0100

    randomized tests: generate requests target wiuth meaningful combiantions of headers and query string params for vegeta

--- tests/randomized/generate-scenarios.php
+++ tests/randomized/generate-scenarios.php
@@ -214,7 +214,81 @@ function generateOne($dockerComposeHandle, $scenarioSeed)
       - mysql
       - httpbin\n");
 
+    $requestsFile = fopen("$scenarioFolder/app/request-targets.txt", 'w');
+    fwrite($requestsFile, generateRequestScenarios(2000));
+    fclose($requestsFile);
+
     return $identifier;
 }
+
+function generateRequestScenarios($number)
+{
+    $availableQueries = [
+        'key' => 'value',
+        'key1' => 'value1',
+        'key.2' => '2',
+        'key_3' => 'value-3',
+        'key%204' => 'value%204',
+    ];
+    $availableHeaders = [
+        'content-type: application/json',
+        'authorization: Bearer abcdef0987654321',
+        'origin: http://some.very.cool:9000',
+        'cache-control: no-cache',
+        'accept: */*',
+    ];
+    $requests = '';
+    for ($idx = 0; $idx < $number; $idx++) {
+        $method = ['GET', 'POST', 'PUT'][rand(0, 2)];
+        $port = [/* nginx */80, /* apache*/ 81][rand(0, 1)];
+        $host = 'http://localhost';
+        // Query String
+        $query = '';
+        if (percentOfTimes(50)) {
+            $query .= '?';
+            // We are adding a query string
+            foreach ($availableQueries as $key => $value) {
+                if (percentOfTimes(70)) {
+                    continue;
+                }
+                $query .= "$key=$value&";
+            }
+        }
+        // Headers
+        //   - distributed traing
+        //   - datadog origin header
+        //   - common headers (e.g. Content-Type, Origin)
+        $headers = [];
+        if (percentOfTimes(30)) {
+            $headers[] = 'x-datadog-trace-id: ' . rand();
+            $headers[] = 'x-datadog-parent-id: ' . rand();
+            $headers[] = 'x-datadog-sampling-priority: ' . (percentOfTimes(70) ? '1.0' : '0.3');
+        }
+        if (percentOfTimes(30)) {
+            $headers[] = 'x-datadog-origin: some-origin';
+        }
+        foreach ($availableHeaders as $header) {
+            if (percentOfTimes(20)) {
+                $headers[] = $header;
+            }
+        }
+
+        $requests .= sprintf(
+            "%s %s:%d%s\n%s\n\n",
+            $method,
+            $host,
+            $port,
+            $query,
+            implode("\n", $headers)
+        );
+    }
+    return $requests;
+}
+
+function percentOfTimes($percent)
+{
+    return rand(0, 100) <= $percent;
+}
+
 
 generate();
