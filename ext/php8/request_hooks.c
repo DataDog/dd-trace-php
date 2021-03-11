8c6092de1789a2849d90b2da7f69a243ed6afece
Levi Morrison <levi.morrison@datadoghq.com>
2021-03-11 08:43:58 -0700

    Fix php_stat for 8.1-dev
    php_stat changed to use zend_string

--- ext/php8/request_hooks.c
+++ ext/php8/request_hooks.c
@@ -132,7 +132,16 @@ void dd_request_init_hook_rinit(TSRMLS_D) {
     }
 
     zval exists_flag;
-    php_stat(DDTRACE_G(request_init_hook), strlen(DDTRACE_G(request_init_hook)), FS_EXISTS, &exists_flag TSRMLS_CC);
+#if PHP_VERSION_ID >= 80100
+    {
+        const char *hook = DDTRACE_G(request_init_hook);
+        zend_string *hook_zstr = zend_string_init(hook, strlen(hook), 1);
+        php_stat(hook_zstr, FS_EXISTS, &exists_flag);
+        zend_string_release_ex(hook_zstr, 1);
+    }
+#else
+    php_stat(DDTRACE_G(request_init_hook), strlen(DDTRACE_G(request_init_hook)), FS_EXISTS, &exists_flag);
+#endif
     if (Z_TYPE(exists_flag) == IS_FALSE) {
         ddtrace_log_debugf("Cannot open request init hook; file does not exist: '%s'", DDTRACE_G(request_init_hook));
         return;
