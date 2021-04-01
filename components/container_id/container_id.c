bc6ef6a8686340d5768c248175f22ddfa8d26570
Sammy Powers <sammyk@datadoghq.com>
2021-04-01 16:40:56 -0400

    Add workarounds to regex API broken by Oniguruma library

--- components/container_id/container_id.c
+++ components/container_id/container_id.c
@@ -1,10 +1,21 @@
 #include "container_id.h"
 
+#include <ctype.h>
 #include <regex.h>
 #include <stdio.h>
 #include <string.h>
 
-#define LINE_REGEX "^[0-9]+:[^:]*:.+$"
+/* The Oniguruma library breaks extended regular expressions (ERE) in POSIX
+ * regex (REG_EXTENDED) until 6.9.5-rc1. Simply linking the library '-lonig'
+ * will cause the bug to occur. Starting in PHP 7.4, Oniguruma is no longer
+ * bundled with ext/mbstring, but is linked in ('-lonig') so we cannot reliably
+ * use ERE here. Basic regular expressions (BRE) do not seem to be affected by
+ * this bug therefore we used BRE syntax for the container ID pattern matching.
+ *
+ * https://github.com/kkos/oniguruma/issues/233
+ * https://www.php.net/manual/en/mbstring.installation.php
+ */
+#define LINE_REGEX "^[0-9]\\{1,20\\}:[^:]*:.*$"  // Original ERE: "^[0-9]+:[^:]*:.+$"
 /* Examples of some valid container IDs.
  *
  * Docker
@@ -19,40 +30,83 @@
  * Fargate < 1.4
  * 11:something:/ecs/5a081c13-b8cf-4801-b427-f4601742204d/432624d2150b349fe35ba397284dea788c2bf66b885d14dfc1569b01890ca7da
  */
-#define CONTAINER_REGEX "[0-9a-f]{64}"
+#define CONTAINER_REGEX "[0-9a-f]\\{64\\}"  // Original ERE: "[0-9a-f]{64}"
 /* Example of a valid task ID.
  *
  * Fargate 1.4+
  * 1:name=systemd:/ecs/34dc0b5e626f2c5c4c5170e34b10e765-1234567890
  */
-#define TASK_REGEX "[0-9a-f]{32}-[0-9]+"
+#define TASK_REGEX "[0-9a-f]\\{32\\}-[0-9]\\{1,20\\}"  // Original ERE: "[0-9a-f]{32}-[0-9]+"
 
 #define MIN_ID_LEN DATADOG_PHP_CONTAINER_ID_MIN_LEN
 #define MAX_ID_LEN DATADOG_PHP_CONTAINER_ID_MAX_LEN
 
-static size_t dd_find_id_regex(regex_t *regex, char *buf, const char *line) {
-    size_t len;
-    regmatch_t pmatch[1];
+static void dd_extract_task_id(char *buf, const char *line) {
+    char *pos = (char *)line;
+    size_t len = strlen(line);
+    unsigned int buf_pos = 0;
 
-    if (regexec(regex, line, 1, pmatch, 0) != 0) return 0;
+    while ((size_t)(pos - line) < len) {
+        // [0-9a-f]{32}
+        while (isxdigit(pos[0]) && buf_pos < 32) {
+            buf[buf_pos++] = pos[0];
+            pos++;
+        }
 
-    len = (size_t)pmatch[0].rm_eo - pmatch[0].rm_so;
-    if (len < MIN_ID_LEN || len > MAX_ID_LEN) return 0;
+        // -
+        if (buf_pos != 32 || pos[0] != '-') {
+            buf_pos = 0;
+            pos++;
+            continue;
+        }
+        buf[buf_pos++] = '-';
+        pos++;
 
-    memcpy(buf, line + pmatch[0].rm_so, len);
-    buf[len] = '\0';
+        // [0-9]{1,20}
+        while (isdigit(pos[0]) && buf_pos < (32 + 1 + 20)) {
+            buf[buf_pos++] = pos[0];
+            pos++;
+        }
+        if (buf_pos <= (32 + 1)) {
+            buf_pos = 0;
+            pos++;
+            continue;
+        }
 
-    return len;
+        /* We have a valid task ID at this point so we can ignore the rest of
+         * the line.
+         */
+        break;
+    }
+
+    buf[buf_pos] = '\0';
+}
+
+static void dd_extract_container_id(char *buf, const char *line) {
+    char *pos = (char *)line;
+    size_t len = strlen(line);
+    unsigned int buf_pos = 0;
+
+    // [0-9a-f]{64}
+    while ((size_t)(pos - line) < len && buf_pos < 64) {
+        if (!isxdigit(pos[0])) {
+            buf_pos = 0;
+            pos++;
+            continue;
+        }
+        buf[buf_pos++] = pos[0];
+        pos++;
+    }
+
+    buf[buf_pos] = '\0';
 }
 
 void datadog_php_container_id(char *buf, const char *file) {
     FILE *fp;
-    char task_buf[MAX_ID_LEN + 1];
 
     if (buf == NULL) return;
 
     buf[0] = '\0';
-    task_buf[0] = '\0';
 
     if (file == NULL || file[0] == '\0' || (fp = fopen(file, "r")) == NULL) return;
 
@@ -70,9 +124,9 @@ void datadog_php_container_id(char *buf, const char *file) {
     regex_t line_regex = {0};
     regex_t task_regex = {0};
     regex_t container_regex = {0};
-    int l_res = regcomp(&line_regex, LINE_REGEX, REG_EXTENDED | REG_NOSUB);
-    int t_res = regcomp(&task_regex, TASK_REGEX, REG_EXTENDED);
-    int c_res = regcomp(&container_regex, CONTAINER_REGEX, REG_EXTENDED);
+    int l_res = regcomp(&line_regex, LINE_REGEX, REG_NOSUB);
+    int t_res = regcomp(&task_regex, TASK_REGEX, REG_NOSUB);
+    int c_res = regcomp(&container_regex, CONTAINER_REGEX, REG_NOSUB);
     if (l_res != 0 || t_res != 0 || c_res != 0) {
         regfree(&container_regex);
         regfree(&task_regex);
@@ -88,20 +142,26 @@ void datadog_php_container_id(char *buf, const char *file) {
         /* Match a valid cgroup line. */
         if (regexec(&line_regex, line, 0, NULL, 0) != 0) continue;
 
-        /* Once we find a task ID we can stop scanning the cgroup file since
-         * those take precedence over a standard container ID. In many examples
-         * such as Fargate 1.4+, the task ID is on the last line of the file
-         * after many lines of valid container IDs. It is for this reason that
-         * we must scan the file until the very end even after finding a valid
-         * container ID.
+        /* Normally we could just use 'regmatch_t' to obtain the results from
+         * this regex match, but unfortunately if Oniguruma <= 6.9.4 is linked
+         * in, the 'regmatch_t' will be mangled so we cannot use it here.
          */
-        size_t task_len = dd_find_id_regex(&task_regex, task_buf, line);
-        if (task_buf[0] != '\0') {
-            memcpy(buf, task_buf, task_len + 1);
+        if (regexec(&task_regex, line, 0, NULL, 0) == 0) {
+            dd_extract_task_id(buf, line);
+            /* We found a task ID which takes precedence over a standard
+             * container ID so we can stop scanning the file.
+             */
             break;
         }
-        if (buf[0] == '\0') {
-            dd_find_id_regex(&container_regex, buf, line);
+
+        if (buf[0] == '\0' && (regexec(&container_regex, line, 0, NULL, 0) == 0)) {
+            dd_extract_container_id(buf, line);
+            /* We found a valid container ID but we cannot stop scanning the
+             * file because there might be a task ID in the file (as is the
+             * case with Fargate 1.4+) and those take precedence over standard
+             * container IDs.
+             */
+            /* break; */
         }
     }
 
