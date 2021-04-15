d3bc9f01302a94bc94465507fc61a01968da050d
Sammy Kaye Powers <sammyk@datadoghq.com>
2021-04-15 13:26:27 -0700

    ZAI SAPI (#1204)
    * Add ZAI SAPI
    
    ZAI SAPI is a new test runner that can test software components that are
    tightly coupled to the Zend Engine.
    
    * Apply suggestions from code review
    
    Co-authored-by: Luca Abbati <luca.abbati@datadoghq.com>
    
    * Return -1 for INI entry error paths
    
    * Remove unbound limits from INI defaults
    
    Co-authored-by: Luca Abbati <luca.abbati@datadoghq.com>

---
+++ zend_abstract_interface/zai_sapi/php7/zai_sapi.c
@@ -0,0 +1,219 @@
+#include "../zai_sapi.h"
+
+#include <main/SAPI.h>
+#include <main/php_main.h>
+#include <main/php_variables.h>
+
+#include "../zai_sapi_ini.h"
+#include "../zai_sapi_io.h"
+
+#define DEFAULT_INI        \
+    "html_errors=0\n"      \
+    "implicit_flush=1\n"   \
+    "output_buffering=0\n" \
+    "\0"
+
+#define UNUSED(x) (void)(x)
+
+static ssize_t ini_entries_len = -1;
+
+static int zs_startup(sapi_module_struct *sapi_module) { return php_module_startup(sapi_module, NULL, 0); }
+
+static int zs_deactivate(void) { return SUCCESS; }
+
+static void zs_send_header(sapi_header_struct *sapi_header, void *server_context) {
+    UNUSED(sapi_header);
+    UNUSED(server_context);
+}
+
+static char *zs_read_cookies(void) { return NULL; }
+
+static void zs_register_variables(zval *track_vars_array) { php_import_environment_variables(track_vars_array); }
+
+static size_t zs_io_write_stdout(const char *str, size_t str_length) {
+    size_t len = zai_sapi_io_write_stdout(str, str_length);
+    if (len == 0) php_handle_aborted_connection();
+    return len;
+}
+
+#if PHP_VERSION_ID >= 70100
+static void zs_io_log_message(char *message, int syslog_type_int) {
+    UNUSED(syslog_type_int);
+    char buf[ZAI_SAPI_IO_ERROR_LOG_MAX_BUF_SIZE];
+    size_t len = zai_sapi_io_format_error_log(message, buf, sizeof buf);
+    /* We ignore the return because PHP does not care if this fails. */
+    (void)zai_sapi_io_write_stderr(buf, len);
+}
+#else
+static void zs_io_log_message(char *message) {
+    char buf[ZAI_SAPI_IO_ERROR_LOG_MAX_BUF_SIZE];
+    size_t len = zai_sapi_io_format_error_log(message, buf, sizeof buf);
+    /* We ignore the return because PHP does not care if this fails. */
+    (void)zai_sapi_io_write_stderr(buf, len);
+}
+#endif
+
+static sapi_module_struct zai_module = {
+    "zai",                     /* name */
+    "Zend Abstract Interface", /* pretty name */
+
+    zs_startup,                  /* startup */
+    php_module_shutdown_wrapper, /* shutdown */
+
+    NULL,          /* activate */
+    zs_deactivate, /* deactivate */
+
+    zs_io_write_stdout, /* unbuffered write */
+    zai_sapi_io_flush,  /* flush */
+
+    NULL, /* get uid */
+    NULL, /* getenv */
+
+    php_error, /* error handler */
+
+    NULL,           /* header handler */
+    NULL,           /* send headers handler */
+    zs_send_header, /* send header handler */
+
+    NULL,            /* read POST data */
+    zs_read_cookies, /* read Cookies */
+
+    zs_register_variables, /* register server variables */
+    zs_io_log_message,     /* Log message */
+    NULL,                  /* Get request time */
+    NULL,                  /* Child terminate */
+
+    NULL, /* php_ini_path_override   */
+#if PHP_VERSION_ID < 70100
+    NULL, /* block_interruptions     */
+    NULL, /* unblock_interruptions   */
+#endif
+    NULL, /* default_post_reader     */
+    NULL, /* treat_data              */
+    NULL, /* executable_location     */
+    0,    /* php_ini_ignore          */
+    0,    /* php_ini_ignore_cwd      */
+    NULL, /* get_fd                  */
+    NULL, /* force_http_10           */
+    NULL, /* get_target_uid          */
+    NULL, /* get_target_gid          */
+    NULL, /* input_filter            */
+    NULL, /* ini_defaults            */
+    0,    /* phpinfo_as_text;        */
+    NULL, /* ini_entries;            */
+    NULL, /* additional_functions    */
+    NULL  /* input_filter_init       */
+};
+
+bool zai_sapi_append_system_ini_entry(const char *key, const char *value) {
+    ssize_t len = zai_sapi_ini_entries_realloc_append(&zai_module.ini_entries, (size_t)ini_entries_len, key, value);
+    if (len <= ini_entries_len) {
+        /* Play it safe and free if writing failed. */
+        zai_sapi_ini_entries_free(&zai_module.ini_entries);
+        return false;
+    }
+    ini_entries_len = len;
+    return true;
+}
+
+#ifdef ZTS
+static void zs_tsrm_startup(void) {
+#if PHP_VERSION_ID >= 70400
+    php_tsrm_startup();
+#else
+    tsrm_startup(1, 1, 0, NULL);
+    (void)ts_resource(0);
+#endif
+    ZEND_TSRMLS_CACHE_UPDATE();
+}
+#endif
+
+bool zai_sapi_sinit(void) {
+#ifdef ZTS
+    zs_tsrm_startup();
+#endif
+
+#if (PHP_VERSION_ID >= 70125 && PHP_VERSION_ID < 70200) || (PHP_VERSION_ID >= 70214 && PHP_VERSION_ID < 70300) || \
+    (PHP_VERSION_ID >= 70301)
+    /* Due to php-src bug #71041, 'zend_signal_startup' was not exported to
+     * shared libs with 'ZEND_API' until PHP 7.1.25, 7.2.14, and 7.3.1+.
+     *
+     * https://bugs.php.net/bug.php?id=71041
+     * https://github.com/php/php-src/commit/11ddf76
+     */
+    zend_signal_startup();
+#endif
+
+    /* Initialize the SAPI globals (memset to '0'), and set up reentrancy. */
+    sapi_startup(&zai_module);
+
+    /* Do not chdir to the script's directory (equivalent to running the CLI
+     * SAPI with '-C').
+     */
+    SG(options) |= SAPI_OPTION_NO_CHDIR;
+
+    /* Allocate the initial SAPI INI settings. Append new INI settings to this
+     * with zai_sapi_append_system_ini_entry() before MINIT is run.
+     */
+    if ((ini_entries_len = zai_sapi_ini_entries_alloc(DEFAULT_INI, &zai_module.ini_entries)) == -1) return false;
+
+    /* Don't load any INI files (equivalent to running the CLI SAPI with '-n').
+     * This will prevent inadvertently loading any extensions that we did not
+     * intend to. It also gives us a consistent clean slate of INI settings.
+     */
+    zai_module.php_ini_ignore = 1;
+
+    /* Show phpinfo()/module info as plain text. */
+    zai_module.phpinfo_as_text = 1;
+
+    /* TODO: When we want to expose a function to userland for testing purposes
+     * (e.g. DDTrace\Testing\trigger_error()), we can add them as custom SAPI
+     * functions here. These functions will only exist in the ZAI SAPI for
+     * testing at the C unit test level and will not be shipped as a public
+     * userland API in the PHP tracer.
+     */
+    zai_module.additional_functions = NULL;
+
+    return true;
+}
+
+void zai_sapi_sshutdown(void) {
+    sapi_shutdown();
+#ifdef ZTS
+    tsrm_shutdown();
+#endif
+    zai_sapi_ini_entries_free(&zai_module.ini_entries);
+}
+
+bool zai_sapi_minit(void) {
+    if (zai_module.startup(&zai_module) == FAILURE) {
+        zai_sapi_sshutdown();
+        return false;
+    }
+    return true;
+}
+
+void zai_sapi_mshutdown(void) { php_module_shutdown(); }
+
+bool zai_sapi_rinit(void) {
+    if (php_request_startup() == FAILURE) {
+        return false;
+    }
+
+    SG(headers_sent) = 1;
+    SG(request_info).no_headers = 1;
+
+    php_register_variable("PHP_SELF", "-", NULL);
+
+    return true;
+}
+
+void zai_sapi_rshutdown(void) { php_request_shutdown((void *)0); }
+
+bool zai_sapi_spinup(void) { return zai_sapi_sinit() && zai_sapi_minit() && zai_sapi_rinit(); }
+
+void zai_sapi_spindown(void) {
+    zai_sapi_rshutdown();
+    zai_sapi_mshutdown();
+    zai_sapi_sshutdown();
+}
---
+++ zend_abstract_interface/zai_sapi/zai_sapi.h
@@ -0,0 +1,73 @@
+/*                   ____   __   __    ____   __   ____  __
+ *                  (__  ) / _\ (  )  / ___) / _\ (  _ \(  )
+ *                   / _/ /    \ )(   \___ \/    \ ) __/ )(
+ *                  (____)\_/\_/(__)  (____/\_/\_/(__)  (__)
+ *
+ * ZAI SAPI is designed to test software components that are tightly coupled to
+ * the Zend Engine.
+ *
+ * ZAI SAPI is a fork of the embed SAPI:
+ * https://github.com/php/php-src/tree/master/sapi/embed
+ */
+#ifndef ZAI_SAPI_H
+#define ZAI_SAPI_H
+
+#include <main/php.h>
+#include <stdbool.h>
+
+#if PHP_VERSION_ID >= 70000 && defined(ZTS)
+ZEND_TSRMLS_CACHE_EXTERN()
+#endif
+
+/* Initializes the SAPI, modules, and request. */
+bool zai_sapi_spinup(void);
+/* Shuts down the request, modules, and SAPI. */
+void zai_sapi_spindown(void);
+
+/* SINIT: SAPI initialization
+ *
+ * Characterized by sapi_startup().
+ *   - Initialize SAPI globals
+ *   - Allocate SAPI INI settings
+ *   - Set startup behavior
+ *   - Start up ZTS & signals
+ */
+bool zai_sapi_sinit(void);
+void zai_sapi_sshutdown(void);
+
+/* MINIT: Module initialization
+ *
+ * Basically a wrapper for php_module_startup().
+ */
+bool zai_sapi_minit(void);
+void zai_sapi_mshutdown(void);
+
+/* RINIT: Request initialization.
+ *
+ * Characterized by php_request_startup().
+ *   - Set late-stage configuration
+ *   - Send headers
+ */
+bool zai_sapi_rinit(void);
+void zai_sapi_rshutdown(void);
+
+/* Appends an INI entry to the existing 'ini_entries'.
+ *
+ *   zai_sapi_append_system_ini_entry("extension", "ddtrace.so");
+ *
+ * Must be called:
+ *   - After SAPI initialization zai_sapi_sinit()
+ *   - Before module initialization zai_sapi_minit()
+ *
+ * We cannot use PHP's zend_alter_ini_entry_*() API to modify INI entries
+ * before MINIT because the configuration hash table does not exist before
+ * MINIT. The SAPI 'ini_entries' (member of the 'sapi_module_struct') and any
+ * INI settings from '.ini' files are parsed and added to the configuration
+ * hash table in php_init_config() during MINIT. If we need to add an INI
+ * setting that is accessed very early in the startup process (i.e.
+ * 'disable_functions' which is read as part of MINIT), then we must reallocate
+ * our C string of SAPI 'ini_entries' and append to it before MINIT occurs.
+ */
+bool zai_sapi_append_system_ini_entry(const char *key, const char *value);
+
+#endif  // ZAI_SAPI_H
