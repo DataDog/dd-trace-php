742f5baa5350339e68b68f26a18cb2dacc83c451
Sammy Powers <sammyk@datadoghq.com>
2021-05-17 16:19:56 -0400

    Fix ZAI sandbox compiler warning

--- zend_abstract_interface/sandbox/php8/sandbox.c
+++ zend_abstract_interface/sandbox/php8/sandbox.c
@@ -4,7 +4,30 @@ extern inline void zai_sandbox_open(zai_sandbox *sandbox);
 extern inline void zai_sandbox_close(zai_sandbox *sandbox);
 
 extern inline void zai_sandbox_error_state_backup(zai_error_state *es);
-extern inline void zai_sandbox_error_state_restore(zai_error_state *es);
+
+/* In PHP 8, we cannot extern inline zai_sandbox_error_state_restore() because
+ * zend_string_release() is static which generates the following compiler
+ * warning:
+ *
+ *   "warning: 'zend_string_release' is static but used in inline function
+ *    'zai_sandbox_error_state_restore' which is not static"
+ */
+void zai_sandbox_error_state_restore(zai_error_state *es) {
+    if (PG(last_error_message)) {
+        if (PG(last_error_message) != es->message) {
+            zend_string_release(PG(last_error_message));
+        }
+        if (PG(last_error_file) != es->file) {
+            free(PG(last_error_file));
+        }
+    }
+    zend_restore_error_handling(&es->error_handling);
+    PG(last_error_type) = es->type;
+    PG(last_error_message) = es->message;
+    PG(last_error_file) = es->file;
+    PG(last_error_lineno) = es->lineno;
+    EG(error_reporting) = es->error_reporting;
+}
 
 extern inline void zai_sandbox_exception_state_backup(zai_exception_state *es);
 extern inline void zai_sandbox_exception_state_restore(zai_exception_state *es);
