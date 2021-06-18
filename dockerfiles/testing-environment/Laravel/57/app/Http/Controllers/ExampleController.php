13a5ff7ab3ecdb55fdbd2ab05e20027034b8c61d
Luca Abbati <luca.abbati@datadoghq.com>
2021-06-18 11:14:46 +0200

    [APMPHP-503] fix L8 PHP 8 instability on PHP 8

--- dockerfiles/testing-environment/Laravel/57/app/Http/Controllers/ExampleController.php
+++ dockerfiles/testing-environment/Laravel/57/app/Http/Controllers/ExampleController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use Illuminate\Foundation\Application;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\DB;
 
@@ -9,6 +11,6 @@ class ExampleController extends BaseController
 {
     public function example()
     {
-        return "Hi from Laravel 5.7 app!\n";
+        return "Hi from Laravel " . Application::VERSION . " app on PHP " . \phpversion() . "!\n";
     }
 }
--- dockerfiles/testing-environment/Laravel/8x/app/Http/Controllers/Controller.php
+++ dockerfiles/testing-environment/Laravel/8x/app/Http/Controllers/Controller.php
@@ -2,17 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Application;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
     public function example()
     {
-        return "Hi from Laravel 8.x app on PHP " . \phpversion() . "!\n";
+        return "Hi from Laravel " . Application::VERSION . " app on PHP " . \phpversion() . "!\n";
     }
 }
