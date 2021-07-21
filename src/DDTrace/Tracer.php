ad139d7f4422d8999218e7b6d7ccd75b6a42cea4
Luca Abbati <luca.abbati@datadoghq.com>
2021-07-21 16:21:38 +0200

    version bump 0.62.0 (#1284)

--- src/DDTrace/Tracer.php
+++ src/DDTrace/Tracer.php
@@ -30,7 +30,7 @@ final class Tracer implements TracerInterface
      * Must begin with a number for Debian packaging requirements
      * Must use single-quotes for packaging script to work
      */
-    const VERSION = '1.0.0-nightly';
+    const VERSION = '0.62.0';
 
     /**
      * @var Span[][]
