95b53e03b78bbc0059cd619293b4abea806b2d87
Bob Weinand <bob.weinand@datadoghq.com>
2021-07-28 15:30:49 +0200

    Version bump 0.62.1
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- src/DDTrace/Tracer.php
+++ src/DDTrace/Tracer.php
@@ -30,7 +30,7 @@ final class Tracer implements TracerInterface
      * Must begin with a number for Debian packaging requirements
      * Must use single-quotes for packaging script to work
      */
-    const VERSION = '0.62.0';
+    const VERSION = '0.62.1';
 
     /**
      * @var Span[][]
