ef64bad1e591823dad41208e8edbf7b513e8c8d6
Luca Abbati <luca.abbati@datadoghq.com>
2021-08-19 14:15:42 +0200

    split _generated.php into _generated_api.php and _generated_internal.php

--- bridge/autoload.php
+++ bridge/autoload.php
@@ -2,11 +2,20 @@
 
 if (getenv('DD_AUTOLOAD_NO_COMPILE') === 'true') {
     // Development
-    $files = include __DIR__ . '/_files.php';
-    foreach ($files as $file) {
+    if (!class_exists('DDTrace\Contracts\Tracer')) {
+        $apiFiles = include __DIR__ . '/_files_api.php';
+        foreach ($apiFiles as $file) {
+            require $file;
+        }
+    }
+    $internalFiles = include __DIR__ . '/_files_internal.php';
+    foreach ($internalFiles as $file) {
         require $file;
     }
 } else {
     // Production
-    require_once __DIR__ . '/_generated.php';
+    if (!class_exists('DDTrace\Contracts\Tracer')) {
+        require_once __DIR__ . '/_generated_api.php';
+    }
+    require_once __DIR__ . '/_generated_internal.php';
 }
