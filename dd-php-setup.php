fa3dda743e69e2b88954c6ccfbc0167c4b1c28df
Luca Abbati <luca.abbati@datadoghq.com>
2021-09-28 11:12:47 +0200

    installer: remove check to exit if ddtrace module is detected

--- dd-php-setup.php
+++ dd-php-setup.php
@@ -10,9 +10,9 @@ const SUPPORTED_PHP_VERSIONS = ['5.4', '5.5', '5.6', '7.0', '7.1', '7.2', '7.3',
 
 function main()
 {
-    if (extension_loaded('ddtrace')) {
-        print_error_and_exit('The installer cannot be executed if ddtrace extension is installed. Did you forget to run it through the dd-php-setup.sh wrapper?');
-    }
+    // if (extension_loaded('ddtrace')) {
+    //     print_error_and_exit('The installer cannot be executed if ddtrace extension is installed. Did you forget to run it through the dd-php-setup.sh wrapper?');
+    // }
 
     if (is_truthy(getenv('DD_TEST_EXECUTION'))) {
         return;
