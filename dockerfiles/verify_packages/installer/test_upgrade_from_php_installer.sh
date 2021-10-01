9784a399c9e90c1e21a050ebd8753c4ef19c80a3
Luca Abbati <luca.abbati@datadoghq.com>
2021-10-01 12:26:56 +0200

    installer: test and fix upgrade process from php installer to php installer

---
+++ dockerfiles/verify_packages/installer/test_upgrade_from_php_installer.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -e
+
+. "$(dirname ${0})/utils.sh"
+
+# Initially no ddtrace
+assert_no_ddtrace
+
+# Install using the php installer
+old_version="0.64.0"
+php dd-library-php-setup.php --php-bin=php --tracer-version="${old_version}"
+assert_ddtrace_version "${old_version}"
+
+# Upgrade using the php installer
+new_version="0.65.1"
+php dd-library-php-setup.php --php-bin=php --tracer-version="${new_version}"
+assert_ddtrace_version "${new_version}"
