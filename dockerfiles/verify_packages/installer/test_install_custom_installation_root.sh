8f84672d1f930db3f8fbff764a36d428eecb9ba6
Luca Abbati <luca.abbati@datadoghq.com>
2021-10-04 16:37:36 +0200

    installer: minor comments

--- dockerfiles/verify_packages/installer/test_install_custom_installation_root.sh
+++ dockerfiles/verify_packages/installer/test_install_custom_installation_root.sh
@@ -14,4 +14,6 @@ new_version="0.65.1"
 php dd-library-php-setup.php --php-bin=php --tracer-version="${new_version}" --install-dir=/
 assert_ddtrace_version "${new_version}"
 assert_file_exists /dd-library/${new_version}/dd-trace-sources/bridge/dd_wrap_autoloader.php
+
+# Making sure a clean install to root / does not rm -rf everything
 assert_file_exists /usr/bin/tail
