40d9639eac4893b0772ba809804c71b6b0f2cc8b
Luca Abbati <luca.abbati@datadoghq.com>
2021-10-06 12:28:20 +0200

    install debug symbols in L57 rel env images

--- dockerfiles/testing-environment/Laravel57/docker-scripts/install-php-and-composer.sh
+++ dockerfiles/testing-environment/Laravel57/docker-scripts/install-php-and-composer.sh
@@ -7,16 +7,29 @@ apt update
 apt install -y software-properties-common
 add-apt-repository -y ppa:ondrej/php
 
+# Enabling ondrej debug symbols repo
+sed -i 's/deb \(.\+\)/deb \1 main\/debug/g' /etc/apt/sources.list.d/ondrej-ubuntu-php-bionic.list
+
+cat /etc/apt/sources.list.d/ondrej-ubuntu-php-bionic.list
+
 # Installing PHP
-apt install -y \
+apt update && apt install -y \
     php${PHP_VERSION}-cli \
+    php${PHP_VERSION}-cli-dbgsym \
     php${PHP_VERSION}-curl \
+    php${PHP_VERSION}-curl-dbgsym \
     php${PHP_VERSION}-fpm \
+    php${PHP_VERSION}-fpm-dbgsym \
     php${PHP_VERSION}-mbstring \
+    php${PHP_VERSION}-mbstring-dbgsym \
     php${PHP_VERSION}-mysql \
+    php${PHP_VERSION}-mysql-dbgsym \
     php${PHP_VERSION}-opcache \
+    php${PHP_VERSION}-opcache-dbgsym \
     php${PHP_VERSION}-xml \
-    php${PHP_VERSION}-zip
+    php${PHP_VERSION}-xml-dbgsym \
+    php${PHP_VERSION}-zip \
+    php${PHP_VERSION}-zip-dbgsym
 # remove unused php-fpm files that will be provided from outside
 rm -rf \
     /etc/php/${PHP_VERSION}/fpm/php-fpm.conf \
