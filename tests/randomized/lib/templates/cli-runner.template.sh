7dfd1fa802d17dc0006b7adabad49cb5f0c50a98
Luca Abbati <luca.abbati@datadoghq.com>
2021-10-26 14:52:37 +0200

    Reduce # of repetitions in CLI randomized tests

--- tests/randomized/lib/templates/cli-runner.template.sh
+++ tests/randomized/lib/templates/cli-runner.template.sh
@@ -4,5 +4,5 @@ set -e
 
 {{envs}} php {{inis}} -d error_log=/var/log/php/error.log /var/www/html/public/long_running_script.php \
     --seed={{seed}} \
-    --repeat=1000 \
+    --repeat=100 \
     --file=/results/memory.out
