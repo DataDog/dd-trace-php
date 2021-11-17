fbfe71a6df9b6d9c9db054b7e669b36f6a04400f
Bob Weinand <bob.weinand@datadoghq.com>
2021-11-17 19:31:31 +0100

    ...
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- zend_abstract_interface/json/json.h
+++ zend_abstract_interface/json/json.h
@@ -14,12 +14,12 @@
 extern void (*php_json_encode)(smart_str *buf, zval *val, int options TSRMLS_DC);
 extern void (*php_json_decode_ex)(zval *return_value, char *str, int str_len, int options, long depth TSRMLS_DC);
 
-static inline void php_json_decode(zval *return_value, char *str, int str_len, zend_bool assoc, long depth TSRMLS_DC)
-{
+static inline void php_json_decode(zval *return_value, char *str, int str_len, zend_bool assoc, long depth TSRMLS_DC) {
     php_json_decode_ex(return_value, str, str_len, assoc ? PHP_JSON_OBJECT_AS_ARRAY : 0, depth TSRMLS_CC);
 }
 #else
 #include <stdbool.h>
+
 #include "zend_smart_str_public.h"
 
 extern int (*php_json_encode)(smart_str *buf, zval *val, int options);
@@ -30,7 +30,6 @@ static inline int php_json_decode(zval *return_value, const char *str, int str_l
     return php_json_decode_ex(return_value, str, str_len, assoc ? PHP_JSON_OBJECT_AS_ARRAY : 0, depth);
 }
 #endif
-
 
 void zai_json_setup_bindings(void);
 
