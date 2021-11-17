3ba8733ba10892226a7736ff80a7ef09613cbd31
Bob Weinand <bob.weinand@datadoghq.com>
2021-11-17 19:14:49 +0100

    ...
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- zend_abstract_interface/json/json.h
+++ zend_abstract_interface/json/json.h
@@ -2,27 +2,38 @@
 #define ZAI_JSON_H
 
 #include "php.h"
-#include <stdbool.h>
-#include "../zai_compat.h"
-#if PHP_VERSION_ID < 70000
-#include "ext/standard/php_smart_str.h"
-#else
-#include "zend_smart_str_public.h"
-#endif
 
 #define php_json_encode _dd_json_encode
 #define php_json_decode_ex _dd_json_decode_ex
 
 #define PHP_JSON_OBJECT_AS_ARRAY (1 << 0)
 
-extern int (*php_json_encode)(smart_str *buf, zval *val, int options ZAI_TSRMLS_CC);
+#if PHP_VERSION_ID < 70000
+#include "ext/standard/php_smart_str.h"
+
+void php_json_encode(smart_str *buf, zval *val, int options TSRMLS_DC);
+void php_json_decode_ex(zval *return_value, char *str, int str_len, int options, long depth TSRMLS_DC);
+
+static inline void php_json_decode(zval *return_value, char *str, int str_len, zend_bool assoc, long depth TSRMLS_DC)
+{
+    php_json_decode_ex(return_value, str, str_len, assoc ? PHP_JSON_OBJECT_AS_ARRAY : 0, depth TSRMLS_CC);
+}
+#else
+#include <stdbool.h>
+#include "zend_smart_str_public.h"
+
+extern int (*php_json_encode)(smart_str *buf, zval *val, int options);
 extern int (*php_json_decode_ex)(zval *return_value, const char *str, size_t str_len, zend_long options,
-                                 zend_long depth ZAI_TSRMLS_CC);
+                                 zend_long depth);
 
 static inline int php_json_decode(zval *return_value, const char *str, int str_len, bool assoc, zend_long depth) {
     return php_json_decode_ex(return_value, str, str_len, assoc ? PHP_JSON_OBJECT_AS_ARRAY : 0, depth);
 }
+#endif
+
 
 void zai_json_setup_bindings(void);
+
+#undef zend_long
 
 #endif  // ZAI_JSON_H
