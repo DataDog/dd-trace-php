831a1fc2fede9268f410465ffc2b0a81b2789206
Joe Watkins <krakjoe@php.net>
2021-11-24 13:05:49 +0100

    ZAI: config/json

--- zend_abstract_interface/zai_compat.h
+++ zend_abstract_interface/zai_compat.h
@@ -6,6 +6,8 @@
 #define ZAI_TSRMLS_CC
 #define ZAI_TSRMLS_D
 #define ZAI_TSRMLS_DC
+#define ZAI_TSRMLS_FETCH()
+
 #define smart_str_length(str) (str)->s->len
 #define smart_str_value(str) ((char *)(str)->s->val)
 #else
@@ -13,6 +15,8 @@
 #define ZAI_TSRMLS_CC TSRMLS_CC
 #define ZAI_TSRMLS_D TSRMLS_D
 #define ZAI_TSRMLS_DC TSRMLS_DC
+#define ZAI_TSRMLS_FETCH() TSRMLS_FETCH()
+
 #define smart_str_length(str) (str)->len
 #define smart_str_value(str) ((char *)(str)->c)
 #endif
