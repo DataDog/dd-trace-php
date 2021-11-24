c6c4042ec2926dc15a2ad23fc8019223dc262057
Joe Watkins <krakjoe@php.net>
2021-11-24 12:42:52 +0100

    ZAI: JSON (#1378)
    * ZAI: JSON
    
    Used by #1366

---
+++ zend_abstract_interface/json/tests/json.cc
@@ -0,0 +1,54 @@
+extern "C" {
+#include "json/json.h"
+#include "zai_sapi/zai_sapi.h"
+#include "zai_sapi/zai_sapi_extension.h"
+}
+
+#include <catch2/catch.hpp>
+#include <cstdlib>
+#include <cstring>
+
+#define TEST(name, code) TEST_CASE(name, "[zai_json]") { \
+        REQUIRE(zai_sapi_sinit()); \
+        REQUIRE(zai_sapi_minit()); \
+        zai_json_setup_bindings(); \
+        REQUIRE(zai_sapi_rinit()); \
+        ZAI_SAPI_TSRMLS_FETCH(); \
+        ZAI_SAPI_ABORT_ON_BAILOUT_OPEN() \
+        { code } \
+        ZAI_SAPI_ABORT_ON_BAILOUT_CLOSE() \
+        zai_sapi_spindown(); \
+    }
+
+TEST("encode", {
+    smart_str buf = {0};
+    zval val;
+
+    ZVAL_LONG(&val, 42);
+
+    php_json_encode(&buf, &val, 0 ZAI_TSRMLS_CC);
+
+    REQUIRE(smart_str_length(&buf) > 0);
+
+    smart_str_free(&buf);
+})
+
+TEST("decode", {
+    smart_str buf = {0};
+    zval val;
+
+    ZVAL_LONG(&val, 42);
+
+    php_json_encode(&buf, &val, 0 ZAI_TSRMLS_CC);
+
+    REQUIRE(smart_str_length(&buf));
+
+    ZVAL_NULL(&val);
+
+    php_json_decode(&val, smart_str_value(&buf), smart_str_length(&buf), 0, 1 ZAI_TSRMLS_CC);
+
+    REQUIRE(Z_TYPE(val) == IS_LONG);
+    REQUIRE(Z_LVAL(val) == 42);
+
+    smart_str_free(&buf);
+});
