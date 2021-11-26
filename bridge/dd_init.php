d6ae539ef29beed0e980601e3793e26da09a77a3
Luca Abbati <luca.abbati@datadoghq.com>
2021-11-26 19:15:04 +0100

    Remove Bootstrap.php

--- bridge/dd_init.php
+++ bridge/dd_init.php
@@ -64,5 +64,4 @@ require __DIR__ . '/autoload.php';
 // Optional classes and functions
 require __DIR__ . '/dd_register_optional_deps_autoloader.php';
 
-Bootstrap::tracerOnce();
 IntegrationsLoader::load();
