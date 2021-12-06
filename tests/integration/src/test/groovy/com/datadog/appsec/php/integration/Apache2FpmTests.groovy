fadd6aae5fcc744b5e6f6a4274f1c148a4d640f9
Gustavo Lopes <mail@geleia.net>
2021-12-06 14:56:38 +0000

    Add test for fpm -i using enabled_on_cli

--- tests/integration/src/test/groovy/com/datadog/appsec/php/integration/Apache2FpmTests.groovy
+++ tests/integration/src/test/groovy/com/datadog/appsec/php/integration/Apache2FpmTests.groovy
@@ -28,4 +28,34 @@ class Apache2FpmTests implements CommonTests {
                     phpVariant: variant,
                     tracerVersion: tracerVersion
             )
+
+
+    @Test
+    void 'php-fpm -i uses enabled_on_cli'() {
+        ExecResult res = CONTAINER.execInContainer(
+                'bash', '-c',
+                'php-fpm -d extension=ddtrace.so -d extension=ddappsec.so ' +
+                        '-d ddappsec.enabled=1 ' +
+                        '-d ddappsec.helper_socket_path=/tmp/foo.sock ' +
+                        '-d ddappsec.helper_lock_path=/tmp/foo.lock ' +
+                        '-i')
+        if (res.exitCode != 0) {
+            throw new AssertionError("Failed executing php-fpm -i: $res.stderr")
+        }
+        res = CONTAINER.execInContainer('test', '-S', '/tmp/foo.sock')
+        assert res.exitCode != 0
+
+        res = CONTAINER.execInContainer(
+                'bash', '-c',
+                'php-fpm -d extension=ddtrace.so -d extension=ddappsec.so ' +
+                        '-d ddappsec.enabled_on_cli=1 ' +
+                        '-d ddappsec.helper_socket_path=/tmp/foo.sock ' +
+                        '-d ddappsec.helper_lock_path=/tmp/foo.lock ' +
+                        '-i')
+        if (res.exitCode != 0) {
+            throw new AssertionError("Failed executing php-fpm -i: $res.stderr")
+        }
+        res = CONTAINER.execInContainer('test', '-S', '/tmp/foo.sock')
+        assert res.exitCode == 0
+    }
 }
