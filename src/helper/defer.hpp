b0984115fc53ac4b99a15e51072afb02193cc432
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2021-12-07 17:35:47 +0000

    Updated defer - tidy

--- src/helper/defer.hpp
+++ src/helper/defer.hpp
@@ -3,12 +3,17 @@
 //
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2021 Datadog, Inc.
+#include <utility>
 
 namespace dds {
 
 template <typename T>
 struct defer {
-    defer(T &&r_): runnable(std::move(r_)) {}
+    explicit defer(T &&r_): runnable(std::move(r_)) {}
+    defer(const defer &) = delete;
+    defer& operator=(const defer&) = delete;
+    defer(defer &&) = delete;
+    defer& operator=(defer&&) = delete;
     ~defer() { runnable(); }
     T runnable;
 };
