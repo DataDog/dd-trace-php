1908b096349e95b6bdc9a2eb6f90124fe3b3736b
Luca Abbati <luca.abbati@datadoghq.com>
2021-12-15 15:57:35 +0100

    Add test for new settings added to the installer

---
+++ dockerfiles/verify_packages/installer/test_install_add_missing_ini_settings.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+set -e
+
+. "$(dirname ${0})/utils.sh"
+
+# Initially no ddtrace
+assert_no_ddtrace
+
+# Install using the php installer
+new_version="0.65.1"
+php dd-library-php-setup.php --php-bin php --tracer-version "${new_version}"
+assert_ddtrace_version "${new_version}"
+
+ini_file="$(get_php_conf_dir)/98-ddtrace.ini"
+
+assert_file_contains "${ini_file}" 'datadog.trace.request_init_hook'
+assert_file_contains "${ini_file}" 'datadog.version'
+
+# Removing an enabled property and a commented out property
+sed -i 's/datadog\.trace\.request_init_hook.*//g' "${ini_file}"
+sed -i 's/datadog\.version.*//g' "${ini_file}"
+
+assert_file_not_contains "${ini_file}" 'datadog.trace.request_init_hook'
+assert_file_not_contains "${ini_file}" 'datadog.version'
+
+php dd-library-php-setup.php --php-bin php --tracer-version "${new_version}"
+
+assert_file_contains "${ini_file}" 'datadog.trace.request_init_hook'
+assert_file_contains "${ini_file}" 'datadog.version'
+
