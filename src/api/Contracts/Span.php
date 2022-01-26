c10150b674900aa0606cc50ea2b04f590ff90721
Bob Weinand <bob.weinand@datadoghq.com>
2022-01-26 15:04:22 +0100

    Fix Span::setError docblock
    Closes #1226.

--- src/api/Contracts/Span.php
+++ src/api/Contracts/Span.php
@@ -117,7 +117,7 @@ interface Span
      * updated and the error.Error() string is included with a default tag key.
      * If the Span has been finished, it will not be modified by this method.
      *
-     * @param \Throwable|\Exception|bool|string|null $error
+     * @param \Throwable|\Exception|bool|null $error
      * @throws \InvalidArgumentException
      */
     public function setError($error);
