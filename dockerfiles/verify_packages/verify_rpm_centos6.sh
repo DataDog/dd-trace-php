a5b3f6c12eb16884df5bda2053b7a91cb954824f
Luca Abbati <luca.abbati@datadoghq.com>
2022-01-28 17:29:35 +0100

    Fix paths resolution in centos now that we have to load bundles

--- dockerfiles/verify_packages/verify_rpm_centos6.sh
+++ dockerfiles/verify_packages/verify_rpm_centos6.sh
@@ -15,7 +15,7 @@ for phpVer in $(ls ${PHP_INSTALL_DIR}); do
     INSTALL_TYPE="${INSTALL_TYPE:-php_installer}"
     if [ "$INSTALL_TYPE" = "native_package" ]; then
         echo "Installing dd-trace-php using the OS-specific package installer"
-        rpm -Uvh /build_src/build/packages/*.rpm
+        rpm -Uvh build/packages/*.rpm
         php --ri=ddtrace
 
         # Uninstall the tracer
@@ -23,7 +23,7 @@ for phpVer in $(ls ${PHP_INSTALL_DIR}); do
         rm -f /opt/datadog-php/etc/ddtrace.ini
     else
         echo "Installing dd-trace-php using the new PHP installer"
-        installable_bundle=$(find "$(pwd)/build/packages" -maxdepth 1 -name 'dd-library-php-*-x86_64-linux-gnu.tar.gz')
+        installable_bundle=$(find "build/packages" -maxdepth 1 -name 'dd-library-php-*-x86_64-linux-gnu.tar.gz')
         php datadog-setup.php --file "$installable_bundle" --php-bin all
     fi
 done
