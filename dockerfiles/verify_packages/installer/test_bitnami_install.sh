ee420e17274a6232c18b936f7f806c598eb98bd7
Luca Abbati <luca.abbati@datadoghq.com>
2022-03-08 15:29:28 +0100

    Add bitnami installation check

---
+++ dockerfiles/verify_packages/installer/test_bitnami_install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -e
+
+. "$(dirname ${0})/utils.sh"
+
+# Initially no ddtrace
+assert_no_ddtrace
+
+# Install using the php installer
+new_version="0.68.0"
+generate_installers "${new_version}"
+php ./build/packages/datadog-setup.php --php-bin php
+assert_ddtrace_version "${new_version}"
+
+assert_request_init_hook_exists
