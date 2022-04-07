0d052b9a2b5e491e8a03d12255ebd8d4d4f1137a
Luca Abbati <luca.abbati@datadoghq.com>
2022-04-07 16:33:30 +0200

    More work on executor

--- tooling/bin/run-in-docker-executor.sh
+++ tooling/bin/run-in-docker-executor.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
-set -euo pipefail
-IFS=$'\n\t'
-
-echo "Hello everyone"
-exit
+set -eux
 
 php_variant=$1
 php_major_minor=$(php -r 'echo PHP_MAJOR_VERSION . "." . PHP_MINOR_VERSION;')
 
 switch-php $php_variant
-
 php -v
+
+make sudo debug install install_ini BUILD_DIR=tmp/build_extension
+
+shift
+$(@)
