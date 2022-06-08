2de9fa2a9e2bafe827bcfee0d05c0b7fe6553f63
Levi Morrison <levi.morrison@datadoghq.com>
2022-06-08 14:21:37 -0600

    f

--- zend_abstract_interface/symbols/symbols.h
+++ zend_abstract_interface/symbols/symbols.h
@@ -12,11 +12,11 @@
  *
  * zai_symbol_new is a single interface for object construction
  */
+#include <php.h>
 #include <stdbool.h>
 #include <stdint.h>
 
 #include "../zai_string/string.h"
-#include "php.h"
 #include "zai_compat.h"
 
 // clang-format off
