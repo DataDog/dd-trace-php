522bbaa3bede20767817a76bad2f988c8b0b2dce
Luca Abbati <luca.abbati@datadoghq.com>
2022-06-13 11:23:30 -0400

    Fix nginx and apache build/configuration

--- tests/randomized/generate-scenarios.php
+++ tests/randomized/generate-scenarios.php
@@ -115,7 +115,7 @@ function generateOne($scenarioSeed, array $restrictedPHPVersions)
         [
             'identifier' => $identifier,
             'scenario_folder' => $scenarioFolder,
-            'image' => "datadog/dd-trace-ci:php-randomizedtests-dev-$selectedOs-$selectedPhpVersion",
+            'image' => "datadog/dd-trace-ci:php-randomizedtests-$selectedOs-$selectedPhpVersion-1",
             'installation_method' => $selectedInstallationMethod,
             'project_root' => '../../../../',
         ]
