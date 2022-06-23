5504cea45e132adb81e143c9b4d7ab010785ac1b
Luca Abbati <luca.abbati@datadoghq.com>
2022-06-23 12:40:34 +0200

    Revert "Manually build mysql-server to support arm64"
    This reverts commit cfba3b24ca0b1343be0672599353c01b626f9e87.

--- dockerfiles/services/mysql/init.sql
+++
@@ -1,9 +0,0 @@
-create table users (
-    id integer not null primary key auto_increment,
-    email varchar(100) not null unique,
-    name varchar(100),
-    password varchar(100),
-    remember_token varchar(100),
-    updated_at timestamp,
-    created_at timestamp
-);
