5b861ea63c1301dbc4ec060b5b846319b44740d3
Bob Weinand <bob.weinand@datadoghq.com>
2022-07-13 12:42:18 +0200

    Enable JIT in randomized tests
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- tests/randomized/config/inis.php
+++ tests/randomized/config/inis.php
@@ -1,9 +1,10 @@
 <?php
 
-// Values fron this array might be selected and set. When an ini value from this list is selected,
+// Values from this array might be selected and set. When an ini value from this list is selected,
 // then there is an equal probability that any of the assigned values from this array can be set.
 // NOTE: flags should be boolean values and are converted while generating the file to the proper value.
 const INIS = [
     'opcache.enable' => [false],
+    'opcache.jit_buffer_size' => ['256M'],
     'zend_extension' => ['datadog-profiling.so'],
 ];
