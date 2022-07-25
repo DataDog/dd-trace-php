74efc138a08cbcbf4b26aa4ab56a31ded923d87d
Levi Morrison <levi.morrison@datadoghq.com>
2022-07-25 10:17:13 -0600

    Be more explicit about profiler deps

--- datadog-setup.php
+++ datadog-setup.php
@@ -68,10 +68,17 @@ function install($options)
     // Checking required libraries
     check_library_prerequisite_or_exit('libcurl');
     if (is_alpine()) {
+        check_library_prerequisite_or_exit('libexecinfo');
         if (is_truthy($options[OPT_ENABLE_PROFILING])) {
             check_library_prerequisite_or_exit('libgcc_s');
         }
-        check_library_prerequisite_or_exit('libexecinfo');
+    } else {
+        if (is_truthy($options[OPT_ENABLE_PROFILING])) {
+            check_library_prerequisite_or_exit('libdl.so');
+            check_library_prerequisite_or_exit('libgcc_s');
+            check_library_prerequisite_or_exit('libpthread');
+            check_library_prerequisite_or_exit('librt');
+        }
     }
 
     // Picking the right binaries to install the library
