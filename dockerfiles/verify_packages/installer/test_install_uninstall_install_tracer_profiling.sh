3de065ceb746824000df04f612d5a86ddefe26d4
Levi Morrison <levi.morrison@datadoghq.com>
2022-07-25 16:45:52 -0600

    Adjust profiling install-uninstall-reinstall test

--- dockerfiles/verify_packages/installer/test_install_uninstall_install_tracer_profiling.sh
+++ dockerfiles/verify_packages/installer/test_install_uninstall_install_tracer_profiling.sh
@@ -10,26 +10,45 @@ assert_no_ddtrace
 extension_dir="$(php -i | grep '^extension_dir' | awk '{ print $NF }')"
 ini_dir="$(php -i | grep '^Scan' | awk '{ print $NF }')"
 
-# Install using the php installer
-new_version="0.75.0"
-generate_installers "${new_version}"
-php ./build/packages/datadog-setup.php --php-bin php
 
-# Uninstall
-php ./build/packages/datadog-setup.php --php-bin php --uninstall
+# Get a relased version of the setup script. I've picked a recent one that at
+# this time doesn't have the switch from zend_extension to extension for the
+# profiling module.
+released_version="0.75.0"
+released_version_sha="76506c5ec222b2975333e1bae85f8b91d7c02eb9ccd4dcc807cdf2f23c667785"
+cd /tmp
+curl -OL https://github.com/DataDog/dd-trace-php/releases/download/${released_version}/datadog-setup.php
+echo "${released_version_sha}  datadog-setup.php" | sha256sum -c
+php datadog-setup.php --php-bin php
+rm -v datadog-setup.php
+cd -
+
+assert_ddtrace_version "${released_version}"
+
+# Parse current version numbers and generate an installer.
+trace_version=$(awk -F\' '/const VERSION/ {print $2}' < src/DDTrace/Tracer.php)
+profiling_version=$(awk -F\" '/^version[ \t]*=/ {print $2}' < profiling/Cargo.toml)
+generate_installers "${trace_version}"
+
+# Uninstall with new version, since it seems likely users will attempt this.
+# This gives us a heads up if it breaks somehow.
+php datadog-setup.php --php-bin php --uninstall
 assert_no_ddtrace
 assert_no_appsec
 assert_no_profiler
 
-php ./build/packages/datadog-setup.php --enable-profiling --php-bin php
-
-assert_ddtrace_version 0.75.0
-assert_profiler_version 0.6.1
+# Lastly, re-install with profiling.
+php ./build/packages/datadog-setup.php --enable-profiling --php-bin php --file "./build/packages/dd-library-php-${trace_version}-x86_64-linux-gnu.tar.gz"
 
 extension_dir="$(php -i | grep '^extension_dir' | awk '{ print $NF }')"
-if [ -f "${extension_dir}/ddtrace.so" ]; then
-    echo "Ok: File ${extension_dir}/ddtrace.so exists."
-else
-    echo "Error. File ${extension_dir}/ddtrace.so should exist."
-    exit 1
-fi
+for extension in ddtrace datadog-profiling ; do
+    if [ -f "${extension_dir}/${extension}.so" ]; then
+        echo "Ok: File ${extension_dir}/${extension}.so exists."
+    else
+        echo "Error. File ${extension_dir}/${extension}.so should exist."
+        exit 1
+    fi
+done
+
+assert_ddtrace_version "${trace_version}"
+assert_profiler_version "${profiling_version}"
