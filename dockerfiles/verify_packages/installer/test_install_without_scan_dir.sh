4b6181594a73a8711b2f5199ccad71a2d0c244e6
Bob Weinand <bob.weinand@datadoghq.com>
2022-08-19 15:51:00 +0200

    Test php installation without scandir
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

---
+++ dockerfiles/verify_packages/installer/test_install_without_scan_dir.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+
+set -e
+
+. "$(dirname ${0})/utils.sh"
+
+# Initially no ddtrace
+assert_no_ddtrace
+
+cat <<- "SCANDIR" >$(dirname "$(which php)")/php-without-scan-dir
+#!/usr/bin/env bash
+php="$(dirname "$0")/php"
+if [[ "$@" == *-i* ]]; then
+  "$php" "$@" | grep -v "Scan this dir for additional .ini files"
+else
+  "$php" "$@"
+fi
+SCANDIR
+chmod +x $(dirname "$(which php)")/php-without-scan-dir
+
+# Install using the php installer
+new_version="0.74.0"
+generate_installers "${new_version}"
+php ./build/packages/datadog-setup.php --php-bin php-without-scan-dir
+assert_ddtrace_version "${new_version}"
+
+ini_file=$(get_php_main_conf)
+
+assert_file_contains "${ini_file}" 'datadog.trace.request_init_hook'
+assert_file_contains "${ini_file}" 'datadog.version'
+
+# Removing an enabled property and a commented out property
+sed -i 's/datadog\.trace\.request_init_hook.*//g' "${ini_file}"
+sed -i 's/datadog\.version.*//g' "${ini_file}"
+
+assert_file_not_contains "${ini_file}" 'datadog.trace.request_init_hook'
+assert_file_not_contains "${ini_file}" 'datadog.version'
+
+php ./build/packages/datadog-setup.php --php-bin php-without-scan-dir
+
+assert_file_contains "${ini_file}" 'datadog.trace.request_init_hook'
+assert_file_contains "${ini_file}" 'datadog.version'
+
+assert_request_init_hook_exists
--- dockerfiles/verify_packages/installer/utils.sh
+++ dockerfiles/verify_packages/installer/utils.sh
@@ -127,6 +127,10 @@ get_php_conf_dir() {
     php -i | grep -i 'scan this dir for additional .ini files' | awk '{print $NF}'
 }
 
+get_php_main_conf() {
+    php -i | grep -i 'Loaded Configuration File' | awk '{print $NF}'
+}
+
 get_php_extension_dir() {
     php -i | grep -i '^extension_dir' | awk '{print $NF}'
 }
