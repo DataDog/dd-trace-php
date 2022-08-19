33c2af2920a0d9e98261913479ced5769fabd1dc
Joe Watkins <krakjoe@php.net>
2022-08-19 17:23:49 +0200

    this should work

--- dockerfiles/verify_packages/installer/test_install_without_scan_dir.sh
+++ dockerfiles/verify_packages/installer/test_install_without_scan_dir.sh
@@ -7,9 +7,12 @@ set -e
 # Initially no ddtrace
 assert_no_ddtrace
 
+# Must be a main php.ini file
+echo "" > /tmp/php-empty.ini
+
 cat <<- "SCANDIR" >$(dirname "$(which php)")/php-without-scan-dir
 #!/usr/bin/env bash
-php="$(dirname "$0")/php"
+php="$(dirname "$0")/php -c /tmp/php-empty.ini"
 if [[ "$@" == *-i* ]]; then
   "$php" "$@" | grep -v "Scan this dir for additional .ini files"
 else
@@ -18,13 +21,10 @@ fi
 SCANDIR
 chmod +x $(dirname "$(which php)")/php-without-scan-dir
 
-# Must be a main php.ini file
-echo "" > /tmp/php-empty.ini
-
 # Install using the php installer
 new_version="0.74.0"
 generate_installers "${new_version}"
-php -c /tmp/php-empty.ini ./build/packages/datadog-setup.php --php-bin php-without-scan-dir
+php ./build/packages/datadog-setup.php --php-bin php-without-scan-dir
 assert_ddtrace_version "${new_version}"
 
 ini_file=$(get_php_main_conf)
@@ -39,7 +39,7 @@ sed -i 's/datadog\.version.*//g' "${ini_file}"
 assert_file_not_contains "${ini_file}" 'datadog.trace.request_init_hook'
 assert_file_not_contains "${ini_file}" 'datadog.version'
 
-php -c /tmp/php-empty.ini ./build/packages/datadog-setup.php --php-bin php-without-scan-dir
+php ./build/packages/datadog-setup.php --php-bin php-without-scan-dir
 
 assert_file_contains "${ini_file}" 'datadog.trace.request_init_hook'
 assert_file_contains "${ini_file}" 'datadog.version'
