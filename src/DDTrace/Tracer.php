808a731bce92d14ba9247b65e04b6fb624ff6bb1
Pierre Bonet <pierre.bonet@datadoghq.com>
2022-09-07 17:21:05 +0200

    forgot file

--- src/DDTrace/Tracer.php
+++ src/DDTrace/Tracer.php
@@ -23,7 +23,7 @@ final class Tracer implements TracerInterface
      * Must begin with a number for Debian packaging requirements
      * Must use single-quotes for packaging script to work
      */
-    const VERSION = '0.78.0';
+    const VERSION = '0.79.0';
 
     /**
      * @var Span[][]
