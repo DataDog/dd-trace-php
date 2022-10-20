a4243930717ae2693d14665827ed39629bf2c73f
Joe Watkins <krakjoe@php.net>
2022-10-21 00:06:30 +0200

    improve ldconfig operation in installer

---
+++ dockerfiles/verify_packages/installer/test_install_no_ldconfig_in_path.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+set -e
+
+. "$(dirname ${0})/utils.sh"
+
+# Initially no ddtrace
+assert_no_ddtrace
+
+# Install using the php installer
+new_version="0.74.0"
+generate_installers "${new_version}"
+
+PHP=$(which php)
+
+# ldconfig is typically found in /sbin
+export PATH=/usr/bin:/bin:/usr/local/bin
+
+set +e
+output=$($PHP ./build/packages/datadog-setup.php --php-bin $PHP)
+exit_status=$?
+set -e
+
+assert_ddtrace_version "${new_version}" $PHP
