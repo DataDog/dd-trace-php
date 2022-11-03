34267c6a9e8875d64cf67e5faee7042cc434651c
Bob Weinand <bob.weinand@datadoghq.com>
2022-11-03 12:48:36 +0100

    Release 0.81.1 (#1786)
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- src/DDTrace/Tracer.php
+++ src/DDTrace/Tracer.php
@@ -23,7 +23,7 @@ final class Tracer implements TracerInterface
      * Must begin with a number for Debian packaging requirements
      * Must use single-quotes for packaging script to work
      */
-    const VERSION = '1.0.0-nightly';
+    const VERSION = '0.81.1';
 
     /**
      * @var Span[][]
