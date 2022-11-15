cab80a5b72783dd6a27c0f20a421442008923ea5
Florian Engelhardt <florian.engelhardt@datadoghq.com>
2022-11-15 11:35:38 +0100

    [PROF-6361] add validation of successful installation of handlers

--- profiling/src/lib.rs
+++ profiling/src/lib.rs
@@ -292,9 +292,29 @@ extern "C" fn minit(r#type: c_int, module_number: c_int) -> ZendResult {
                     Some(alloc_profiling_realloc),
                 );
             }
-            // TODO: check if we are installed, either via zend_mm_get_custom_handlers() or
-            // zend_mm_is_custom_heap(), the later is just an indication, could also be that other custom
-            // handlers are installed
+
+            let mut custom_mm_malloc: Option<zend::VmMmCustomAllocFn> = None;
+            let mut custom_mm_free: Option<zend::VmMmCustomFreeFn> = None;
+            let mut custom_mm_realloc: Option<zend::VmMmCustomReallocFn> = None;
+            unsafe {
+                zend::zend_mm_get_custom_handlers(
+                    zend::zend_mm_get_heap(),
+                    &mut custom_mm_malloc,
+                    &mut custom_mm_free,
+                    &mut custom_mm_realloc,
+                );
+            }
+            if custom_mm_free.is_none() ||
+                custom_mm_malloc.is_none() ||
+                custom_mm_realloc.is_none() ||
+                custom_mm_free != Some(alloc_profiling_free) ||
+                custom_mm_malloc != Some(alloc_profiling_malloc) ||
+                custom_mm_realloc != Some(alloc_profiling_realloc)
+            {
+                info!("Memory Allocation profiling could not be enabled. Pleas feel free to fill an issue stating the PHP version and installed modules.");
+            } else {
+                info!("Memory Allocation profiler enabled")
+            }
         }
     }
 
