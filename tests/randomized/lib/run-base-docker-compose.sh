3c8b416651e640dad8c616fdfd7318f06c3eab5f
Bob Weinand <bob.weinand@datadoghq.com>
2022-11-22 18:52:23 +0100

    More retrying in CI for less flakiness
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- tests/randomized/lib/run-base-docker-compose.sh
+++ tests/randomized/lib/run-base-docker-compose.sh
@@ -7,13 +7,19 @@ while true; do
   if mkdir $LOCK 2>/dev/null; then
     trap "rmdir $LOCK" EXIT
     if [[ ${1:-} == shutdown ]]; then
+      # check for running tests; in that case don't shutdown
       if ! docker network ls | grep -q randomized-; then
-        docker-compose down
+        sleep 1
+        # prevent race conditions
+        if ! docker network ls | grep -q randomized-; then
+          docker-compose down
+        fi
       fi
     else
-      if ! docker network ls | grep -q randomized_tests_baseservices; then
+      # while to make absolutely sure it's running
+      while ! docker network ls | grep -q randomized_tests_baseservices || { sleep 1; ! docker network ls | grep -q randomized_tests_baseservices }; do
         docker-compose up -d
-      fi
+      done
     fi
     exit 0
   fi
