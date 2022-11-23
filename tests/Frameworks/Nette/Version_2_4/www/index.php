a39ba91930260361f7c9388c1442364318d68ec3
Bob Weinand <bob.weinand@datadoghq.com>
2022-11-23 13:22:22 +0100

    Add debug information for occasional PDO constructor being invoked in Nette testsuite

--- tests/Frameworks/Nette/Version_2_4/www/index.php
+++ tests/Frameworks/Nette/Version_2_4/www/index.php
@@ -1,5 +1,10 @@
 <?php
 
+// Debug stuff:
+\DDTrace\hook_method('PDO', '__construct', function () {
+    error_log((string)new \Exception("PDO constructor invoked"));
+});
+
 $container = require __DIR__ . '/../app/bootstrap.php';
 
 $container->getByType(Nette\Application\Application::class)
--- tests/Frameworks/Nette/Version_3_0/www/index.php
+++ tests/Frameworks/Nette/Version_3_0/www/index.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// Debug stuff:
+\DDTrace\hook_method('PDO', '__construct', function () {
+    error_log((string)new \Exception("PDO constructor invoked"));
+});
+
 App\Bootstrap::boot()
     ->createContainer()
     ->getByType(Nette\Application\Application::class)
