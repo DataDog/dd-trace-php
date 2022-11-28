955d8e10ecb7d2af26863f93c2cf26ae5e0ba5c3
Florian Engelhardt <florian.engelhardt@datadoghq.com>
2022-11-28 21:15:39 +0100

    fix compile time warning when compiling with features

--- profiling/src/bindings/mod.rs
+++ profiling/src/bindings/mod.rs
@@ -10,8 +10,13 @@ use std::sync::atomic::AtomicBool;
 
 pub type VmInterruptFn = unsafe extern "C" fn(execute_data: *mut zend_execute_data);
 
+#[cfg(feature = "allocation_profiling")]
 pub type VmMmCustomAllocFn = unsafe extern "C" fn(u64) -> *mut libc::c_void;
+
+#[cfg(feature = "allocation_profiling")]
 pub type VmMmCustomReallocFn = unsafe extern "C" fn(*mut libc::c_void, u64) -> *mut libc::c_void;
+
+#[cfg(feature = "allocation_profiling")]
 pub type VmMmCustomFreeFn = unsafe extern "C" fn(*mut libc::c_void);
 
 // todo: this a lie on some PHP versions; is it a problem even though zend_bool
