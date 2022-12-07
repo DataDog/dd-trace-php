2bb7aa6b5aa653095e813c8b064894463651d6c0
wconti27 <william.conti@datadoghq.com>
2022-12-07 16:38:43 -0500

    Fix deprecation warning

--- src/api/Tag.php
+++ src/api/Tag.php
@@ -16,7 +16,8 @@ class Tag
     const DB_STATEMENT = 'sql.query';
     const ERROR = 'error';
     const ERROR_MESSAGE = 'error.message'; // string representing the error message
-    const ERROR_MSG = 'error.msg';/** @deprecated Use ERROR_MESSAGE instead. */
+    /** @deprecated Use ERROR_MESSAGE instead. */
+    const ERROR_MSG = 'error.msg';
     const ERROR_TYPE = 'error.type'; // string representing the type of the error
     const ERROR_STACK = 'error.stack'; // human readable version of the stack
     const HTTP_METHOD = 'http.method';
