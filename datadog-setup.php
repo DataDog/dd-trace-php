f0d8ab1a0aefb3067fb52838c7405a6445dc7f92
Levi Morrison <levi.morrison@datadoghq.com>
2022-12-19 14:46:44 -0700

    Remove libexecinfo on Alpine
    Fix GH-1824.
    
    The quick summary is Alpine 3.17 removed this library, and on older
    versions it's apparently not great quality anyway.

--- datadog-setup.php
+++ datadog-setup.php
@@ -66,7 +66,6 @@ function install($options)
     // Checking required libraries
     check_library_prerequisite_or_exit('libcurl');
     if (is_alpine()) {
-        check_library_prerequisite_or_exit('libexecinfo');
         if (is_truthy($options[OPT_ENABLE_PROFILING])) {
             check_library_prerequisite_or_exit('libgcc_s');
         }
