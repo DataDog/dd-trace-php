1fc44c8863f1fa37beee76533e7c5aaecb641edf
Levi Morrison <levi.morrison@datadoghq.com>
2022-12-20 18:55:34 -0700

    Hack around plesk issue

--- dockerfiles/verify_packages/installer/test_install_on_plesk.sh
+++ dockerfiles/verify_packages/installer/test_install_on_plesk.sh
@@ -5,11 +5,14 @@ set -e
 . "$(dirname ${0})/utils.sh"
 
 # Install using the php installer
-new_version="0.75.0"
+new_version="0.83.0"
 generate_installers "${new_version}"
-/opt/plesk/php/7.4/bin/php ./build/packages/datadog-setup.php --php-bin=all
+/opt/plesk/php/8.0/bin/php ./build/packages/datadog-setup.php --php-bin=all
 
-assert_ddtrace_version "${new_version}" /opt/plesk/php/7.4/bin/php
-assert_ddtrace_version "${new_version}" /opt/plesk/php/7.4/sbin/php-fpm
+# Around 2022-12-20, plesk/plesk:18.0 was updated and the PHP 7.4 install
+# was no longer present. I don't know if this is a temporary regression, or if
+# it's purposefully pulled since PHP 7.4 went EOL.
+#assert_ddtrace_version "${new_version}" /opt/plesk/php/7.4/bin/php
+#assert_ddtrace_version "${new_version}" /opt/plesk/php/7.4/sbin/php-fpm
 assert_ddtrace_version "${new_version}" /opt/plesk/php/8.0/bin/php
 assert_ddtrace_version "${new_version}" /opt/plesk/php/8.0/sbin/php-fpm
