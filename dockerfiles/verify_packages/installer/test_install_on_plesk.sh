39e27002ffcc95f9775f583cd3648f0abce15ac2
Levi Morrison <levi.morrison@datadoghq.com>
2023-01-02 19:04:53 -0700

    Remove PHP 7.4 on Plesk (removed upstream)

--- dockerfiles/verify_packages/installer/test_install_on_plesk.sh
+++ dockerfiles/verify_packages/installer/test_install_on_plesk.sh
@@ -7,12 +7,9 @@ set -e
 # Install using the php installer
 new_version="0.82.0"
 generate_installers "${new_version}"
-/opt/plesk/php/8.0/bin/php ./build/packages/datadog-setup.php --php-bin=all
 
-# Around 2022-12-20, plesk/plesk:18.0 was updated and the PHP 7.4 install
-# was no longer present. I don't know if this is a temporary regression, or if
-# it's purposefully pulled since PHP 7.4 went EOL.
-#assert_ddtrace_version "${new_version}" /opt/plesk/php/7.4/bin/php
-#assert_ddtrace_version "${new_version}" /opt/plesk/php/7.4/sbin/php-fpm
-assert_ddtrace_version "${new_version}" /opt/plesk/php/8.0/bin/php
-assert_ddtrace_version "${new_version}" /opt/plesk/php/8.0/sbin/php-fpm
+prefix="/opt/plesk/php/8.0"
+"$prefix/bin/php" ./build/packages/datadog-setup.php --php-bin=all
+
+assert_ddtrace_version "${new_version}" "$prefix/bin/php"
+assert_ddtrace_version "${new_version}" "$prefix/sbin/php-fpm"
