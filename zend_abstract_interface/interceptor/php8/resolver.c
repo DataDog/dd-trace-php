ffc97081fc2c86cfa2209435258d9bc53af693ff
Bob Weinand <bob.weinand@datadoghq.com>
2023-01-16 18:09:18 +0100

    Work around broken enum functions run_time_cache on PHP 8.2.0 and 8.2.1
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- zend_abstract_interface/interceptor/php8/resolver.c
+++ zend_abstract_interface/interceptor/php8/resolver.c
@@ -5,8 +5,28 @@ static void zai_interceptor_function_declared(zend_op_array *op_array, zend_stri
     zai_hook_resolve_function((zend_function *)op_array, name);
 }
 
+#if PHP_VERSION_ID < 80300
+#include "zend_extensions.h"
+static inline void zai_reset_enum_rt_cache(zend_class_entry *ce, zend_string *name) {
+    zend_function *func;
+    if ((func = zend_hash_find_ptr(&ce->function_table, name))) {
+        memset(RUN_TIME_CACHE(&func->op_array), 0, zend_internal_run_time_cache_reserved_size());
+    }
+}
+#endif
+
 static void zai_interceptor_class_linked(zend_class_entry *ce, zend_string *name) {
     zai_hook_resolve_class(ce, name);
+
+#if PHP_VERSION_ID < 80300
+    if (ce->ce_flags & ZEND_ACC_ENUM) {
+        zai_reset_enum_rt_cache(ce, ZSTR_KNOWN(ZEND_STR_CASES));
+        if (ce->enum_backing_type != IS_UNDEF) {
+            zai_reset_enum_rt_cache(ce, ZSTR_KNOWN(ZEND_STR_FROM));
+            zai_reset_enum_rt_cache(ce, ZSTR_KNOWN(ZEND_STR_TRYFROM_LOWERCASE));
+        }
+    }
+#endif
 }
 
 static zend_op_array *(*prev_compile_file)(zend_file_handle *file_handle, int type);
