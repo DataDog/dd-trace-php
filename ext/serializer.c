b9f9d94481900c3eef00010168abb343f1032025
Pierre Bonet <pierre.bonet@datadoghq.com>
2023-01-31 11:03:08 +0100

    Revert "Stop collecting IP on tracer (#1774)"
    This reverts commit b8caa1cc5dfb0eba4aa1ee1c8a4e43057678a2d3.

--- ext/serializer.c
+++ ext/serializer.c
@@ -22,6 +22,7 @@
 #include "ddtrace.h"
 #include "engine_api.h"
 #include "engine_hooks.h"
+#include "ip_extraction.h"
 #include "logging.h"
 #include "mpack/mpack.h"
 #include "priority_sampling/priority_sampling.h"
@@ -577,6 +578,12 @@ void ddtrace_set_root_span_properties(ddtrace_span_data *span) {
             } else {
                 ZVAL_COPY(prop_resource, &http_method);
             }
+        }
+    }
+
+    if (!get_DD_TRACE_CLIENT_IP_HEADER_DISABLED()) {
+        if (Z_TYPE(PG(http_globals)[TRACK_VARS_SERVER]) == IS_ARRAY || zend_is_auto_global_str(ZEND_STRL("_SERVER"))) {
+            ddtrace_extract_ip_from_headers(&PG(http_globals)[TRACK_VARS_SERVER], meta);
         }
     }
 
