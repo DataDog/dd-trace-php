ba94536bb1c384451223ca01a93e510e4fad4a69
Ralph Schindler <ralph@ralphschindler.com>
2023-01-31 09:30:33 -0600

    Add support for tracing events in Laravel 5.8+

--- src/Integrations/Integrations/Laravel/LaravelIntegration.php
+++ src/Integrations/Integrations/Laravel/LaravelIntegration.php
@@ -128,9 +128,24 @@ class LaravelIntegration extends Integration
             }
         );
 
+        // used by Laravel < 5.8
         \DDTrace\trace_method(
             'Illuminate\Events\Dispatcher',
             'fire',
+            function (SpanData $span, $args) use ($integration) {
+                $span->name = 'laravel.event.handle';
+                $span->type = Type::WEB_SERVLET;
+                $span->service = $integration->getServiceName();
+                $span->resource = $args[0];
+                $span->meta[Tag::COMPONENT] = LaravelIntegration::NAME;
+            }
+        );
+
+        // used by Laravel >= 5.8
+        // More details: https://laravel.com/docs/5.8/upgrade#events
+        \DDTrace\trace_method(
+            'Illuminate\Events\Dispatcher',
+            'dispatch',
             function (SpanData $span, $args) use ($integration) {
                 $span->name = 'laravel.event.handle';
                 $span->type = Type::WEB_SERVLET;
