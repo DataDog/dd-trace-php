71deafa528fb33ce85608b66ac8244380e0878c6
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2023-02-02 15:11:30 +0000

    Add exported function to close all spans and flush (#1902)

--- ext/auto_flush.h
+++ ext/auto_flush.h
@@ -5,7 +5,9 @@
 #include <php.h>
 #include <stdbool.h>
 
+ZEND_RESULT_CODE ddtrace_flush_tracer(bool force_on_startup);
+
 // This function is exported and used by appsec
-DDTRACE_PUBLIC ZEND_RESULT_CODE ddtrace_flush_tracer(bool force_on_startup);
+DDTRACE_PUBLIC void ddtrace_close_all_spans_and_flush(void);
 
 #endif  // DDTRACE_AUTO_FLUSH_H
