03353ef41af093960411fc93f5aed070de71af0b
wconti27 <william.conti@datadoghq.com>
2023-02-07 16:06:39 -0500

    Fix errors

--- tests/Integrations/PHPRedis/V4/PHPRedisClusterTest.php
+++ tests/Integrations/PHPRedis/V4/PHPRedisClusterTest.php
@@ -1393,7 +1393,8 @@ class PHPRedisClusterTest extends IntegrationTestCase
                 "RedisCluster.restore"
             )->withExactTags([
                 Tag::SPAN_KIND => 'client',
-                Tag::COMPONENT => 'phpredis', Tag::DB_SYSTEM => 'redis',,
+                Tag::COMPONENT => 'phpredis', 
+                Tag::DB_SYSTEM => 'redis',
             ]),
         ]);
 
