f9c04c652e91b433c30a417cd9e66dd9a0617649
wconti27 <william.conti@datadoghq.com>
2023-04-10 08:49:32 -0400

    change test to use request replayer

--- tests/Integration/DisabledTest.php
+++ tests/Integration/DisabledTest.php
@@ -20,6 +20,8 @@ final class DisabledTest extends CLITestCase
         $this->script = 'LongRunning/long_running_script_manual.php';
         $agentRequest = $this->getAgentRequestFromCommand('', [
             'DD_TRACE_CLI_ENABLED' => 'false',
+            'DD_AGENT_HOST' => 'request-replayer',
+            'DD_TRACE_AGENT_PORT' => '80',
         ]);
         $logger = $this->withDebugLogger();
         $logger->debug(LogLevel::DEBUG, $agentRequest);
