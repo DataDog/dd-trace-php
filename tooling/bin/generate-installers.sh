08fdd823d493bb671fe532a1c54f1d0d941d1dea
Levi Morrison <levi.morrison@datadoghq.com>
2023-04-12 16:55:19 -0600

    Maybe fix generate-installers.php

--- tooling/bin/generate-installers.sh
+++ tooling/bin/generate-installers.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
 
@@ -9,6 +9,6 @@ packages_build_dir=$2
 # Installers
 ########################
 sed "s|@release_version@|${release_version}|g" ./datadog-setup.php > "${packages_build_dir}/datadog-setup.php"
-if [[ ${release_version} == "1.0.0-nightly" && -n ${CIRCLE_WORKFLOW_JOB_ID:-} ]]; then
+if [[ ${release_version} =~ ".*\+.*" && -n ${CIRCLE_WORKFLOW_JOB_ID:-} ]]; then
   sed -ri "s|define\('RELEASE_URL_PREFIX'[^;]+|const RELEASE_URL_PREFIX = 'https://output.circle-artifacts.com/output/job/${CIRCLE_WORKFLOW_JOB_ID}/artifacts/0/'|" "${packages_build_dir}/datadog-setup.php"
 fi
