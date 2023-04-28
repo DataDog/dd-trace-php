f07a0135e87453b8d2b23e3c06915a7320a32c17
Alexandre Choura <alexandre.choura@datadoghq.com>
2023-04-28 17:04:35 +0200

    Remove some dead code

--- zend_abstract_interface/json/json.c
+++ zend_abstract_interface/json/json.c
@@ -24,22 +24,6 @@ __attribute__((weak)) int php_json_decode_ex(zval *return_value, const char *str
 #endif
 __attribute__((weak)) zend_class_entry *php_json_serializable_ce;
 
-void zai_fetch_json_serializable_ce(zend_module_entry *json_me) {
-    if (php_json_serializable_ce) return;
-
-    if (json_me == NULL) {
-        json_me = zend_hash_str_find_ptr(&module_registry, ZEND_STRL("json"));
-    }
-
-    zend_class_entry **tmp_json_serializable_ce = (zend_class_entry **)DL_FETCH_SYMBOL(json_me->handle, "php_json_serializable_ce");
-    if (tmp_json_serializable_ce == NULL) {
-        tmp_json_serializable_ce = (zend_class_entry **)DL_FETCH_SYMBOL(json_me->handle, "_php_json_serializable_ce");
-    }
-    if (tmp_json_serializable_ce != NULL) {
-        php_json_serializable_ce = *tmp_json_serializable_ce;
-    }
-}
-
 bool zai_json_setup_bindings(void) {
     if (php_json_encode && php_json_decode_ex && php_json_serializable_ce) {
         zai_json_encode = php_json_encode;
