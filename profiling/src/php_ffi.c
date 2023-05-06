3dbc24656a902943f5f8b8e8e63e58925e2d65dd
Levi Morrison <levi.morrison@datadoghq.com>
2023-05-05 18:15:48 -0600

    fix: use zend_string_init, check errors

--- profiling/src/php_ffi.c
+++ profiling/src/php_ffi.c
@@ -152,47 +152,57 @@ zend_execute_data* ddog_php_prof_get_current_execute_data()
 }
 
 #if CFG_STACK_WALKING_TESTS
-zend_execute_data* ddog_php_test_create_fake_zend_execute_data(int depth) {
-    if (depth <= 0) {
-        return NULL;
+static int (*og_snprintf)(char *, size_t, const char *, ...);
+
+// "weak" let's us polyfill, needed by zend_string_init(..., persistent: 1).
+void *__attribute__((weak)) __zend_malloc(size_t len) {
+    void *tmp = malloc(len);
+    if (EXPECTED(tmp || !len)) {
+        return tmp;
     }
-    zend_execute_data *execute_data = (zend_execute_data *) malloc(sizeof(zend_execute_data));
-    memset(execute_data, 0, sizeof(zend_execute_data));
+    fprintf(stderr, "Out of memory\n");
+    exit(1);
+}
 
-    execute_data->prev_execute_data = ddog_php_test_create_fake_zend_execute_data(depth - 1);
+static zend_execute_data *create_fake_frame(int depth) {
+    zend_execute_data *execute_data = calloc(1, sizeof(zend_execute_data));
+    zend_op_array *op_array = calloc(1, sizeof(zend_function));
+    op_array->type = ZEND_USER_FUNCTION;
+    execute_data->func = (zend_function *)op_array;
 
-    int (*original_snprintf)(char *, size_t, const char *, ...);
-    original_snprintf = dlsym(RTLD_NEXT, "snprintf");
+    char buffer[64] = {0};
+    int len = og_snprintf(buffer, sizeof buffer, "function name %03d", depth) + 1;
+    ZEND_ASSERT(len >= 0 && sizeof buffer > (size_t)len);
+    op_array->function_name = zend_string_init(buffer, len - 1, true);
 
-    int len = 0;
-
-    // add function name to stack frame
-    len = original_snprintf(NULL, 0, "function name %03d", depth) + 1;
-    zend_string *func_name = malloc(sizeof(zend_string) + len);
-    func_name->h = 0;
-    func_name->len = len-1;
-    original_snprintf(func_name->val, len, "function name %03d", depth);
-
-    execute_data->func = (zend_function *) malloc(sizeof(zend_function));
-    memset(execute_data->func, 0, sizeof(zend_function));
-    execute_data->func->common.function_name = func_name;
-
-    // add file name to stack frame
-    len = original_snprintf(NULL, 0, "filename-%03d.php", depth) + 1;
-    zend_string *file_name = malloc(sizeof(zend_string) + len);
-    file_name->h = 0;
-    file_name->len = len-1;
-    original_snprintf(file_name->val, len, "filename-%03d.php", depth);
-    execute_data->func->op_array.filename = file_name;
-    execute_data->func->type = ZEND_USER_FUNCTION;
+    len = og_snprintf(buffer, sizeof buffer, "filename-%03d.php", depth) + 1;
+    ZEND_ASSERT(len >= 0 && sizeof buffer > (size_t)len);
+    op_array->filename = zend_string_init(buffer, len - 1, true);
 
     return execute_data;
 }
 
-void ddog_php_test_free_fake_zend_execute_data(zend_execute_data *execute_data) {
-    if (!execute_data) {
-        return;
+static zend_execute_data *create_fake_zend_execute_data(int depth) {
+    if (depth <= 0) return NULL;
+    zend_execute_data *execute_data = create_fake_frame(depth);
+    execute_data->prev_execute_data = create_fake_zend_execute_data(depth - 1);
+    return execute_data;
+}
+
+zend_execute_data *ddog_php_test_create_fake_zend_execute_data(int depth) {
+    if (!og_snprintf) {
+        og_snprintf = dlsym(RTLD_NEXT, "snprintf");
+        if (!og_snprintf) {
+            fprintf(stderr, "Failed to locate symbol: %s", dlerror());
+            exit(1);
+        }
     }
+
+    return create_fake_zend_execute_data(depth);
+}
+
+void ddog_php_test_free_fake_zend_execute_data(zend_execute_data *execute_data) {
+    if (!execute_data) return;
 
     ddog_php_test_free_fake_zend_execute_data(execute_data->prev_execute_data);
 
