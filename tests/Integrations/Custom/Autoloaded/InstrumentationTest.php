f184aa1352427f341eafdc4770ab0bcd9af26024
wconti27 <william.conti@datadoghq.com>
2023-05-16 09:53:05 -0400

    change files to use new branch for test-agent

--- tests/Integrations/Custom/Autoloaded/InstrumentationTest.php
+++ tests/Integrations/Custom/Autoloaded/InstrumentationTest.php
@@ -18,8 +18,8 @@ final class InstrumentationTest extends WebFrameworkTestCase
     {
         return array_merge(parent::getEnvs(), [
             'APP_NAME' => 'custom_autoloaded_app',
-            'DD_TRACE_AGENT_PORT' => 80,
-            'DD_AGENT_HOST' => 'request-replayer',
+            'DD_TRACE_AGENT_PORT' => 9126,
+            'DD_AGENT_HOST' => 'test-agent',
         ]);
     }
 
