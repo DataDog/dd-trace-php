1f3b0ed10db7d833ce631094fae2a666277bddf2
Florian Engelhardt <florian.engelhardt@datadoghq.com>
2023-05-30 16:36:58 +0200

    foobar

--- benchmark/runall.sh
+++ benchmark/runall.sh
@@ -3,8 +3,6 @@
 set -ex
 
 cd ../profiling/
-sed -i ".bck" -e "s/crate-type.*$/crate-type = [\"rlib\"]/g" Cargo.toml
+sed -e "s/crate-type.*$/crate-type = [\"rlib\"]/g" Cargo.toml
 
 cargo bench --features stack_walking_tests
-
-mv Cargo.toml.bck Cargo.toml
