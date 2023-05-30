931af43d7627412e0b102711fcbf06dfbffc5358
Florian Engelhardt <florian.engelhardt@datadoghq.com>
2023-05-30 16:25:55 +0200

    update benchmark running

---
+++ benchmark/runall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ex
+
+cd ../profiling/
+sed -i ".bck" "s/crate-type.*$/crate-type = [\"rlib\"]/g" Cargo.toml
+
+cargo bench --features stack_walking_tests
+
+mv Cargo.toml.bck Cargo.toml
