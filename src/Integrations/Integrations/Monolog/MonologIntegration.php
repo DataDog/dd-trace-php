d0b24697569bccf529e5f149b49c5fb477b46184
Alexandre Choura <alexandre.choura@datadoghq.com>
2023-06-02 10:16:56 +0200

    Save changes

--- src/Integrations/Integrations/Monolog/MonologIntegration.php
+++ src/Integrations/Integrations/Monolog/MonologIntegration.php
@@ -24,132 +24,50 @@ class MonologIntegration extends Integration
 
     public function init()
     {
-        /*
-        install_hook(
-            "Psr\Log\LoggerInterface::log",
-            function (HookData $hook) {
-                Logger::get()->debug('Setting context for log (LoggerInterface)');
-                $hook->args[2]['dd'] = [
-                    'trace_id' => trace_id_128(),
-                    'span_id'  => dd_trace_peek_span_id()
-                ];
-
-                $hook->overrideArguments($hook->args);
-            }
-        );
-
-        install_hook(
-            "Psr\Log\AbstractLogger::log",
-            function (HookData $hook) {
-                Logger::get()->debug('Setting context for log (AbstractLogger)');
-                $hook->args[2]['dd'] = [
-                    'trace_id' => trace_id_128(),
-                    'span_id'  => dd_trace_peek_span_id()
-                ];
-
-                $hook->overrideArguments($hook->args);
-            }
-        );
-        */
-
-        /*
-        install_hook(
-            "Illuminate\Log\LogManager::log",
-            function (HookData $hook) {
-                Logger::get()->debug('Setting context for log (LogManager)');
-                $hook->args[2]['dd'] = [
-                    'trace_id' => trace_id_128(),
-                    'span_id'  => dd_trace_peek_span_id()
-                ];
-
-                $hook->overrideArguments($hook->args);
-            }
-        );
-        */
-
         $levels = [
-            'debug' => 100,
-            'info' => 200,
-            'notice' => 250,
-            'warning' => 300,
-            'error' => 400,
-            'critical' => 500,
-            'alert' => 550,
-            'emergency' => 600
+            'debug',
+            'info',
+            'notice',
+            'warning',
+            'error',
+            'critical',
+            'alert',
+            'emergency'
         ];
 
-        foreach ($levels as $levelName => $level) {
+        foreach ($levels as $level) {
             install_hook(
-                "Psr\Log\LoggerInterface::$levelName",
-                function (HookData $hook) use ($levelName, $level) {
-                    Logger::get()->debug("Setting context for Psr\Log\LoggerInterface::$level");
+                "Psr\Log\LoggerInterface::$level",
+                function (HookData $hook) use ($level) {
+                    //Logger::get()->debug("Setting context for Psr\Log\LoggerInterface::$level");
+                    $traceId = trace_id_128();
+                    $spanId = dd_trace_peek_span_id();
+
+                    $hook->args[0] = $hook->args[0] . " [dd.trace_id=\"$traceId\" dd.span_id=\"$spanId\"]";
+
+                    /*
+                    $currentContext = \DDTrace\current_context();
+
                     $hook->args[1]['dd'] = [
                         'trace_id' => trace_id_128(),
                         'span_id' => dd_trace_peek_span_id(),
                         'service' => ddtrace_config_app_name()
                     ];
-                    $hook->args[1]['level'] = $level; // TODO: That certainly shouldn't be here and handled in the pipeline/logs-backend (integrations-core)
 
-                    $hook->overrideArguments($hook->args);
+                    if ($currentContext['version']) {
+                        $hook->args[1]['dd']['version'] = $currentContext['version'];
+                    }
+
+                    if ($currentContext['env']) {
+                        $hook->args[1]['dd']['env'] = $currentContext['env'];
+                    }
+                    */
+
+                    $hook->overrideArguments([$hook->args[0], $hook->args[1] ?? []]);
                 }
             );
         }
 
         return Integration::LOADED;
-    }
-
-    public static function monologProcessorV1($record)
-    {
-        /*
-        $record['message'] .= sprintf(
-            ' [dd.trace_id=%s dd.span_id=%s]',
-            trace_id_128(),
-            dd_trace_peek_span_id()
-        );
-        return $record;
-        */
-        $record['dd'] = [
-            'trace_id' => trace_id_128(),
-            'span_id'  => dd_trace_peek_span_id()
-        ];
-        return $record;
-    }
-
-    public static function monologProcessorV2($record)
-    {
-        /*
-        return $record->with(message: $record['message'] . sprintf(
-                ' [dd.trace_id=%s dd.span_id=%s]',
-                trace_id_128(),
-                dd_trace_peek_span_id()
-            ));
-        */
-
-        /*
-        $record['dd'] = [
-            'trace_id' => trace_id_128(),
-            'span_id'  => dd_trace_peek_span_id()
-        ];
-        */
-
-        //Logger::get()->debug("Setting extra...");
-        /*
-        $record['extra']['dd'] = [
-            'trace_id' => trace_id_128(),
-            'span_id'  => dd_trace_peek_span_id(),
-            'service' => ddtrace_config_app_name()
-        ];
-        */
-        //Logger::get()->debug("Set extra");
-        return $record;
-    }
-
-    public static function monologProcessorV3($record)
-    {
-        $record->extra['dd'] = [
-            'trace_id' => trace_id_128(),
-            'span_id'  => dd_trace_peek_span_id()
-        ];
-        return $record;
     }
 }
