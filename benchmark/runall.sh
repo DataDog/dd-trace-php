ba3422d2ef73e4bfb2f3e60b54faa507f1b5ada9
Florian Engelhardt <florian.engelhardt@datadoghq.com>
2023-06-05 16:04:40 +0200

    we do not need plots

--- benchmark/runall.sh
+++ benchmark/runall.sh
@@ -5,4 +5,4 @@ set -ex
 cd ../profiling/
 sed -i -e "s/crate-type.*$/crate-type = [\"rlib\"]/g" Cargo.toml
 
-cargo bench --features stack_walking_tests
+cargo bench --features stack_walking_tests -- --noplot
