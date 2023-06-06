a03253f1488510b2455322c080411d87cb7a785d
Alexandre Choura <alexandre.choura@datadoghq.com>
2023-06-06 16:58:52 +0200

    Change 'build' directory name to 'github-actions-helpers'

--- github-actions-helpers/build.ps1
+++ github-actions-helpers/build.ps1
@@ -13,7 +13,7 @@ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 # CONFIGURATION
 ###########################################################################
 
-$BuildProjectFile = "$PSScriptRoot\build\_build.csproj"
+$BuildProjectFile = "$PSScriptRoot\github-actions-helpers\_build.csproj"
 $TempDirectory = "$PSScriptRoot\\.nuke\temp"
 
 $DotNetGlobalFile = "$PSScriptRoot\\global.json"
