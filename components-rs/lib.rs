ccba04c4b3fdd0ef45cd535e14ca5b873183eeb3
Levi Morrison <levi.morrison@datadoghq.com>
2023-06-14 10:57:05 -0600

    style: fix clippy lints (#2101)
    * style: fix clippy lints
    
    * fix: header needed regenerated

--- components-rs/lib.rs
+++ components-rs/lib.rs
@@ -11,18 +11,20 @@ pub use ddtelemetry_ffi::*;
 
 #[no_mangle]
 #[allow(non_upper_case_globals)]
-pub static mut ddtrace_runtime_id: Uuid = Uuid::from_bytes([0; 16]);
+pub static mut ddtrace_runtime_id: Uuid = Uuid::nil();
 
-#[must_use]
+/// # Safety
+/// Must be called from a single-threaded context, such as MINIT.
 #[no_mangle]
 pub unsafe extern "C" fn ddtrace_generate_runtime_id() {
     ddtrace_runtime_id = Uuid::new_v4();
 }
 
-#[must_use]
 #[no_mangle]
-pub unsafe extern "C" fn ddtrace_format_runtime_id(buf: &mut [u8; 36]) {
-    ddtrace_runtime_id.as_hyphenated().encode_lower(buf);
+pub extern "C" fn ddtrace_format_runtime_id(buf: &mut [u8; 36]) {
+    // Safety: ddtrace_runtime_id is only supposed to be mutated from single-
+    // threaded contexts, so reads should always be safe.
+    unsafe { ddtrace_runtime_id.as_hyphenated().encode_lower(buf) };
 }
 
 #[must_use]
@@ -31,7 +33,6 @@ pub extern "C" fn ddtrace_get_container_id() -> CharSlice<'static> {
     get_container_id().map_or(CharSlice::default(), CharSlice::from)
 }
 
-#[must_use]
 #[no_mangle]
 pub unsafe extern "C" fn ddtrace_set_container_cgroup_path(path: CharSlice) {
     set_cgroup_file(String::from(path.try_to_utf8().unwrap()))
