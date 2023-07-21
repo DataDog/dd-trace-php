f301f6448aa91707406afeaafe6dce33345142b3
Alexandre Choura <alexandre.choura@datadoghq.com>
2023-07-21 14:54:33 +0200

    Remove payload debug log

--- ext/auto_flush.c
+++ ext/auto_flush.c
@@ -39,7 +39,6 @@ ZEND_RESULT_CODE ddtrace_flush_tracer(bool force_on_startup, bool collect_cycles
     char *payload;
     size_t size, limit = get_global_DD_TRACE_AGENT_MAX_PAYLOAD_SIZE();
     if (ddtrace_serialize_simple_array_into_c_string(&traces, &payload, &size)) {
-        ddtrace_log_debugf("Payload: %s", payload);
         if (size > limit) {
             ddtrace_log_errf("Agent request payload of %zu bytes exceeds configured %zu byte limit; dropping request", size, limit);
             success = false;
