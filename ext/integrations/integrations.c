82eaa9d27674ce8b55cd052d0b3c10368f31a181
Alexandre Choura <alexandre.choura@datadoghq.com>
2023-08-01 16:22:26 +0200

    Remove erroneous typo

--- ext/integrations/integrations.c
+++ ext/integrations/integrations.c
@@ -162,7 +162,7 @@ void ddtrace_integrations_minit(void) {
                                            "DDTrace\\Integrations\\Curl\\CurlIntegration");
 
     DD_SET_UP_DEFERRED_LOADING_BY_METHOD(DDTRACE_INTEGRATION_DRUPAL, "Drupal\\Core\\DrupalKernel", "__construct",
-                                         "DDTrace\\Integrations\\Drupal\\DrupalIntegration.php");
+                                         "DDTrace\\Integrations\\Drupal\\DrupalIntegration");
 
     DD_SET_UP_DEFERRED_LOADING_BY_METHOD_POST(DDTRACE_INTEGRATION_CODEIGNITER, "CI_Router", "_set_routing",
                                          "DDTrace\\Integrations\\CodeIgniter\\V2\\CodeIgniterIntegration");
