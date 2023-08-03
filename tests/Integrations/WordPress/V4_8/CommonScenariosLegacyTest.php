c93c74262344576936b77be3fb9bb8a1e18606c9
Alexandre Choura <alexandre.choura@datadoghq.com>
2023-08-03 15:06:57 +0200

    Generate legacy snapshots

--- tests/Integrations/WordPress/V4_8/CommonScenariosLegacyTest.php
+++ tests/Integrations/WordPress/V4_8/CommonScenariosLegacyTest.php
@@ -2,12 +2,12 @@
 
 namespace DDTrace\Tests\Integrations\WordPress\V4_8;
 
-class CommonScenariosCallbacksTest extends CommonScenariosTest
+class CommonScenariosLegacyTest extends CommonScenariosTest
 {
     protected static function getEnvs()
     {
         return array_merge(parent::getEnvs(), [
-            'DD_TRACE_WORDPRESS_CALLBACKS' => '1'
+            'DD_TRACE_WORDPRESS_ENHANCED_INTEGRATION' => '0'
         ]);
     }
 }
--- tests/Integrations/WordPress/V5_5/CommonScenariosCallbacksTest.php
+++ tests/Integrations/WordPress/V5_5/CommonScenariosCallbacksTest.php
@@ -7,7 +7,6 @@ final class CommonScenariosCallbacksTest extends CommonScenariosTest
     protected static function getEnvs()
     {
         return array_merge(parent::getEnvs(), [
-            'DD_SERVICE' => 'wordpress_59_test_app',
             'DD_TRACE_WORDPRESS_CALLBACKS' => '1'
         ]);
     }
