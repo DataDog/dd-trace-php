ed8ce11e298a6fe332816795281d2ede7171ad37
Levi Morrison <levi.morrison@datadoghq.com>
2023-08-11 06:29:49 -0600

    build(profiling): enable cross-language LTO
    The profiler is mostly written in Rust and has a few small bits in C.
    Most of these functions in C would be inlined if they were written in
    the same language so the compilers could see the definitions.
    
    Enter cross-language link-time optimization, or cross-language LTO.
    This requires a compatible C toolchain and some LLVM tools.

---
+++ profiling/build-linux-lto.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -eu
+
+# These are needed for cross-language LTO
+AR="$(command -v llvm-ar)"
+CC="$(command -v clang)"
+RANLIB="$(command -v llvm-ranlib)"
+export AR
+export CC
+export CFLAGS="-flto=thin"
+export RANLIB
+
+RUSTFLAGS="-Clinker-plugin-lto -C linker=clang -C link-arg=-fuse-ld=lld" \
+    cargo build --release -vv
