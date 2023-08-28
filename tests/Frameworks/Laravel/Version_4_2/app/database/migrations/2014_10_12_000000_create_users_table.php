6c816c544fb0dc5eb2c1730bc6418bc02be6f03f
Alexandre Choura <alexandre.choura@datadoghq.com>
2023-08-28 09:57:10 +0200

    Drop users table if it exists (Laravel 4.2)

--- tests/Frameworks/Laravel/Version_4_2/app/database/migrations/2014_10_12_000000_create_users_table.php
+++ tests/Frameworks/Laravel/Version_4_2/app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('users');
+
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->nullable();
