784806052829063cb61abbce47ef0be24bf018c8
Alexandre Choura <alexandre.choura@datadoghq.com>
2023-08-31 14:36:40 +0200

    Ensure a clean state before running the CLI test cases

--- tests/Integrations/CLI/Laravel/V10_X/CommonScenariosTest.php
+++ tests/Integrations/CLI/Laravel/V10_X/CommonScenariosTest.php
@@ -12,4 +12,46 @@ class CommonScenariosTest extends \DDTrace\Tests\Integrations\CLI\Laravel\V8_X\C
     {
         return __DIR__ . '/../../../../Frameworks/Laravel/Version_10_x/artisan';
     }
+
+    public function testCommandWithError()
+    {
+        $this->retrieveDumpedData();
+
+        $traces = $this->getTracesFromCommand('foo:error');
+
+        $this->assertFlameGraph($traces, [
+            SpanAssertion::build(
+                'laravel.artisan',
+                'artisan_test_app',
+                'cli',
+                'artisan foo:error'
+            )->withExactTags([
+                Tag::COMPONENT => 'laravel',
+            ])->withExistingTagsNames([
+                Tag::ERROR_MSG,
+                'error.stack'
+            ])->withChildren([
+                SpanAssertion::exists(
+                    'laravel.provider.load',
+                    'Illuminate\Foundation\ProviderRepository::load'
+                ),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+            ])->setError(),
+        ]);
+    }
 }
