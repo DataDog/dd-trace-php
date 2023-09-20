73e1e7e15413fbbedc7df3e5e3474a80b1c1e739
Bob Weinand <bob.weinand@datadoghq.com>
2023-09-20 13:56:52 +0200

    Fix toggling datadog.trace.enabled with perdir configuation (#2269)
    And ensure in ZAI config that perdir configuration is handled the same with fpm and apache.
    
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

---
+++ tests/Integrations/Custom/NotAutoloaded/EarlyDisableTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DDTrace\Tests\Integrations\Custom\NotAutoloaded;
+
+use DDTrace\Tests\Common\SpanAssertion;
+use DDTrace\Tests\Common\WebFrameworkTestCase;
+use DDTrace\Tests\Frameworks\Util\Request\GetSpec;
+
+final class EarlyDisableTest extends WebFrameworkTestCase
+{
+    protected static function getAppIndexScript()
+    {
+        return __DIR__ . '/../../../Frameworks/Custom/Version_Not_Autoloaded/index.php';
+    }
+
+    protected static function getEnvs()
+    {
+        return array_merge(parent::getEnvs(), [
+            'APP_NAME' => 'custom_no_autoloaded_app',
+            'DD_TRACE_NO_AUTOLOADER' => 'true',
+        ]);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testTracingDisabledByPerdirConfig()
+    {
+        $sapi = \getenv('DD_TRACE_TEST_SAPI');
+        if ($sapi != 'fpm-fcgi' && $sapi != "apache2handler") {
+            $this->markTestSkipped('Only run under apache and fpm-fcgi SAPI');
+        }
+
+        $traces = $this->tracesFromWebRequest(function () {
+            $response = $this->call(GetSpec::create('A web request to a disabled endpoint', '/per-dir-disabled/test.php'));
+            $this->assertSame("Tracing enabled: 0", $response);
+        });
+
+        $this->assertSpans($traces, []);
+    }
+}
