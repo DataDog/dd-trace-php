8400073608b511904bec07fbf03dc10e921afb1f
Alexandre Choura <alexandre.choura@datadoghq.com>
2023-09-25 15:25:52 +0200

    debug: Totally remove magento test suites

--- tests/Frameworks/Magento/Version_2_4/app/code/CustomElement/Datadog/registration.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'CustomElement_Datadog', __DIR__);
--- tests/Frameworks/Magento/Version_2_4/dev/tests/integration/framework/tests/unit/testsuite/Magento/Test/Annotation/_files/sample_fixture_three.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-// Empty fixture script for test purpose
-putenv('sample_fixture_three=3');
