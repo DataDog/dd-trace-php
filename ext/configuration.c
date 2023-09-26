mlocati/php-datadog_trace

Add Windows support
Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>
Initial windows version of sidecar
Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

ext/configuration.c
@@ -66,14 +66,28 @@ static bool dd_parse_dbm_mode(zai_string_view value, zval *decoded_value, bool p
     return true;
 }
 
+#define CALIAS_EXPAND(name) {.ptr = name, .len = sizeof(name) - 1},
+
+#ifndef _WIN32
 // Allow for partially defined struct initialization here
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#else
+#define CONFIG(...)
+#define CALIASES(...) {APPLY_N(CALIAS_EXPAND, ##__VA_ARGS__)}
+#define CALIAS(type, name, default, aliases, ...) const zai_string_view dd_config_aliases_##name[] = aliases;
+DD_CONFIGURATION
+#undef CALIAS
+#undef CONFIG
+#endif
 
 #define CUSTOM(...) CUSTOM
-#define CALIAS_EXPAND(name) {.ptr = name, .len = sizeof(name) - 1},
-#define CALIASES(...) ((zai_string_view[]){APPLY_N(CALIAS_EXPAND, ##__VA_ARGS__)})
 #define CONFIG(type, name, ...) ZAI_CONFIG_ENTRY(DDTRACE_CONFIG_##name, name, type, __VA_ARGS__),
+#ifndef _WIN32
+#define CALIASES(...) ((zai_string_view[]){APPLY_N(CALIAS_EXPAND, ##__VA_ARGS__)})
 #define CALIAS(type, name, ...) ZAI_CONFIG_ALIASED_ENTRY(DDTRACE_CONFIG_##name, name, type, __VA_ARGS__),
+#else
+#define CALIAS(type, name, default, aliases, ...) ZAI_CONFIG_ALIASED_ENTRY(DDTRACE_CONFIG_##name, name, type, default, dd_config_aliases_##name, ##__VA_ARGS__),
+#endif
 static zai_config_entry config_entries[] = {DD_CONFIGURATION};
 #undef CALIAS
 #undef CONFIG

ext/engine_hooks.c
@@ -5,8 +5,18 @@
 
 #include "ddtrace.h"
 #include "logging.h"
+#include "zend_hrtime.h"
 #include "span.h"
 #include "zend_extensions.h"
+
+#ifdef PHP_WIN32
+#include "win32/param.h"
+#include "win32/winutil.h"
+#define GET_DL_ERROR()	php_win_err()
+#else
+#include <sys/param.h>
+#define GET_DL_ERROR()	DL_ERROR()
+#endif
 
 ZEND_EXTERN_MODULE_GLOBALS(ddtrace);
 
@@ -17,9 +27,10 @@ static void _compile_mshutdown(void);
 
 void (*ddtrace_prev_error_cb)(DDTRACE_ERROR_CB_PARAMETERS);
 
-void (*profiling_notify_trace_finished)(uint64_t local_root_span_id,
+typedef void (*profiling_notify_trace_finished_t)(uint64_t local_root_span_id,
                                         zai_string_view span_type,
-                                        zai_string_view resource) = NULL;
+                                        zai_string_view resource);
+profiling_notify_trace_finished_t profiling_notify_trace_finished = NULL;
 
 void (*profiling_interrupt_function)(zend_execute_data *) = NULL;
 
@@ -29,15 +40,15 @@ void dd_search_for_profiling_symbols(void *arg) {
     if (extension->name && strcmp(extension->name, "datadog-profiling") == 0) {
         DL_HANDLE handle = extension->handle;
 
-        profiling_interrupt_function = DL_FETCH_SYMBOL(handle, "datadog_profiling_interrupt_function");
+        profiling_interrupt_function = (void(*)(zend_execute_data *))DL_FETCH_SYMBOL(handle, "datadog_profiling_interrupt_function");
         if (UNEXPECTED(!profiling_interrupt_function)) {
             ddtrace_log_debugf("[Datadog Trace] Profiling was detected, but locating symbol %s failed: %s\n", "datadog_profiling_interrupt_function",
-                               DL_ERROR());
+                               GET_DL_ERROR());
         }
 
-        profiling_notify_trace_finished = DL_FETCH_SYMBOL(handle, "datadog_profiling_notify_trace_finished");
+        profiling_notify_trace_finished = (profiling_notify_trace_finished_t)DL_FETCH_SYMBOL(handle, "datadog_profiling_notify_trace_finished");
         if (UNEXPECTED(!profiling_notify_trace_finished)) {
-            ddtrace_log_debugf("[Datadog Trace] Profiling v%s was detected, but locating symbol failed: %s\n", extension->version, DL_ERROR());
+            ddtrace_log_debugf("[Datadog Trace] Profiling v%s was detected, but locating symbol failed: %s\n", extension->version, GET_DL_ERROR());
         }
     }
 }
@@ -61,19 +72,11 @@ void ddtrace_engine_hooks_mshutdown(void) {
     _compile_mshutdown();
 }
 
-static uint64_t _get_microseconds() {
-    struct timespec time;
-    if (clock_gettime(CLOCK_MONOTONIC, &time) == 0) {
-        return time.tv_sec * 1000000U + time.tv_nsec / 1000U;
-    }
-    return 0U;
-}
-
 static zend_op_array *_dd_compile_file(zend_file_handle *file_handle, int type) {
     zend_op_array *res;
-    uint64_t start = _get_microseconds();
+    uint64_t start = zend_hrtime();
     res = _prev_compile_file(file_handle, type);
-    DDTRACE_G(compile_time_microseconds) += (int64_t)(_get_microseconds() - start);
+    DDTRACE_G(compile_time_microseconds) += (int64_t)(zend_hrtime() - start);
     return res;
 }
 

zend_abstract_interface/env/env.h
@@ -60,9 +60,8 @@ typedef struct zai_env_buffer_s {
  * and 'buf.ptr' will be an empty string; e.g. this API does not attempt to
  * truncate the value to accommodate the buffer size.
  */
-zai_env_result __attribute__((warn_unused_result))
-zai_getenv_ex(zai_string_view name, zai_env_buffer buf, bool pre_rinit);
-static inline __attribute__((warn_unused_result)) zai_env_result zai_getenv(zai_string_view name, zai_env_buffer buf) {
+zai_env_result zai_getenv_ex(zai_string_view name, zai_env_buffer buf, bool pre_rinit);
+static inline zai_env_result zai_getenv(zai_string_view name, zai_env_buffer buf) {
     return zai_getenv_ex(name, buf, false);
 }
 

DataDog/dd-trace-php

ext/configuration.c
@@ -5,6 +5,7 @@
 #include "circuit_breaker.h"
 #include "logging.h"
 #include <components/log/log.h>
+#include <zai_string/string.h>
 
 #define DD_TO_DATADOG_INC 5 /* "DD" expanded to "datadog" */
 
@@ -16,9 +17,15 @@
 #define APPLY_NAME_EXPAND(count) APPLY_##count
 #define APPLY_NAME(count) APPLY_NAME_EXPAND(count)
 #define APPLY_COUNT(_0, _1, _2, _3, _4, N, ...) N
+#if defined(_MSVC_TRADITIONAL) && _MSVC_TRADITIONAL
+#define EXPAND(...) __VA_ARGS__
+#define APPLY_N(macro, ...) APPLY_NAME(EXPAND(APPLY_COUNT(0, ##__VA_ARGS__, 4, 3, 2, 1, 0)))EXPAND((macro, ##__VA_ARGS__))
+#else
 #define APPLY_N(macro, ...) APPLY_NAME(APPLY_COUNT(0, ##__VA_ARGS__, 4, 3, 2, 1, 0))(macro, ##__VA_ARGS__)
+#endif
 
-// static assert name lengths, number of configs and number of aliases
+// static assert name lengths, number of configs and number of aliases (Visual Studio 2017 and older do not support _Static_assert)
+#ifndef _WIN32
 #define CALIAS CONFIG
 #define CONFIG(...) 1,
 #define NUMBER_OF_CONFIGURATIONS sizeof((uint8_t[]){DD_CONFIGURATION})
@@ -51,6 +58,7 @@ DD_CONFIGURATION
 #undef CALIAS
 #undef CALIASES
 #undef CONFIG
+#endif
 
 static bool dd_parse_dbm_mode(zai_str value, zval *decoded_value, bool persistent) {
     UNUSED(persistent);
@@ -67,14 +75,29 @@ static bool dd_parse_dbm_mode(zai_str value, zval *decoded_value, bool persisten
     return true;
 }
 
+#define CALIAS_EXPAND(name) {.ptr = name, .len = sizeof(name) - 1},
+
+#ifndef _WIN32
 // Allow for partially defined struct initialization here
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#else
+#define CONFIG(...)
+#define CALIASES(...) {APPLY_N(CALIAS_EXPAND, ##__VA_ARGS__)}
+#define CALIAS(type, name, default, aliases, ...) const zai_str dd_config_aliases_##name[] = aliases;
+DD_CONFIGURATION
+#undef CALIAS
+#undef CONFIG
+#endif
 
 #define CUSTOM(...) CUSTOM
-#define CALIAS_EXPAND(name) {.ptr = name, .len = sizeof(name) - 1},
+#define EXPAND_CALL(macro, args) macro args // I hate the "traditional" MSVC preprocessor
+#define CONFIG(type, name, ...) EXPAND_CALL(ZAI_CONFIG_ENTRY, (DDTRACE_CONFIG_##name, name, type, __VA_ARGS__)),
+#ifndef _WIN32
 #define CALIASES(...) ((zai_str[]){APPLY_N(CALIAS_EXPAND, ##__VA_ARGS__)})
-#define CONFIG(type, name, ...) ZAI_CONFIG_ENTRY(DDTRACE_CONFIG_##name, name, type, __VA_ARGS__),
 #define CALIAS(type, name, ...) ZAI_CONFIG_ALIASED_ENTRY(DDTRACE_CONFIG_##name, name, type, __VA_ARGS__),
+#else
+#define CALIAS(type, name, default, aliases, ...) ZAI_CONFIG_ALIASED_ENTRY(DDTRACE_CONFIG_##name, name, type, default, dd_config_aliases_##name, ##__VA_ARGS__),
+#endif
 static zai_config_entry config_entries[] = {DD_CONFIGURATION};
 #undef CALIAS
 #undef CONFIG
@@ -83,6 +106,9 @@ bool runtime_config_first_init = false;
 
 static char dd_tolower_ascii(char c) { return c >= 'A' && c <= 'Z' ? c - ('A' - 'a') : c; }
 
+#if defined(_WIN32) && PHP_VERSION_ID < 80000 && !defined(restrict)
+#define restrict
+#endif
 static void dd_copy_tolower(char *restrict dst, const char *restrict src) {
     while (*src) {
         *(dst++) = dd_tolower_ascii(*(src++));

ext/engine_hooks.c
@@ -5,9 +5,19 @@
 
 #include "ddtrace.h"
 #include <components/log/log.h>
+#include "zend_hrtime.h"
 #include "span.h"
 #include "zend_extensions.h"
 #include <zai_string/string.h>
+
+#ifdef PHP_WIN32
+#include "win32/param.h"
+#include "win32/winutil.h"
+#define GET_DL_ERROR()	php_win_err()
+#else
+#include <sys/param.h>
+#define GET_DL_ERROR()	DL_ERROR()
+#endif
 
 ZEND_EXTERN_MODULE_GLOBALS(ddtrace);
 
@@ -18,9 +28,10 @@ static void _compile_mshutdown(void);
 
 void (*ddtrace_prev_error_cb)(DDTRACE_ERROR_CB_PARAMETERS);
 
-void (*profiling_notify_trace_finished)(uint64_t local_root_span_id,
+typedef void (*profiling_notify_trace_finished_t)(uint64_t local_root_span_id,
                                         zai_str span_type,
-                                        zai_str resource) = NULL;
+                                        zai_str resource);
+profiling_notify_trace_finished_t profiling_notify_trace_finished = NULL;
 
 void (*profiling_interrupt_function)(zend_execute_data *) = NULL;
 
@@ -30,15 +41,15 @@ void dd_search_for_profiling_symbols(void *arg) {
     if (extension->name && strcmp(extension->name, "datadog-profiling") == 0) {
         DL_HANDLE handle = extension->handle;
 
-        profiling_interrupt_function = DL_FETCH_SYMBOL(handle, "datadog_profiling_interrupt_function");
+        profiling_interrupt_function = (void(*)(zend_execute_data *))DL_FETCH_SYMBOL(handle, "datadog_profiling_interrupt_function");
         if (UNEXPECTED(!profiling_interrupt_function)) {
             LOG(Warn, "[Datadog Trace] Profiling was detected, but locating symbol %s failed: %s\n", "datadog_profiling_interrupt_function",
-                               DL_ERROR());
+                               GET_DL_ERROR());
         }
 
-        profiling_notify_trace_finished = DL_FETCH_SYMBOL(handle, "datadog_profiling_notify_trace_finished");
+        profiling_notify_trace_finished = (profiling_notify_trace_finished_t)DL_FETCH_SYMBOL(handle, "datadog_profiling_notify_trace_finished");
         if (UNEXPECTED(!profiling_notify_trace_finished)) {
-            LOG(Warn, "[Datadog Trace] Profiling v%s was detected, but locating symbol failed: %s\n", extension->version, DL_ERROR());
+            LOG(Warn, "[Datadog Trace] Profiling v%s was detected, but locating symbol failed: %s\n", extension->version, GET_DL_ERROR());
         }
     }
 }
@@ -62,19 +73,11 @@ void ddtrace_engine_hooks_mshutdown(void) {
     _compile_mshutdown();
 }
 
-static uint64_t _get_microseconds() {
-    struct timespec time;
-    if (clock_gettime(CLOCK_MONOTONIC, &time) == 0) {
-        return time.tv_sec * 1000000U + time.tv_nsec / 1000U;
-    }
-    return 0U;
-}
-
 static zend_op_array *_dd_compile_file(zend_file_handle *file_handle, int type) {
     zend_op_array *res;
-    uint64_t start = _get_microseconds();
+    uint64_t start = zend_hrtime();
     res = _prev_compile_file(file_handle, type);
-    DDTRACE_G(compile_time_microseconds) += (int64_t)(_get_microseconds() - start);
+    DDTRACE_G(compile_time_microseconds) += (int64_t)(zend_hrtime() - start);
     return res;
 }
 

zend_abstract_interface/env/env.h
@@ -61,9 +61,8 @@ typedef struct zai_env_buffer_s {
  * and 'buf.ptr' will be an empty string; e.g. this API does not attempt to
  * truncate the value to accommodate the buffer size.
  */
-zai_env_result __attribute__((warn_unused_result))
-zai_getenv_ex(zai_str name, zai_env_buffer buf, bool pre_rinit);
-static inline __attribute__((warn_unused_result)) zai_env_result zai_getenv(zai_str name, zai_env_buffer buf) {
+zai_env_result zai_getenv_ex(zai_str name, zai_env_buffer buf, bool pre_rinit);
+static inline zai_env_result zai_getenv(zai_str name, zai_env_buffer buf) {
     return zai_getenv_ex(name, buf, false);
 }
 