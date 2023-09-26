18c80107fedaf39d8471bdc74e9021b851954249
Alexandre Choura <alexandre.choura@datadoghq.com>
2023-09-26 14:11:59 +0200

    debug: Set open_basedir :shrug:

--- tests/Integrations/Laravel/V10_x/CommonScenariosTest.php
+++ tests/Integrations/Laravel/V10_x/CommonScenariosTest.php
@@ -8,4 +8,10 @@ class CommonScenariosTest extends \DDTrace\Tests\Integrations\Laravel\V9_x\Commo
     {
         return __DIR__ . '/../../../Frameworks/Laravel/Version_10_x/public/index.php';
     }
+
+    public static function ddSetUpBeforeClass()
+    {
+        parent::ddSetUpBeforeClass();
+        ini_set('open_basedir', __DIR__ . '/../../../Frameworks/Laravel/Version_10_x/');
+    }
 }
