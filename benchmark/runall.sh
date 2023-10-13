666547d9cfe20d61830fcc62ba7fc3cfab14ed8b
Florian Engelhardt <florian.engelhardt@datadoghq.com>
2023-10-13 14:15:05 +0200

    Add timeline memory test

--- benchmark/runall.sh
+++ benchmark/runall.sh
@@ -3,6 +3,11 @@
 set -ex
 
 cd ../profiling/
+
+cargo build --release --features trigger_time_samples
+
+sirun benches/memory.json
+
 sed -i -e "s/crate-type.*$/crate-type = [\"rlib\"]/g" Cargo.toml
 
 cargo bench --features stack_walking_tests -- --noplot
