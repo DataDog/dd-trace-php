5b4f421a3f104aeda4ff313360fa132d6eecd6d4
Alexandre Choura <alexandre.choura@datadoghq.com>
2023-11-09 15:25:46 +0100

    Remove useless comment

--- bridge/dd_init.php
+++ bridge/dd_init.php
@@ -7,6 +7,7 @@ use DDTrace\Integrations\IntegrationsLoader;
 if (\PHP_VERSION_ID < 70000) {
     \date_default_timezone_set(@\date_default_timezone_get());
 }
+
 // Required classes and functions
 require __DIR__ . '/autoload.php';
 
--- tests/Unit/Util/ConventionTest.php
+++ tests/Unit/Util/ConventionTest.php
@@ -19,33 +19,6 @@ final class ConventionTest extends BaseTestCase
 
     public function providerConventionData()
     {
-        /*
-         * [
-            ("http.server.request", SK_SERVER, {"http.request.method": "GET"}),
-            ("http.client.request", SK_CLIENT, {"http.request.method": "GET"}),
-            ("redis.query", SK_CLIENT, {"db.system": "Redis"}),
-            ("kafka.receive", SK_CLIENT, {"messaging.system": "Kafka", "messaging.operation": "Receive"}),
-            ("kafka.receive", SK_SERVER, {"messaging.system": "Kafka", "messaging.operation": "Receive"}),
-            ("kafka.receive", SK_PRODUCER, {"messaging.system": "Kafka", "messaging.operation": "Receive"}),
-            ("kafka.receive", SK_CONSUMER, {"messaging.system": "Kafka", "messaging.operation": "Receive"}),
-            ("aws.s3.request", SK_CLIENT, {"rpc.system": "aws-api", "rpc.service": "S3"}),
-            ("aws.client.request", SK_CLIENT, {"rpc.system": "aws-api"}),
-            ("grpc.client.request", SK_CLIENT, {"rpc.system": "GRPC"}),
-            ("grpc.server.request", SK_SERVER, {"rpc.system": "GRPC"}),
-            ("aws.my-function.invoke", SK_CLIENT, {"faas.invoked_provider": "aws", "faas.invoked_name": "My-Function"}),
-            ("datasource.invoke", SK_SERVER, {"faas.trigger": "Datasource"}),
-            ("graphql.server.request", SK_SERVER, {"graphql.operation.type": "query"}),
-            ("amqp.server.request", SK_SERVER, {"network.protocol.name": "Amqp"}),
-            ("server.request", SK_SERVER, None),
-            ("amqp.client.request", SK_CLIENT, {"network.protocol.name": "Amqp"}),
-            ("client.request", SK_CLIENT, None),
-            ("internal", SK_INTERNAL, None),
-            ("consumer", SK_CONSUMER, None),
-            ("producer", SK_PRODUCER, None),
-            ("otel_unknown", None, None),
-        ],
-         */
-
         return [
             ['http.server.request',     Tag::SPAN_KIND_VALUE_SERVER,        ['http.request.method' => 'GET']],
             ['http.client.request',     Tag::SPAN_KIND_VALUE_CLIENT,        ['http.request.method' => 'GET']],
