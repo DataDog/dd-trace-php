45653474dead62ab9c1c6bd1abc47d48fff1d2a9
Alexandre Choura <alexandre.choura@datadoghq.com>
2023-11-20 16:25:25 +0100

    Add release notes for 0.94.0

--- src/DDTrace/Tracer.php
+++ src/DDTrace/Tracer.php
@@ -24,7 +24,7 @@ final class Tracer implements TracerInterface
      * Must begin with a number for Debian packaging requirements
      * Must use single-quotes for packaging script to work
      */
-    const VERSION = '0.93.0';
+    const VERSION = '0.94.0';
 
     /**
      * @var Span[][]
