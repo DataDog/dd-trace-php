46b89d068518b1599883ce057e7409ae96e5f0a5
Alexandre Choura <alexandre.choura@datadoghq.com>
2023-12-21 13:20:06 +0100

    Run `benchmarks` & `benchmarks_opcache`

--- benchmark/runall.sh
+++ benchmark/runall.sh
@@ -2,6 +2,7 @@
 
 set -exu
 
+# Run Profiling Benchmarks
 cd ../profiling/
 
 cargo build --release --features trigger_time_sample
@@ -11,3 +12,12 @@ sirun benches/memory.json > "$ARTIFACTS_DIR/sirun_mem.ndjson"
 sed -i -e "s/crate-type.*$/crate-type = [\"rlib\"]/g" Cargo.toml
 
 cargo bench --features stack_walking_tests -- --noplot
+
+# Run Trace Benchmarks
+cd ..
+## Non-OPCache Benchmarks > tests/Benchmarks/results.csv
+make benchmarks
+cp tests/Benchmarks/tracer-bench-results.csv "$ARTIFACTS_DIR"
+## OPCache Benchmarks > tests/Benchmarks/results-opcache.csv
+make benchmarks_opcache
+cp tests/Benchmarks/tracer-bench-results-opcache.csv "$ARTIFACTS_DIR"
