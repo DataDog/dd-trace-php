102a580bdbcd69382bb136996a9a501f891e2992
Alexandre Choura <alexandre.choura@datadoghq.com>
2023-12-29 14:29:00 +0100

    Consider a `profiler` and `tracer` scenario

--- benchmark/runall.sh
+++ benchmark/runall.sh
@@ -2,23 +2,27 @@
 
 set -exu
 
-# Run Profiling Benchmarks
-cd ../profiling/
+if [ "$SCENARIO" = "profiler" ]; then
+  # Run Profiling Benchmarks
+  cd ../profiling/
 
-cargo build --release --features trigger_time_sample
+  cargo build --release --features trigger_time_sample
 
-sirun benches/memory.json > "$ARTIFACTS_DIR/sirun_mem.ndjson"
+  sirun benches/memory.json > "$ARTIFACTS_DIR/sirun_mem.ndjson"
 
-sed -i -e "s/crate-type.*$/crate-type = [\"rlib\"]/g" Cargo.toml
+  sed -i -e "s/crate-type.*$/crate-type = [\"rlib\"]/g" Cargo.toml
 
-cargo bench --features stack_walking_tests -- --noplot
+  cargo bench --features stack_walking_tests -- --noplot
+elif [ "$SCENARIO" = "tracer" ]; then
+  # Run Trace Benchmarks
+  cd ..
+  make composer_tests_update
 
-# Run Trace Benchmarks
-cd ..
-make composer_tests_update
-## Non-OPCache Benchmarks
-make benchmarks
-cp tests/Benchmarks/reports/tracer-bench-results.csv "$ARTIFACTS_DIR"
-## OPCache Benchmarks
-make benchmarks_opcache
-cp tests/Benchmarks/reports/tracer-bench-results-opcache.csv "$ARTIFACTS_DIR"
+  ## Non-OPCache Benchmarks
+  make benchmarks
+  cp tests/Benchmarks/reports/tracer-bench-results.csv "$ARTIFACTS_DIR"
+
+  ## OPCache Benchmarks
+  make benchmarks_opcache
+  cp tests/Benchmarks/reports/tracer-bench-results-opcache.csv "$ARTIFACTS_DIR"
+fi
