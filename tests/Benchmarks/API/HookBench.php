cc8519fe4a2ca6d8b20482686256b2ca2e3c4455
Alexandre Choura <alexandre.choura@datadoghq.com>
2023-12-29 14:39:15 +0100

    Compare the different hooking functions

--- tests/Benchmarks/API/HookBench.php
+++ tests/Benchmarks/API/HookBench.php
@@ -15,31 +15,108 @@ class HookBench
     public function benchWithoutHook()
     {
         for ($i = 0; $i < 1000; $i++) {
-            $this->dummyFunction();
+            $this->dummyMethod1();
         }
     }
 
-    public function setUp()
-    {
-        \DDTrace\trace_method('Benchmarks\API\HookBench', 'dummyFunction', function () { });
-    }
-
     /**
-     * @BeforeMethods("setUp")
+     * @BeforeMethods("setUpTraceMethod")
      * @Revs(1000)
      * @Iterations(10)
      * @OutputTimeUnit("microseconds")
      * @RetryThreshold(10.0)
      */
-    public function benchHookOverhead()
+    public function benchHookOverheadTraceMethod()
     {
         for ($i = 0; $i < 1000; $i++) {
-            $this->dummyFunction();
+            $this->dummyMethod1();
         }
     }
 
-    public function dummyFunction(): int
+    /**
+     * @BeforeMethods("setUpTraceFunction")
+     * @Revs(1000)
+     * @Iterations(10)
+     * @OutputTimeUnit("microseconds")
+     * @RetryThreshold(10.0)
+     */
+    public function benchHookOverheadTraceFunction()
+    {
+        for ($i = 0; $i < 1000; $i++) {
+            dummyFunction1();
+        }
+    }
+
+    /**
+     * @BeforeMethods("setUpInstallHookOnMethod")
+     * @Revs(1000)
+     * @Iterations(10)
+     * @OutputTimeUnit("microseconds")
+     * @RetryThreshold(10.0)
+     */
+    public function benchHookOverheadInstallHookOnMethod()
+    {
+        for ($i = 0; $i < 1000; $i++) {
+            $this->dummyMethod2();
+        }
+    }
+
+    /**
+     * @BeforeMethods("setUpInstallHookOnFunction")
+     * @Revs(1000)
+     * @Iterations(10)
+     * @OutputTimeUnit("microseconds")
+     * @RetryThreshold(10.0)
+     */
+    public function benchHookOverheadInstallHookOnFunction()
+    {
+        for ($i = 0; $i < 1000; $i++) {
+            dummyFunction2();
+        }
+    }
+
+    public function setUpTraceMethod()
+    {
+        \DDTrace\trace_method('Benchmarks\API\HookBench', 'dummyMethod1', function () { });
+    }
+
+    public function setUpTraceFunction()
+    {
+        \DDTrace\trace_function('Benchmarks\API\dummyFunction1', function () { });
+    }
+
+    public function setUpInstallHookOnMethod()
+    {
+        \DDTrace\install_hook('Benchmarks\API\HookBench::dummyMethod2', function () { });
+    }
+
+    public function setUpInstallHookOnFunction()
+    {
+        \DDTrace\install_hook('Benchmarks\API\dummyFunction2', function () { });
+    }
+
+    public function dummyMethod1(): int
     {
         return time();
     }
+
+    public function dummyMethod2(): int
+    {
+        return time();
+    }
+
+    public function dummyMethod3(): int
+    {
+        return time();
+    }
+}
+
+function dummyFunction1(): int
+{
+    return time();
+}
+
+function dummyFunction2(): int
+{
+    return time();
 }
