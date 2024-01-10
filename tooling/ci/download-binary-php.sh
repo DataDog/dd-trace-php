78bc763f5a9a4cbf8562704bec4d22366850d65f
roberto montero <roberto.montero@datadoghq.com>
2024-01-10 11:07:42 +0100

    generate arm images

--- tooling/ci/download-binary-php.sh
+++ tooling/ci/download-binary-php.sh
@@ -1,4 +1,5 @@
 VERSION=${1:-'dev'}
+PLATFORM=${2:-'x86_64'}
 
 echo "Load $VERSION binary "
 
@@ -9,11 +10,11 @@ cd tooling/ci/binaries
 source /download-binary-tracer.sh
 
 if [ $VERSION = 'dev' ]; then
-    get_circleci_artifact "gh/DataDog/dd-trace-php" "build_packages" "package extension" "datadog-php-tracer-.*.x86_64.tar.gz" "datadog-php-tracer.x86_64.tar.gz"
-    get_github_action_artifact "DataDog/dd-appsec-php" "package.yml" "master" "dd-appsec-php-*-amd64.tar.gz" "dd-appsec-php-amd64.tar.gz"
+    get_circleci_artifact "gh/DataDog/dd-trace-php" "build_packages" "package extension" "dd-library-php-.*-$PLATFORM-linux-gnu.tar.gz" "dd-library-php-$PLATFORM-linux-gnu.tar.gz"
+    get_circleci_artifact "gh/DataDog/dd-trace-php" "build_packages" "package extension" "datadog-setup.php" "datadog-setup.php"
 elif [ $VERSION = 'prod' ]; then
-    get_github_release_asset "DataDog/dd-trace-php" "datadog-php-tracer-.*.x86_64.tar.gz" "datadog-php-tracer.x86_64.tar.gz"
-    get_github_release_asset "DataDog/dd-appsec-php" "dd-appsec-php-.*-amd64.tar.gz" "dd-appsec-php-amd64.tar.gz"
+    get_github_release_asset "DataDog/dd-trace-php" "dd-library-php-.*-$$PLATFORM-linux-gnu.tar.gz" "dd-library-php-$$PLATFORM-linux-gnu.tar.gz"
+    get_github_release_asset "DataDog/dd-trace-php" "datadog-setup.php" "datadog-setup.php"
 else
     echo "Don't know how to load version $VERSION for $TARGET"
 fi
