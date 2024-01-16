5334fb55ab669081abab950bf812fdd81cb84d6d
Florian Engelhardt <florian.engelhardt@datadoghq.com>
2024-01-16 23:01:01 +0100

    fix(profiler) prevent emitting empty profiles (#2397)
    * panic!
    
    * do not send empty samples to the other thread
    
    * ignore empty samples in sampling thread
    
    * Update profiling/src/profiling/mod.rs
    
    Co-authored-by: Levi Morrison <levi.morrison@datadoghq.com>
    
    * if profiling is disabled, we do not need any other checks
    
    ---------
    
    Co-authored-by: Levi Morrison <levi.morrison@datadoghq.com>

--- profiling/src/profiling/mod.rs
+++ profiling/src/profiling/mod.rs
@@ -346,6 +346,12 @@ impl TimeCollector {
         profiles: &mut HashMap<ProfileIndex, InternalProfile>,
         started_at: &WallTime,
     ) {
+        if message.key.sample_types.len() == 0 {
+            // profiling disabled, this should not happen!
+            warn!("A sample with no sample types was recorded in the profiler. Please report this to Datadog.");
+            return;
+        }
+
         let profile: &mut InternalProfile = if let Some(value) = profiles.get_mut(&message.key) {
             value
         } else {
@@ -1034,6 +1040,7 @@ impl Profiler {
 
         let mut sample_types = Vec::with_capacity(SAMPLE_TYPES.len());
         let mut sample_values = Vec::with_capacity(SAMPLE_TYPES.len());
+
         if locals.profiling_enabled {
             // sample, wall-time, cpu-time
             let len = 2 + locals.profiling_experimental_cpu_time_enabled as usize;
