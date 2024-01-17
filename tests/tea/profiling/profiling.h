991594b830299ff8159088203e41932e16f9812b
Florian Engelhardt <florian.engelhardt@datadoghq.com>
2024-01-17 16:41:29 +0100

    update symbol

--- tests/tea/profiling/profiling.h
+++ tests/tea/profiling/profiling.h
@@ -9,7 +9,7 @@ BEGIN_EXTERN_C()
 #include <components/stack-sample/stack-sample.h>
 
 ZEND_API datadog_php_stack_sample tea_get_last_stack_sample(void);
-ZEND_API void datadog_profiling_interrupt_function(zend_execute_data *execute_data);
+ZEND_API void ddog_php_prof_interrupt_function(zend_execute_data *execute_data);
 END_EXTERN_C()
 
 #endif
