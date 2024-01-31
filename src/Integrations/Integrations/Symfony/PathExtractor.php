371ac24bbbdb3904ad023deb255cb0ea3ccad75d
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2024-01-31 14:09:11 +0100

    Refactor path extractor

--- src/Integrations/Integrations/Symfony/PathExtractor.php
+++ src/Integrations/Integrations/Symfony/PathExtractor.php
@@ -3,7 +3,6 @@
 namespace DDTrace\Integrations\Symfony;
 
 use ReflectionClass;
-use ReflectionMethod;
 use ReflectionAttribute;
 use ReflectionException;
 
@@ -23,57 +22,45 @@ class PathExtractor
 
         if (!class_exists('\Symfony\Component\Routing\Annotation\Route') &&
             class_exists('\Sensio\Bundle\FrameworkExtraBundle\Configuration\Route')
-         ) {
+        ) {
             $this->routeAnnotationClass = Sensio\Bundle\FrameworkExtraBundle\Configuration\Route::class;
         }
     }
 
     public function extract($classMethod, $routeName, $locale)
     {
-        $className = $classMethod; //It may not come with method when invokable controller
-        $methodName = null;
-        if (strpos($classMethod, "::") !== false) {
-            $exploded = explode("::", $classMethod);
-            $className = $exploded[0];
-            $methodName = $exploded[1];
-        }
-
-        if (!class_exists($className)) {
+        if (!$routeName) {
             return;
         }
-        $class = new ReflectionClass($className);
 
-        $globals = $this->getGlobals($class);
-        if ($methodName == null) {
-            if (!$class->hasMethod('__invoke')) {
-                return;
-            }
-            $methodName = '__invoke';
-            $globals = $this->resetGlobals();
-        }
+        $className = strtok($classMethod, "::");
+        $methodName = strtok("::");
+        $invoke = false;
 
         try {
-            $method = $class->getMethod($methodName);
+            if ($methodName === false) {
+                $invoke = true;
+                $methodName = '__invoke';
+            }
+            $method = new \ReflectionMethod($className, $methodName);
+            $class = $method->getDeclaringClass();
+
+            if ($invoke) {
+                $globals = $this->resetGlobals();
+            } else {
+                $globals = $this->getGlobals($class);
+            }
         } catch (ReflectionException $e) {
             return;
         }
 
-        $paths = [];
-        $annotationsTarget = $methodName == '__invoke' ? $class: $method;
+        $annotationsTarget = $invoke ? $class: $method;
 
         foreach ($this->getAnnotations($annotationsTarget) as $annot) {
-            $path = $this->getPath($annot, $globals, $class, $method, $routeName);
+            $path = $this->getPath($annot, $globals, $className, $methodName, $routeName, $locale);
             if ($path !== null) {
-                $paths = array_merge($paths, $path);
+                return $path;
             }
-        }
-        if (!empty($paths) && is_array($paths)) {
-            if (isset($paths[$locale])) {
-                return $paths[$locale];
-            } elseif (isset($paths[$this->defaultRouteIndex])) {
-                return $paths[$this->defaultRouteIndex];
-            }
-            return reset($paths);
         }
 
         return;
@@ -102,47 +89,47 @@ class PathExtractor
         }
     }
 
-    protected function getPath($annot, array $globals, ReflectionClass $class, ReflectionMethod $method, $routeName)
+    protected function getPath($annot, array $globals, $className, $methodName, $routeName, $receivedLocale)
     {
-        $name = $annot->getName() ? $annot->getName(): $this->getDefaultRouteName($class, $method);
+        $name = $annot->getName() ? $annot->getName(): $this->getDefaultRouteName($className, $methodName);
         $name = $globals['name'].$name;
+
+        if ($routeName !== $name) {
+            return;
+        }
 
         $path = \method_exists($annot, 'getLocalizedPaths') && $annot->getLocalizedPaths() ?
             $annot->getLocalizedPaths():
             $annot->getPath();
         $prefix = $globals['localized_paths'] ? $globals['localized_paths']: $globals['path'];
-        $paths = [];
+
         if (\is_array($path)) {
             if (!\is_array($prefix)) {
-                foreach ($path as $locale => $localePath) {
-                    if ($routeName ==  $name) {
-                        $paths[$locale] = $prefix.$localePath;
-                    }
+                if (isset($path[$receivedLocale])) {
+                    return $prefix.$path[$receivedLocale];
                 }
+                return $prefix.reset($path);
             } elseif (array_diff_key($prefix, $path)) {
                 return;
             } else {
-                foreach ($path as $locale => $localePath) {
-                    if (!isset($prefix[$locale])) {
-                        return;
-                    }
-                    if ($routeName ==  $name) {
-                        $paths[$locale] = $prefix[$locale].$localePath;
-                    }
+                if (isset($prefix[$receivedLocale]) && isset($path[$receivedLocale])) {
+                    return $prefix[$receivedLocale].$path[$receivedLocale];
+                } elseif (isset($prefix[$receivedLocale])) {
+                    return $prefix[$receivedLocale].reset($path);
+                } elseif (isset($path[$receivedLocale])) {
+                    return reset($prefix).$path[$receivedLocale];
                 }
+                return reset($prefix).reset($path);
             }
         } elseif (\is_array($prefix)) {
-            foreach ($prefix as $locale => $localePrefix) {
-                if ($routeName ==  $name) {
-                    $paths[$locale] = $localePrefix.$path;
-                }
+            if (isset($prefix[$receivedLocale])) {
+                return $prefix[$receivedLocale].$path;
             }
-        } elseif ($routeName ==  $name) {
-            $path = $prefix.$path;
-            $paths[] = empty($path) ? '/': $path;
+            return reset($prefix).$path;
         }
 
-        return $paths;
+        $path = $prefix.$path;
+        return empty($path) ? '/': $path;
     }
 
     private function resetGlobals(): array
@@ -186,9 +173,9 @@ class PathExtractor
         return $globals;
     }
 
-    protected function getDefaultRouteName(ReflectionClass $class, ReflectionMethod $method)
+    protected function getDefaultRouteName($className, $methodName)
     {
-        $name = str_replace('\\', '_', $class->name).'_'.$method->name;
+        $name = str_replace('\\', '_', $className).'_'.$methodName;
         $name = \function_exists('mb_strtolower') && preg_match('//u', $name) ? mb_strtolower($name, 'UTF-8') : strtolower($name);
         if ($this->defaultRouteIndex > 0) {
             $name .= '_'.$this->defaultRouteIndex;
@@ -197,7 +184,7 @@ class PathExtractor
 
         $name = preg_replace('/(bundle|controller)_/', '_', $name);
 
-        if (str_ends_with($method->name, 'Action') || str_ends_with($method->name, '_action')) {
+        if (str_ends_with($methodName, 'Action') || str_ends_with($methodName, '_action')) {
             $name = preg_replace('/action(_\d+)?$/', '\\1', $name);
         }
 
