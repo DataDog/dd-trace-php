bda6c7e6aedaeda08e427522015e7b754bd74865
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2024-02-08 12:59:11 +0100

    Add login events to Laravel 10

--- tests/Integrations/Laravel/V10_x/LoginEventsTest.php
+++ tests/Integrations/Laravel/V10_x/LoginEventsTest.php
@@ -53,6 +53,24 @@ class LoginEventsTest extends WebFrameworkTestCase
         $this->connection()->exec("insert into users (id, name, email, password) VALUES (".$id.", '".$name."', '".$email."', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi')");
     }
 
+    public function testUserLoginSuccessEvent()
+    {
+        $id = 1234;
+        $name = 'someName';
+        $email = 'test-user@email.com';
+        $this->createUser($id, $name, $email);
+
+        $this->login($email);
+
+        $events = AppsecStatus::getInstance()->getEvents();
+        $this->assertEquals(1, count($events));
+        $this->assertEquals($id, $events[0]['userId']);
+        $this->assertEquals($name, $events[0]['metadata']['name']);
+        $this->assertEquals($email, $events[0]['metadata']['email']);
+        $this->assertTrue($events[0]['automated']);
+        $this->assertEquals('track_user_login_success_event', $events[0]['eventName']);
+    }
+
     public function testLoggedInCalls()
     {
         $id = 1234;
@@ -88,5 +106,43 @@ class LoginEventsTest extends WebFrameworkTestCase
         $this->assertEquals($id, $meta['usr.id']);
         $this->assertEquals($name, $meta['usr.name']);
         $this->assertEquals($email, $meta['usr.email']);
+    }
+
+    public function testUserLoginFailureEvent()
+    {
+        $email = 'test-user-non-existing@email.com';
+
+        $this->login($email);
+
+        $events = AppsecStatus::getInstance()->getEvents();
+        $this->assertEquals(1, count($events));
+        $this->assertTrue($events[0]['automated']);
+        $this->assertEquals('track_user_login_failure_event', $events[0]['eventName']);
+    }
+
+    public function testUserSignUp()
+    {
+        $email = 'test-user-new@email.com';
+        $name = 'somename';
+        $password = 'somepassword';
+
+       $this->call(
+           GetSpec::create('Signup', sprintf('/login/signup?email=%s&name=%s&password=%s',$email, $email, $password))
+       );
+
+       $users = $this->connection()->query("SELECT * FROM users where email='".$email."'")->fetchAll();
+
+        $this->assertEquals(1, count($users));
+
+        $signUpEvent = null;
+        foreach(AppsecStatus::getInstance()->getEvents() as $event)
+        {
+            if ($event['eventName'] == 'track_user_signup_event') {
+                $signUpEvent = $event;
+            }
+        }
+
+        $this->assertTrue($signUpEvent['automated']);
+        $this->assertEquals($users[0]['id'], $signUpEvent['userId']);
     }
 }
