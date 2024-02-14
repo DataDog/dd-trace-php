e90aafd9a44f3480f60913be697e869f282ec3ff
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2024-02-14 10:38:28 +0100

    Configure database modes

--- tests/Frameworks/Laravel/Version_9_x/config/database.php
+++ tests/Frameworks/Laravel/Version_9_x/config/database.php
@@ -61,6 +61,14 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'modes' => [
+                'ONLY_FULL_GROUP_BY',
+                'STRICT_TRANS_TABLES',
+                'NO_ZERO_IN_DATE',
+                'NO_ZERO_DATE',
+                'ERROR_FOR_DIVISION_BY_ZERO',
+                'NO_ENGINE_SUBSTITUTION'
+            ],
         ],
 
         'pgsql' => [
