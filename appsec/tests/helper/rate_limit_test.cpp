abe74509f87297afba3d470174312ac012e773d3
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2024-02-28 16:28:23 +0100

    Add more tests

--- appsec/tests/helper/rate_limit_test.cpp
+++ appsec/tests/helper/rate_limit_test.cpp
@@ -4,6 +4,7 @@
 // This product includes software developed at Datadog
 // (https://www.datadoghq.com/). Copyright 2021 Datadog, Inc.
 #include "common.hpp"
+#include <chrono>
 #include <iostream>
 #include <queue>
 #include <rate_limit.hpp>
@@ -15,22 +16,9 @@ namespace mock {
 
 class timer : public dds::timer {
 public:
-    system_clock::duration time_since_epoch()
-    {
-        if (responses.empty()) {
-            return system_clock::duration(0);
-        }
-        auto result = responses.front();
-
-        if (responses.size() >
-            1) { // Last element will be used during the rest of the calls
-            responses.pop();
-        }
-
-        return result;
-    }
+    system_clock::duration time_since_epoch() { return time; }
     ~timer() = default;
-    std::queue<system_clock::duration> responses;
+    system_clock::duration time;
 };
 
 class rate_limiter : public dds::rate_limiter {
@@ -45,14 +33,14 @@ public:
 };
 } // namespace mock
 
-TEST(RateLimitTest, OnlyAllowedMaxPerSecond)
+TEST(RateLimitTest, OnlyAllowedMaxPerSecondNonConsecutiveSeconds)
 {
-    auto first_round_time = system_clock::now();
+    auto first_round_time = system_clock::duration(1708963615);
     auto second_round_time = first_round_time + std::chrono::seconds(5);
 
     std::unique_ptr<mock::timer> timer = std::make_unique<mock::timer>();
     // Four calls within the same second
-    timer->responses.push(first_round_time.time_since_epoch());
+    timer->time = first_round_time;
 
     mock::rate_limiter rate_limiter(2);
     rate_limiter.set_timer(std::move(timer));
@@ -66,7 +54,7 @@ TEST(RateLimitTest, OnlyAllowedMaxPerSecond)
     EXPECT_EQ(2, allowed);
 
     timer = std::make_unique<mock::timer>();
-    timer->responses.push(second_round_time.time_since_epoch());
+    timer->time = second_round_time;
     rate_limiter.set_timer(std::move(timer));
 
     allowed = 0;
@@ -76,6 +64,40 @@ TEST(RateLimitTest, OnlyAllowedMaxPerSecond)
         }
     }
     EXPECT_EQ(2, allowed);
+}
+
+TEST(RateLimitTest, OnlyAllowedMaxPerSecondConsecutiveSeconds)
+{
+    auto first_round_time = system_clock::duration(1708963615);
+    auto second_round_time = first_round_time + std::chrono::seconds(1);
+
+    std::unique_ptr<mock::timer> timer = std::make_unique<mock::timer>();
+    // Four calls within the same second
+    timer->time = first_round_time;
+
+    mock::rate_limiter rate_limiter(2);
+    rate_limiter.set_timer(std::move(timer));
+
+    int allowed = 0;
+    for (int i = 0; i < 10; i++) {
+        if (rate_limiter.allow()) {
+            allowed++;
+        }
+    }
+    EXPECT_EQ(2, allowed);
+
+    timer = std::make_unique<mock::timer>();
+    timer->time = second_round_time;
+    rate_limiter.set_timer(std::move(timer));
+
+    allowed = 0;
+    for (int i = 0; i < 10; i++) {
+        if (rate_limiter.allow()) {
+            allowed++;
+        }
+    }
+    // It is a bit random
+    EXPECT_TRUE(allowed >= 0 && allowed <= 2);
 }
 
 TEST(RateLimitTest, WhenNotMaxPerSecondItAlwaysAllow)
