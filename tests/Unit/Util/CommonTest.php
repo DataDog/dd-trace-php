43c5fa0a12840656a50cadb6220a1bf5990c8816
Alexandre Choura <alexandre.choura@datadoghq.com>
2024-03-04 14:17:03 +0100

    fix: run OrphansTest in a separate process with correct envs setup

--- tests/Unit/Util/CommonTest.php
+++ tests/Unit/Util/CommonTest.php
@@ -3,15 +3,11 @@
 namespace DDTrace\Tests\Unit\Util;
 
 use DDTrace\Tests\Common\IntegrationTestCase;
-use DDTrace\Tests\Common\MultiPHPUnitVersionAdapter;
-use DDTrace\Tests\Common\SnapshotTestTrait;
 use DDTrace\Tests\Common\TracerTestTrait;
-use DDTrace\Tests\Common\Utils;
 use DDTrace\Util\Common;
 
-final class CommonTest extends IntegrationTestCase
+final class OrphansTest extends IntegrationTestCase
 {
-    use SnapshotTestTrait;
     use TracerTestTrait;
 
     static function foo()
@@ -21,17 +17,18 @@ final class CommonTest extends IntegrationTestCase
 
     protected function ddSetUp()
     {
-        Utils::putEnvAndReloadConfig([
+        $this->putEnvAndReloadConfig([
             'DD_TRACE_REMOVE_AUTOINSTRUMENTATION_ORPHANS=1',
             'DD_TRACE_GENERATE_ROOT_SPAN=0',
-            'DD_TRACE_DEBUG=1',
             'DD_TRACE_AUTO_FLUSH_ENABLED=1',
-            'DD_TRACE_AGENT_FLUSH_INTERVAL=333',
-            'DD_INSTRUMENTATION_TELEMETRY_ENABLED=0',
-            'DD_TRACE_AGENT_RETRIES=3',
-            'DD_TRACE_AGENT_URL=http://request-replayer:80',
-            'DD_TRACE_AGENT_PORT=80'
+            'DD_TRACE_CURL_ENABLED=0'
         ]);
+    }
+
+    static function getSampling($response) {
+        $root = json_decode($response[0]["body"], true);
+        $spans = $root["chunks"][0]["spans"] ?? $root[0];
+        return $spans[0]["metrics"]["_sampling_priority_v1"];
     }
 
     function testOrphansRemovalWithAgentSampling()
@@ -42,22 +39,22 @@ final class CommonTest extends IntegrationTestCase
 
         $this->resetRequestDumper();
 
-        $this->setResponse(["rate_by_service" => ["service:,env:" => 0]]);
-        CommonTest::foo();
-        usleep(5000 * 2 * 1000);
-        $response = $this->retrieveDumpedTraceData();
-        echo json_encode($response, JSON_PRETTY_PRINT) . PHP_EOL;
-
-        $this->setResponse(["rate_by_service" => ["service:,env:" => 0]]);
-        CommonTest::foo();
-        usleep(5000 * 2 * 1000);
-        $response = $this->retrieveDumpedTraceData();
-        echo json_encode($response, JSON_PRETTY_PRINT) . PHP_EOL;
-
         $this->setResponse(["rate_by_service" => ["service:,env:" => 1]]);
-        CommonTest::foo();
-        usleep(5000 * 2 * 1000);
+        self::foo();
+        usleep(333 * 2 * 1000); // DD_TRACE_AGENT_FLUSH_INTERVAL * 2 * 1000
         $response = $this->retrieveDumpedTraceData();
-        echo json_encode($response, JSON_PRETTY_PRINT) . PHP_EOL;
+        $this->assertEquals(0, self::getSampling($response));
+
+        $this->setResponse(["rate_by_service" => ["service:,env:" => 0]]);
+        self::foo();
+        usleep(333 * 2 * 1000);
+        $response = $this->retrieveDumpedTraceData();
+        $this->assertEquals(0, self::getSampling($response));
+
+        $this->setResponse(["rate_by_service" => ["service:,env:" => 0.5]]);
+        self::foo();
+        usleep(333 * 2 * 1000);
+        $response = $this->retrieveDumpedTraceData();
+        $this->assertEquals(0, self::getSampling($response));
     }
 }
