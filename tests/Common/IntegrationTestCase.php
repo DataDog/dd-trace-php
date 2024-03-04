20ec1dfbd15540c00415b658c7a6323f03711eb9
Pablo Martínez Bernardo <pablo.martinezbernardo@datadoghq.com>
2024-03-04 12:20:19 +0100

    Add slash to folder

--- tests/Common/IntegrationTestCase.php
+++ tests/Common/IntegrationTestCase.php
@@ -35,12 +35,12 @@ abstract class IntegrationTestCase extends BaseTestCase
             $csv = $csv . $ext . ';' . phpversion($ext) . '\n';
         }
 
-        $artifactsDir = '/tmp/artifacts/';
+        $artifactsDir = '/tmp/artifacts';
         if ( !file_exists( $artifactsDir ) && !is_dir( $artifactsDir ) ) {
             mkdir($artifactsDir, 0755, true);
         }
 
-        file_put_contents($artifactsDir . get_class($this) . '.csv', $csv, FILE_APPEND);
+        file_put_contents($artifactsDir . '/' . get_class($this) . '.csv', $csv, FILE_APPEND);
 
         $this->errorReportingBefore = error_reporting();
         $this->putEnv("DD_TRACE_GENERATE_ROOT_SPAN=0");
