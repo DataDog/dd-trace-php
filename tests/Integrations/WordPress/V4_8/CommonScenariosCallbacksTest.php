dc24c312627c5b910c9d36eaecdf9def4e7499ca
Alexandre Choura <42672104+PROFeNoM@users.noreply.github.com>
2024-03-05 14:50:24 +0100

    test: Don't check for mysqli spans in WordPress tests (#2556)

--- tests/Integrations/WordPress/V4_8/CommonScenariosCallbacksTest.php
+++ tests/Integrations/WordPress/V4_8/CommonScenariosCallbacksTest.php
@@ -7,7 +7,8 @@ class CommonScenariosCallbacksTest extends CommonScenariosTest
     protected static function getEnvs()
     {
         return array_merge(parent::getEnvs(), [
-            'DD_TRACE_WORDPRESS_CALLBACKS' => '1'
+            'DD_TRACE_WORDPRESS_CALLBACKS' => '1',
+            'DD_TRACE_MYSQLI_ENABLED' => '0'
         ]);
     }
 }
