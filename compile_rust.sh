8de3b1fb481960857fff1d55d5e1862f7cf39fca
Luc Vieillescazes <luc@vieillescazes.net>
2024-04-04 17:06:21 +0200

    Fix compilation on macOs (#2613)

--- compile_rust.sh
+++ compile_rust.sh
@@ -9,4 +9,10 @@ if test -n "$SHARED"; then
   RUSTFLAGS="$RUSTFLAGS --cfg php_shared_build"
 fi
 
+case "${host_os}" in
+  darwin*)
+    RUSTFLAGS="$RUSTFLAGS -Clink-arg=-undefined -Clink-arg=dynamic_lookup";
+    ;;
+esac
+
 RUSTFLAGS="$RUSTFLAGS" RUSTC_BOOTSTRAP=1 "${DDTRACE_CARGO:-cargo}" build $(test "${PROFILE:-debug}" = "debug" || echo --profile "$PROFILE") "$@"
