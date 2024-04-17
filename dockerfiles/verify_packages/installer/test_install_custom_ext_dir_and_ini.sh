8180fdb0550076e2f7003fe529b875da71cf95ea
Alexandre Choura <42672104+PROFeNoM@users.noreply.github.com>
2024-04-17 16:57:55 +0200

    fix: Installer tests version lock (#2630)
    * fix: Installer tests version lock
    
    * fix: Installer tests version lock
    
    * fix: Installer tests version lock

--- dockerfiles/verify_packages/installer/test_install_custom_ext_dir_and_ini.sh
+++ dockerfiles/verify_packages/installer/test_install_custom_ext_dir_and_ini.sh
@@ -8,8 +8,7 @@ set -e
 assert_no_ddtrace
 
 # Install using the php installer
-trace_version="0.99.0"
-generate_installers "${trace_version}"
+trace_version=$(cat VERSION)
 custom_ini_file="$(get_php_conf_dir)/40-ddtrace.ini"
 php ./build/packages/datadog-setup.php --php-bin php --extension-dir /custom-ext-dir --enable-profiling --ini "$custom_ini_file"
 
@@ -19,4 +18,4 @@ assert_ddtrace_version "${trace_version}"
 assert_profiler_installed
 assert_appsec_installed
 
-assert_request_init_hook_exists
+assert_sources_path_exists
