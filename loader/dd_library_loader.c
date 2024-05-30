202dc8ee50e49ed3e33d42442b562af26dc608e6
Luc Vieillescazes <luc.vieillescazes@datadoghq.com>
2024-05-30 17:11:10 +0200

    Make the code more robust and PHP 5 compatible

--- loader/dd_library_loader.c
+++ loader/dd_library_loader.c
@@ -10,6 +10,20 @@
 #include "php_dd_library_loader.h"
 
 static bool debug_logs = false;
+static bool force_load = false;
+
+static int module_api_no = 0;
+static int module_api_no_embed = 0;
+char module_build_id[32] = {0};
+bool is_zts = false;
+bool is_debug = false;
+
+// Public only starting from PHP 5.6
+ZEND_API void zend_append_version_info(const zend_extension *extension) __attribute__((weak));
+
+static void ddloader_error_handler_php5(int error_num, const char *error_filename, const uint error_lineno, const char *format, va_list args) {}
+
+static void ddloader_error_handler(int error_num, zend_string *error_filename, const uint32_t error_lineno, zend_string *message) { }
 
 #define LOG(str) ddloader_log(str);
 
@@ -38,19 +52,21 @@ static char *ddloader_find_ext_path(const char *ext_dir, const char *ext_name, i
     return full_path;
 }
 
-static int ddloader_load_ddtrace(void) {
-    // Use the 'Core' extension as reference for zend_api and build_id
-    zend_module_entry *core_ext = zend_hash_str_find_ptr(&module_registry, ZEND_STRL("core"));
-    if (!core_ext) {
-        LOG("Cannot find the 'core' extension");
-        return SUCCESS;
+/**
+ * Try to load a symbol from a library handle.
+ * As some OS prepend _ to symbol names, we try to load with and without it.
+*/
+static void *ddloader_dl_fetch_symbol(void *handle, const char *symbol_name_with_underscoe) {
+    void *symbol = DL_FETCH_SYMBOL(handle, symbol_name_with_underscoe + 1);
+    if (!symbol) {
+        symbol = DL_FETCH_SYMBOL(handle, symbol_name_with_underscoe);
     }
 
-    bool is_zts = (strstr(core_ext->build_id, "NTS") == NULL);
-    bool is_debug = (strstr(core_ext->build_id, "debug") != NULL);
+    return symbol;
+}
 
-    char *ext_path = ddloader_find_ext_path("trace", "ddtrace", core_ext->zend_api, is_zts, is_debug);
-    // Extension not found
+static int ddloader_load_ddtrace() {
+    char *ext_path = ddloader_find_ext_path("trace", "ddtrace", module_api_no, is_zts, is_debug);
     if (!ext_path) {
         LOG("Extension file not found");
         return SUCCESS;
@@ -68,16 +84,7 @@ static int ddloader_load_ddtrace(void) {
         goto abort;
     }
 
-    get_module = (zend_module_entry * (*)(void)) DL_FETCH_SYMBOL(handle, "get_module");
-
-    /* Some OS prepend _ to symbol names while their dynamic linker
-     * does not do that automatically. Thus we check manually for
-     * _get_module. */
-
-    if (!get_module) {
-        get_module = (zend_module_entry * (*)(void)) DL_FETCH_SYMBOL(handle, "_get_module");
-    }
-
+    get_module = (zend_module_entry * (*)(void)) ddloader_dl_fetch_symbol(handle, "_get_module");
     if (!get_module) {
         LOG("Cannot fetch the module entry");
         goto abort_and_unload;
@@ -85,21 +92,60 @@ static int ddloader_load_ddtrace(void) {
 
     module_entry = get_module();
 
-    if (zend_hash_str_exists(&module_registry, module_entry->name, strlen(module_entry->name))) {
-        LOG("The extension is already loaded");
-        goto abort_and_unload;
-    }
-    if (module_entry->zend_api != core_ext->zend_api) {
+    if (module_entry->zend_api != module_api_no_embed) {
         LOG("Wrong API number");
         goto abort_and_unload;
     }
-    if (strcmp(module_entry->build_id, core_ext->build_id)) {
+    if (strcmp(module_entry->build_id, module_build_id)) {
         LOG("Wrong Build ID");
         goto abort_and_unload;
     }
-    if ((module_entry = zend_register_internal_module(module_entry)) == NULL) {
+
+    // Register the module, catching all errors that can happen (already loaded, unsatisied dep, ...)
+    if (module_api_no < 20151012) { // PHP 5
+        void (*old_error_handler)(int, const char *, const uint, const char*, va_list);
+        old_error_handler = zend_error_cb;
+        zend_error_cb = ddloader_error_handler_php5;
+        module_entry = zend_register_internal_module(module_entry);
+        zend_error_cb = old_error_handler;
+    } else { // PHP 7+
+        void (*old_error_handler)(int, zend_string *, const uint32_t, zend_string *);
+        old_error_handler = zend_error_cb;
+        zend_error_cb = ddloader_error_handler;
+        module_entry = zend_register_internal_module(module_entry);
+        zend_error_cb = old_error_handler;
+    }
+
+    if (module_entry == NULL) {
         LOG("Cannot register the module");
         goto abort_and_unload;
+    }
+
+    // As the Zend extension are started up after the regular extension,
+    // we need to start it up manually.
+
+    module_entry->handle = handle;
+    if (zend_startup_module_ex(module_entry) == FAILURE) {
+        LOG("Cannot start the module");
+        goto abort;
+    }
+
+    // The ddtrace Zend Extension should have been loaded by the ddtrace module.
+    zend_extension *ddtrace = zend_get_extension("ddtrace");
+    if (!ddtrace) {
+        LOG("The ddtrace Zend extension cannot be found");
+        goto abort;
+    }
+
+    // Copy of private function zend_extension_startup();
+    if (ddtrace->startup) {
+        if (ddtrace->startup(ddtrace) != SUCCESS) {
+            LOG("An error occurred during the startup of ddtrace Zend extension");
+            goto abort;
+        }
+        if (zend_append_version_info) {
+            zend_append_version_info(ddtrace);
+        }
     }
 
     return SUCCESS;
@@ -114,42 +160,94 @@ abort:
     return SUCCESS;
 }
 
-static int ddloader_api_no_check(int api_no) {
-    // Enable logs as soon as possible
+static inline void ddloader_configure() {
     if (getenv("DD_LOADER_ENABLE_LOGS")) {
         debug_logs = true;
     }
+    if (getenv("DD_LOADER_FORCE")) {
+        force_load = true;
+    }
+}
+
+static int ddloader_api_no_check(int api_no) {
+    ddloader_configure();
 
     switch (api_no) {
-        case 320151012:  // 7.0
-        case 320160303:  // 7.1
-        case 320170718:  // 7.2
-        case 320180731:  // 7.3
-        case 320190902:  // 7.4
-        case 420200930:  // 8.0
-        case 420210902:  // 8.1
-        case 420220829:  // 8.2
-        case 420230831:  // 8.3
+        case 220100525: // 5.4
+            module_api_no_embed = api_no % 100000000;
+            api_no = 220100412;
+            break;
+        case 220121212: // 5.5
+            module_api_no_embed = api_no % 100000000;
+            api_no = 220121113;
+            break;
+        case 220131226: // 5.6
+            module_api_no_embed = api_no % 100000000;
+            api_no = 220131106;
+            // FIXME
+            break;
+        case 320151012: // 7.0
+        case 320160303: // 7.1
+        case 320170718: // 7.2
+        case 320180731: // 7.3
+        case 320190902: // 7.4
+        case 420200930: // 8.0
+        case 420210902: // 8.1
+        case 420220829: // 8.2
+        case 420230831: // 8.3
             break;
 
         default:
-            LOG("Unknown Zend API");
-            if (!getenv("DD_LOADER_FORCE")) {
-                return FAILURE;
+            LOG("Unknown api no");
+            if (!force_load) {
+                // If we return FAILURE, this Zend extension would be unload, BUT it would produce an error
+                // similar to "The Zend Engine API version 220100525 which is installed, is newer."
+                return SUCCESS;
             }
-            LOG("Force load anyway");
+            LOG("Continue to load the extension even if the api no is not supported");
             break;
     }
 
-    ddloader_load_ddtrace();
+    // api_no is the Zend extension API number, similar to "420220829"
+    // It is an int, but represented as a string, we must remove the first char to get the PHP module API number
+    module_api_no = api_no % 100000000;
+
+    if (!module_api_no_embed) {
+        module_api_no_embed = module_api_no;
+    }
 
     return SUCCESS;
 }
 
-static int ddloader_build_id_check(const char *build_id) { return SUCCESS; }
+static int ddloader_build_id_check(const char *build_id) {
+    // Guardrail
+    if (!module_api_no) {
+        return SUCCESS;
+    }
+
+    is_zts = (strstr(build_id, "NTS") == NULL);
+    is_debug = (strstr(build_id, "debug") != NULL);
+
+    // build_id is the Zend extension build ID, similar to "API420220829,TS"
+    // We must remove the 4th char to get the PHP module build ID
+    size_t build_id_len = strlen(build_id);
+    if (build_id_len >= 12 && build_id_len <= 32) {
+        memcpy(module_build_id, build_id, sizeof(char) * 3);
+        memcpy(module_build_id + 3, build_id + 4, sizeof(char) * (build_id_len - 4 + 1));
+    }
+
+    return SUCCESS;
+}
 
 // Required. Otherwise the zend_extension is not loaded
 static int ddloader_zend_extension_startup(zend_extension *ext) {
+    // Guardrail
+    if (*module_build_id == '\0') {
+        return SUCCESS;
+    }
+
+    ddloader_load_ddtrace();
+
     return SUCCESS;
 }
 
