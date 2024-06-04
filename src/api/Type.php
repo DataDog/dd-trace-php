a94a28b95c66ebc63eb4133a844e016009a9a9ea
Alexandre Choura <alexandre.choura@datadoghq.com>
2024-06-04 14:52:17 -0400

    fix: unit test

--- src/api/Type.php
+++ src/api/Type.php
@@ -13,8 +13,6 @@ class Type
     const MESSAGE_CONSUMER = 'queue';
     const MESSAGE_PRODUCER = 'queue';
 
-    const LLM = 'llm';
-
     const CASSANDRA = 'cassandra';
     const ELASTICSEARCH = 'elasticsearch';
     const MEMCACHED = 'memcached';
--- tests/api/Unit/UserAvailableConstantsTest.php
+++ tests/api/Unit/UserAvailableConstantsTest.php
@@ -31,6 +31,7 @@ class UserAvailableConstantsTest extends BaseTestCase
             [Type::CASSANDRA, 'cassandra'],
             [Type::ELASTICSEARCH, 'elasticsearch'],
             [Type::MEMCACHED, 'memcached'],
+            [Type::OPENAI, 'openai'],
             [Type::MONGO, 'mongodb'],
             [Type::REDIS, 'redis'],
             [Type::SYSTEM, 'system'],
