ee1d523e5914485ca3105f590bf67f2e9f167cd4
Alexandre Choura <alexandre.choura@datadoghq.com>
2024-06-05 14:34:52 -0400

    tests: Update Symfony 2.3 Tests

--- tests/Integrations/Symfony/V2_3/RouteNameTest.php
+++ tests/Integrations/Symfony/V2_3/RouteNameTest.php
@@ -14,38 +14,14 @@ class RouteNameTest extends WebFrameworkTestCase
         return __DIR__ . '/../../../Frameworks/Symfony/Version_2_3/web/app.php';
     }
 
-    /**
-     * @throws \Exception
-     */
-    public function testResource2UriMapping()
+    public function testResourceToURIMapping()
     {
-        // memory leak exists in PHP 5.5-7.0, independently of ddtrace
-        $this->checkWebserverErrors = false;
-
-        $traces = $this->tracesFromWebRequest(function () {
-            $spec  = GetSpec::create('Resource name properly set to route', '/app.php');
+        $this->tracesFromWebRequestSnapshot(function () {
+            $spec = GetSpec::create(
+                'Resource name properly set to route',
+                '/app.php?key=value&pwd=should_redact'
+            );
             $this->call($spec);
         });
-
-        // Under Apache Symfony does redirection magic
-        $isApache = \getenv('DD_TRACE_TEST_SAPI') == 'apache2handler';
-        $this->assertFlameGraph($traces, [
-            SpanAssertion::build(
-                'symfony.request',
-                'symfony',
-                'web',
-                'AppBundle\Controller\DefaultController testingRouteNameAction'
-            )->withExactTags([
-                'http.method' => 'GET',
-                'http.url' => 'http://localhost:' . self::PORT . '/' . ($isApache ? '' : 'app.php'),
-                'http.status_code' => '200',
-                Tag::SPAN_KIND => 'server',
-                Tag::COMPONENT => 'symfony',
-            ])->withChildren([
-                SpanAssertion::exists('symfony.httpkernel.kernel.handle')->withChildren([
-                    SpanAssertion::exists('symfony.httpkernel.kernel.boot'),
-                ]),
-            ]),
-        ]);
     }
 }
