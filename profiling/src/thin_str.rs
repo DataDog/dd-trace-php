3c9d2da735e009112e0d6feee02d22c890d5c1cf
Levi Morrison <levi.morrison@datadoghq.com>
2024-06-11 10:57:21 -0600

    refactor: extract ThinPtr

--- profiling/src/thin_str.rs
+++ profiling/src/thin_str.rs
@@ -1,5 +1,5 @@
 use crate::string_set::ArenaAllocator;
-use datadog_alloc::AllocError;
+use datadog_alloc::{AllocError, Allocator};
 use std::alloc::Layout;
 use std::borrow::Borrow;
 use std::hash;
@@ -7,35 +7,12 @@ use std::marker::PhantomData;
 use std::ops::Deref;
 use std::ptr::NonNull;
 
-#[repr(C)]
-struct StaticInlineString<const N: usize> {
-    /// Stores the len of `data`.
-    size: [u8; core::mem::size_of::<usize>()],
-    data: [u8; N],
-}
-
-const USIZE_WIDTH: usize = core::mem::size_of::<usize>();
-
-static EMPTY_INLINE_STRING: StaticInlineString<0> = StaticInlineString::<0> {
-    size: [0; USIZE_WIDTH],
-    data: [],
-};
-
 /// A struct which acts like a thin &str. It does this by storing the size
 /// of the string just before the bytes of the string.
 #[derive(Copy, Clone)]
 #[repr(transparent)]
 pub struct ThinStr<'a> {
-    /// Points to the beginning of a struct which looks like this:
-    /// ```
-    /// #[repr(C)]
-    /// struct InlineString {
-    ///     /// Stores the len of `data`.
-    ///     size: [u8; core::mem::size_of::<usize>()],
-    ///     data: [u8],
-    /// }
-    /// ```
-    size_ptr: NonNull<u8>,
+    thin_ptr: ThinPtr,
 
     /// Since [ThinStr] doesn't hold a reference but acts like one, indicate
     /// this to the compiler with phantom data. This takes up no space.
@@ -44,11 +21,8 @@ pub struct ThinStr<'a> {
 
 impl ThinStr<'static> {
     pub fn new() -> ThinStr<'static> {
-        let ptr = core::ptr::addr_of!(EMPTY_INLINE_STRING)
-            .cast::<u8>()
-            .cast_mut();
         Self {
-            size_ptr: unsafe { NonNull::new_unchecked(ptr) },
+            thin_ptr: EMPTY_INLINE_STRING.as_thin_ptr(),
             _marker: PhantomData,
         }
     }
@@ -58,47 +32,16 @@ impl<'a> ThinStr<'a> {
     // todo: move ArenaAllocator trait to `datadog_alloc` as a marker trait
     //       (meaning, remove the associated method and leave that in prof)?
     pub fn new_in(str: &str, arena: &'a impl ArenaAllocator) -> Result<Self, AllocError> {
-        let inline_size = str.len() + USIZE_WIDTH;
-
-        let layout = match Layout::from_size_align(inline_size, 1) {
-            Ok(l) => l,
-            Err(_) => return Err(AllocError),
-        };
-        let allocation = arena.allocate(layout)?.cast::<u8>().as_ptr();
-
-        let size = allocation.cast::<[u8; USIZE_WIDTH]>();
-        // SAFETY: writing into uninitialized new allocation at correct place.
-        unsafe { size.write(str.len().to_ne_bytes()) };
-
-        // SAFETY: the data pointer is just after the header, and the
-        // allocation is at least that long.
-        let data = unsafe { allocation.add(USIZE_WIDTH) };
-
-        // SAFETY: the allocation is big enough, locations are distinct, and
-        // the alignment is 1 (so it's always aligned), and the memory is safe
-        // for writing.
-        unsafe { core::ptr::copy_nonoverlapping(str.as_bytes().as_ptr(), data, str.len()) };
-
-        Ok(Self {
-            size_ptr: unsafe { NonNull::new_unchecked(allocation) },
-            _marker: PhantomData,
-        })
-    }
-
-    /// Reads the size prefix to get the length of the string.
-    fn slice_len(&self) -> usize {
-        // SAFETY: ThinStr points to the size prefix of the string.
-        let size = unsafe { self.size_ptr.cast::<[u8; USIZE_WIDTH]>().as_ptr().read() };
-        usize::from_ne_bytes(size)
+        let thin_ptr = ThinPtr::new_in(str, arena)?;
+        let _marker = PhantomData;
+        Ok(Self { thin_ptr, _marker })
     }
 
     /// Gets the layout of a ThinStr, such as to deallocate it.
     #[allow(unused)]
     #[inline]
     pub fn layout(&self) -> Layout {
-        let len = self.slice_len();
-        // SAFETY: since this object exists, its layout must be valid.
-        unsafe { Layout::from_size_align_unchecked(len + USIZE_WIDTH, 1) }
+        self.thin_ptr.layout()
     }
 }
 
@@ -107,12 +50,12 @@ impl<'a> Deref for ThinStr<'a> {
 
     fn deref(&self) -> &Self::Target {
         let slice = {
-            let len = self.slice_len();
-            // SAFETY: data is located immediately after the header. There are
-            // no padding bytes at play.
-            let data = unsafe { self.size_ptr.as_ptr().add(USIZE_WIDTH) };
+            let len = self.thin_ptr.len();
+            let data = self.thin_ptr.data().as_ptr();
+
             // SAFETY: bytes are never handed out as mut, so const slices are
-            // not going to break aliasing rules.
+            // not going to break aliasing rules, and this is the correct
+            // lifetime for the data.
             unsafe { core::slice::from_raw_parts(data, len) }
         };
 
@@ -142,10 +85,102 @@ impl<'a> Borrow<str> for ThinStr<'a> {
     }
 }
 
+#[derive(Clone, Copy)]
+struct ThinPtr {
+    /// Points to the beginning of a struct which looks like this:
+    /// ```
+    /// #[repr(C)]
+    /// struct InlineString {
+    ///     /// Stores the len of `data`.
+    ///     size: [u8; core::mem::size_of::<usize>()],
+    ///     data: [u8],
+    /// }
+    /// ```
+    size_ptr: NonNull<u8>,
+}
+
+impl ThinPtr {
+    /// Reads the size prefix to get the length of the string.
+    const fn len(self) -> usize {
+        // SAFETY: ThinStr points to the size prefix of the string.
+        let size = unsafe { self.size_ptr.cast::<[u8; USIZE_WIDTH]>().as_ptr().read() };
+        usize::from_ne_bytes(size)
+    }
+
+    /// Returns a pointer to the string data (not to the header).
+    const fn data(self) -> NonNull<u8> {
+        // SAFETY: ThinStr points to the size prefix of the string, and the
+        // string data is located immediately after without padding.
+        let ptr = unsafe { self.size_ptr.as_ptr().add(USIZE_WIDTH) };
+
+        // SAFETY: derived from a NonNull, so it's also NonNull.
+        unsafe { NonNull::new_unchecked(ptr) }
+    }
+
+    /// Gets the layout of a ThinStr, such as to deallocate it.
+    #[allow(unused)]
+    #[inline]
+    fn layout(self) -> Layout {
+        let len = self.len();
+        // SAFETY: since this object exists, its layout must be valid.
+        unsafe { Layout::from_size_align_unchecked(len + USIZE_WIDTH, 1) }
+    }
+
+    fn new_in(str: &str, arena: &impl Allocator) -> Result<Self, AllocError> {
+        let inline_size = str.len() + USIZE_WIDTH;
+
+        let layout = match Layout::from_size_align(inline_size, 1) {
+            Ok(l) => l,
+            Err(_) => return Err(AllocError),
+        };
+        let allocation = arena.allocate(layout)?.cast::<u8>().as_ptr();
+
+        let size = allocation.cast::<[u8; USIZE_WIDTH]>();
+        // SAFETY: writing into uninitialized new allocation at correct place.
+        unsafe { size.write(str.len().to_ne_bytes()) };
+
+        // SAFETY: the data pointer is just after the header, and the
+        // allocation is at least that long.
+        let data = unsafe { allocation.add(USIZE_WIDTH) };
+
+        // SAFETY: the allocation is big enough, locations are distinct, and
+        // the alignment is 1 (so it's always aligned), and the memory is safe
+        // for writing.
+        unsafe { core::ptr::copy_nonoverlapping(str.as_bytes().as_ptr(), data, str.len()) };
+
+        let size_ptr = unsafe { NonNull::new_unchecked(allocation) };
+        Ok(ThinPtr { size_ptr })
+    }
+}
+
+#[repr(C)]
+struct StaticInlineString<const N: usize> {
+    /// Stores the len of `data`.
+    size: [u8; core::mem::size_of::<usize>()],
+    data: [u8; N],
+}
+
+impl<const N: usize> StaticInlineString<N> {
+    fn as_thin_ptr(&self) -> ThinPtr {
+        let ptr = core::ptr::addr_of!(EMPTY_INLINE_STRING).cast::<u8>();
+        // SAFETY: derived from static address, and ThinStr does not allow
+        // modifications, so the mut-cast is also fine.
+        let size_ptr = unsafe { NonNull::new_unchecked(ptr.cast_mut()) };
+        ThinPtr { size_ptr }
+    }
+}
+
+const USIZE_WIDTH: usize = core::mem::size_of::<usize>();
+
+static EMPTY_INLINE_STRING: StaticInlineString<0> = StaticInlineString::<0> {
+    size: [0; USIZE_WIDTH],
+    data: [],
+};
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use datadog_alloc::{Allocator, Global};
+    use datadog_alloc::Global;
     use datadog_profiling::collections::string_table::wordpress_test_data;
 
     // Not really, please manually de-allocate strings when done with them.
@@ -172,7 +207,7 @@ mod tests {
         }
 
         for thin_str in thin_strs.drain(..) {
-            unsafe { alloc.deallocate(thin_str.size_ptr, thin_str.layout()) };
+            unsafe { alloc.deallocate(thin_str.thin_ptr.size_ptr, thin_str.layout()) };
         }
     }
 }
