c29505367fdbd2e38e0fc1a9e0dacc3e4465e51a
Gustavo Lopes <mail@geleia.net>
2024-06-27 11:04:30 +0100

    Improve AppSec integration tests
    * Support aarch64
    * Show the log of the sidecar
    * Update dependencies
    * Compile rust code in the tracer in debug mode
    * Fix build failing when gradle finds broken symlinks
    * Add PHP debug images (for troubleshooting)
    
    The images are now multiarch. See https://registry.hub.docker.com/r/datadog/dd-appsec-php-ci/tags

--- appsec/tests/integration/src/docker/toolchain/Toolchain.cmake
+++ appsec/tests/integration/src/docker/toolchain/Toolchain.cmake
@@ -1,8 +1,17 @@
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
+execute_process(
+    COMMAND arch
+    OUTPUT_VARIABLE ARCHITECTURE
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+if(ARCHITECTURE MATCHES "x86_64")
+    set(ARCH x86_64)
+else()
+    set(ARCH aarch64)
+endif()
 set(CMAKE_SYSROOT /build/muslsysroot)
 set(CMAKE_AR /usr/bin/llvm-ar-11)
-set(triple x86_64-none-linux-musl)
+set(triple ${ARCH}-none-linux-musl)
 set(CMAKE_ASM_COMPILER_TARGET ${triple})
 set(CMAKE_C_COMPILER /usr/bin/clang-11)
 set(CMAKE_C_COMPILER_TARGET ${triple})
@@ -11,8 +20,8 @@ set(CMAKE_C_FLAGS ${c_cxx_flags})
 set(CMAKE_CXX_COMPILER /usr/bin/clang++-11)
 set(CMAKE_CXX_COMPILER_TARGET ${triple})
 set(CMAKE_CXX_FLAGS "-stdlib=libc++ -isystem${CMAKE_SYSROOT}/include/c++/v1 ${c_cxx_flags}")
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-v -fuse-ld=lld -static -nodefaultlibs -lc++ -lc++abi ${CMAKE_SYSROOT}/lib/linux/libclang_rt.builtins-x86_64.a -lunwind -lc ${CMAKE_SYSROOT}/lib/linux/libclang_rt.builtins-x86_64.a")
-set(CMAKE_SHARED_LINKER_FLAGS_INIT "-v -fuse-ld=lld -nodefaultlibs -Wl,-Bstatic -lc++ -lc++abi ${CMAKE_SYSROOT}/lib/linux/libclang_rt.builtins-x86_64.a -lunwind -Wl,-Bdynamic -lc ${CMAKE_SYSROOT}/lib/linux/libclang_rt.builtins-x86_64.a")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-v -fuse-ld=lld -static -nodefaultlibs -lc++ -lc++abi ${CMAKE_SYSROOT}/lib/linux/libclang_rt.builtins-${ARCH}.a -lunwind -lc ${CMAKE_SYSROOT}/lib/linux/libclang_rt.builtins-${ARCH}.a")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-v -fuse-ld=lld -nodefaultlibs -Wl,-Bstatic -lc++ -lc++abi ${CMAKE_SYSROOT}/lib/linux/libclang_rt.builtins-${ARCH}.a -lunwind -Wl,-Bdynamic -lc ${CMAKE_SYSROOT}/lib/linux/libclang_rt.builtins-${ARCH}.a")
 
 set(CMAKE_NM /usr/bin/llvm-nm-11)
 set(CMAKE_RANLIB /usr/bin/llvm-ranlib-11)
