582fcb6341d8f13e074f7e51712066e4d17a63ff
Luc Vieillescazes <luc@vieillescazes.net>
2024-07-11 13:33:18 +0200

    Add a check on the max glibc version (#2756)

--- loader/bin/test.sh
+++ loader/bin/test.sh
@@ -17,7 +17,7 @@ if [[ ! -f run-tests.php ]]; then
 fi
 
 if [[ -z "${DD_LOADER_PACKAGE_PATH:-}" ]]; then
-    DEFAULT="/home/circleci/app/dd-library-php"
+    DEFAULT="/home/circleci/app/dd-library-php-ssi"
     echo "Note: env var 'DD_LOADER_PACKAGE_PATH' is required but not defined, using the default value (${DEFAULT})"
     export DD_LOADER_PACKAGE_PATH="${DEFAULT}"
 fi
