bf6e1ef8cc0a3e8b5e17dd6a7325838e5a895e0e
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2024-08-22 16:29:08 +0200

    Add some changes so benchmarks can run

--- appsec/src/extension/attributes.h
+++ appsec/src/extension/attributes.h
@@ -1,5 +1,6 @@
 // Unless explicitly stated otherwise all files in this repository are
-// dual-licensed under the Apache-2.0 License or BSD-3-Clause License.
+// dual-licensed under the Apache-2.0 License
+// or BSD-3-Clause License.
 //
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2021 Datadog, Inc.
