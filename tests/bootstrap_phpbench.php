c8981e34a01864fe51869b8e85428a2beb5d0ca5
Alexandre Choura <alexandre.choura@datadoghq.com>
2024-08-27 13:34:51 +0200

    Use bootstrap_phpunit instead of bootstrap_common

--- tests/bootstrap_phpbench.php
+++ tests/bootstrap_phpbench.php
@@ -2,4 +2,4 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/Benchmarks/vendor/autoload.php';
-require_once __DIR__ . '/bootstrap_common.php';
+require_once __DIR__ . '/bootstrap_phpunit.php';
