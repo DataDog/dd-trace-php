457016c2f23713502aedc6ebfd13be3441cf0c99
iamluc <luc.vieillescazes@datadoghq.com>
2024-10-01 10:42:10 +0200

    Fix OpenTracing test flakiness

--- tests/Frameworks/Custom/OpenTracing/index.php
+++ tests/Frameworks/Custom/OpenTracing/index.php
@@ -2,15 +2,11 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-try {
-    $otTracer = new \DDTrace\OpenTracer1\Tracer(\DDTrace\GlobalTracer::get());
-    $scope = $otTracer->startActiveSpan('web.request');
-    $span = $scope->getSpan();
-    $span->setTag('service.name', 'service_name');
-    $span->setTag('resource.name', 'resource_name');
-    $span->setTag('span.type', 'web');
-    $span->setTag('http.method', $_SERVER['REQUEST_METHOD']);
-    $span->finish();
-} finally {
-    \dd_trace_internal_fn("finalize_telemetry");
-}
+$otTracer = new \DDTrace\OpenTracer1\Tracer(\DDTrace\GlobalTracer::get());
+$scope = $otTracer->startActiveSpan('web.request');
+$span = $scope->getSpan();
+$span->setTag('service.name', 'service_name');
+$span->setTag('resource.name', 'resource_name');
+$span->setTag('span.type', 'web');
+$span->setTag('http.method', $_SERVER['REQUEST_METHOD']);
+$span->finish();
