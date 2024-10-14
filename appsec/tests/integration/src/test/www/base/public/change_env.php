ac7eb62d81f1d9e2a041a275c9569ab706986fc3
Gustavo Lopes <gustavo.lopes@datadoghq.com>
2024-10-14 03:01:29 +0100

    AppSec: improve behavior with empty DD_SERVICE/DD_ENV (#2888)

--- appsec/tests/integration/src/test/www/base/public/change_env.php
+++ appsec/tests/integration/src/test/www/base/public/change_env.php
@@ -1,7 +1,10 @@
 <?php
 
 $root_span = \DDTrace\root_span();
-sleep(1);
-$root_span->env = $_GET['env'];
+if (key_exists('ini', $_GET)) {
+    ini_set('datadog.env', $_GET['env']);
+} else {
+    $root_span->env = $_GET['env'];
+}
 
 var_dump($root_span);
