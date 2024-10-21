3090ecf8796669928b7058ae782a286925642bd6
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2024-10-21 16:49:24 +0200

    Avoid reporting filesytem integration on telemetry

--- ext/integrations/integrations.c
+++ ext/integrations/integrations.c
@@ -7,6 +7,9 @@
 #include <hook/hook.h>
 #include <sandbox/sandbox.h>
 #undef INTEGRATION
+#undef INTEGRATION_CUSTOM_ENABLED
+
+static bool is_filesystem_enabled() { return get_DD_TRACE_FILESYSTEM_ENABLED() && get_DD_APPSEC_RASP_ENABLED(); }
 
 #define DDTRACE_DEFERRED_INTEGRATION_LOADER(class, fname, integration_name)             \
     dd_hook_method_and_unhook_on_first_call((zai_str)ZAI_STRL(class), (zai_str)ZAI_STRL(fname), \
@@ -24,13 +27,15 @@
     dd_set_up_deferred_loading_by_method(name, (zai_str)ZAI_STR_EMPTY, (zai_str)ZAI_STRL(fname), \
                                          (zai_str)ZAI_STRL(integration), false)
 
-#define INTEGRATION(id, lcname, ...)                                    \
+#define INTEGRATION(id, lcname, ...) INTEGRATION_AUX(id, lcname, get_DD_TRACE_##id##_ENABLED)
+#define INTEGRATION_CUSTOM_ENABLED(id, lcname, is_enabled_func, ...) INTEGRATION_AUX(id, lcname, is_enabled_func)
+#define INTEGRATION_AUX(id, lcname, is_enabled_func)                   \
     {                                                                  \
         .name = DDTRACE_INTEGRATION_##id,                              \
         .name_ucase = #id,                                             \
         .name_lcase = (lcname),                                        \
         .name_len = sizeof(lcname) - 1,                                \
-        .is_enabled = get_DD_TRACE_##id##_ENABLED,              \
+        .is_enabled = is_enabled_func,                                 \
         .is_analytics_enabled = get_DD_TRACE_##id##_ANALYTICS_ENABLED, \
         .get_sample_rate = get_DD_TRACE_##id##_ANALYTICS_SAMPLE_RATE,  \
         .aux = {0},                                                    \
