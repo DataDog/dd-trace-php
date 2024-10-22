5ef917f5bcaadab12f65ad3e826185b739820d3b
Bob Weinand <bob.weinand@datadoghq.com>
2024-10-22 23:22:39 +0200

    Fix bad hasThis() check on PHP 7.0
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- ext/compatibility.h
+++ ext/compatibility.h
@@ -620,7 +620,11 @@ static zend_always_inline zend_result zend_call_function_with_return_value(zend_
 #undef ZEND_RAW_FENTRY
 #define ZEND_RAW_FENTRY(zend_name, name, arg_info, flags, ...)   { zend_name, name, arg_info, (uint32_t) (sizeof(arg_info)/sizeof(struct _zend_internal_arg_info)-1), flags },
 
+#if PHP_VERSION_ID < 70100
+#define hasThis() (Z_OBJ_P(ZEND_THIS) != NULL)
+#else
 #define hasThis() (Z_TYPE_P(ZEND_THIS) == IS_OBJECT)
+#endif
 #endif
 
 #endif  // DD_COMPATIBILITY_H
