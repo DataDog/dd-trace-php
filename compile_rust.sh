9a5642b7fc329697cfc3112ea4bdbee24cb6c57e
Bob Weinand <bob.weinand@datadoghq.com>
2024-10-24 23:30:18 +0200

    Fix sidecar asan
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- compile_rust.sh
+++ compile_rust.sh
@@ -19,7 +19,7 @@ set -x
 
 if test -n "$COMPILE_ASAN"; then
   export LDFLAGS="-fsanitize=address $(if cc -v 2>&1 | grep -q clang; then echo "-shared-libsan"; fi)"
-  export CFLAGS="-fsanitize=address -fno-omit-frame-pointer"
+  export CFLAGS="$LDFLAGS -fno-omit-frame-pointer" # the cc buildtools will only pick up CFLAGS it seems
 fi
 
 SIDECAR_VERSION=$(cat ../VERSION) RUSTFLAGS="$RUSTFLAGS" RUSTC_BOOTSTRAP=1 "${DDTRACE_CARGO:-cargo}" build $(test "${PROFILE:-debug}" = "debug" || echo --profile "$PROFILE") "$@"
