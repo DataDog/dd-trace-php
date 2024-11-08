63a495df2b75f249962f51c29a8bf2ebd6c827db
iamluc <luc.vieillescazes@datadoghq.com>
2024-11-08 15:08:12 +0100

    Fix compatilibity with PHP 5
    Previous build could be loaded with php:5.6 docker image and I believe on centos 7 also.
    But on ubuntu:22.04, there were issues like "undefined symbol: __zend_malloc".
    
    This PR fixes this.

--- loader/compat_php.h
+++ loader/compat_php.h
@@ -7,6 +7,8 @@ zend_string *ddloader_zend_string_alloc(int php_api_no, size_t len, int persiste
 zend_string *ddloader_zend_string_init(int php_api_no, const char *str, size_t len, bool persistent);
 void ddloader_zend_string_release(int php_api_no, zend_string *s);
 zval *ddloader_zend_hash_set_bucket_key(int php_api_no, HashTable *ht, Bucket *b, zend_string *key);
+void *ddloader_zend_hash_str_find_ptr(int php_api_no, const HashTable *ht, const char *str, size_t len);
+void ddloader_zend_hash_str_del(int php_api_no, HashTable *ht, const char *str, size_t len);
 void ddloader_replace_zend_error_cb(int php_api_no);
 void ddloader_restore_zend_error_cb();
 zval *ddloader_zend_hash_update(HashTable *ht, zend_string *key, zval *pData);
