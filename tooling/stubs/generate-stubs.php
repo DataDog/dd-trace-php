56ab089244a8db9f0edb8d37fac42b5077381446
Alexandre Choura <alexandre.choura@datadoghq.com>
2024-11-12 15:15:06 +0100

    fix: src dir

--- tooling/stubs/generate-stubs.php
+++ tooling/stubs/generate-stubs.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 
 use StubsGenerator\{StubsGenerator, Finder};
 
-const SRC_DIR = "/home/circleci/app/src/";
+$SRC_DIR = dirname(__DIR__, 3) . "/src/";
 const FILES_TO_LOAD = [
     "../../src/bridge/_files_api.php",
     "../../src/bridge/_files_tracer.php",
@@ -13,14 +13,14 @@ const FILES_TO_LOAD = [
 
 $files = array_merge(...array_map(fn($file) => require $file, FILES_TO_LOAD));
 
-$files = array_map(fn($file) => str_replace(SRC_DIR . "bridge/../", "", $file), $files);
+$files = array_map(fn($file) => str_replace($SRC_DIR . "bridge/../", "", $file), $files);
 
 $generator = new StubsGenerator();
-$finder = Finder::create()->in(SRC_DIR)->path($files);
+$finder = Finder::create()->in($SRC_DIR)->path($files);
 
 $stubs = $generator->generate($finder)->prettyPrint();
 
-$outputFile = SRC_DIR . "ddtrace_php_api.stubs.php";
+$outputFile = $SRC_DIR . "ddtrace_php_api.stubs.php";
 file_put_contents($outputFile, $stubs);
 
 echo sprintf("Stubs generated at %s\n", $outputFile);
