e4c17bb5fd06a1ce163437a1765255570185db93
Grégoire Paris <gregoire.paris@manomano.com>
2024-12-04 17:23:06 +0100

    Add 8.4 to supported versions
    Without it, the installer script could only find the main binary
    /usr/bin/php because it didn't had a version number included in the path.
    Inspired by 7e6ff34e9c0e12a2a15c4ae2ebf819198a2b8dc7

--- datadog-setup.php
+++ datadog-setup.php
@@ -2408,7 +2408,7 @@ function get_ini_settings($sourcesDir, $appsecHelperPath, $appsecRulesPath)
  */
 function get_supported_php_versions()
 {
-    return ['7.0', '7.1', '7.2', '7.3', '7.4', '8.0', '8.1', '8.2', '8.3'];
+    return ['7.0', '7.1', '7.2', '7.3', '7.4', '8.0', '8.1', '8.2', '8.3', '8.4'];
 }
 
 main();
