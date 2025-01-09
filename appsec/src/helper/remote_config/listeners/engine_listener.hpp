219b63edcca7d8e0c88fa08846bfc9d8c9fccacb
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2025-01-09 17:32:59 +0100

    Remove LFI remote config product

--- appsec/src/helper/remote_config/listeners/engine_listener.hpp
+++ appsec/src/helper/remote_config/listeners/engine_listener.hpp
@@ -33,8 +33,7 @@ public:
 
     [[nodiscard]] std::unordered_set<product> get_supported_products() override
     {
-        return {known_products::ASM, known_products::ASM_DD,
-            known_products::ASM_DATA, known_products::ASM_RASP_LFI};
+        return {known_products::ASM, known_products::ASM_DD, known_products::ASM_DATA};
     }
 
 protected:
