d0d5899744f8b26ac5e69be3dc6e87ea1373f7f0
Bob Weinand <bob.weinand@datadoghq.com>
2025-01-11 00:08:21 +0100

    Fix stats computation of background sender
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- ext/coms.c
+++ ext/coms.c
@@ -695,7 +695,9 @@ static struct curl_slist *dd_agent_headers_alloc(void) {
     dd_append_header(&list, "Datadog-Meta-Lang-Interpreter", sapi_module.name);
     dd_append_header(&list, "Datadog-Meta-Lang-Version", ZSTR_VAL(ddtrace_php_version));
     dd_append_header(&list, "Datadog-Meta-Tracer-Version", PHP_DDTRACE_VERSION);
-    dd_append_header(&list, "Datadog-Client-Computed-Stats", get_global_DD_EXPERIMENTAL_APPSEC_STANDALONE_ENABLED() ? "true" : "false");
+    if (get_global_DD_EXPERIMENTAL_APPSEC_STANDALONE_ENABLED()) {
+        dd_append_header(&list, "Datadog-Client-Computed-Stats", "true");
+    }
 
     ddog_CharSlice id = ddtrace_get_container_id();
     if (id.len) {
