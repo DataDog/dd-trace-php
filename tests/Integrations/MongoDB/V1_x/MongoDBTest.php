64db5bff60edf368eec26548464355ae0a04c3d1
Alexandre Choura <alexandre.choura@datadoghq.com>
2025-01-22 10:48:45 +0100

    fix mongodb & yii tests

--- tests/Integrations/MongoDB/V1_x/MongoDBTest.php
+++ tests/Integrations/MongoDB/V1_x/MongoDBTest.php
@@ -2,6 +2,8 @@
 
 namespace DDTrace\Tests\Integrations\Mongo\V1_x;
 
+include __DIR__ . '/../Latest/MongoDBTest.php';
+
 class MongoDBTest extends \DDTrace\Tests\Integrations\Mongo\Latest\MongoDBTest
 {
     //
--- tests/Integrations/Yii/V2_0_49/YiiDetailsTest.php
+++ tests/Integrations/Yii/V2_0_49/YiiDetailsTest.php
@@ -2,6 +2,8 @@
 
 namespace DDTrace\Tests\Integrations\Yii\V2_0_49;
 
+include __DIR__ . '/../Latest/YiiDetailsTest.php';
+
 class LazyLoadingIntegrationsFromYiiTest extends \DDTrace\Tests\Integrations\Yii\Latest\LazyLoadingIntegrationsFromYiiTest
 {
     //
