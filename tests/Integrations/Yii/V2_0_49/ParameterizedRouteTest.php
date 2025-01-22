93065f10a7891d1e6f6f781c0e20131db11e1bf4
Alexandre Choura <alexandre.choura@datadoghq.com>
2025-01-22 11:02:48 +0100

    fix yii path

--- tests/Integrations/Yii/V2_0_49/ParameterizedRouteTest.php
+++ tests/Integrations/Yii/V2_0_49/ParameterizedRouteTest.php
@@ -4,5 +4,8 @@ namespace DDTrace\Tests\Integrations\Yii\V2_0_49;
 
 class ParameterizedRouteTest extends \DDTrace\Tests\Integrations\Yii\Latest\ParameterizedRouteTest
 {
-    //
+    public static function getAppIndexScript()
+    {
+        return __DIR__ . '/../../../Frameworks/Yii/Version_2_0_49/web/index.php';
+    }
 }
--- tests/Integrations/Yii/V2_0_49/YiiDetailsTest.php
+++ tests/Integrations/Yii/V2_0_49/YiiDetailsTest.php
@@ -6,5 +6,8 @@ include __DIR__ . '/../Latest/YiiDetailsTest.php';
 
 class LazyLoadingIntegrationsFromYiiTest extends \DDTrace\Tests\Integrations\Yii\Latest\LazyLoadingIntegrationsFromYiiTest
 {
-    //
+    public static function getAppIndexScript()
+    {
+        return __DIR__ . '/../../../Frameworks/Yii/Version_2_0_49/web/index.php';
+    }
 }
