976a80458d07c48b3730d3de6b59951a61721a12
iamluc <luc.vieillescazes@datadoghq.com>
2025-02-05 10:57:50 +0100

    Test profiling/appssec are loaded

--- loader/tests/functional/test_php_output.php
+++ loader/tests/functional/test_php_output.php
@@ -6,15 +6,23 @@ skip_if_php5();
 $output = runCLI('-v', false);
 assertNotContains($output, 'ddtrace');
 assertNotContains($output, 'dd_library_loader');
+assertNotContains($output, 'datadog-profiling');
+assertNotContains($output, 'ddappsec');
 
 $output = runCLI('-v', true);
 assertContains($output, 'with ddtrace v');
 assertContains($output, 'with dd_library_loader v');
+assertContains($output, 'with datadog-profiling v');
+assertContains($output, 'with ddappsec v');
 
 $output = runCLI('-m', false);
 assertNotContains($output, 'ddtrace');
 assertNotContains($output, 'dd_library_loader');
+assertNotContains($output, 'datadog-profiling');
+assertNotContains($output, 'ddappsec');
 
 $output = runCLI('-m', true);
 assertContains($output, 'ddtrace');
 assertContains($output, 'dd_library_loader');
+assertContains($output, 'datadog-profiling');
+assertContains($output, 'ddappsec');
