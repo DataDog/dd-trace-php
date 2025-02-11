641811a9ae072ec9ec964564ef3d4a8a49f9577b
Alexandre Choura <alexandre.choura@datadoghq.com>
2025-02-11 10:12:29 +0100

    tests: Laravel x InferredProxy

---
+++ tests/Integrations/Laravel/Latest/InferredProxyTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace DDTrace\Tests\Integrations\Laravel\Latest;
+
+use DDTrace\Tests\Common\WebFrameworkTestCase;
+use DDTrace\Tests\Frameworks\Util\Request\GetSpec;
+
+class InferredProxyTest extends WebFrameworkTestCase
+{
+    public static function getAppIndexScript()
+    {
+        return __DIR__ . '/../../../Frameworks/Laravel/Latest/public/index.php';
+    }
+
+    protected static function getEnvs()
+    {
+        return array_merge(parent::getEnvs(), [
+            'APP_NAME' => 'laravel_test_app',
+            'DD_SERVICE' => 'my_service',
+            'DD_ENV' => 'local-test',
+            'DD_VERSION' => '1.0',
+            'DD_TRACE_INFERRED_PROXY_SERVICES_ENABLED' => 'true',
+        ]);
+    }
+
+    public function testInferredProxy()
+    {
+        $this->tracesFromWebRequestSnapshot(function () {
+            $this->call(
+                GetSpec::create(
+                    'A simple GET request returning a string',
+                    '/simple?key=value&pwd=should_redact',
+                    [
+                        'x-dd-proxy: aws-apigateway',
+                        'x-dd-proxy-request-time-ms: 1739261376000',
+                        'x-dd-proxy-path: /test',
+                        'x-dd-proxy-httpmethod: GET',
+                        'x-dd-proxy-domain-name: example.com',
+                        'x-dd-proxy-stage: aws-prod',
+                    ]
+                )
+            );
+        });
+    }
+
+    public function testInferredProxyException()
+    {
+        $this->tracesFromWebRequestSnapshot(function () {
+            $this->call(
+                GetSpec::create(
+                    'A GET throwing an exception',
+                    '/error?key=value&pwd=should_redact',
+                    [
+                        'x-dd-proxy: aws-apigateway',
+                        'x-dd-proxy-request-time-ms: 1739261376000',
+                        'x-dd-proxy-path: /test',
+                        'x-dd-proxy-httpmethod: GET',
+                        'x-dd-proxy-domain-name: example.com',
+                        'x-dd-proxy-stage: aws-prod',
+                    ]
+                )
+            );
+        });
+    }
+}
