c6ffdcf68018ed05ead9a29ec04d78beb5c68bac
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2025-02-18 11:59:44 +0100

    Allow users to be registered on Wordpres 6.1

--- tests/Integrations/WordPress/V6_1/AutomatedLoginEventsTest.php
+++ tests/Integrations/WordPress/V6_1/AutomatedLoginEventsTest.php
@@ -10,9 +10,14 @@ use DDTrace\Tests\Integrations\WordPress\AutomatedLoginEventsTestSuite;
 class AutomatedLoginEventsTest extends AutomatedLoginEventsTestSuite
 {
     public static $database = "wp61";
+    protected $users_table = 'wp_users';
 
     public static function getAppIndexScript()
     {
         return __DIR__ . '/../../../Frameworks/WordPress/Version_6_1/index.php';
+    }
+
+    protected function databaseDump() {
+        return file_get_contents(__DIR__ . '/../../../Frameworks/WordPress/Version_6_1/scripts/wp_initdb.sql');
     }
 }
