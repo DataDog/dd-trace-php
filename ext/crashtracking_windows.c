1a4a11214132dace51d09c4004542e5f8a77ad0f
Kevin Gosse <kevin.gosse@datadoghq.com>
2025-02-20 13:19:18 +0100

    Checkpoint

--- ext/crashtracking_windows.c
+++ ext/crashtracking_windows.c
@@ -1,61 +1,51 @@
-
-#include <windows.h>
+#include "ddtrace.h"
+#include "sidecar.h"
 
 #include "version.h"
 #include "crashtracking_windows.h"
 
+#include <components/log/log.h>
+
 #include <components-rs/common.h>
 #include <components-rs/crashtracker.h>
+#include <components-rs/ddtrace.h>
 #include <components-rs/sidecar.h>
 
-HMODULE currentModule;
-
-BOOL APIENTRY DllMain(HMODULE hModule,
-    DWORD  ul_reason_for_call,
-    LPVOID lpReserved) {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-        currentModule = hModule;
-        break;
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
-}
-
 bool init_crash_tracking(void) {
+    ddog_Vec_Tag tags = ddog_Vec_Tag_new();
 
-    char* path = ddog_setup_crashtracking();
+    ddog_Vec_Tag_push(&tags, DDOG_CHARSLICE_C("is_crash"), DDOG_CHARSLICE_C("true"));
+    ddog_Vec_Tag_push(&tags, DDOG_CHARSLICE_C("severity"), DDOG_CHARSLICE_C("crash"));
+    ddog_Vec_Tag_push(&tags, DDOG_CHARSLICE_C("library_version"), DDOG_CHARSLICE_C(PHP_DDTRACE_VERSION));
+    ddog_Vec_Tag_push(&tags, DDOG_CHARSLICE_C("language"), DDOG_CHARSLICE_C("php"));
+    ddog_Vec_Tag_push(&tags, DDOG_CHARSLICE_C("runtime"), DDOG_CHARSLICE_C("php"));
 
-    if (!path) {
-        OutputDebugStringW(L"Failed to setup crashtracking, path is null");
-        return false;
-    }
-    else {
-        OutputDebugStringW(L"Crashtracking path:");
-        OutputDebugStringA(path);
-    }
+    uint8_t runtime_id[36];
+    ddtrace_format_runtime_id(&runtime_id);
+    ddog_Vec_Tag_push(&tags, DDOG_CHARSLICE_C("runtime-id"), (ddog_CharSlice) { .ptr = (char*)runtime_id, .len = sizeof(runtime_id) });
 
-    HMODULE crashtracking_module = LoadLibraryA(path);
+    const char* runtime_version = zend_get_module_version("Reflection");
+    ddog_Vec_Tag_push(&tags, DDOG_CHARSLICE_C("runtime_version"), (ddog_CharSlice) { .ptr = (char*)runtime_version, .len = strlen(runtime_version) });
 
-    if (!crashtracking_module) {
-        OutputDebugStringW(L"Failed to load crashtracking module");
-        return false;
-    }
-
-    ddog_crasht_Metadata metadata = {
-        .library_name = DDOG_CHARSLICE_C_BARE("php_ddtrace"),
+    const ddog_crasht_Metadata metadata = {
+        .library_name = DDOG_CHARSLICE_C_BARE("dd-trace-php"),
         .library_version = DDOG_CHARSLICE_C_BARE(PHP_DDTRACE_VERSION),
         .family = DDOG_CHARSLICE_C("php"),
-        .tags = NULL
+        .tags = &tags
     };
 
-    OutputDebugStringW(L"Loaded crashtracking wrapper");
+    ddog_Endpoint* agent_endpoint = ddtrace_sidecar_agent_endpoint();
+    bool result = ddog_setup_crashtracking(agent_endpoint, metadata);
 
-    ddog_Endpoint *endpoint = ddog_endpoint_from_filename(DDOG_CHARSLICE_C("C:/temp/crash.json"));
+    if (result) {
+        LOG(INFO, "Crashtracking is initialized");
+    }
+    else {
+        LOG(WARN, "An error occured while initializing crashtracking");
+    }
 
-    return ddog_crasht_init_windows(crashtracking_module, endpoint, metadata);
+    ddog_endpoint_drop(agent_endpoint);
+    ddog_Vec_Tag_drop(tags);
+
+    return result;
 }
