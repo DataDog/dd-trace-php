bb97229a9b5ab8e5784fd60de5c5519b24ef8aaa
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2025-03-04 18:41:13 +0100

    Test blocking on all versions of Laravel
    This is expected to fail in some versions. Using this to get feedback from the pipeline

---
+++ tests/Frameworks/Laravel/Version_8_x/app/Http/Controllers/RaspTestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class RaspTestController extends Controller
+{
+    public function rasp()
+    {
+        file_get_contents($_REQUEST["data"]);
+
+        return 'Rasp page';
+    }
---
+++ tests/Integrations/Laravel/RaspEventsTestSuite.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace DDTrace\Tests\Integrations\Laravel;
+
+use DDTrace\Tests\Common\AppsecTestCase;
+use DDTrace\Tests\Frameworks\Util\Request\GetSpec;
+use datadog\appsec\AppsecStatus;
+
+class RaspEventsTestSuite extends AppsecTestCase
+{
+    public function testWhenRequestIsBlockedLaravelPageIsNotDisplayed()
+    {
+        $eventToBlock = [
+            "rasp_rule" => "lfi",
+            0 => [
+              "server.io.fs.file" => "index.php"
+            ],
+            "eventName" => "push_addresses"
+        ];
+        AppsecStatus::getInstance()->simulateBlockOnEvent($eventToBlock);
+        $response = $this->call(
+            GetSpec::create('Rasp endpoint', "/rasp?data=index.php")
+        );
+        $this->assertEquals('&nbsp;', $response);
+    }
+}
