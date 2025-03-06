52803ea785c69b3ad776ef27178af20df3de34fa
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2025-03-06 18:09:49 +0100

    Amend Laravel 4

--- tests/Frameworks/Laravel/Version_4_2/app/routes.php
+++ tests/Frameworks/Laravel/Version_4_2/app/routes.php
@@ -27,4 +27,4 @@ Route::group(array('before' => 'auth'), function()
 {
    Route::get('/behind_auth', 'LoginTestController@behind_auth');
 });
-Route::get('rasp', 'RaspTestController@rasp']);
+Route::get('rasp', 'RaspTestController@rasp');
