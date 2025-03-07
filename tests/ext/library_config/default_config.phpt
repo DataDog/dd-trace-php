b366718acfcd24e337ff716020c690faefb98959
iamluc <luc.vieillescazes@datadoghq.com>
2025-03-07 12:02:58 +0100

    Apply config + tests

---
+++ tests/ext/library_config/default_config.phpt
@@ -0,0 +1,19 @@
+--TEST--
+Check that the library config files overloads env vars
+--SKIPIF--
+<?php
+copy(__DIR__.'/default_config.yaml', '/tmp/test_c_default_config.yaml');
+?>
+--ENV--
+_DD_TEST_LIBRARY_CONFIG_LOCAL_FILE=/tmp/test_c_default_config.yaml
+_DD_TEST_LIBRARY_CONFIG_FLEET_FILE=/foo
+--FILE--
+<?php
+
+echo 'DD_SERVICE: '.dd_trace_env_config("DD_SERVICE")."\n";
+echo 'DD_ENV: '.dd_trace_env_config("DD_ENV")."\n";
+
+?>
+--EXPECT--
+DD_SERVICE: service_default_config
+DD_ENV: env_from_default_config
--- zend_abstract_interface/config/config_stable_file.c
+++ zend_abstract_interface/config/config_stable_file.c
@@ -2,47 +2,80 @@
 
 #include <components-rs/library-config.h>
 
+#include "config.h"
 #include "config_stable_file.h"
 
-#define DDOG_SLICE_CHARSLICE(arr)                                                                  \
-  ((ddog_Slice_CharSlice){.ptr = arr, .len = sizeof(arr) / sizeof(arr[0])})
+ddog_Configurator *configurator = NULL;
 
-void zai_config_stable_file_rinit(void) {
-    // FIXME: should be done directly in the library-config crate?
-    ddog_CharSlice args[] = {
-        DDOG_CHARSLICE_C("/usr/bin/php"),
-    };
+// Due to the current state of libdatadog, the function 'ddog_library_configurator_get' reads and parses the config files on every call.
+// In phase 1, as the config won't change between requests, we'll call it only in MINIT to avoid repeated parsing.
+// In phase 2, as planned, the call will move to RINIT
+ddog_Result_VecLibraryConfig config_result = {0};
 
-    ddog_CharSlice envp[] = {
-        DDOG_CHARSLICE_C("FOO=BAR"),
-    };
-    ddog_ProcessInfo process_info = {
-        .args = DDOG_SLICE_CHARSLICE(args),
-        .envp = DDOG_SLICE_CHARSLICE(envp),
-        .language = DDOG_CHARSLICE_C("php")
-    };
+void zai_config_stable_file_minit(void) {
+    configurator = ddog_library_configurator_new(false, DDOG_CHARSLICE_C("php"));
 
-    ddog_Configurator *configurator = ddog_library_configurator_new(false, DDOG_CHARSLICE_C("php"));
-
-    ddog_library_configurator_with_process_info(configurator, process_info);
-
-    ddog_Result_VecLibraryConfig config_result = ddog_library_configurator_get(configurator);
-
-    if (config_result.tag != DDOG_RESULT_VEC_LIBRARY_CONFIG_ERR_VEC_LIBRARY_CONFIG) {
-        ddog_Vec_LibraryConfig configs = config_result.ok;
-        for (uintptr_t i = 0; i < configs.len; i++) {
-            const ddog_LibraryConfig *cfg = &configs.ptr[i];
-            ddog_CStr name = ddog_library_config_name_to_env(cfg->name);
-
-            (void)(name);
-            // printf("Setting env variable: %s=%s\n", name.ptr, cfg->value.ptr);
-            // setenv(name.ptr, cfg->value.ptr, 1);
-        }
-    } else {
-        ddog_Error err = config_result.err;
-        // fprintf(stderr, "%.*s", (int)err.message.len, err.message.ptr);
-        ddog_Error_drop(&err);
+    char *file = getenv("_DD_TEST_LIBRARY_CONFIG_LOCAL_FILE");
+    if (file) {
+        ddog_CStr path = {.ptr = file, .length = strlen(file)+1}; // FIXME: +1??
+        ddog_library_configurator_with_local_path(configurator, path);
+    }
+    file = getenv("_DD_TEST_LIBRARY_CONFIG_FLEET_FILE");
+    if (file) {
+        ddog_CStr path = {.ptr = file, .length = strlen(file)+1}; // FIXME: +1??
+        ddog_library_configurator_with_fleet_path(configurator, path);
     }
 
-    ddog_library_configurator_drop(configurator);
+    config_result = ddog_library_configurator_get(configurator);
+}
+
+void zai_config_stable_file_mshutdown(void) {
+    if (configurator) {
+        ddog_library_configurator_drop(configurator);
+    }
+    if (config_result.tag == DDOG_RESULT_VEC_LIBRARY_CONFIG_ERR_VEC_LIBRARY_CONFIG) {
+        ddog_Error err = config_result.err;
+        ddog_Error_drop(&err);
+    }
+}
+
+static bool zai_config_stable_file_find_entry(zai_str config_name, zai_config_id *id) {
+    for (zai_config_id i = 0; i < zai_config_memoized_entries_count; ++i) {
+        zai_config_memoized_entry *memoized = &zai_config_memoized_entries[i];
+        for (uint8_t n = 0; n < memoized->names_count; ++n) {
+            zai_str name = ZAI_STR_NEW(memoized->names[n].ptr, memoized->names[n].len);
+            if (strcmp(name.ptr, config_name.ptr) == 0) {
+                *id = i;
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+void zai_config_stable_file_rinit(void) {
+    if (config_result.tag == DDOG_RESULT_VEC_LIBRARY_CONFIG_ERR_VEC_LIBRARY_CONFIG) {
+        return;
+    }
+
+    ddog_Vec_LibraryConfig configs = config_result.ok;
+    for (uintptr_t i = 0; i < configs.len; i++) {
+        const ddog_LibraryConfig *cfg = &configs.ptr[i];
+        ddog_CStr name = ddog_library_config_name_to_env(cfg->name);
+
+        zai_config_id config_id;
+        zai_str config_name = {.ptr = name.ptr, .len = name.length};
+        if (zai_config_stable_file_find_entry(config_name, &config_id)) {
+            zval new_zv;
+            ZVAL_UNDEF(&new_zv);
+            zai_config_memoized_entry *memoized = &zai_config_memoized_entries[config_id];
+
+            zai_str config_value = {.ptr = cfg->value.ptr, .len = cfg->value.length};
+            if (zai_config_decode_value(config_value, memoized->type, memoized->parser, &new_zv, /* persistent */ false)) {
+                zai_config_replace_runtime_config(config_id, &new_zv);
+                zval_ptr_dtor(&new_zv);
+            }
+        }
+    }
 }
