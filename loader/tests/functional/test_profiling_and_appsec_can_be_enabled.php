db5860b73ee82283e10ef4d6a8c689d41e968496
iamluc <luc.vieillescazes@datadoghq.com>
2025-03-12 08:44:49 +0100

    Add test

---
+++ loader/tests/functional/test_profiling_and_appsec_can_be_enabled.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__."/includes/autoload.php";
+skip_if_not_at_least_php71();
+
+$telemetryLogPath = tempnam(sys_get_temp_dir(), 'test_loader_');
+
+$output = runCLI('-r "echo ini_get(\"datadog.profiling.enabled\");"', true, [
+    'FAKE_FORWARDER_LOG_PATH='.$telemetryLogPath,
+    'DD_TELEMETRY_FORWARDER_PATH='.__DIR__.'/../../bin/fake_forwarder.sh',
+    'DD_PROFILING_ENABLED=1',
+]);
+assertContains($output, '1');
+
+$output = runCLI('-r "echo ini_get(\"datadog.appsec.enabled\");"', true, [
+    'FAKE_FORWARDER_LOG_PATH='.$telemetryLogPath,
+    'DD_TELEMETRY_FORWARDER_PATH='.__DIR__.'/../../bin/fake_forwarder.sh',
+    'DD_APPSEC_ENABLED=1',
+]);
+assertContains($output, '1');
