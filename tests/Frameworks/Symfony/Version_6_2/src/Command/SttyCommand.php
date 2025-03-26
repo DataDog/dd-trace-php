0d86cb301be3ed4c5ad6c97eb7bf47963b2b0ae2
Alexandre Choura <alexandre.choura@datadoghq.com>
2025-03-26 08:07:38 +0100

    tests: Add testSilencedSpansAreDropped for Symfony CLI Tests

---
+++ tests/Frameworks/Symfony/Version_6_2/src/Command/SttyCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Terminal;
+
+class SttyCommand extends Command
+{
+    protected static $defaultName = 'app:stty';
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $terminal = new Terminal();
+        $terminal->hasSttyAvailable();
+
+        return Command::SUCCESS;
+    }
+}
