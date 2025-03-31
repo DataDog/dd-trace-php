6a524da641028c05841c3dd4b3abc08664f7da23
Florian Engelhardt <florian.engelhardt@datadoghq.com>
2025-03-31 20:19:11 +0200

    Fetch active span id different to avoid NULL check (#3164)

--- ext/profiling.c
+++ ext/profiling.c
@@ -10,7 +10,7 @@ DDTRACE_PUBLIC struct ddtrace_profiling_context ddtrace_get_profiling_context(vo
     struct ddtrace_profiling_context context = {0, 0};
     if (DDTRACE_G(active_stack) && DDTRACE_G(active_stack)->root_span && get_DD_TRACE_ENABLED()) {
         context.local_root_span_id = DDTRACE_G(active_stack)->root_span->span_id;
-        context.span_id = ddtrace_active_span()->span_id;
+        context.span_id = SPANDATA(DDTRACE_G(active_stack)->active)->span_id;
     }
     return context;
 }
