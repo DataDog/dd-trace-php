91344dbbf045834a8f52ca37f8612e174b50877b
Bob Weinand <bob.weinand@datadoghq.com>
2025-04-01 20:17:56 +0200

    Handle flushing of multiple traces at once
    Working around the agent complaining about "foreign_span". With the agent each array of spans must have the same trace id.
    This is already naturally separated with the closed_stack logic, but it was never actually split into different arrays.
    
    This needs to also move the asm logic as now there's definitely no longer one single trace returned from ddtrace_serialize_closed_spans.
    
    The asm logic for event emitted is instead stored on the root span where the event happens (or inherited from global scope if there was no root span at emission time). And the priority sampling / rate limiting applied when the root span is closed.
    Moving it to the root span close also simplifies the logic and allows possibly straightforwardly dropping the whole trace in future.
    This also fixes the asm event accidentally persisting in worker modes.
    
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- ext/ddtrace.c
+++ ext/ddtrace.c
@@ -2120,8 +2120,25 @@ PHP_FUNCTION(dd_trace_serialize_closed_spans) {
 
     ddtrace_mark_all_span_stacks_flushable();
 
-    array_init(return_value);
-    ddtrace_serialize_closed_spans_with_cycle(return_value);
+    zval traces;
+    array_init(&traces);
+    ddtrace_serialize_closed_spans_with_cycle(&traces);
+
+    if (zend_hash_num_elements(Z_ARR(traces)) == 1) {
+        ZVAL_COPY(return_value, zend_hash_get_current_data(Z_ARR(traces)));
+    } else {
+        array_init(return_value);
+        zval *spans;
+        ZEND_HASH_FOREACH_VAL(Z_ARR(traces), spans) {
+            zval *span;
+            ZEND_HASH_FOREACH_VAL(Z_ARR_P(spans), span) {
+                Z_ADDREF_P(span);
+                zend_hash_next_index_insert_new(Z_ARR_P(return_value), span);
+            } ZEND_HASH_FOREACH_END();
+        } ZEND_HASH_FOREACH_END();
+    }
+
+    zval_ptr_dtor(&traces);
 
     ddtrace_free_span_stacks(false);
     ddtrace_init_span_stacks();
