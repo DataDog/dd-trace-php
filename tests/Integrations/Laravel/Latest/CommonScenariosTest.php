7d44c7f1255d5711a4a3266c434274d2c74b221e
Alexandre Choura <alexandre.choura@datadoghq.com>
2025-04-04 10:37:53 +0200

    update snapshots

--- tests/Integrations/Laravel/Latest/CommonScenariosTest.php
+++ tests/Integrations/Laravel/Latest/CommonScenariosTest.php
@@ -2,6 +2,8 @@
 
 namespace DDTrace\Tests\Integrations\Laravel\Latest;
 
+use DDTrace\Tests\Frameworks\Util\Request\GetSpec;
+
 class CommonScenariosTest extends \DDTrace\Tests\Integrations\Laravel\V9_x\CommonScenariosTest
 {
     public static $database = "laravelX";
@@ -9,5 +11,17 @@ class CommonScenariosTest extends \DDTrace\Tests\Integrations\Laravel\V9_x\Commo
     public static function getAppIndexScript()
     {
         return __DIR__ . '/../../../Frameworks/Laravel/Latest/public/index.php';
+    }
+
+    public function testScenarioGetWithIgnoredException()
+    {
+        $this->tracesFromWebRequestSnapshot(function () {
+            $this->call(
+                GetSpec::create(
+                    'A GET request with an ignored exception',
+                    '/ignored_exception?key=value&pwd=should_redact'
+                )
+            );
+        });
     }
 }
