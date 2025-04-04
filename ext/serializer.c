b37c4f49a1c4017df17b575909d3eb6f8b1d6f82
Munir Abdinur <munir_abdinur@hotmail.com>
2025-04-04 09:29:21 -0400

    missing piece, check len of string

--- ext/serializer.c
+++ ext/serializer.c
@@ -449,8 +449,8 @@ void ddtrace_set_global_span_properties(ddtrace_span_data *span) {
     zval *prop_version = &span->property_version;
 
     ZEND_HASH_FOREACH_STR_KEY_VAL(global_tags, global_key, global_val) {
-        if ((zend_string_equals_literal(global_key, "env") && Z_TYPE_P(prop_env) > IS_NULL) ||
-            (zend_string_equals_literal(global_key, "version") && Z_TYPE_P(prop_version) > IS_NULL) ||
+        if ((zend_string_equals_literal(global_key, "env") && Z_TYPE_P(prop_env) == IS_STRING && Z_STRLEN_P(prop_env) > 0) ||
+            (zend_string_equals_literal(global_key, "version") && Z_TYPE_P(prop_version) == IS_STRING && Z_STRLEN_P(prop_version) > 0) ||
             zend_string_equals_literal(global_key, "service")) {
             continue;
         }
