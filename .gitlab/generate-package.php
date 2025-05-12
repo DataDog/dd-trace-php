3ee03d94e63798d81dbbc59a4e5bbcaf56d6de1e
Bob Weinand <bob.weinand@datadoghq.com>
2025-05-12 12:58:41 +0200

    Try system-tests with docker-ce
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- .gitlab/generate-package.php
+++ .gitlab/generate-package.php
@@ -1105,7 +1105,12 @@ endforeach;
 
       # Install system dependencies
       apt-get update -o dir::state::lists="$APT_CACHE/lists"
-      apt-get install -y --no-install-recommends -o dir::state::lists="$APT_CACHE/lists" -o dir::cache::archives="$APT_CACHE/archives" docker.io git
+      apt-get install -y --no-install-recommends -o dir::state::lists="$APT_CACHE/lists" -o dir::cache::archives="$APT_CACHE/archives" ca-certificates curl git
+      mkdir -p /etc/apt/keyrings
+      curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+      echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" > /etc/apt/sources.list.d/docker.list
+      apt-get update -o dir::state::lists="$APT_CACHE/lists"
+      apt-get install -y --no-install-recommends -o dir::state::lists="$APT_CACHE/lists" -o dir::cache::archives="$APT_CACHE/archives" docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
       # Install Python dependencies
       pip install -U pip virtualenv
