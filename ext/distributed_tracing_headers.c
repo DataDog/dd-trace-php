6538d50329772b578e1392993c1f5e88eeda0eb5
Rachel Yang <rachel.yang@datadoghq.com>
2025-05-20 14:32:46 +0200

    WIP: baggage span tags

--- ext/distributed_tracing_headers.c
+++ ext/distributed_tracing_headers.c
@@ -566,6 +566,21 @@ ddtrace_distributed_tracing_result ddtrace_read_distributed_tracing_ids(ddtrace_
     return result;
 }
 
+void apply_baggage_span_tags(zend_string *key, zval *value, zend_array *meta) {
+    if (zend_hash_num_elements(get_DD_TRACE_BAGGAGE_TAG_KEYS) == 1) {
+        zval *value = zend_hash_get_current_data(get_DD_TRACE_BAGGAGE_TAG_KEYS);
+        if (zend_string_equals_literal(Z_STR_P(value), "*")) {
+            goto add_baggage;
+        }
+    }
+    if (zend_hash_exists (get_DD_TRACE_BAGGAGE_TAG_KEYS(), key)) {
+        add_baggage:
+        key = zend_strpprintf(0, "baggage.%s", ZSTR_VAL(key));
+        zend_hash_update(root_meta, key, val);
+        Z_TRY_ADDREF_P(val);
+    }
+}
+
 void ddtrace_apply_distributed_tracing_result(ddtrace_distributed_tracing_result *result, ddtrace_root_span_data *span) {
     zval zv;
 
@@ -608,6 +623,7 @@ void ddtrace_apply_distributed_tracing_result(ddtrace_distributed_tracing_result
         ZEND_HASH_FOREACH_KEY_VAL(&result->baggage, key_i, key, val) {
             if (key) {
                 zend_hash_update(existing_baggage, key, val);
+                apply_baggage_span_tags(key, val, root_meta);
             } else {
                 zend_hash_index_update(existing_baggage, key_i, val);
             }
@@ -636,7 +652,12 @@ void ddtrace_apply_distributed_tracing_result(ddtrace_distributed_tracing_result
         DDTRACE_G(tracestate) = result->tracestate;  
         zend_hash_destroy(&DDTRACE_G(baggage));
         DDTRACE_G(baggage) = result->baggage;
-        
+        ZEND_HASH_FOREACH_KEY_VAL(&result->baggage, key_i, key, val) {
+            if (key) {
+                apply_baggage_span_tags(key, val, &DDTRACE_G(root_span_tags_preset));
+            }
+        } ZEND_HASH_FOREACH_END();
+
         if (result->trace_id.low || result->trace_id.high) {
             DDTRACE_G(distributed_trace_id) = result->trace_id;
             DDTRACE_G(distributed_parent_trace_id) = result->parent_id;
