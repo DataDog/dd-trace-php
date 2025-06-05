5ba681e8aa54899a06d0c16197d02aab01e3fd12
brettlangdon <brett.langdon@datadoghq.com>
2025-06-05 13:42:44 -0400

    update datadog-setup.php to point to s3 bucket for dev builds

--- tooling/bin/generate-installers.sh
+++ tooling/bin/generate-installers.sh
@@ -9,6 +9,7 @@ packages_build_dir=$2
 # Installers
 ########################
 sed "s|@release_version@|${release_version}|g" ./datadog-setup.php > "${packages_build_dir}/datadog-setup.php"
-if echo "${release_version}" | grep -qE '\+' && [ -n ${CIRCLE_WORKFLOW_JOB_ID:-} ]; then
-  sed -ri "s|define\('RELEASE_URL_PREFIX'[^;]+|const RELEASE_URL_PREFIX = 'https://output.circle-artifacts.com/output/job/${CIRCLE_WORKFLOW_JOB_ID}/artifacts/0/'|" "${packages_build_dir}/datadog-setup.php"
+if echo "${release_version}" | grep -qE '\+' && [ -n ${CI_JOB_ID:-} ]; then
+  replacement="const RELEASE_URL_PREFIX = 'https://s3.us-east-1.amazonaws.com/dd-trace-php-builds/' . RELEASE_VERSION . '/'"
+  sed -ri "s|define\('RELEASE_URL_PREFIX'[^;]+|${replacement}|" "${packages_build_dir}/datadog-setup.php"
 fi
