43032c4d63d973f3d97b6fdfbd20ab481ef58706
brettlangdon <brett.langdon@datadoghq.com>
2025-06-09 16:10:29 -0400

    actually fix download urls?

--- tooling/bin/generate-installers.sh
+++ tooling/bin/generate-installers.sh
@@ -8,8 +8,12 @@ packages_build_dir=$2
 ########################
 # Installers
 ########################
-sed "s|@release_version@|${release_version}|g" ./datadog-setup.php > "${packages_build_dir}/datadog-setup.php"
 if echo "${release_version}" | grep -qE '\+' && [ -n ${CI_JOB_ID:-} ]; then
-  replacement="define('RELEASE_URL_PREFIX', 'https://s3.us-east-1.amazonaws.com/dd-trace-php-builds/' . urlencode('${release_version}') . '/')"
+  replacement="define('RELEASE_VERSION', urlencode('${release_version}'))"
+  sed -ri "s|const RELEASE_VERSION[^;]+|${replacement}|" "${packages_build_dir}/datadog-setup.php"
+
+  replacement="define('RELEASE_URL_PREFIX', 'https://s3.us-east-1.amazonaws.com/dd-trace-php-builds/' . RELEASE_VERSION . '/')"
   sed -ri "s|define\('RELEASE_URL_PREFIX'[^;]+|${replacement}|" "${packages_build_dir}/datadog-setup.php"
+else
+  sed "s|@release_version@|${release_version}|g" ./datadog-setup.php > "${packages_build_dir}/datadog-setup.php"
 fi
