df2bac9daca18574b1d37d077c9f7c2262354b98
brettlangdon <brett.langdon@datadoghq.com>
2025-06-09 10:22:00 -0400

    fix datadog-setup.php printing at end of upload

--- .gitlab/generate-package.php
+++ .gitlab/generate-package.php
@@ -1340,7 +1340,7 @@ foreach ($arch_targets as $arch) {
     VERSION="$(<VERSION)"
     [[ -z "${VERSION}" ]] && echo "VERSION file is empty or not present" && exit 1
     cd packages/ && aws s3 cp --recursive . "s3://dd-trace-php-builds/${VERSION}/"
-    echo "https://s3.us-east-1.amazonaws.com/dd-trace-php-builds/$(echo VERSION | sed 's/+/%2B/')/datadog-setup.php"
+    echo "https://s3.us-east-1.amazonaws.com/dd-trace-php-builds/$(echo $VERSION | sed 's/+/%2B/')/datadog-setup.php"
   artifacts:
     paths:
       - packages/datadog-setup.php
