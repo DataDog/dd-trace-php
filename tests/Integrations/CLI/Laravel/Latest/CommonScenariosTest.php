d8c3bb84f9d89fbd228b57fb3de9e535d419371e
Bob Weinand <bob.weinand@datadoghq.com>
2025-06-16 15:33:23 +0200

    Fix shared & Symfony web tests (#3298)
    * Bump symfony flex for web tests
    
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>
    
    * Fixup Laravel latest job
    
    * Fix
    
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>
    
    * Fix
    
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>
    
    * Avoid distinct tests for apache and non-apache on symfony
    
    * Fix shared php flavour
    
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>
    
    ---------
    
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- tests/Integrations/CLI/Laravel/Latest/CommonScenariosTest.php
+++ tests/Integrations/CLI/Laravel/Latest/CommonScenariosTest.php
@@ -6,10 +6,147 @@ use DDTrace\Tag;
 use DDTrace\Tests\Common\SpanAssertion;
 use DDTrace\Tests\Common\CLITestCase;
 
-class CommonScenariosTest extends \DDTrace\Tests\Integrations\CLI\Laravel\V10_X\CommonScenariosTest
+class CommonScenariosTest extends CLITestCase
 {
+    public function ddSetUp()
+    {
+        parent::ddSetUp();
+        $this->putEnv("DD_TRACE_GENERATE_ROOT_SPAN=true");
+    }
+
     protected function getScriptLocation()
     {
         return __DIR__ . '/../../../../Frameworks/Laravel/Latest/artisan';
+    }
+
+    protected static function getEnvs()
+    {
+        return array_merge(parent::getEnvs(), [
+            'APP_NAME' => 'artisan_test_app',
+        ]);
+    }
+
+    public function testCommandWithNoArguments()
+    {
+        $this->retrieveDumpedData();
+
+        $traces = $this->getTracesFromCommand();
+
+        $this->assertFlameGraph($traces, [
+            SpanAssertion::build(
+                'laravel.artisan',
+                'artisan_test_app',
+                'cli',
+                'artisan'
+            )->withExactTags([
+                Tag::COMPONENT => 'laravel',
+            ])->withChildren([
+                SpanAssertion::exists(
+                    'laravel.provider.load',
+                    'Illuminate\Foundation\ProviderRepository::load'
+                ),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+            ]),
+        ]);
+    }
+
+    public function testCommandWithArgument()
+    {
+        $traces = $this->getTracesFromCommand('route:list');
+
+        $this->assertFlameGraph($traces, [
+            SpanAssertion::build(
+                'laravel.artisan',
+                'artisan_test_app',
+                'cli',
+                'artisan route:list'
+            )->withExactTags([
+                Tag::COMPONENT => 'laravel',
+            ])->withChildren([
+                SpanAssertion::exists(
+                    'laravel.provider.load',
+                    'Illuminate\Foundation\ProviderRepository::load'
+                ),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+            ]),
+        ]);
+    }
+
+    public function testCommandWithError()
+    {
+        $traces = $this->getTracesFromCommand('foo:error');
+
+        $this->assertFlameGraph($traces, [
+            SpanAssertion::build(
+                'laravel.artisan',
+                'artisan_test_app',
+                'cli',
+                'artisan foo:error'
+            )->withExactTags([
+                Tag::COMPONENT => 'laravel',
+            ])->withExistingTagsNames([
+                Tag::ERROR_MSG,
+                'error.stack'
+            ])->withChildren([
+                SpanAssertion::exists(
+                    'laravel.provider.load',
+                    'Illuminate\Foundation\ProviderRepository::load'
+                ),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+                SpanAssertion::exists('laravel.event.handle'),
+            ])->setError(),
+        ]);
     }
 }
