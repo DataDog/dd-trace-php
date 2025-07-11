f58346d0d7a414f1c151c9bc1737ab1cc27d293a
Alexandre Choura <alexandre.choura@datadoghq.com>
2025-07-11 08:37:21 +0200

    chore(generate-tracer): remove GitHub CLI installation commands from CI setup

--- .gitlab/generate-tracer.php
+++ .gitlab/generate-tracer.php
@@ -734,12 +734,6 @@ foreach ($xdebug_test_matrix as [$major_minor, $xdebug]):
       if [[ "${CI_COMMIT_REF_NAME}" == "master" ]] || [[ "${CI_COMMIT_REF_NAME}" =~ ^alex/ ]]; then
         echo "Changes detected, creating/updating PR..."
         
-        # Install GitHub CLI
-        curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-        sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-        echo "deb [signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-        sudo apt update && sudo apt install -y gh
-        
         CURRENT_BRANCH=${CI_COMMIT_REF_NAME}
         TARGET_BRANCH="update-supported-versions"
         
