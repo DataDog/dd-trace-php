2f303eb9ea24be9ca1bd2ef2b403dd4eeea14524
Florian Engelhardt <florian.engelhardt@datadoghq.com>
2025-07-15 15:36:50 +0200

    fix mem leak

--- ext/dogstatsd_client.c
+++ ext/dogstatsd_client.c
@@ -36,7 +36,7 @@ void ddtrace_dogstatsd_client_rinit(void) {
 
     while (health_metrics_enabled) {
         struct addrinfo *addrs = NULL;
-        const char *url = ddtrace_dogstatsd_url();
+        char *url = ddtrace_dogstatsd_url();
         if (strlen(url) > 7 && strncmp("unix://", url, 7) == 0) {
             addrs = dd_alloc_unix_addr(url + 7, strlen(url) - 7);
         } else if (strlen(url) > 6 && strncmp("udp://", url, 6) == 0) {
@@ -45,6 +45,7 @@ void ddtrace_dogstatsd_client_rinit(void) {
                 LOG(WARN,
                     "Dogstatsd client encountered an invalid udp:// DD_DOGSTATSD_URL: %s, missing a colon followed by a port",
                     url);
+                free(url);
                 break;
             }
 
@@ -55,6 +56,7 @@ void ddtrace_dogstatsd_client_rinit(void) {
                 LOG(WARN, "Dogstatsd client failed looking up %s:%s: %s", host, port,
                                     (err == EAI_SYSTEM) ? strerror(errno) : gai_strerror(err));
                 efree(host);
+                free(url);
                 break;
             }
             efree(host);
@@ -62,6 +64,7 @@ void ddtrace_dogstatsd_client_rinit(void) {
             LOG(WARN,
                 "Dogstatsd client encountered an invalid url: %s, expecting url starting with unix:// or udp://",
                 url);
+            free(url);
             break;
         }
 
@@ -79,6 +82,7 @@ void ddtrace_dogstatsd_client_rinit(void) {
                     freeaddrinfo(addrs);
                 }
             }
+            free(url);
             break;
         }
 
@@ -93,6 +97,7 @@ void ddtrace_dogstatsd_client_rinit(void) {
             })
         }
 
+        free(url);
         break;
     }
     _set_dogstatsd_client_globals(client);
