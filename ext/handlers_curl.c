2982c4822836ec395c60180334ab0270288379d4
Alexandre Rulleau <alexandre.rulleau@datadoghq.com>
2025-07-29 13:50:16 +0200

    feat(tracer): handle new header formatting mode
    Signed-off-by: Alexandre Rulleau <alexandre.rulleau@datadoghq.com>

--- ext/handlers_curl.c
+++ ext/handlers_curl.c
@@ -86,7 +86,7 @@ static void dd_inject_distributed_tracing_headers(zend_object *ch) {
         array_init(&headers);
     }
 
-    ddtrace_inject_distributed_headers(Z_ARR(headers), false);
+    ddtrace_inject_distributed_headers(Z_ARR(headers), HEADER_MODE_ARRAY);
 
     zend_function *setopt_fn = zend_hash_str_find_ptr(EG(function_table), ZEND_STRL("curl_setopt"));
 
