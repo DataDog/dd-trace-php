9797c33a35d3e4b7a28b5ac77e4822f32f441845
Alexandre Rulleau <alexandre.rulleau@datadoghq.com>
2025-08-04 15:19:41 +0200

    feat(tracer): add integration flag and fix tests
    Signed-off-by: Alexandre Rulleau <alexandre.rulleau@datadoghq.com>

--- ext/ddtrace.c
+++ ext/ddtrace.c
@@ -1386,71 +1386,81 @@ static php_stream_wrapper ddtrace_traced_https_wrapper;
 #define DDTRACE_STREAM_OPENER_CALL_ARGS \
     wrapper, filename, mode, options, opened_path, context STREAMS_REL_CC
 
+static bool dd_load_http_stream_integration() {
+    return get_DD_DISTRIBUTED_TRACING() && get_DD_TRACE_ENABLED();
+}
+
 static php_stream *ddtrace_stream_opener(
     php_stream_wrapper *original_wrapper,
     DDTRACE_STREAM_OPENER_ARGS
 ) {
     bool clean_context = false;
+
     if (!context) {
         context = php_stream_context_alloc();
         clean_context = true;
     }
 
-    // Ensure context->options is safe to modify
     zval *options_zv = &context->options;
-    SEPARATE_ARRAY(options_zv);
 
-    // Retrieve or create the "http" subarray
-    zval *http_context_zv = zend_hash_str_find_deref(Z_ARRVAL_P(options_zv), "http", sizeof("http") - 1);
-    if (!http_context_zv) {
-        zval tmp;
-        array_init(&tmp);
-        zend_hash_str_update(Z_ARRVAL_P(options_zv), "http", sizeof("http") - 1, &tmp);
-        http_context_zv = zend_hash_str_find_deref(Z_ARRVAL_P(options_zv), "http", sizeof("http") - 1);
-    }
+    if (dd_load_http_stream_integration()) {
+        SEPARATE_ARRAY(options_zv);
 
-    if (Z_TYPE_P(http_context_zv) == IS_ARRAY) {
-        SEPARATE_ARRAY(http_context_zv);
-        HashTable *http_context = Z_ARRVAL_P(http_context_zv);
+        // Retrieve or create the "http" subarray
+        zval *http_context_zv = zend_hash_str_find_deref(Z_ARRVAL_P(options_zv), "http", sizeof("http") - 1);
+        if (!http_context_zv) {
+            zval tmp;
+            array_init(&tmp);
+            zend_hash_str_update(Z_ARRVAL_P(options_zv), "http", sizeof("http") - 1, &tmp);
+            http_context_zv = zend_hash_str_find_deref(Z_ARRVAL_P(options_zv), "http", sizeof("http") - 1);
+        }
 
-        zval *header_zv = zend_hash_str_find(http_context, ZEND_STRL("header"));
-        if (header_zv && Z_TYPE_P(header_zv) == IS_ARRAY) {
-            SEPARATE_ARRAY(header_zv);
-            ddtrace_inject_distributed_headers(Z_ARRVAL_P(header_zv), HEADER_MODE_ARRAY);
-        } else {
-            ddtrace_inject_distributed_headers(http_context, HEADER_MODE_CONTEXT);
+        if (Z_TYPE_P(http_context_zv) == IS_ARRAY) {
+            SEPARATE_ARRAY(http_context_zv);
+            HashTable *http_context = Z_ARRVAL_P(http_context_zv);
+
+            zval *header_zv = zend_hash_str_find(http_context, ZEND_STRL("header"));
+            if (header_zv && Z_TYPE_P(header_zv) == IS_ARRAY) {
+                SEPARATE_ARRAY(header_zv);
+                ddtrace_inject_distributed_headers(Z_ARRVAL_P(header_zv), HEADER_MODE_ARRAY);
+            } else {
+                ddtrace_inject_distributed_headers(http_context, HEADER_MODE_CONTEXT);
+            }
         }
     }
 
     // Open internal span
-    ddtrace_span_data *span = ddtrace_alloc_execute_data_span(-2, EG(current_execute_data));
-    if (span) {
-        ddtrace_set_global_span_properties(span);
+    ddtrace_span_data *span = NULL;
+    if (ddtrace_integrations[DDTRACE_INTEGRATION_HTTPSTREAM].is_enabled()) {
+        span = ddtrace_alloc_execute_data_span(-2, EG(current_execute_data));
+        if (span) {
+            ddtrace_set_global_span_properties(span);
 
-        zend_array *meta = ddtrace_property_array(&span->property_meta);
-        zval zv;
+            zend_array *meta = ddtrace_property_array(&span->property_meta);
+            zval zv;
 
-        ZVAL_STRING(&zv, "php.stream");
-        zend_hash_str_update(meta, ZEND_STRL("component"), &zv);
+            ZVAL_STRING(&zv, "php.stream");
+            zend_hash_str_update(meta, ZEND_STRL("component"), &zv);
 
-        ZVAL_STRING(&zv, "client");
-        zend_hash_str_update(meta, ZEND_STRL("span.kind"), &zv);
+            ZVAL_STRING(&zv, "client");
+            zend_hash_str_update(meta, ZEND_STRL("span.kind"), &zv);
 
-        ZVAL_STRING(&zv, filename);
-        zend_hash_str_update(meta, ZEND_STRL("http.url"), &zv);
+            ZVAL_STRING(&zv, filename);
+            zend_hash_str_update(meta, ZEND_STRL("http.url"), &zv);
 
-        zval *method_zv = zend_hash_str_find(Z_ARRVAL_P(options_zv), "method", sizeof("method") - 1);
-        if (method_zv && Z_TYPE_P(method_zv) == IS_STRING) {
-            zend_hash_str_update(meta, ZEND_STRL("http.method"), method_zv);
-        }
+            zval *method_zv = zend_hash_str_find(Z_ARRVAL_P(options_zv), "method", sizeof("method") - 1);
+            if (method_zv && Z_TYPE_P(method_zv) == IS_STRING) {
+                zend_hash_str_update(meta, ZEND_STRL("http.method"), method_zv);
+            }
 
-        const char *host_start = strstr(filename, "://");
-        if (host_start) {
-            host_start += 3; // skip "://"
-            const char *host_end = strchr(host_start, '/');
-            size_t host_len = host_end ? (size_t)(host_end - host_start) : strlen(host_start);
-            ZVAL_STRINGL(&zv, host_start, host_len);
-            zend_hash_str_update(meta, ZEND_STRL("network.destination.name"), &zv);
+            const char *host_start = strstr(filename, "://");
+            if (host_start) {
+                host_start += 3; // skip "://"
+                const char *host_end = strchr(host_start, '/');
+                size_t host_len = host_end ? (size_t)(host_end - host_start) : strlen(host_start);
+                ZVAL_STRINGL(&zv, host_start, host_len);
+                zend_hash_str_update(meta, ZEND_STRL("network.destination.name"), &zv);
+            }
         }
     }
 
