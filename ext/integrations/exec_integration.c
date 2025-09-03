995a84d6d4832070a7fc5cf494c2a6ad43a611b0
Bob Weinand <bob.weinand@datadoghq.com>
2025-09-03 16:49:31 +0200

    Fix possible crash with freed resource
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- ext/integrations/exec_integration.c
+++ ext/integrations/exec_integration.c
@@ -237,7 +237,7 @@ PHP_FUNCTION(DDTrace_Integrations_Exec_proc_get_span) {
     }
 
     zend_resource *span_res = proc_h->pipes[proc_h->npipes - 1];
-    if (span_res->type != le_proc_span) {
+    if (span_res->type != le_proc_span || !span_res->ptr) {
         RETURN_NULL();
     }
 
