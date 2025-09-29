2ad2b85b3ee52c34fa8d927d7fc9d35b7ec919e5
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2025-09-29 10:43:28 +0200

    Add authentication vector

--- components-rs/ddtrace.h
+++ components-rs/ddtrace.h
@@ -193,6 +193,8 @@ struct ddog_Vec_CChar *ddog_CharSlice_to_owned(ddog_CharSlice str);
 
 struct ddog_Vec_I32 *ddog_number_to_owned_i32(int32_t number);
 
+struct ddog_Vec_Authentication *ddog_number_to_owned_Authentication(ddog_Authentication auth);
+
 void ddog_Vec_CChar_drop(struct ddog_Vec_CChar *ptr);
 
 bool ddog_remote_configs_service_env_change(struct ddog_RemoteConfigState *remote_config,
