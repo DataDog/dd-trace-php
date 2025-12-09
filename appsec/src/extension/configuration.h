6ed6f08a535baf218841906574483a5a7a4a1db9
Gustavo Lopes <mail@geleia.net>
2025-12-09 17:05:57 +0000

    Appsec: use socket in abstract ns

--- appsec/src/extension/configuration.h
+++ appsec/src/extension/configuration.h
@@ -29,7 +29,7 @@ extern bool runtime_config_first_init;
 #define DD_BASE(path) "/opt/datadog-php/"
 
 // clang-format off
-#define DD_CONFIGURATION \
+#define DD_CONFIGURATION_GENERAL \
     SYSCFG(BOOL, DD_APPSEC_ENABLED, "false")                                                                                          \
     SYSCFG(BOOL, DD_APPSEC_CLI_START_ON_RINIT, "false")                                                                               \
     SYSCFG(STRING, DD_APPSEC_RULES, "")                                                                                               \
@@ -49,7 +49,6 @@ extern bool runtime_config_first_init;
     SYSCFG(BOOL, DD_APPSEC_RASP_ENABLED , "true")                                                                                     \
     SYSCFG(INT, DD_APPSEC_MAX_STACK_TRACE_DEPTH, "32")                                                                                \
     SYSCFG(INT, DD_APPSEC_MAX_STACK_TRACES, "2")                                                                                      \
-    CONFIG(STRING, DD_APPSEC_HELPER_RUNTIME_PATH, "/tmp", .ini_change = dd_on_runtime_path_update)                                    \
     SYSCFG(STRING, DD_APPSEC_HELPER_LOG_FILE, "/dev/null")                                                                            \
     SYSCFG(STRING, DD_APPSEC_HELPER_LOG_LEVEL, "info")                                                                                \
     CONFIG(CUSTOM(SET), DD_EXTRA_SERVICES, "", .parser = _parse_list)                                                                 \
@@ -70,7 +69,17 @@ extern bool runtime_config_first_init;
     CONFIG(STRING, DD_APPSEC_HTTP_BLOCKED_TEMPLATE_JSON, "")                                                                          \
     CONFIG(BOOL, DD_APM_TRACING_ENABLED, "true")                                                                                      \
     CONFIG(BOOL, DD_API_SECURITY_ENABLED, "true", .ini_change = zai_config_system_ini_change)                                         \
-    CONFIG(DOUBLE, DD_API_SECURITY_SAMPLE_DELAY, "30.0", .ini_change = zai_config_system_ini_change)                                  \
+    CONFIG(DOUBLE, DD_API_SECURITY_SAMPLE_DELAY, "30.0", .ini_change = zai_config_system_ini_change)
+
+#ifdef __linux__
+#define DD_CONFIGURATION \
+    DD_CONFIGURATION_GENERAL \
+    CONFIG(STRING, DD_APPSEC_HELPER_RUNTIME_PATH, "@/ddappsec", .ini_change = dd_on_runtime_path_update)
+#else
+#define DD_CONFIGURATION \
+    DD_CONFIGURATION_GENERAL \
+    CONFIG(STRING, DD_APPSEC_HELPER_RUNTIME_PATH, "/tmp", .ini_change = dd_on_runtime_path_update)
+#endif
 // clang-format on
 
 #define CALIAS CONFIG
