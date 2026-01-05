358ec9c1b986f8bab20f309f7d25cb7984b40580
Gustavo Lopes <mail@geleia.net>
2026-01-05 12:30:44 -0300

    RFC 1076: fallback on http.endpoint for schema sampler

--- appsec/src/extension/request_lifecycle.c
+++ appsec/src/extension/request_lifecycle.c
@@ -942,24 +942,67 @@ static uint64_t _calc_sampling_key(zend_object *root_span, int status_code)
         return 0;
     }
 
+    zend_string *route_or_endpoint = NULL;
+    bool free_route_or_endpoint = false;
+
     zval *route = zend_hash_str_find(Z_ARRVAL_P(meta), ZEND_STRL("http.route"));
-    if (!route || Z_TYPE_P(route) != IS_STRING) {
-        mlog_g(dd_log_debug, "No http.route tag; not sampling");
-        return 0;
+    const bool has_http_route = route && Z_TYPE_P(route) == IS_STRING;
+    if (has_http_route) {
+        route_or_endpoint = Z_STR_P(route);
+    } else {
+        // http.route is absent, check for http.endpoint
+        zval *endpoint =
+            zend_hash_str_find(Z_ARRVAL_P(meta), ZEND_STRL("http.endpoint"));
+        const bool has_http_endpoint =
+            endpoint && Z_TYPE_P(endpoint) == IS_STRING;
+        if (has_http_endpoint) {
+            // http.endpoint is already computed
+            // Use it unless status code is 404
+#define HTTP_NOT_FOUND 404
+            if (status_code == HTTP_NOT_FOUND) {
+                mlog_g(dd_log_debug,
+                    "http.endpoint present but status is 404; not sampling");
+            } else {
+                route_or_endpoint = Z_STR_P(endpoint);
+            }
+        } else {
+            // http.endpoint not computed, compute it now without setting the
+            // tag
+            zval *url =
+                zend_hash_str_find(Z_ARRVAL_P(meta), ZEND_STRL("http.url"));
+            const bool has_http_url = url && Z_TYPE_P(url) == IS_STRING;
+            if (has_http_url) {
+                route_or_endpoint = dd_trace_guess_endpoint_from_url(
+                    Z_STRVAL_P(url), Z_STRLEN_P(url));
+                if (route_or_endpoint) {
+                    free_route_or_endpoint = true;
+                } else {
+                    mlog_g(dd_log_debug,
+                        "Failed to compute http.endpoint; not sampling");
+                }
+            } else {
+                mlog_g(dd_log_debug,
+                    "No http.route, http.endpoint, or http.url; not sampling");
+            }
+        }
+    }
+
+    if (!route_or_endpoint) {
+        goto error;
     }
 
     zval *method =
         zend_hash_str_find(Z_ARRVAL_P(meta), ZEND_STRL("http.method"));
     if (!method || Z_TYPE_P(method) != IS_STRING) {
         mlog_g(dd_log_debug, "No http.method tag; not sampling");
-        return 0;
+        goto error;
     }
 
-    // use fnv-1a hash with: <http.route tag> NULL <http.method tag> NULL
+    // use fnv-1a hash with: <route_or_endpoint> NULL <http.method tag> NULL
     // status_code
 
     uint64_t hash = FNV_OFFSET_BASIS;
-    hash = _hash_zend_string(hash, Z_STR_P(route));
+    hash = _hash_zend_string(hash, route_or_endpoint);
     hash *= FNV_PRIME; // hash NUL byte
     hash = _hash_zend_string(hash, Z_STR_P(method));
     hash *= FNV_PRIME; // hash NUL byte
@@ -970,7 +1013,22 @@ static uint64_t _calc_sampling_key(zend_object *root_span, int status_code)
         snprintf(status_str, sizeof(status_str), "%d", status_code);
     hash = _hash_string(hash, status_str, status_len);
 
+    mlog_g(dd_log_debug,
+        "Will use sampling key: %" PRIu64
+        " for route/endpoint %.*s, method %.*s, status %d",
+        hash, (int)ZSTR_LEN(route_or_endpoint), ZSTR_VAL(route_or_endpoint),
+        (int)ZSTR_LEN(Z_STR_P(method)), ZSTR_VAL(Z_STR_P(method)), status_code);
+
+    if (free_route_or_endpoint) {
+        zend_string_release(route_or_endpoint);
+    }
     return hash;
+
+error:
+    if (free_route_or_endpoint) {
+        zend_string_release(route_or_endpoint);
+    }
+    return 0;
 }
 
 PHP_FUNCTION(datadog_appsec_testing_dump_req_lifecycle_state)
