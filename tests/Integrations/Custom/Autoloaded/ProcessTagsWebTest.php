3a8038ca66aa61ba3ad1a6622713a6113f8e9325
Louis Tricot <louis.tricot@datadoghq.com>
2026-01-13 16:42:40 +0100

    fix test

--- tests/Integrations/Custom/Autoloaded/ProcessTagsWebTest.php
+++ tests/Integrations/Custom/Autoloaded/ProcessTagsWebTest.php
@@ -48,9 +48,9 @@ class ProcessTagsWebTest extends WebFrameworkTestCase
         }
 
         $this->assertArrayHasKey('entrypoint.workdir', $tags, 'entrypoint.workdir should be present');
-        $this->assertArrayHasKey('entrypoint.name', $tags, 'entrypoint.name should be present for web SAPI');
-        $this->assertArrayHasKey('entrypoint.basedir', $tags, 'entrypoint.basedir should be present for web SAPI');
         $this->assertArrayHasKey('entrypoint.type', $tags, 'entrypoint.type should be present for web SAPI');
+        $this->assertArrayNotHasKey('entrypoint.name', $tags, 'entrypoint.name should not be present for web SAPI');
+        $this->assertArrayNotHasKey('entrypoint.basedir', $tags, 'entrypoint.basedir should not be present for web SAPI');
 
         // Verify server.type is one of the expected SAPIs tested in CI
         $expectedSapis = ['cli-server', 'cgi-fcgi', 'apache2handler', 'fpm-fcgi'];
@@ -63,11 +63,6 @@ class ProcessTagsWebTest extends WebFrameworkTestCase
                 $tags['runtime.sapi']
             )
         );
-        $this->assertEquals($tags['entrypoint.name'], 'index');
-        $this->assertEquals($tags['entrypoint.workdir'], 'app');
         $this->assertEquals($tags['entrypoint.type'], 'script');
-        $this->assertEquals($tags['entrypoint.basedir'], 'public');
-
-        // $this->assert(1 == 0);
     }
 }
