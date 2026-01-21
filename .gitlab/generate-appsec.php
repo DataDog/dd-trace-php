3fb2b55df1d1d49faef28e7a8699400d0e22c11c
Bob Weinand <bob.weinand@datadoghq.com>
2026-01-21 14:32:40 +0100

    Silent junit output
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- .gitlab/generate-appsec.php
+++ .gitlab/generate-appsec.php
@@ -145,7 +145,7 @@ stages:
   after_script:
     - mkdir -p "${CI_PROJECT_DIR}/artifacts"
     - find appsec/tests/integration/build/test-results -name "*.xml" -exec cp --parents '{}' "${CI_PROJECT_DIR}/artifacts/" \;
-    - .gitlab/upload-junit-to-datadog.sh "test.source.file:appsec"
+    - .gitlab/silent-upload-junit-to-datadog.sh "test.source.file:appsec"
   artifacts:
     reports:
       junit: "artifacts/**/test-results/**/TEST-*.xml"
