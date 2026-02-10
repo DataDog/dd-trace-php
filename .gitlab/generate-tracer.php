3a4696d2b1e3aecdb90b728e3f52e2956ecc732e
Alexandre Rulleau <alexandre.rulleau@datadoghq.com>
2026-02-10 15:12:21 +0100

    test(CI): add manual cleanup in before_script step
    Signed-off-by: Alexandre Rulleau <alexandre.rulleau@datadoghq.com>

--- .gitlab/generate-tracer.php
+++ .gitlab/generate-tracer.php
@@ -118,19 +118,32 @@ stages:
       - PHP_MAJOR_MINOR: <?= json_encode($windows_minor_major_targets) ?>
 
   variables:
-    GIT_CONFIG_COUNT: 2
-    GIT_CONFIG_KEY_0: core.longpaths
-    GIT_CONFIG_VALUE_0: true
-    GIT_CONFIG_KEY_1: core.symlinks
-    GIT_CONFIG_VALUE_1: true
     CONTAINER_NAME: $CI_JOB_NAME_SLUG
-    GIT_STRATEGY: clone
-    GIT_CLEAN_FLAGS: -ffdxq
+    GIT_STRATEGY: none
     IMAGE: "registry.ddbuild.io/images/mirror/datadog/dd-trace-ci:php-${PHP_MAJOR_MINOR}_windows"
   script: |
+    # Agressive Git cleanup
+    Write-Host "Performing aggressive workspace cleanup with cmd.exe..."
+    cmd /c "if exist .git rmdir /s /q .git" 2>$null
+    cmd /c "for /d %d in (*) do @rmdir /s /q ""%d""" 2>$null
+    cmd /c "del /f /s /q *" 2>$null
+    Write-Host "Cleanup complete."
+
     # Make sure we actually fail if a command fails
     $ErrorActionPreference = 'Stop'
     $PSNativeCommandUseErrorActionPreference = $true
+
+    # Manual git clone with proper config
+    Write-Host "Cloning repository..."
+    git config --global core.longpaths true
+    git config --global core.symlinks true
+    git clone --branch $env:CI_COMMIT_REF_NAME $env:CI_REPOSITORY_URL .
+    git checkout $env:CI_COMMIT_SHA
+
+    # Initialize submodules
+    Write-Host "Initializing submodules..."
+    git submodule update --init --recursive
+    Write-Host "Git setup complete."
 
     mkdir dumps
 
