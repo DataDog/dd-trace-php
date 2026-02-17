317b1aab8b89578adbdb0c37d8df6522b3480e76
Bob Weinand <bob.weinand@datadoghq.com>
2026-02-17 14:57:27 +0100

    Block sidecar notification signal while sleeping
    Otherwise users may sleep a lot less than they expect.
    
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>

--- ext/handlers_signal.c
+++ ext/handlers_signal.c
@@ -79,6 +79,8 @@ static void dd_handle_signal(zif_handler original_function, INTERNAL_FUNCTION_PA
     x(ldap_connect) \
     x(sqlsrv_connect) \
     x(pcntl_sigwaitinfo) \
+    x(sleep) \
+    x(usleep) \
 
 #define BLOCKMETH(x) \
     x(PDO, connect) \
