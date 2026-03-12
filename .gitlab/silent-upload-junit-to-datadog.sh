de3f3b36a3b0657e2c857b392b471cdf4c6a3c39
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2026-03-12 18:17:22 +0100

    Remove debug instruction

--- .gitlab/silent-upload-junit-to-datadog.sh
+++ .gitlab/silent-upload-junit-to-datadog.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 OUTFILE=/tmp/datadog-junit-upload.txt
 "$(dirname -- "${BASH_SOURCE[0]}")"/upload-junit-to-datadog.sh "$@" >$OUTFILE 2>&1
-
-cat $OUTFILE
-
 if [[ $? -ne 0 ]]; then
   cat $OUTFILE
 else
