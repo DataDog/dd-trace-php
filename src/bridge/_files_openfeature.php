eaa44fc1852b8577c00064e40529b9feb0526637
Leo Romanovsky <leo.romanovsky@datadoghq.com>
2026-05-22 14:51:53 -0400

    Avoid duplicate OpenFeature bridge preloads

--- src/bridge/_files_openfeature.php
+++ src/bridge/_files_openfeature.php
@@ -1,18 +1,5 @@
 <?php
 
 return [
-    __DIR__ . '/../api/FeatureFlags/EvaluationType.php',
-    __DIR__ . '/../api/FeatureFlags/EvaluationReason.php',
-    __DIR__ . '/../api/FeatureFlags/EvaluationErrorCode.php',
-    __DIR__ . '/../api/FeatureFlags/EvaluationDetails.php',
-    __DIR__ . '/../api/FeatureFlags/Internal/WarningEmitter.php',
-    __DIR__ . '/../api/FeatureFlags/Internal/Evaluator.php',
-    __DIR__ . '/../api/FeatureFlags/Internal/ResultMapper.php',
-    __DIR__ . '/../api/FeatureFlags/Internal/UnavailableEvaluator.php',
-    __DIR__ . '/../api/FeatureFlags/Internal/RemoteConfigClient.php',
-    __DIR__ . '/../api/FeatureFlags/Internal/NativeEvaluator.php',
-    __DIR__ . '/../api/FeatureFlags/Internal/NoopWarningEmitter.php',
-    __DIR__ . '/../api/FeatureFlags/Internal/TriggerErrorWarningEmitter.php',
-    __DIR__ . '/../api/FeatureFlags/Client.php',
     __DIR__ . '/../DDTrace/OpenFeature/DataDogProvider.php',
 ];
