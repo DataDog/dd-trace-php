bdaca4fbb01ca7ae567e5911ad57fa2a0df50173
Bob Weinand <bob.weinand@datadoghq.com>
2026-06-02 19:41:14 +0200

    Fix compile error

--- tracer/ffe.h
+++ tracer/ffe.h
@@ -2,7 +2,7 @@
 #define DDTRACE_FFE_H
 
 #include <stdbool.h>
-#include <zend_string.h>
+#include <zend.h>
 
 void ddtrace_ffe_record_exposure(zend_string *flag_key, zend_string *targeting_key, zend_string *subject_attributes_json, zend_string *allocation_key, zend_string *variant);
 bool ddtrace_ffe_flush_exposures(void);
