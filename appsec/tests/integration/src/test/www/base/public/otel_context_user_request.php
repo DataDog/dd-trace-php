d86b4a3d279078522afe32cfef4d09eb7bcfa2cb
Gustavo André dos Santos Lopes <gustavo.lopes@datadoghq.com>
2026-06-23 16:17:53 +0100

    Add tests for otel ctx/thread local

---
+++ appsec/tests/integration/src/test/www/base/public/otel_context_user_request.php
@@ -0,0 +1,41 @@
+<?php
+
+$outerSpan = \DDTrace\root_span();
+$userRequestSpan = \DDTrace\start_trace_span();
+$userRequestSpan->name = 'otel_context.user_request';
+$userRequestSpan->resource = 'otel_context.user_request';
+
+\DDTrace\UserRequest\notify_start($userRequestSpan, [
+    '_GET' => $_GET,
+    '_POST' => $_POST,
+    '_SERVER' => $_SERVER,
+    '_FILES' => $_FILES,
+    '_COOKIE' => $_COOKIE,
+]);
+
+if ($outerSpan) {
+    \DDTrace\switch_stack($outerSpan);
+}
+
+$waited = \datadog\appsec\testing\wait_for_debugger();
+
+$response = [
+    'waited' => $waited,
+    'trace_id' => $userRequestSpan->traceId,
+    'span_id' => $userRequestSpan->hexId(),
+    'local_root_span_id' => $userRequestSpan->hexId(),
+    'outer_span_id' => $outerSpan ? $outerSpan->hexId() : null,
+];
+
+\DDTrace\switch_stack($userRequestSpan);
+\DDTrace\UserRequest\notify_commit($userRequestSpan, 200, [
+    'Content-Type' => ['application/json'],
+]);
+\DDTrace\close_span();
+
+if ($outerSpan) {
+    \DDTrace\switch_stack($outerSpan);
+}
+
+header('Content-Type: application/json');
+echo json_encode($response);
