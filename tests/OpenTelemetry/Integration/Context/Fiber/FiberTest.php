d2c2ddb073d7ae1cdb3d42195c101d0ca12474c9
Levi Morrison <levi.morrison@datadoghq.com>
2026-08-04 09:26:21 -0600

    test(otel): account for PHP 8.5 compatibility gaps

--- tests/OpenTelemetry/Integration/Context/Fiber/FiberTest.php
+++ tests/OpenTelemetry/Integration/Context/Fiber/FiberTest.php
@@ -40,6 +40,9 @@ final class FiberTest extends BaseTestCase
         if (version_compare(PHP_VERSION, '8.1.0', '<')) {
             $this->markTestSkipped('Fibers are only supported in PHP 8.1+');
         }
+        if (PHP_VERSION_ID >= 80500) {
+            $this->markTestSkipped('OpenTelemetry fiber stack interoperability is not supported on PHP 8.5 yet');
+        }
 
         $traces = $this->isolateTracer(function () {
             $tracer = (new TracerProvider())->getTracer('OpenTelemetry.TestTracer');
