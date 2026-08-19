5b69b1e6e7dc617bcf7723a133e902876d56428d
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2026-08-19 15:44:03 +0200

    Fix pipeline

--- tracer/routing_cache.h
+++ tracer/routing_cache.h
@@ -5,8 +5,8 @@
 
 #define DDTRACE_ROUTING_CACHE_CAPACITY 500
 
-void ddtrace_routing_cache_ginit(void);
-void ddtrace_routing_cache_gshutdown(void);
+void ddtrace_routing_cache_ginit(HashTable *rcache);
+void ddtrace_routing_cache_gshutdown(HashTable *rcache);
 
 PHP_FUNCTION(DDTrace_routing_cache_get);
 PHP_FUNCTION(DDTrace_routing_cache_set);
