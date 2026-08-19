d318dbe8864165e9f9524e93e2de660fe5b554ca
Alejandro Estringana Ruiz <alejandro.estringanaruiz@datadoghq.com>
2026-08-19 11:44:42 +0200

    Make hashtable thread safety

--- tracer/routing_cache.c
+++ tracer/routing_cache.c
@@ -1,8 +1,6 @@
 #include "routing_cache.h"
 #include "ddtrace.h"
 
-static HashTable ddtrace_rcache;
-
 static void ddtrace_routing_cache_dtor(zval *pz) {
     zend_string_release_ex((zend_string *)Z_PTR_P(pz), 1);
 }
@@ -12,19 +10,19 @@ static void ddtrace_routing_cache_evict_oldest(void) {
     zend_string *key;
     zend_ulong num_idx;
 
-    zend_hash_internal_pointer_reset_ex(&ddtrace_rcache, &pos);
-    if (zend_hash_get_current_key_type_ex(&ddtrace_rcache, &pos) == HASH_KEY_IS_STRING) {
-        zend_hash_get_current_key_ex(&ddtrace_rcache, &key, &num_idx, &pos);
-        zend_hash_del(&ddtrace_rcache, key);
+    zend_hash_internal_pointer_reset_ex(&DDTRACE_G(rcache), &pos);
+    if (zend_hash_get_current_key_type_ex(&DDTRACE_G(rcache), &pos) == HASH_KEY_IS_STRING) {
+        zend_hash_get_current_key_ex(&DDTRACE_G(rcache), &key, &num_idx, &pos);
+        zend_hash_del(&DDTRACE_G(rcache), key);
     }
 }
 
-void ddtrace_routing_cache_minit(void) {
-    zend_hash_init(&ddtrace_rcache, DDTRACE_ROUTING_CACHE_CAPACITY, NULL, ddtrace_routing_cache_dtor, 1);
+void ddtrace_routing_cache_ginit(void) {
+    zend_hash_init(&DDTRACE_G(rcache), DDTRACE_ROUTING_CACHE_CAPACITY, NULL, ddtrace_routing_cache_dtor, 1);
 }
 
-void ddtrace_routing_cache_mshutdown(void) {
-    zend_hash_destroy(&ddtrace_rcache);
+void ddtrace_routing_cache_gshutdown(void) {
+    zend_hash_destroy(&DDTRACE_G(rcache));
 }
 
 /* DDTrace\routing_cache_get(string $key): string|false */
@@ -34,7 +32,7 @@ PHP_FUNCTION(DDTrace_routing_cache_get) {
         Z_PARAM_STR(key)
     ZEND_PARSE_PARAMETERS_END();
 
-    zend_string *value = zend_hash_find_ptr(&ddtrace_rcache, key);
+    zend_string *value = zend_hash_find_ptr(&DDTRACE_G(rcache), key);
     if (!value) {
         RETURN_FALSE;
     }
@@ -49,11 +47,11 @@ PHP_FUNCTION(DDTrace_routing_cache_set) {
         Z_PARAM_STR(value)
     ZEND_PARSE_PARAMETERS_END();
 
-    if (zend_hash_num_elements(&ddtrace_rcache) >= DDTRACE_ROUTING_CACHE_CAPACITY
-        && !zend_hash_find_ptr(&ddtrace_rcache, key)) {
+    if (zend_hash_num_elements(&DDTRACE_G(rcache)) >= DDTRACE_ROUTING_CACHE_CAPACITY
+        && !zend_hash_find_ptr(&DDTRACE_G(rcache), key)) {
         ddtrace_routing_cache_evict_oldest();
     }
 
     zend_string *persistent_value = zend_string_init(ZSTR_VAL(value), ZSTR_LEN(value), 1);
-    zend_hash_str_update_ptr(&ddtrace_rcache, ZSTR_VAL(key), ZSTR_LEN(key), persistent_value);
+    zend_hash_str_update_ptr(&DDTRACE_G(rcache), ZSTR_VAL(key), ZSTR_LEN(key), persistent_value);
 }
