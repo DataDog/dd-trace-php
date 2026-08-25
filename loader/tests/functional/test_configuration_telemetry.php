d22fa5bc568dec4fe7805663f7e136ff32a3dd88
Levi Morrison <levi.morrison@datadoghq.com>
2026-08-24 20:12:14 -0600

    ci: improve a variety of testing flakes (#4131)
    * test: wait for SSI configuration telemetry
    
    * test: diagnose agent info setup failures
    
    * test: isolate crashtracker JIT requests
    
    * test: poll telemetry after waiting
    
    * test: isolate SSI telemetry output
    
    Without it, the trace can be written to the file instead of telemetry
    
    * test: accept split configuration telemetry
    
    * test: diagnose missing debugger diagnostics
    
    * ci: collect PHP language test core dumps

--- loader/tests/functional/test_configuration_telemetry.php
+++ loader/tests/functional/test_configuration_telemetry.php
@@ -11,14 +11,23 @@ $output = runCLI('-r "echo \'foo\'; dd_trace_internal_fn(\'finalize_telemetry\')
     'DD_INJECT_FORCE=true',
     'DD_INJECTION_ENABLED=tracer', // Normally set by the injector
     'DD_SERVICE=loader',
+    'DD_TRACE_GENERATE_ROOT_SPAN=0',
 ]);
 
 assertMatchesFormat($output, '%A"loaded_by_ssi":true%s%A');
 
-// Let time to write the telemetry log
-usleep(300000);
+// The sidecar writes asynchronously. Poll for the first expected entry instead
+// of assuming it will always start and flush within a fixed delay.
+$instrumentationSource = '{"name":"instrumentation_source","value":"ssi","origin":"default","config_id":null,"seq_id":null}';
+$deadline = microtime(true) + 5;
+do {
+    usleep(10000);
+    $content = file_get_contents($telemetryLogPath);
+    if (strpos($content, $instrumentationSource) !== false) {
+        break;
+    }
+} while (microtime(true) < $deadline);
 
-$content = file_get_contents($telemetryLogPath);
-assertContains($content, '{"name":"instrumentation_source","value":"ssi","origin":"default","config_id":null,"seq_id":null}');
+assertContains($content, $instrumentationSource);
 assertContains($content, '{"name":"ssi_injection_enabled","value":"tracer","origin":"env_var","config_id":null,"seq_id":null}');
 assertContains($content, '{"name":"ssi_forced_injection_enabled","value":"True","origin":"env_var","config_id":null,"seq_id":null}');
