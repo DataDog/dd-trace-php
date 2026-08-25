d25ed63624017468298b8d5e23fb560531181fda
Laplie Anderson <laplie.anderson@datadoghq.com>
2026-08-25 12:35:54 -0400

    use a different base image with datadog-ci built in

--- .gitlab/generate-package.php
+++ .gitlab/generate-package.php
@@ -1807,16 +1807,15 @@ deploy_to_reliability_env:
 
 "test install datadog ci":
   stage: pre-release
-  image: registry.ddbuild.io/images/base/gbi-ubuntu_2404:release
+  image: registry.ddbuild.io/images/bazel:dynamic-22.04
   tags: [ "arch:amd64" ]
   needs: []
   script:
-    - .gitlab/install-datadog-ci.sh
     - datadog-ci version
 
 "upload SSI debug symbols":
   stage: pre-release
-  image: registry.ddbuild.io/images/base/gbi-ubuntu_2404:release
+  image: registry.ddbuild.io/images/bazel:dynamic-22.04
   tags: [ "arch:amd64" ]
   only:
     - tags
