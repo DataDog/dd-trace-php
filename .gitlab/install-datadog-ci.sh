0d686c1aa88da25db93b421a68f192a51cae263c
Laplie Anderson <laplie.anderson@datadoghq.com>
2026-08-25 11:43:39 -0400

    get rid of the output swallowing

--- .gitlab/install-datadog-ci.sh
+++ .gitlab/install-datadog-ci.sh
@@ -2,10 +2,10 @@
 
 export NVM_DIR="${HOME}/.nvm"
 if ! command -v npx &>/dev/null; then
-  curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash - >/dev/null 2>&1
-  . "${NVM_DIR}/nvm.sh" >/dev/null 2>&1
-  nvm install --lts --no-progress >/dev/null 2>&1
+  curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash -
+  . "${NVM_DIR}/nvm.sh"
+  nvm install --lts --no-progress
 fi
-[ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh" >/dev/null 2>&1
+[ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"
 
-npm install -g @datadog/datadog-ci >/dev/null 2>&1
+npm install -g @datadog/datadog-ci
