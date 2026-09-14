d9ee7320312752a23444863d5bc86167811a66b6
Colin Higgins <colin.higgins@datadoghq.com>
2026-09-14 09:48:23 -0400

    Add newline terminator to stub generator

--- tooling/stubs/generate-stubs.php
+++ tooling/stubs/generate-stubs.php
@@ -20,6 +20,9 @@ $generator = new StubsGenerator();
 $finder = Finder::create()->in($SRC_DIR)->path($files)->sortByName();
 
 $stubs = $generator->generate($finder)->prettyPrint(new CustomPrinter());
+if ($stubs === '' || substr($stubs, -1) !== "\n") {
+    $stubs .= "\n";
+}
 
 $outputFile = $SRC_DIR . "ddtrace_php_api.stubs.php";
 file_put_contents($outputFile, $stubs);
