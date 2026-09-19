48f24f9356ec0cc8b2751cf351e8bf9984102ee6
Gustavo André dos Santos Lopes <gustavo.lopes@datadoghq.com>
2026-09-19 16:26:56 +0100

    fix(windows): wait for remote config callbacks at shutdown

--- ext/remote_config.h
+++ ext/remote_config.h
@@ -10,6 +10,10 @@ void datadog_rshutdown_remote_config(void);
 void datadog_check_for_new_config_now(void);
 
 
+#ifdef _WIN32
+DATADOG_PUBLIC DWORD WINAPI datadog_set_all_thread_vm_interrupt(LPVOID unused);
+#else
 DATADOG_PUBLIC void datadog_set_all_thread_vm_interrupt(void);
+#endif
 
 #endif
