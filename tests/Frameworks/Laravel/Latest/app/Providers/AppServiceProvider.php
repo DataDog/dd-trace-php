6c9020e12a58435a1e0d9809c158ea4b934e9506
Gustavo André dos Santos Lopes <gustavo.lopes@datadoghq.com>
2026-09-19 02:11:24 +0100

    fix(ci): avoid eager Laravel database connections

--- tests/Frameworks/Laravel/Latest/app/Providers/AppServiceProvider.php
+++ tests/Frameworks/Laravel/Latest/app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Builder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
+        Builder::defaultStringLength(191);
     }
 }
