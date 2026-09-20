0dd9063ef70970532c940b0ff5f976f7b27dbcf3
Gustavo André dos Santos Lopes <gustavo.lopes@datadoghq.com>
2026-09-20 18:57:18 +0100

    ci: test installers with local CI bundles

--- tooling/bin/generate-installers.sh
+++ tooling/bin/generate-installers.sh
@@ -12,7 +12,9 @@ if echo "${release_version}" | grep -qE '\+' && [ -n ${CI_JOB_ID:-} ]; then
   replacement="define('RELEASE_VERSION', urlencode('${release_version}'))"
   sed -r "s|const RELEASE_VERSION[^;]+|${replacement}|g" ./datadog-setup.php > "${packages_build_dir}/datadog-setup.php"
 
-  replacement="define('RELEASE_URL_PREFIX', 'https://s3.us-east-1.amazonaws.com/dd-trace-php-builds/' . RELEASE_VERSION . '/')"
+  replacement="define('RELEASE_URL_PREFIX', getenv('DD_TEST_INSTALLER_REPO')"
+  replacement="${replacement} ? getenv('DD_TEST_INSTALLER_REPO') . '/releases/download/' . RELEASE_VERSION . '/'"
+  replacement="${replacement} : 'https://s3.us-east-1.amazonaws.com/dd-trace-php-builds/' . RELEASE_VERSION . '/')"
   sed -ri "s|define\('RELEASE_URL_PREFIX'[^;]+|${replacement}|" "${packages_build_dir}/datadog-setup.php"
 else
   sed "s|@release_version@|${release_version}|g" ./datadog-setup.php > "${packages_build_dir}/datadog-setup.php"
